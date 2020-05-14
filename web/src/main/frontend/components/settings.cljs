5a2e1178c1c462dc8d46be01cd1dd80e9560a432
Tienson Qin <tiensonqin@gmail.com>
2020-05-14 13:42:25 +0800

    Updated style

--- web/src/main/frontend/components/settings.cljs
+++ web/src/main/frontend/components/settings.cljs
@@ -8,19 +8,19 @@
 (rum/defcs set-email < (rum/local "" ::email)
   [state]
   (let [email (get state ::email)]
-    [:div.p-8.flex.items-center.justify-center.bg-white
-    [:div.w-full.max-w-xs.mx-auto
-     [:div
+    [:div.p-8.flex.items-center.justify-center
+     [:div.w-full.mx-auto
       [:div
-       [:h2 "Your email:" ]
-       [:span.text-gray-500.text-sm.pl-1 "(Git commit requires)"]
-       [:div.mt-2.mb-2.relative.rounded-md.shadow-sm
-        [:input#.form-input.block.w-full.pl-2.sm:text-sm.sm:leading-5
-         {:autoFocus true
-          :on-change (fn [e]
-                       (reset! email (util/evalue e)))}]]]]
-     (ui/button
-       "Submit"
-       :on-click
-       (fn []
-         (handler/set-email! @email)))]]))
+       [:div
+        [:h2 "Your email:" ]
+        [:span.text-gray-500.text-sm.pl-1 "(Git commit requires)"]
+        [:div.mt-2.mb-2.relative.rounded-md.shadow-sm
+         [:input#.form-input.block.w-full.pl-2.sm:text-sm.sm:leading-5
+          {:autoFocus true
+           :on-change (fn [e]
+                        (reset! email (util/evalue e)))}]]]]
+      (ui/button
+        "Submit"
+        :on-click
+        (fn []
+          (handler/set-email! @email)))]]))
