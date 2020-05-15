3c1aceebedbd2d05364aadc19f891a3e9ca02d8f
Tienson Qin <tiensonqin@gmail.com>
2020-05-15 13:23:01 +0800

    Don't diff if markup length is less than the text length

--- web/src/main/frontend/diff.cljs
+++ web/src/main/frontend/diff.cljs
@@ -12,31 +12,36 @@
 ;; (find-position "** hello _w_" "hello w")
 (defn find-position
   [markup text]
-  (try
-    (let [markup (string/lower-case markup)
-         text (-> (string/replace text #"\s+" "")
-                  (string/lower-case))
-         v1 (vec markup)
-         v2 (vec text)]
-     (loop [v1-chars v1
-            v2-chars v2
-            v1-idx 0
-            v2-idx 0]
-       (cond
-         (empty? v2-chars)
-         (dec v1-idx)
+  (cond
+    (<= (count markup) (count text))
+    (dec (count markup))
 
-         :else
-         (if (= (nth v1 v1-idx)
-                (nth v2 v2-idx))
-           (recur (rest v1-chars)
-                  (rest v2-chars)
-                  (inc v1-idx)
-                  (inc v2-idx))
-           (recur (rest v1-chars)
-                  v2-chars
-                  (inc v1-idx)
-                  v2-idx)))))
-    (catch js/Error e
-      (prn "diff error: " e)
-      (dec (count markup)))))
+    :else
+    (try
+      (let [markup (string/lower-case markup)
+            text (-> (string/replace text #"\s+" "")
+                     (string/lower-case))
+            v1 (vec markup)
+            v2 (vec text)]
+        (loop [v1-chars v1
+               v2-chars v2
+               v1-idx 0
+               v2-idx 0]
+          (cond
+            (empty? v2-chars)
+            (dec v1-idx)
+
+            :else
+            (if (= (nth v1 v1-idx)
+                   (nth v2 v2-idx))
+              (recur (rest v1-chars)
+                     (rest v2-chars)
+                     (inc v1-idx)
+                     (inc v2-idx))
+              (recur (rest v1-chars)
+                     v2-chars
+                     (inc v1-idx)
+                     v2-idx)))))
+      (catch js/Error e
+        (prn "diff error: " e)
+        (dec (count markup))))))
