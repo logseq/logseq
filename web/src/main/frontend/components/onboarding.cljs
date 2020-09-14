a09d2cde30ab2277be3115cacfa2591db697b50f
Haoji Xu <haoji.xu@outlook.com>
2020-09-14 16:03:08 +0800

    feat: onboarding strings support

--- web/src/main/frontend/components/onboarding.cljs
+++ web/src/main/frontend/components/onboarding.cljs
@@ -2,217 +2,219 @@
   (:require [rum.core :as rum]
             [frontend.components.svg :as svg]
             [frontend.extensions.latex :as latex]
-            [frontend.extensions.code :as code]))
+            [frontend.extensions.code :as code]
+            [frontend.context.i18n :as i18n]))
 
 (rum/defc intro
   []
-  [:div#intro.pl-1
-   [:div.flex-1
-    [:div.flex.flex-col.pl-1.ls-block
-     [:hr {:style {:margin-top 200}}]
-     [:h1.title.welcome-title
-      "Hi, welcome to Logseq!"]
-     [:p
-      {}
-      [:strong {} "Logseq"]
-      " is a "
-      [:a {:href "https://www.inkandswitch.com/local-first.html"
-           :target "_blank"}
-       [:i {} "local-first"]]
-      ", "
-      [:a {:href "https://tiddlywiki.com/"
-           :target "_blank"}
-       [:i {} "non-linear"]]
-      ", "
-      [:a {:href "https://orgmode.org/"
-           :target "_blank"}
-       [:i {} "outliner"]]
-      " notebook for organizing and "
-      [:a {:href "https://logseq.com/blog"
-           :target "_blank"} "sharing"]
-      " your personal knowledge base."]
-     [:div.flex.flex-row.admonitionblock.align-items {:class "warning"}
-      [:div.pr-4.admonition-icon.flex.flex-col.justify-center
-       {:title "Warning"} (svg/warning)]
-      [:div.ml-4.text-lg
-       "Notice that this project is in its early days and under quick development, files might be corrupted."]]
-     [:p
-      {}
-      "Use it to organize your todo list, to write your journals, or to record your unique life."]
-     [:p
-      {}
-      "The server will never store or analyze your private notes. Your data are plain text files, we support both Markdown and Emacs Org mode for the time being. Even if the website is down or can't be maintained, your data is always yours."]
-     [:p
-      {}
-      [:strong {} "Logseq"]
-      " is hugely inspired by "
-      [:a {:href "https://roamresearch.com/"
-           :target "_blank"} "Roam Research"]
-      ", "
-      [:a {:href "https://orgmode.org/"
-           :target "_blank"} "Org Mode"]
-      ", "
-      [:a {:href "https://tiddlywiki.com/"
-           :target "_blank"} "Tiddlywiki"]
-      " and "
-      [:a {:href "https://workflowy.com/"
-           :target "_blank"} "Workflowy"]
-      ", hats off to all of them!"]]
+  (rum/with-context [[t] i18n/*tongue-context*]
+    [:div#intro.pl-1
+     [:div.flex-1
+      [:div.flex.flex-col.pl-1.ls-block
+       [:hr {:style {:margin-top 200}}]
+       [:h1.title.welcome-title
+        (t :on-boarding/title)]
+       [:p
+        {}
+        [:strong {} "Logseq"]
+        (t :on-boarding/is-a)
+        [:a {:href "https://www.inkandswitch.com/local-first.html"
+             :target "_blank"}
+         [:i {} (t :on-boarding/local-first)]]
+        ", "
+        [:a {:href "https://tiddlywiki.com/"
+             :target "_blank"}
+         [:i {} (t :on-boarding/non-linear)]]
+        ", "
+        [:a {:href "https://orgmode.org/"
+             :target "_blank"}
+         [:i {} (t :on-boarding/outliner)]]
+        (t :on-boarding/notebook-for-organizing-and)
+        [:a {:href "https://logseq.com/blog"
+             :target "_blank"} (t :on-boarding/sharing)]
+        (t :on-boarding/your-personal-knowledge-base)]
+       [:div.flex.flex-row.admonitionblock.align-items {:class "warning"}
+        [:div.pr-4.admonition-icon.flex.flex-col.justify-center
+         {:title "Warning"} (svg/warning)]
+        [:div.ml-4.text-lg
+         (t :on-boarding/notice)]]
+       [:p
+        {}
+        (t :on-boarding/features-desc)]
+       [:p
+        {}
+        (t :on-boarding/privacy)]
+       [:p
+        {}
+        [:strong {} "Logseq"]
+        (t :on-boarding/inspired-by)
+        [:a {:href "https://roamresearch.com/"
+             :target "_blank"} "Roam Research"]
+        ", "
+        [:a {:href "https://orgmode.org/"
+             :target "_blank"} "Org Mode"]
+        ", "
+        [:a {:href "https://tiddlywiki.com/"
+             :target "_blank"} "Tiddlywiki"]
+        " and "
+        [:a {:href "https://workflowy.com/"
+             :target "_blank"} "Workflowy"]
+        ", hats off to all of them!"]]
 
-    [:img.shadow-2xl
-     {:src
-      "https://cdn.logseq.com/%2F8b9a461d-437e-4ca5-a2da-18b51077b5142020_07_25_Screenshot%202020-07-25%2013-29-49%20%2B0800.png?Expires=4749255017&Signature=Qbx6jkgAytqm6nLxVXQQW1igfcf~umV1OcG6jXUt09TOVhgXyA2Z5jHJ3AGJASNcphs31pZf4CjFQ5mRCyVKw6N8wb8Nn-MxuTJl0iI8o-jLIAIs9q1v-2cusCvuFfXH7bq6ir8Lpf0KYAprzuZ00FENin3dn6RBW35ENQwUioEr5Ghl7YOCr8bKew3jPV~OyL67MttT3wJig1j3IC8lxDDT8Ov5IMG2GWcHERSy00F3mp3tJtzGE17-OUILdeuTFz6d-NDFAmzB8BebiurYz0Bxa4tkcdLUpD5ToFHU08jKzZExoEUY8tvaZ1-t7djmo3d~BAXDtlEhC2L1YC2aVQ__&Key-Pair-Id=APKAJE5CCD6X7MP6PTEA"
-      :alt "screenshot"}]
+      [:img.shadow-2xl
+       {:src
+        "https://cdn.logseq.com/%2F8b9a461d-437e-4ca5-a2da-18b51077b5142020_07_25_Screenshot%202020-07-25%2013-29-49%20%2B0800.png?Expires=4749255017&Signature=Qbx6jkgAytqm6nLxVXQQW1igfcf~umV1OcG6jXUt09TOVhgXyA2Z5jHJ3AGJASNcphs31pZf4CjFQ5mRCyVKw6N8wb8Nn-MxuTJl0iI8o-jLIAIs9q1v-2cusCvuFfXH7bq6ir8Lpf0KYAprzuZ00FENin3dn6RBW35ENQwUioEr5Ghl7YOCr8bKew3jPV~OyL67MttT3wJig1j3IC8lxDDT8Ov5IMG2GWcHERSy00F3mp3tJtzGE17-OUILdeuTFz6d-NDFAmzB8BebiurYz0Bxa4tkcdLUpD5ToFHU08jKzZExoEUY8tvaZ1-t7djmo3d~BAXDtlEhC2L1YC2aVQ__&Key-Pair-Id=APKAJE5CCD6X7MP6PTEA"
+        :alt "screenshot"}]
 
-    [:div.flex.flex-col.ls-block.intro-docs
-     [:h2 {} "Where are my notes saved?"]
-     [:p
-      {}
-      "Your notes will be stored in the local browser storage using "
-      [:a {:href "https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API"
-           :target "_blank"}
-       "IndexedDB"]
-      "."]
-     [:h2 {} "How do I use it?"]
-     [:h3 {} "1. Sync between multiple devices"]
-     [:p
-      {}
-      "Currently, we only support syncing through Github, more options (Self-host git, WebDAV, Google Drive, etc.) will be added soon."]
-     [:p
-      "Check out this awesome video by "
-      [:a {:href "https://twitter.com/EdTravelling"
-           :target "_blank"}
-       "@EdTravelling"]
-      ":"]
-     [:iframe
-      {:allowfullscreen "allowfullscreen",
-       :allow
-       "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture",
-       :frameborder "0",
-       :src "https://www.youtube.com/embed/Vw-x7yTTO0s",
-       :height "367",
-       :width "653"}]
+      [:div.flex.flex-col.ls-block.intro-docs
+       [:h2 {} (t :on-boarding/where-are-my-notes-saved)]
+       [:p
+        {}
+        (t :on-boarding/storage)
+        [:a {:href "https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API"
+             :target "_blank"}
+         "IndexedDB"]
+        "."]
+       [:h2 {} (t :on-boarding/how-do-i-use-it)]
+       [:h3 {} (t :on-boarding/use-1)]
+       [:p
+        {}
+        (t :on-boarding/use-1-desc)]
+       [:p
+        (t :on-boarding/use-1-video)
+        [:a {:href "https://twitter.com/EdTravelling"
+             :target "_blank"}
+         "@EdTravelling"]
+        ":"]
+       [:iframe
+        {:allowfullscreen "allowfullscreen"
+         :allow
+         "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+         :frameborder "0"
+         :src "https://www.youtube.com/embed/Vw-x7yTTO0s"
+         :height "367"
+         :width "653"}]
 
-     [:h3 {} "2. Use it locally (no need to login)"]
-     [:p
-      {}
-      "Remember to backup your notes periodically!"]
-     [:h2 {} "Features"]
-     [:ul
-      {}
-      [:li {} "Backlinks between " [:code {} "[[Page]]"] "s"]
-      [:li {} "Block embed"]
-      [:li {} "Page embed"]
-      [:li {} "Graph visualization"]
-      [:li {} "Heading properties"]
-      [:li
-       {}
-       "Datalog queries, the notes db is powered by "
-       [:a {:href "https://github.com/tonsky/datascript"
-            :target "_blank"} "Datascript"]]
-      [:li {} "Custom view component"]
-      [:li
-       {}
-       [:a {:href "https://excalidraw.com/"
-            :target "_blank"} "Excalidraw"]
-       " integration"]
-      [:li
-       {}
-       [:a {:href "https://revealjs.com/"
-            :target "_blank"} "reveal.js"]
-       " slide support"]
-      [:li
-       {}
-       "Document built-in supports:"
+       [:h3 {} (t :on-boarding/use-2)]
+       [:p
+        {}
+        (t :on-boarding/use-2-desc)]
+       [:h2 {} (t :on-boarding/features)]
        [:ul
         {}
-        [:li {} "Code highlights"]
-        [:li {} "Katex latex"]
+        [:li {} (t :on-boarding/features-backlinks)]
+        [:li {} (t :on-boarding/features-block-embed)]
+        [:li {} (t :on-boarding/features-page-embed)]
+        [:li {} (t :on-boarding/features-graph-vis)]
+        [:li {} (t :on-boarding/features-heading-properties)]
         [:li
          {}
-         "Raw "
-         [:a {:href "https://github.com/weavejester/hiccup"
-              :target "_blank"} "hiccup"]]
-        [:li {} "Raw html"]]]]
-     [:h2 {} "Learn more"]
-     [:ul
-      {}
-      [:li
-       {}
-       "Twitter: "
-       [:a
-        {:href "https://twitter.com/logseq"
-         :target "_blank"}
-        "https://twitter.com/logseq"]]
-      [:li
-       {}
-       "Discord: "
-       [:a
-        {:href "https://discord.gg/KpN4eHY"
-         :target "_blank"}
-        "https://discord.gg/KpN4eHY"]
-       " where the community ask questions and share tips"]
-      [:li
-       {}
-       "Github: "
-       [:a
-        {:href "https://github.com/logseq/logseq"
-         :target "_blank"}
-        "https://github.com/logseq/logseq"]
-       " everyone is encouraged to report issues!"]
-      [:li
-       {}
-       "Our blog: "
-       [:a
-        {:href "https://logseq.com/blog"
-         :target "_blank"}
-        "https://logseq.com/blog"]]]
-     [:h2 "Credits to"]
-     [:ul
-      {}
-      [:li [:a {:href "https://roamresearch.com/"
-                :target "_blank"} "Roam Research"]]
-      [:li [:a {:href "https://orgmode.org/"
-                :target "_blank"} "Org Mode"]]
-      [:li [:a {:href "https://tiddlywiki.com/"
-                :target "_blank"} "Tiddlywiki"]]
-      [:li
-       [:a {:href "https://workflowy.com/"
-            :target "_blank"} "Workflowy"]]
-      [:li
-       [:a
-        {:href "https://clojure.org"
-         :target "_blank"}
-        "Clojure && Clojurescript"]
-       " - A dynamic, functional, general-purpose programming language"]
-      [:li
-       [:a {:href "https://github.com/tonsky/datascript"
-            :target "_blank"} "Datascript"]
-       " - Immutable database and Datalog query engine for Clojure, ClojureScript and JS"]
-      [:li
-       [:a {:href "https://ocaml.org/"
-            :target "_blank"} "OCaml"]
-       " && "
-       [:a
-        {:href "https://github.com/inhabitedtype/angstrom"
-         :target "_blank"}
-        "Angstrom"]
-       ", the document "
-       [:a {:href "https://github.com/mldoc/mldoc"
-            :target "_blank"} "parser"]
-       " is built on Angstrom."]
-      [:li
-       [:a {:href "https://github.com/talex5/cuekeeper"
-            :target "_blank"} "Cuekeeper"]
-       " - Browser-based GTD (TODO list) system."]
-      [:li
-       [:a {:href "https://github.com/borkdude/sci"
-            :target "_blank"} "sci"]
-       " - Small Clojure Interpreter"]
-      [:li
-       [:a {:href "https://isomorphic-git.org/"
-            :target "_blank"} "isomorphic-git"]
-       " - A pure JavaScript implementation of git for node and browsers!"]]]]])
+         (t :on-boarding/features-datalog)
+         [:a {:href "https://github.com/tonsky/datascript"
+              :target "_blank"} "Datascript"]]
+        [:li {} (t :on-boarding/features-custom-view-component)]
+        [:li
+         {}
+         [:a {:href "https://excalidraw.com/"
+              :target "_blank"} "Excalidraw"]
+         (t :on-boarding/integration)]
+        [:li
+         {}
+         [:a {:href "https://revealjs.com/"
+              :target "_blank"} "reveal.js"]
+         (t :on-boarding/slide-support)]
+        [:li
+         {}
+         (t :on-boarding/built-in-supports)
+         [:ul
+          {}
+          [:li {} (t :on-boarding/supports-code-highlights)]
+          [:li {} (t :on-boarding/supports-katex-latex)]
+          [:li
+           {}
+           (t :on-boarding/raw)
+           [:a {:href "https://github.com/weavejester/hiccup"
+                :target "_blank"} "hiccup"]]
+          [:li {} (t :on-boarding/raw-html)]]]]
+       [:h2 {} (t :on-boarding/learn-more)]
+       [:ul
+        {}
+        [:li
+         {}
+         "Twitter: "
+         [:a
+          {:href "https://twitter.com/logseq"
+           :target "_blank"}
+          "https://twitter.com/logseq"]]
+        [:li
+         {}
+         "Discord: "
+         [:a
+          {:href "https://discord.gg/KpN4eHY"
+           :target "_blank"}
+          "https://discord.gg/KpN4eHY"]
+         (t :on-boarding/discord-desc)]
+        [:li
+         {}
+         "Github: "
+         [:a
+          {:href "https://github.com/logseq/logseq"
+           :target "_blank"}
+          "https://github.com/logseq/logseq"]
+         (t :on-boarding/github-desc)]
+        [:li
+         {}
+         (t :on-boarding/our-blog)
+         [:a
+          {:href "https://logseq.com/blog"
+           :target "_blank"}
+          "https://logseq.com/blog"]]]
+       [:h2 (t :on-boarding/credits-to)]
+       [:ul
+        {}
+        [:li [:a {:href "https://roamresearch.com/"
+                  :target "_blank"} "Roam Research"]]
+        [:li [:a {:href "https://orgmode.org/"
+                  :target "_blank"} "Org Mode"]]
+        [:li [:a {:href "https://tiddlywiki.com/"
+                  :target "_blank"} "Tiddlywiki"]]
+        [:li
+         [:a {:href "https://workflowy.com/"
+              :target "_blank"} "Workflowy"]]
+        [:li
+         [:a
+          {:href "https://clojure.org"
+           :target "_blank"}
+          "Clojure && Clojurescript"]
+         (t :on-boarding/clojure-desc)]
+        [:li
+         [:a {:href "https://github.com/tonsky/datascript"
+              :target "_blank"} "Datascript"]
+         (t :on-boarding/datascript-desc)]
+        [:li
+         [:a {:href "https://ocaml.org/"
+              :target "_blank"} "OCaml"]
+         " && "
+         [:a
+          {:href "https://github.com/inhabitedtype/angstrom"
+           :target "_blank"}
+          "Angstrom"]
+         (t :on-boarding/angstrom-desc-1)
+         [:a {:href "https://github.com/mldoc/mldoc"
+                                            :target "_blank"} (t :on-boarding/angstrom-desc-2)]
+         (t :on-boarding/angstrom-desc-3)]
+        [:li
+         [:a {:href "https://github.com/talex5/cuekeeper"
+              :target "_blank"} "Cuekeeper"]
+         (t :on-boarding/cuekeeper-desc)]
+        [:li
+         [:a {:href "https://github.com/borkdude/sci"
+              :target "_blank"} "sci"]
+         (t :on-boarding/sci-desc)]
+        [:li
+         [:a {:href "https://isomorphic-git.org/"
+              :target "_blank"} "isomorphic-git"]
+         (t :on-boarding/isomorphic-git-desc)]]]]]))
 
 (defn help
   []
