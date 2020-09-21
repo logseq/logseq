32fb439f2a88bb8a5ddabb8b3488314f2423c913
Tienson Qin <tiensonqin@gmail.com>
2020-09-21 16:29:26 +0800

    Fixed saving the first block on empty page not working
    https://github.com/logseq/logseq/issues/349

--- web/src/main/frontend/db.cljs
+++ web/src/main/frontend/db.cljs
@@ -143,7 +143,7 @@
   []
   (let [state @query-state
         state (->> (filter (fn [[[_repo k] v]]
-                             (contains? #{:blocks :block/block :page/blocks} k)) state)
+                             (contains? #{:blocks :block/block} k)) state)
                    (into {}))]
     (reset! query-state state)))
 
