0456b40db34a95e16ea8f7f1d0bed4b1a993145a
Tienson Qin <tiensonqin@gmail.com>
2020-10-12 17:50:03 +0800

    Add debugging

--- web/src/main/frontend/handler/repo.cljs
+++ web/src/main/frontend/handler/repo.cljs
@@ -403,6 +403,7 @@
            (db/cloned? repo-url)
            (not (state/get-edit-input-id)))
       (-> (p/let [files (js/window.workerThread.getChangedFiles (util/get-repo-dir (state/get-current-repo)))]
+            (prn {:changed-files files})
             (when (or
                    ;; FIXME:
                    force?
