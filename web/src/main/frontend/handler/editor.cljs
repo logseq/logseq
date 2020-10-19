99bcd95a93c2f481ebe2ed7ee750f4000c30beb1
Tienson Qin <tiensonqin@gmail.com>
2020-10-19 18:33:52 +0800

    Turn off single $ autopair
    Related to https://github.com/logseq/logseq/issues/499

--- web/src/main/frontend/handler/editor.cljs
+++ web/src/main/frontend/handler/editor.cljs
@@ -1273,7 +1273,6 @@
   {"[" "]"
    "{" "}"
    "(" ")"
-   "$" "$"                              ; math
    "`" "`"
    "~" "~"
    "*" "*"
@@ -1284,6 +1283,15 @@
 (def reversed-autopair-map
   (zipmap (vals autopair-map)
           (keys autopair-map)))
+
+(def delete-map
+  (assoc autopair-map
+         "$" "$"
+         ":" ":"))
+
+(def reversed-delete-map
+  (zipmap (vals delete-map)
+          (keys delete-map)))
 
 (defn autopair
   [input-id prefix format {:keys [restore?]
