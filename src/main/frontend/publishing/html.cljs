77214e12bf5c68ef75bdd079eee7fd7e0db1e36c
Tienson Qin <tiensonqin@gmail.com>
2020-10-28 00:10:14 +0800

    fix: hiccups/html warning

--- src/main/frontend/publishing/html.cljs
+++ src/main/frontend/publishing/html.cljs
@@ -7,53 +7,54 @@
   [transit-db app-state]
   (let [{:keys [icon name alias title description url]} (:project (state/get-config))
         icon (or icon "/static/img/logo.png")
-        project (or alias name)
-        content (hiccups/html
-                 [:head
-                  [:meta {:charset "utf-8"}]
-                  [:meta
-                   {:content
-                    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no",
-                    :name "viewport"}]
-                  [:link {:type "text/css", :href "/static/style.css", :rel "stylesheet"}]
-                  [:link
-                   {:href icon
-                    :type "image/png",
-                    :rel "shortcut icon"}]
-                  [:link
-                   {:href icon
-                    :sizes "192x192",
-                    :rel "shortcut icon"}]
-                  [:link
-                   {:href icon
-                    :rel "apple-touch-icon"}]
+        project (or alias name)]
+    (str "<!DOCTYPE html>\n"
+         (hiccups/html
+           [:head
+            [:meta {:charset "utf-8"}]
+            [:meta
+             {:content
+              "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no",
+              :name "viewport"}]
+            [:link {:type "text/css", :href "/static/style.css", :rel "stylesheet"}]
+            [:link
+             {:href icon
+              :type "image/png",
+              :rel "shortcut icon"}]
+            [:link
+             {:href icon
+              :sizes "192x192",
+              :rel "shortcut icon"}]
+            [:link
+             {:href icon
+              :rel "apple-touch-icon"}]
 
-                  [:meta {:name "apple-mobile-web-app-title" :content project}]
-                  [:meta {:name "apple-mobile-web-app-capable" :content "yes"}]
-                  [:meta {:name "apple-touch-fullscreen" :content "yes"}]
-                  [:meta {:name "apple-mobile-web-app-status-bar-style" :content "black-translucent"}]
-                  [:meta {:name "mobile-web-app-capable" :content "yes"}]
+            [:meta {:name "apple-mobile-web-app-title" :content project}]
+            [:meta {:name "apple-mobile-web-app-capable" :content "yes"}]
+            [:meta {:name "apple-touch-fullscreen" :content "yes"}]
+            [:meta {:name "apple-mobile-web-app-status-bar-style" :content "black-translucent"}]
+            [:meta {:name "mobile-web-app-capable" :content "yes"}]
 
-                  [:meta {:content title, :property "og:title"}]
-                  [:meta {:content "site", :property "og:type"}]
-                  (when url [:meta {:content url, :property "og:url"}])
-                  [:meta
-                   {:content icon
-                    :property "og:image"}]
-                  [:meta
-                   {:content description
-                    :property "og:description"}]
-                  [:title title]
-                  [:meta {:content project, :property "og:site_name"}]
-                  [:meta
-                   {:description description}]]
-                 [:body
-                  [:div#root]
-                  [:script (str "window.logseq_db=" transit-db)]
-                  [:script (str "window.logseq_state=" (js/JSON.stringify app-state))]
-                  [:script {:src "/static/js/mldoc.min.js"}]
-                  [:script {:type "text/javascript"}
-                   "// Single Page Apps for GitHub Pages
+            [:meta {:content title, :property "og:title"}]
+            [:meta {:content "site", :property "og:type"}]
+            (when url [:meta {:content url, :property "og:url"}])
+            [:meta
+             {:content icon
+              :property "og:image"}]
+            [:meta
+             {:content description
+              :property "og:description"}]
+            [:title title]
+            [:meta {:content project, :property "og:site_name"}]
+            [:meta
+             {:description description}]]
+           [:body
+            [:div#root]
+            [:script (str "window.logseq_db=" transit-db)]
+            [:script (str "window.logseq_state=" (js/JSON.stringify app-state))]
+            [:script {:src "/static/js/mldoc.min.js"}]
+            [:script {:type "text/javascript"}
+             "// Single Page Apps for GitHub Pages
       // https://github.com/rafgraph/spa-github-pages
       // Copyright (c) 2016 Rafael Pedicini, licensed under the MIT License
       // ----------------------------------------------------------------------
@@ -80,7 +81,6 @@
           }
         }
       }(window.location))"]
-                   ;; TODO: should make this configurable
-                  [:script {:src "/static/js/highlight.min.js"}]
-                  [:script {:src "/static/js/publishing.js"}]])]
-    (str "<!DOCTYPE html>\n" content)))
+            ;; TODO: should make this configurable
+            [:script {:src "/static/js/highlight.min.js"}]
+            [:script {:src "/static/js/publishing.js"}]]))))
