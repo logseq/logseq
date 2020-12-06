8fe8e0c924147d1c54a19a77b66ed20054075cc1
Tienson Qin <tiensonqin@gmail.com>
2020-12-07 00:45:08 +0800

    fix: add missing externs

--- externs.js
+++ externs.js
@@ -34,6 +34,7 @@ dummy.folder = function() {};
 dummy.file = function() {};
 dummy.generateAsync = function() {};
 dummy.showOpenFilePicker = function() {};
+dummy.showDirectoryPicker = function() {};
 dummy.getDirectoryHandle = function() {};
 dummy.removeEntry = function() {};
 dummy.getFile = function() {};
