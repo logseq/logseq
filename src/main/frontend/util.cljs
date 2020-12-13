9ce6b441f3de4b539d0e423568aa4f7b8e2b1158
charlie <xyhp915@qq.com>
2020-12-13 12:57:52 +0800

    fix: scroll to element

--- src/main/frontend/util.cljs
+++ src/main/frontend/util.cljs
@@ -24,6 +24,9 @@
   (-pr-writer [sym writer _]
     (-write writer (str "\"" (.toString sym) "\""))))
 
+;; doms
+(defonce html-node js/document.documentElement)
+
 ;; envs
 (defn mac?
   []
@@ -328,7 +331,7 @@
   (when-not (re-find #"^/\d+$" elem-id)
     (when elem-id
       (when-let [elem (gdom/getElement elem-id)]
-        (.scroll (gdom/getElement "main-content")
+        (.scroll html-node
                  #js {:top (let [top (element-top elem 0)]
                              (if (> top 68)
                                (- top 68)
@@ -337,10 +340,9 @@
 
 (defn scroll-to
   [pos]
-  (when-let [main-content (gdom/getElement "main-content")]
-    (.scroll main-content
-             #js {:top pos
-                  :behavior "smooth"})))
+  (.scroll html-node
+           #js {:top      pos
+                :behavior "smooth"}))
 
 (defn scroll-to-top
   []
