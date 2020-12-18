6c1a1701bb5e9a1b2962b3545e11bf918411cd21
Tienson Qin <tiensonqin@gmail.com>
2020-12-18 18:46:30 +0800

    fix: css styles

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,5 @@ module.exports = (ctx) => ({
   plugins: [
     require('postcss-nested'),
     require('tailwindcss')('tailwind.config.js'),
-    ctx.env === 'production' ? require('cssnano')({ preset: 'default' }) : null,
   ],
 })
