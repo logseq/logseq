c84e42c9c3d94cc27aa37b74eb063a01e69ffc0c
Kan-Ru Chen <kanru@kanru.info>
2021-01-14 00:29:07 +0900

    Add functions for generating and retrieiving keys

--- src/main/frontend/encrypt.cljs
+++ src/main/frontend/encrypt.cljs
@@ -1,6 +1,8 @@
 (ns frontend.encrypt
   (:require [frontend.utf8 :as utf8]
             [frontend.db.utils :as db-utils]
+            [frontend.db :as db]
+            [frontend.state :as state]
             ["bip39" :as bip39]
             ["buffer" :as buffer]
             ["@kanru/rage-wasm" :as rage]))
@@ -11,9 +13,24 @@
   []
   (db-utils/get-key-value :db/encrypted?))
 
+(defn save-mnemonic
+  [repo mnemonic]
+  (db/set-key-value repo :db/secret-phrase mnemonic)
+  (db/set-key-value repo :db/encrypted? true))
+
 (defn generate-mnemonic
   []
   (bip39/generateMnemonic 256))
+
+(defn generate-mnemonic-and-save
+  []
+  (let [repo (state/get-current-repo)
+        mnemonic (generate-mnemonic)]
+    (save-mnemonic repo mnemonic)))
+
+(defn get-mnemonic
+  []
+  (db-utils/get-key-value :db/secret-phrase))
 
 (defn derive-key-from-mnemonic
   [mnemonic]
@@ -22,15 +39,20 @@
         keys (rage/keygen_from_random_bytes entropy)]
     keys))
 
-(let [keys (derive-key-from-mnemonic secret-phrase)]
-  (defonce secret-key (first keys))
-  (defonce public-key (second keys)))
+(defn get-public-key
+  []
+  (second (derive-key-from-mnemonic (get-mnemonic))))
+
+(defn get-secret-key
+  []
+  (first (derive-key-from-mnemonic (get-mnemonic))))
 
 (defn encrypt
   [content]
   (cond
     (encrypted-db?)
     (let [content (utf8/encode content)
+          public-key (get-public-key)
           encrypted (rage/encrypt_with_x25519 public-key content true)]
       (utf8/decode encrypted))
     :else
@@ -41,6 +63,7 @@
   (cond
     (encrypted-db?)
     (let [content (utf8/encode content)
+          secret-key (get-secret-key)
           decrypted (rage/decrypt_with_x25519 secret-key content)]
       (utf8/decode decrypted))
     :else
