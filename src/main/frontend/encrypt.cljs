cfb965925ab94d1edd3aacda84fbc015d53b0ca5
Kan-Ru Chen <kanru@kanru.info>
2021-01-14 00:14:13 +0900

    Only encrypt/decrypt in encrypted db

--- src/main/frontend/encrypt.cljs
+++ src/main/frontend/encrypt.cljs
@@ -1,10 +1,15 @@
 (ns frontend.encrypt
   (:require [frontend.utf8 :as utf8]
+            [frontend.db.utils :as db-utils]
             ["bip39" :as bip39]
             ["buffer" :as buffer]
             ["@kanru/rage-wasm" :as rage]))
 
 (defonce secret-phrase "canal this pluck bar elite tape olive toilet cry surprise dish rival wrist tragic click honey solar kangaroo cook cabin replace harvest horse wrong")
+
+(defn encrypted-db?
+  []
+  (db-utils/get-key-value :db/encrypted?))
 
 (defn generate-mnemonic
   []
@@ -23,12 +28,20 @@
 
 (defn encrypt
   [content]
-  (let [content (utf8/encode content)
-        encrypted (rage/encrypt_with_x25519 public-key content true)]
-    (utf8/decode encrypted)))
+  (cond
+    (encrypted-db?)
+    (let [content (utf8/encode content)
+          encrypted (rage/encrypt_with_x25519 public-key content true)]
+      (utf8/decode encrypted))
+    :else
+    content))
 
 (defn decrypt
   [content]
-  (let [content (utf8/encode content)
-        decrypted (rage/decrypt_with_x25519 secret-key content)]
-    (utf8/decode decrypted)))
+  (cond
+    (encrypted-db?)
+    (let [content (utf8/encode content)
+          decrypted (rage/decrypt_with_x25519 secret-key content)]
+      (utf8/decode decrypted))
+    :else
+    content))
