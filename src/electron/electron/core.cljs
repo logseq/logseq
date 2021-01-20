8e5a58988504ec2cbb4e91fbcc88ac4078c878a8
Tienson Qin <tiensonqin@gmail.com>
2021-01-20 23:22:29 +0800

    electron: add ipc layer

--- src/electron/electron/core.cljs
+++ src/electron/electron/core.cljs
@@ -1,6 +1,5 @@
 (ns electron.core
-  (:require [electron.init :refer [init-channel]]
-            [electron.handler :as handler]
+  (:require [electron.handler :as handler]
             ["fs" :as fs]
             ["path" :as path]
             ["electron" :refer [BrowserWindow app] :as electron]
@@ -25,8 +24,10 @@
   (let [win-opts {:width  980
                   :height 700
                   :webPreferences
-                  {:nodeIntegration true            ;; FIXME
-}}
+                  {:nodeIntegration false
+                   :nodeIntegrationInWorker false
+                   :contextIsolation true
+                   :preload (path/join js/__dirname "js/preload.js")}}
         url MAIN_WINDOW_ENTRY
         win (BrowserWindow. (clj->js win-opts))]
     (.loadURL win url)
@@ -73,7 +74,7 @@
            (setup-updater! nil)
 
            ;; init stuffs
-           (init-channel win)
+           (handler/set-ipc-handler! win)
 
            ;; main window events
            (.on win "close" #(if (or @*quitting? win32?)
--- src/main/frontend/fs/node.cljs
+++ src/main/frontend/fs/node.cljs
@@ -1,23 +1,41 @@
 (ns frontend.fs.node
   (:require [frontend.fs.protocol :as protocol]
             [frontend.util :as util]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [promesa.core :as p]
+            [electron.ipc :as ipc]))
+
+;; (defonce fs (when (util/electron?)
+;;               (js/require "fs")))
+
+;; (defonce path (when (util/electron?)
+;;                 (js/require "path")))
+
+;; (defn ls-dir [dir]
+;;   (->> (tree-seq
+;;         (fn [f] (.isDirectory (.statSync fs f) ()))
+;;         (fn [d] (map #(.join path d %) (.readdirSync fs d)))
+;;         dir)
+;;        (apply concat)
+;;        (doall)))
 
 (defrecord Node []
   protocol/Fs
   (mkdir! [this dir]
-    nil)
-  (readdir [this dir]
-    nil)
-  (unlink! [this path opts]
-    nil)
+    (ipc/ipc "mkdir" {:path dir}))
+  (readdir [this dir]                   ; recursive
+    (ipc/ipc "readdir" {:dir dir}))
+  (unlink! [this path _opts]
+    (ipc/ipc "unlink" {:path path}))
   (rmdir! [this dir]
     nil)
   (read-file [this dir path]
-    nil)
+    (ipc/ipc "readFile" {:path (str dir "/" path)}))
   (write-file! [this repo dir path content opts]
-    nil)
+    (ipc/ipc "writeFile" {:path (str dir "/" path)
+                          :content content}))
   (rename! [this repo old-path new-path]
-    nil)
+    (ipc/ipc "rename" {:old-path old-path
+                       :new-path new-path}))
   (stat [this dir path]
-    nil))
+    (ipc/ipc "stat" {:path (str dir path)})))
