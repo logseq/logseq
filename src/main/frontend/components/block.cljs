bd3121374fb138295b7994c9d63c65f4b4e23361
akhater <tony@multynet.net>
2021-01-23 10:27:57 +0200

    Update block.cljs
    added "backward compatibility" to support {{youtube VidID}} directly without URL

--- src/main/frontend/components/block.cljs
+++ src/main/frontend/components/block.cljs
@@ -764,7 +764,10 @@
         (let [url (first arguments)]
           ;; javascript regex "^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)(?<id>[\w\-]+)(\S+)?$"
           (def YouTube-regex #"^((?:https?:)?//)?((?:www|m).)?((?:youtube.com|youtu.be))(/(?:[\w-]+\?v=|embed/|v/)?)([\w-]+)(\S+)?$")
-          (when-let [youtube-id (nth (re-find YouTube-regex url) 5)]
+          (when-let [youtube-id (cond
+                                  (== 11 (count url)) url
+                                  :else
+                                  (nth (re-find YouTube-regex url) 5))]
             (when-not (string/blank? youtube-id)
               (let [width (min (- (util/get-width) 96)
                                560)
