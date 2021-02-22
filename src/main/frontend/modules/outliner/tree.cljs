88121ae9d390e652df57ece31324219273ec808a
defclass <defclass@outlook.com>
2021-02-22 14:31:44 +0800

    refactor(outliner): remvoe cursor impl

--- src/main/frontend/modules/outliner/tree.cljs
+++ src/main/frontend/modules/outliner/tree.cljs
@@ -105,62 +105,6 @@
           (recur node (conj children node))
           children)))))
 
-;; tangent-v is node stack '(... node-depth-2 node-depth-1 node-root)
-;; direction is either :down or :right
-(defrecord Cursor [tangent-v direction])
-
-(defn cursor?
-  [cursor]
-  (instance? Cursor cursor))
-
-(defn turn-right
-  [cursor]
-  {:pre [(cursor? cursor)]}
-  (assoc cursor :direction :right))
-
-(defn turn-down
-  [cursor]
-  {:pre [(cursor? cursor)]}
-  (assoc cursor :direction :down))
-
-(defn add-node-and-turn-down
-  [cursor node]
-  {:pre [(cursor? cursor)]}
-  (-> (update cursor :tangent-v conj node)
-      (turn-down)))
-
-(defn replace-node-and-turn-down
-  [cursor node]
-  {:pre [(cursor? cursor)]}
-  (-> (update cursor :tangent-v (fn [x] (cons node (rest x))))
-      (turn-down)))
-
-(defn remove-node-and-turn-right
-  [cursor]
-  {:pre [(cursor? cursor)]}
-  (-> (update cursor :tangent-v rest)
-      (turn-right)))
-
-(defn cursor-drained?
-  [cursor]
-  {:pre [(cursor? cursor)]}
-  (not (seq (:tangent-v cursor))))
-
-(defn get-head-&-direction
-  [cursor]
-  {:pre [(cursor? cursor)]}
-  {:head (first (:tangent-v cursor))
-   :direction (:direction cursor)})
-
-(defn init-cursor?
-  [cursor]
-  {:pre [(cursor? cursor)]}
-  (= 1 (count (:tangent-v cursor))))
-
-(defn init-cursor
-  [root]
-  {:pre [(satisfies? INode root)]}
-  (->Cursor (list root) :down))
 
 (defn insert-node-as-first
   "Insert a node as first child of its parent."
@@ -208,41 +152,6 @@
     (if (nil? left-node)
       (insert-node-as-first root parent-node)
       (insert-node-after-first root left-node))))
-
-(defn get-node-list-with-cursor
-  "Get node list with cursor.
-  Args:
-    number(int): the block count of every page.
-    cursor(Cursor): current cursor.
-
-  Return:
-    acc: list of nodes of current page.
-    cursor: current cursor after search."
-  [number cursor]
-  (letfn [(find [number cursor acc]
-            (if (or (cursor-drained? cursor)
-                    (neg? number))
-              {:cursor cursor :acc acc}
-              (let [{:keys [head direction]} (get-head-&-direction cursor)]
-                (case direction
-                  :down
-                  (if-let [node (-get-down head)]
-                    (let [cursor (add-node-and-turn-down cursor node)]
-                      (find (dec number) cursor (conj acc node)))
-                    (let [cursor (turn-right cursor)]
-                      (find number cursor acc)))
-                  :right
-                  (if-let [node (-get-right head)]
-                    (let [cursor (replace-node-and-turn-down cursor node)]
-                      (find (dec number) cursor (conj acc node)))
-                    (let [cursor (remove-node-and-turn-right cursor)]
-                      (find number cursor acc)))
-
-                  (throw (js/Error "Unknown direction."))))))]
-    (if (init-cursor? cursor)
-      (let [root-node (first (:tangent-v cursor))]
-        (find (- number 2) cursor [root-node]))
-      (find (dec number) cursor []))))
 
 (defn render-react-tree
   [init-node node-number
--- src/test/frontend/modules/outliner/tree_test.cljs
+++ src/test/frontend/modules/outliner/tree_test.cljs
@@ -2,9 +2,7 @@
   (:require [cljs.test :refer [deftest is are testing use-fixtures]]
             [frontend.modules.outliner.tree :as tree]
             [frontend.db.conn :as conn]
-            [frontend.db.outliner :as db-outliner]
-            [datascript.core :as d]
-            [frontend.util :as util]))
+            [datascript.core :as d]))
 
 (defn build-block-by-ident
   ([id]
@@ -181,26 +179,6 @@
        (is (= [6 9] old-parent's-children))
        (is (= [13 14 3 15] new-parent's-children))))))
 
-(deftest test-get-node-list-with-cursor
-  (binding [conn/*outline-db* (conn/create-outliner-db)]
-    (build-sql-records node-tree)
-    (let [cursor (-> (build-by-block-id 1 nil nil)
-                     (tree/init-cursor))
-          number 7
-          {:keys [acc cursor]}
-          (tree/get-node-list-with-cursor number cursor)]
-      (is (= [1 2 3 4 5 6 7] (mapv #(-> % :data :block/id) acc)))
-
-      (let [{:keys [acc cursor]}
-            (tree/get-node-list-with-cursor number cursor)]
-
-        (is (= [8 9 10 11 12 13 14] (mapv #(-> % :data :block/id) acc)))
-
-        (let [{:keys [acc]}
-              (tree/get-node-list-with-cursor number cursor)]
-          (is (= [15 16 17] (mapv #(-> % :data :block/id) acc))))))))
-
-
 (defn- get-block-id
   [block]
   (get-in block [:data :block/id]))
@@ -244,44 +222,3 @@
                       [6 [[7 [8]]]]
                       [9 [10]]]]]]]
              result)))))
-
-(comment
-  (defn build-node-from-sql-record
-    "build node from RDS records"
-    [node-id sql-records]
-    (letfn [(get-right
-              [node-id children]
-              (some #(when (= (:left %) node-id)
-                       %)
-                    children))
-
-            (sort-children
-              [parent-node-id children]
-              (loop [node-id parent-node-id
-                     result []]
-                (if-let [node (get-right node-id children)]
-                  (let [result (conj result node)]
-                    (recur (:id node) result))
-                  (do
-                    (when (not= (count children) (count result))
-                      (throw (js/Error "children data error, ")))
-                    result))))
-
-            (get-children
-              [node-id]
-              (filter #(= (:parent %) node-id) sql-records))
-
-            (build [node-id depth]
-              (when (= depth 20)
-                (throw (js/Error "Recur depth is too large.")))
-              (let [children (some->> (get-children node-id)
-                                      (sort-children node-id))
-                    children (mapv #(build (:id %) (inc depth)) children)]
-                (->RenderNode node-id children)))]
-      (build node-id 0)))
-
-  (deftest test-serialize-&-deserialize-tree
-    (let [tree-record (build-render-tree tree)
-          sql-record (build-sql-records tree-record)
-          tree (build-node-from-sql-record 1 sql-record)]
-      (is (= tree tree-record)))))
