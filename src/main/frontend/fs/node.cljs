ee1704729e04e8bef70c5958fac3dafbb8457f39
Tienson Qin <tiensonqin@gmail.com>
2021-03-05 22:29:25 +0800

    chore: re-enable mtime check when writing to disk

--- src/main/frontend/fs/node.cljs
+++ src/main/frontend/fs/node.cljs
@@ -25,34 +25,22 @@
 
 (defn- write-file-impl!
   [repo dir path content {:keys [ok-handler error-handler] :as opts} stat]
-  (->
-   (p/let [result (ipc/ipc "writeFile" path content)
-           mtime (gobj/get result "mtime")]
-     (db/set-file-last-modified-at! repo path mtime)
-     (when ok-handler
-       (ok-handler repo path result))
-     result)
-   (p/catch (fn [error]
-              (if error-handler
-                (error-handler error)
-                (log/error :write-file-failed error)))))
-  ;; (p/let [disk-mtime (when stat (gobj/get stat "mtime"))
-  ;;         db-mtime (db/get-file-last-modified-at repo path)]
-  ;;   (if (not= disk-mtime db-mtime)
-  ;;     (js/alert (str "The file has been modified in your local disk! File path: " path
-  ;;                    ", please save your changes and click the refresh button to reload it."))
-  ;;     (->
-  ;;      (p/let [result (ipc/ipc "writeFile" path content)
-  ;;              mtime (gobj/get result "mtime")]
-  ;;        (db/set-file-last-modified-at! repo path mtime)
-  ;;        (when ok-handler
-  ;;          (ok-handler repo path result))
-  ;;        result)
-  ;;      (p/catch (fn [error]
-  ;;                 (if error-handler
-  ;;                   (error-handler error)
-  ;;                   (log/error :write-file-failed error)))))))
-)
+  (p/let [disk-mtime (when stat (gobj/get stat "mtime"))
+          db-mtime (db/get-file-last-modified-at repo path)]
+    (if (not= disk-mtime db-mtime)
+      (js/alert (str "The file has been modified in your local disk! File path: " path
+                     ", please save your changes and click the refresh button to reload it."))
+      (->
+       (p/let [result (ipc/ipc "writeFile" path content)
+               mtime (gobj/get result "mtime")]
+         (db/set-file-last-modified-at! repo path mtime)
+         (when ok-handler
+           (ok-handler repo path result))
+         result)
+       (p/catch (fn [error]
+                  (if error-handler
+                    (error-handler error)
+                    (log/error :write-file-failed error))))))))
 
 (defrecord Node []
   protocol/Fs
