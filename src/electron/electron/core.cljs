0a6310f86dba20dd900762201783da0d22d6f608
Weihua Lu <luwh364@gmail.com>
2021-03-11 17:54:17 +0800

    just use not mac actually is enough

--- src/electron/electron/core.cljs
+++ src/electron/electron/core.cljs
@@ -1,7 +1,7 @@
 (ns electron.core
   (:require [electron.handler :as handler]
             [electron.updater :refer [init-updater]]
-            [electron.utils :refer [mac? linux? win32? prod? dev? logger open]]
+            [electron.utils :refer [mac? win32? prod? dev? logger open]]
             [clojure.string :as string]
             ["fs" :as fs]
             ["path" :as path]
@@ -23,8 +23,8 @@
   []
   (let [win-opts {:width         980
                   :height        700
-                  :frame         (or win32? linux?)
-                  :autoHideMenuBar win32?
+                  :frame         (not mac?)
+                  :autoHideMenuBar (not mac?)
                   :titleBarStyle (if mac? "hidden" nil)
                   :webPreferences
                   {:nodeIntegration         false
@@ -34,7 +34,6 @@
                    :preload                 (path/join js/__dirname "js/preload.js")}}
         url MAIN_WINDOW_ENTRY
         win (BrowserWindow. (clj->js win-opts))]
-    (when linux?  (.removeMenu win))
     (.loadURL win url)
     (when dev? (.. win -webContents (openDevTools)))
     win))
