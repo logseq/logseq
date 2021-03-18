2625552e76bf5b921c957832561b41b3c9eebd9d
Charlie <xyhp915@qq.com>
2021-03-18 19:46:39 +0800

    improve(dev): support tailwind jit mode (#1473)

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = (ctx) => ({
   plugins: [
-    require('postcss-nested'),
-    require('tailwindcss')('tailwind.config.js'),
+    require('postcss-nested')({}),
+    require('postcss-import-ext-glob')({}),
+    require('postcss-import')({}),
+    require('@tailwindcss/jit')('tailwind.config.js'),
   ],
 })
