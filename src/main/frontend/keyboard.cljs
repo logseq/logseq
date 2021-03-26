b05d32153a590835cc04e521dcc9c7a760ce56c1
Weihua Lu <luwh364@gmail.com>
2021-03-26 10:38:08 +0800

    handle mod key and multiple binding per id

--- src/main/frontend/keyboard.cljs
+++ src/main/frontend/keyboard.cljs
@@ -1,10 +1,12 @@
 (ns frontend.keyboard
-  (:require [goog.events :as events]
-            [lambdaisland.glogi :as log]
-            [frontend.util :refer [keyname]]
+  (:require [clojure.string :as str]
             [frontend.keyboards.config :as kb-config]
-            [goog.ui.KeyboardShortcutHandler.EventType :as EventType])
-  (:import  [goog.ui KeyboardShortcutHandler]))
+            [frontend.state :as state]
+            [frontend.util :refer [keyname mac?]]
+            [goog.events :as events]
+            [goog.ui.KeyboardShortcutHandler.EventType :as EventType]
+            [lambdaisland.glogi :as log])
+  (:import [goog.ui KeyboardShortcutHandler]))
 
 ;; Copy from https://github.com/tonsky/rum/blob/gh-pages/doc/useful-mixins.md#keyboard-shortcut
 
@@ -33,22 +35,36 @@
          (.unregisterShortcut handler key)
          (unlisten-fn))))))
 
+(defn- mod-key [shortcut]
+  (str/replace shortcut #"(?i)mod"
+               (if mac? "meta" "ctrl")))
+
 (defn shortcut-binding
   [id]
-  (get kb-config/default-shortcuts id))
+  (let [shortcut (or (state/get-shortcut id)
+                     (get kb-config/default-shortcuts id))]
+    (when-not shortcut
+      (log/error :keyboard/shorcut-binding-notfound {:id id}))
+    (->>
+     (if (string? shortcut)
+       [shortcut]
+       shortcut)
+     (mapv mod-key))))
 
+(defn register-shortcuts
+  [^js handler id]
+  (doseq [k (shortcut-binding id)]
+    (.registerShortcut handler (keyname id) k)))
 
-; each dispatcher key should be present in shortcut config
-(defn- valid-dispatcher? [dispatcher]
-  (->> dispatcher
-       keys
-       (every? (fn [k] (contains? kb-config/default-shortcuts k)))))
+(defn unregister-shortcuts
+  [^js handler id]
+  (doseq [k (shortcut-binding id)]
+    (.unregisterShortcut handler k)))
 
 (defn install-shortcuts!
   ([dispatcher]
    (install-shortcuts! dispatcher js/window))
   ([dispatcher target]
-   (assert (valid-dispatcher? dispatcher) "forget to register shortcut in config?")
    (let [handler (new KeyboardShortcutHandler target)]
 
      ;; register shortcuts
@@ -56,7 +72,7 @@
        (log/info :keyboard/install-shortcut {:id id :shortcut (shortcut-binding id)})
        ;; do i need this?
        ;; (.unregisterShortcut handler (shortcut-binding id))
-       (.registerShortcut handler (keyname id) (shortcut-binding id)))
+       (register-shortcuts handler id))
 
      (let [f (fn [e]
                (let [dispatch-fn (get dispatcher (keyword (.-identifier e)))]
@@ -69,10 +85,12 @@
        (fn []
          (doseq [[id _] dispatcher]
            (log/info :keyboard/remove-shortcut {:id id :shortcut (shortcut-binding id)})
-           (.unregisterShortcut handler (shortcut-binding id)))
+           (unregister-shortcuts handler id))
          (unlisten-fn))))))
+
 
 (comment
   ; dispatcher example
   (def dispatcher {:test/test (fn [e] (println "trigger test"))})
+
   (def f (install-shortcuts! dispatcher)))
