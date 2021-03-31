3c88a29db3cdd2fe18864932210c75da8e0d4cf7
Tienson Qin <tiensonqin@gmail.com>
2021-03-31 13:16:24 +0800

    fix: file corrupts when editing code blocks
    Related to #1498

--- src/main/frontend/extensions/code.cljs
+++ src/main/frontend/extensions/code.cljs
@@ -50,43 +50,38 @@
   [editor textarea config state]
   (.save editor)
   (let [value (gobj/get textarea "value")
-        default-value (gobj/get textarea "defaultValue")]
-    (cond
-      (:block/uuid config)
-      (let [block (db/pull [:block/uuid (:block/uuid config)])
-            format (:block/format block)
-            ;; Get newest state
-            pos-meta (:pos-meta state)
-            content (:block/content block)
-            {:keys [start_pos end_pos]} @pos-meta
-            prev-content (utf8/substring (utf8/encode content)
-                                         0 start_pos)
-            value (str (if (not= "\n" (last prev-content))
-                         "\n")
-                       (string/trimr value)
-                       "\n")
-            content' (utf8/insert! content
-                                   start_pos
-                                   end_pos
-                                   value)]
-        (reset! pos-meta {:start_pos start_pos
-                          :end_pos (+ start_pos
-                                      (utf8/length (utf8/encode value)))})
-        (editor-handler/save-block-if-changed! block content'))
+        default-value (gobj/get textarea "defaultValue")
+        pos-meta (:pos-meta state)]
+    (when (not= value default-value)
+      (cond
+       (:block/uuid config)
+       (let [block (db/pull [:block/uuid (:block/uuid config)])
+             format (:block/format block)
+             content (:block/content block)
+             {:keys [start_pos end_pos]} @pos-meta
+             value (if (= \n (last value)) value (str value "\n"))
+             content' (utf8/insert! content start_pos end_pos value)]
+         (state/set-editor-op! :code-editor)
+         (editor-handler/save-block-if-changed! block content')
+         (let [new-pos-meta {:start_pos start_pos
+                             :end_pos (+ start_pos
+                                         (utf8/length (utf8/encode value)))}
+               old-pos-meta @pos-meta]
+           (reset! pos-meta new-pos-meta)))
 
-      (:file-path config)
-      (let [path (:file-path config)
-            content (db/get-file-no-sub path)
-            value (some-> (gdom/getElement path)
-                          (gobj/get "value"))]
-        (when (and
-               (not (string/blank? value))
-               (not= (string/trim value) (string/trim content)))
-          (file-handler/alter-file (state/get-current-repo) path (string/trim value)
-                                   {:re-render-root? true})))
+       (:file-path config)
+       (let [path (:file-path config)
+             content (db/get-file-no-sub path)
+             value (some-> (gdom/getElement path)
+                           (gobj/get "value"))]
+         (when (and
+                (not (string/blank? value))
+                (not= (string/trim value) (string/trim content)))
+           (file-handler/alter-file (state/get-current-repo) path (string/trim value)
+                                    {:re-render-root? true})))
 
-      :else
-      nil)))
+       :else
+       nil))))
 
 (defn- text->cm-mode
   [text]
@@ -109,10 +104,11 @@
 
 (defn render!
   [state]
-  (let [editor-atom (:editor-atom state)]
+  (let [editor-atom (:editor-atom state)
+        esc-pressed? (atom nil)]
     (if @editor-atom
       @editor-atom
-      (let [[config id attr code pos_meta] (:rum/args state)
+      (let [[config id attr code] (:rum/args state)
             original-mode (get attr :data-lang)
             mode (or original-mode "javascript")
             clojure? (contains? #{"clojure" "clj" "text/x-clojure" "cljs" "cljc"} mode)
@@ -138,11 +134,16 @@
                                                                         (if changed?
                                                                           (save!)
                                                                           (editor-handler/edit-block! block :max (:block/format block) block-id)))
-                                                                      (save!))))}})))]
+                                                                      (save!)))
+                                                                  ;; TODO: return "handled" or false doesn't always prevent event bubbles
+                                                                  (reset! esc-pressed? true)
+                                                                  (js/setTimeout #(reset! esc-pressed? false) 10))}})))]
         (when editor
           (let [element (.getWrapperElement editor)]
-            (.on editor "blur" (fn []
-                                 (save-file-or-block-when-blur-or-esc! editor textarea config state)))
+            (.on editor "blur" (fn [_cm e]
+                                 (util/stop e)
+                                 (when-not @esc-pressed?
+                                   (save-file-or-block-when-blur-or-esc! editor textarea config state))))
             (.addEventListener element "click"
                                (fn [e]
                                  (util/stop e)))
@@ -167,7 +168,7 @@
    :did-remount (fn [old_state state]
                   (load-and-render! state)
                   state)}
-  [state config id attr code pos_meta]
+  [state config id attr code pos-meta]
   [:div.extensions__code
    {:on-mouse-down (fn [e] (util/stop e))}
    [:div.extensions__code-lang
