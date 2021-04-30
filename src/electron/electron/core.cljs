a3053c5184cddd70a67a2f780d9dbedf0d198d3a
charlie <xyhp915@qq.com>
2021-04-30 14:43:34 +0800

    chore(canary): add logo & close updater

--- src/electron/electron/core.cljs
+++ src/electron/electron/core.cljs
@@ -49,10 +49,11 @@
 
 (defn setup-updater! [^js win]
   ;; manual/auto updater
-  (when-not linux?
-    (init-updater {:repo   "logseq/logseq"
-                   :logger logger
-                   :win    win})))
+  ;;(when-not linux?
+  ;;  (init-updater {:repo   "logseq/logseq"
+  ;;                 :logger logger
+  ;;                 :win    win}))
+)
 
 (defn setup-interceptor! []
   (.registerFileProtocol
