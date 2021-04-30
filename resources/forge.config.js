df7531273c5f61ea718a08176f898b9c0a7c91a9
charlie <xyhp915@qq.com>
2021-04-30 14:43:34 +0800

    chore(canary): add logo & close updater

--- resources/forge.config.js
+++ resources/forge.config.js
@@ -2,33 +2,22 @@ const path = require('path')
 
 module.exports = {
   packagerConfig: {
-    icon: './icons/logseq_big_sur.icns',
-    osxSign: {
-      identity: 'Developer ID Application: Tiansheng Qin',
-      'hardened-runtime': true,
-      entitlements: 'entitlements.plist',
-      'entitlements-inherit': 'entitlements.plist',
-      'signature-flags': 'library'
-    },
-    osxNotarize: {
-      appleId: "my-fake-apple-id",
-      appleIdPassword: "my-fake-apple-id-password",
-    },
+    icon: './icons/canary/logseq_big_sur.icns',
   },
   makers: [
     {
       'name': '@electron-forge/maker-squirrel',
       'config': {
-        'name': 'Logseq',
-        'setupIcon': './icons/logseq.ico'
+        'name': 'Logseq Canary',
+        'setupIcon': './icons/canary/logseq.ico'
       }
     },
     {
       name: '@electron-forge/maker-dmg',
       config: {
         format: 'ULFO',
-        icon: './icons/logseq_big_sur.icns',
-        name: 'Logseq'
+        icon: './icons/canary/logseq_big_sur.icns',
+        name: 'Logseq Canary'
       }
     },
     {
