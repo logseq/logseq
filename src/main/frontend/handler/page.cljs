d256982c9cff5404946402d61ff60f9220e1239e
Tienson Qin <tiensonqin@gmail.com>
2021-05-23 12:33:35 +0800

    fix: adding a "#tag" at the front of a block clear the whole block

--- src/main/frontend/handler/page.cljs
+++ src/main/frontend/handler/page.cljs
@@ -441,13 +441,19 @@
         (let [wrapped? (= "[[" (util/safe-subs edit-content (- pos 2) pos))
               chosen (if (and (re-find #"\s+" chosen) (not wrapped?))
                        (util/format "[[%s]]" chosen)
-                       chosen)]
+                       chosen)
+              q (if @editor-handler/*selected-text "" q)
+              [last-pattern forward-pos] (if wrapped?
+                                           [q 3]
+                                           (if (= \# (first q))
+                                             [(subs q 1) 1]
+                                             [q 2]))
+              last-pattern (str "#" (when wrapped? "[[") last-pattern)]
           (editor-handler/insert-command! id
                                           (str "#" (when wrapped? "[[") chosen)
                                           format
-                                          {:last-pattern (let [q (if @editor-handler/*selected-text "" q)]
-                                                           (str "#" (when wrapped? "[[") q))
-                                           :forward-pos (if wrapped? 3 2)})))
+                                          {:last-pattern last-pattern
+                                           :forward-pos forward-pos})))
       (fn [chosen _click?]
         (state/set-editor-show-page-search! false)
         (let [page-ref-text (get-page-ref-text chosen)]
