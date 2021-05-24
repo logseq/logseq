5ec6e31bfd4984676bf7b3bbfdf1e4b13ac4e937
Tienson Qin <tiensonqin@gmail.com>
2021-05-24 09:28:28 +0800

    chore: switch back to Logseq from Canary

--- resources/forge.config.js
+++ resources/forge.config.js
@@ -2,24 +2,32 @@ const path = require('path')
 
 module.exports = {
   packagerConfig: {
-    icon: './icons/canary/logseq_big_sur.icns',
-    name: 'Logseq Canary',
+    icon: './icons/logseq_big_sur.icns',
+    osxSign: {
+      identity: 'Developer ID Application: Tiansheng Qin',
+      'hardened-runtime': true,
+      entitlements: 'entitlements.plist',
+      'entitlements-inherit': 'entitlements.plist',
+      'signature-flags': 'library'
+    },
+    osxNotarize: {
+      appleId: "my-fake-apple-id",
+      appleIdPassword: "my-fake-apple-id-password",
+    },
   },
   makers: [
     {
       'name': '@electron-forge/maker-squirrel',
       'config': {
-        title: 'Logseq Canary',
-        name: 'LogseqCanary', // ID name
-        setupIcon: './icons/canary/logseq.ico'
+        'name': 'Logseq'
       }
     },
     {
       name: '@electron-forge/maker-dmg',
       config: {
         format: 'ULFO',
-        icon: './icons/canary/logseq_big_sur.icns',
-        name: 'Logseq Canary'
+        icon: './icons/logseq_big_sur.icns',
+        name: 'Logseq'
       }
     },
     {
