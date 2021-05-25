30e0bef128895fa945445f226d38ad891a3caa77
charlie <xyhp915@qq.com>
2021-05-25 10:12:43 +0800

    fix: open external link in new window

--- src/main/frontend/components/settings.cljs
+++ src/main/frontend/components/settings.cljs
@@ -139,7 +139,7 @@
 (rum/defc outdenting-hint
   []
   [:div
-   [:p "See more details at " [:a {:href "https://discuss.logseq.com/t/whats-your-preferred-outdent-behavior-the-direct-one-or-the-logical-one/978"} "here"] "."]
+   [:p "See more details at " [:a {:target "_blank" :href "https://discuss.logseq.com/t/whats-your-preferred-outdent-behavior-the-direct-one-or-the-logical-one/978"} "here"] "."]
    [:p "default(left) vs logical(right)"]
    [:img {:src "https://discuss.logseq.com/uploads/default/original/1X/e8ea82f63a5e01f6d21b5da827927f538f3277b9.gif"
           :width 500
