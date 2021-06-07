3532ecb40ccca841a60a7aba1b59bbce313f7606
Tienson Qin <tiensonqin@gmail.com>
2021-06-07 18:22:57 +0800

    fix: clear indentation for paragraphs

--- src/main/frontend/debug.cljs
+++ src/main/frontend/debug.cljs
@@ -1,10 +1,9 @@
 (ns frontend.debug
   (:require [cljs.pprint :as pprint]
-            [frontend.config :as config]
             [frontend.state :as state]))
 
 (defn pprint
   [& xs]
-  (when (or config/dev? (state/developer-mode?))
+  (when (state/developer-mode?)
     (doseq [x xs]
       (pprint/pprint x))))
