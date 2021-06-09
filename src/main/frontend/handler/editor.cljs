ede4c9a1e79b3592e090f1a45eb2744eb30bedd3
Weihua Lu <luwh364@gmail.com>
2021-06-09 12:05:01 +0800

    refactor: clean duplicate methods

--- src/main/frontend/handler/editor.cljs
+++ src/main/frontend/handler/editor.cljs
@@ -150,7 +150,7 @@
     (when-let [cursor-range (state/get-cursor-range)]
       (when-let [range cursor-range]
         (let [pos (diff/find-position markup range)]
-          (util/set-caret-pos! node pos))))))
+          (cursor/move-cursor-to node pos))))))
 
 (defn highlight-block!
   [block-uuid]
@@ -714,7 +714,7 @@
       (let [new-pos (commands/compute-pos-delta-when-change-marker
                      current-input content new-content marker (util/get-input-pos current-input))]
         (state/set-edit-content! edit-input-id new-content)
-        (util/set-caret-pos! current-input new-pos)))))
+        (cursor/move-cursor-to current-input new-pos)))))
 
 
 (defn set-marker
@@ -2148,8 +2148,8 @@
     (cond
       (not= selected-start selected-end)
       (if up?
-        (util/set-caret-pos! input selected-start)
-        (util/set-caret-pos! input selected-end))
+        (cursor/move-cursor-to input selected-start)
+        (cursor/move-cursor-to input selected-end))
 
       (or (and up? (cursor/textarea-cursor-first-row? input))
           (and down? (cursor/textarea-cursor-last-row? input)))
@@ -2191,8 +2191,8 @@
       (cond
         (not= selected-start selected-end)
         (if left?
-          (util/set-caret-pos! input selected-start)
-          (util/set-caret-pos! input selected-end))
+          (cursor/move-cursor-to input selected-start)
+          (cursor/move-cursor-to input selected-end))
 
         (or (and left? (util/input-start? input))
             (and right? (util/input-end? input)))
