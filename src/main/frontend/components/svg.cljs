9c3233aa96b55b94621d34c8c63480167b37d4ce
Peng Xiao <pengxiao@freewheel.tv>
2021-06-12 13:56:27 +0800

    fix: some minor issues

--- src/main/frontend/components/svg.cljs
+++ src/main/frontend/components/svg.cljs
@@ -531,7 +531,7 @@
 
 (def edit
   [:svg.h-6.w-6
-   {:stroke "currentColor", :viewbox "0 0 24 24", :fill "none"}
+   {:stroke "currentColor", :view-box "0 0 24 24", :fill "none"}
    [:path
     {:d
      "M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z",
