31ac958a481bc836885620f7f7de2cb504403abb
Tienson Qin <tiensonqin@gmail.com>
2021-06-16 21:53:54 +0800

    code refactor

---
+++ cypress/integration/app/util.cljs
@@ -0,0 +1,38 @@
+(ns app.util
+  (:refer-clojure :exclude [first get]))
+
+(def cy js/cy)
+
+(defn back-to-home
+  []
+  (.. cy (get ".cp__header-logo")
+      (first)
+      (click)))
+
+(defn edit-block
+  [content]
+  (.. cy (get "textarea")
+      (first)
+      (click)
+      (type content)))
+
+(defn tab
+  []
+  (.. cy (realPress "Tab")))
+
+(defn shift+tab
+  []
+  (.. cy (realPress #js ["Shift" "Tab"])))
+
+(defn backspace
+  []
+  (edit-block "{backspace}"))
+
+(defn delete
+  []
+  (.. cy (realPress "Delete")))
+
+(defn match-content
+  [value]
+  (.. cy (get "textarea") (first)
+      (should "have.value" value)))
