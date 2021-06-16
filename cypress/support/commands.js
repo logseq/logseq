69fa58ff9cdedb885dc352b7aa0bd30fc11cf8a2
Tienson Qin <tiensonqin@gmail.com>
2021-06-16 16:47:43 +0800

    test: add some tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,22 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('clearIndexedDB', async () => {
+  const databases = await window.indexedDB.databases();
+
+  await Promise.all(
+    databases.map(
+      ({ name }) =>
+      new Promise((resolve, reject) => {
+        const request = window.indexedDB.deleteDatabase(name);
+
+        request.addEventListener('success', resolve);
+        // Note: we need to also listen to the "blocked" event
+        // (and resolve the promise) due to https://stackoverflow.com/a/35141818
+        request.addEventListener('blocked', resolve);
+        request.addEventListener('error', reject);
+      }),
+    ),
+  );
+});
