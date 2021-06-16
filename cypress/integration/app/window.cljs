8782f00f5631c30656855ec9a0e69a8fe8384e47
Tienson Qin <tiensonqin@gmail.com>
2021-06-16 16:47:43 +0800

    test: add some tests

--- cypress/integration/app/window.cljs
+++ cypress/integration/app/window.cljs
@@ -1,10 +1,52 @@
 (ns app.window
-  (:require-macros [latte.core :refer [describe beforeEach it]]))
+  (:require-macros [latte.core :refer [describe beforeEach before it]])
+  (:require [latte.chai :refer (expect)])
+  (:refer-clojure :exclude [first get]))
 
 (def cy js/cy)
 
+(defn back-to-home
+  []
+  (.. cy (get ".cp__header-logo")
+      (first)
+      (click)))
+
+(defn edit-block
+  [content]
+  (.. cy (get "textarea")
+      (first)
+      (click)
+      (type content)))
+
+
 (describe "Window"
   (beforeEach []
-    (.visit cy "https://example.cypress.io/commands/window"))
-  (it "cy.window() - get the global window object" []
-    (.should (.window cy) "have.property" "top")))
+              (.clearIndexedDB cy))
+  (before []
+          (.visit cy "http://localhost:3001"))
+
+  (it "Search" []
+    (.. cy
+        (get "#search-field")
+        (click)
+        (type "welcome to Logseq"))
+    (.. cy (get "#ui__ac-inner")
+        (should (fn [result]
+                  (expect result :to.have.length 1))))
+    (back-to-home)
+
+    ;; create new page
+    (.. cy
+        (get "#search-field")
+        (click)
+        (type "new page"))
+
+    (.wait cy 1000)
+
+    (.. cy
+        (get "#search-field")
+        (type "{enter}"))
+
+    ;; edit bullet
+    (edit-block "this is my first bullet {enter}")
+    (edit-block "this is my second bullet {enter}")))
