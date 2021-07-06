a821bf3e6c46249a0deb198b0ef9521df86ab79f
Tienson Qin <tiensonqin@gmail.com>
2021-07-06 23:06:23 +0800

    fix: don't check ast version for GitHub

--- src/main/frontend/handler.cljs
+++ src/main/frontend/handler.cljs
@@ -115,7 +115,8 @@
 
                             (watch-for-date!)
                             (file-handler/watch-for-local-dirs!)
-                            (state/pub-event! [:after-db-restore repos])))
+                            (when-not (state/logged?)
+                              (state/pub-event! [:after-db-restore repos]))))
                          (p/catch (fn [error]
                                     (log/error :db/restore-failed error))))))]
     ;; clear this interval
