69426ae54b4852e691afcac7062aa86bb38a2db3
Tienson Qin <tiensonqin@gmail.com>
2021-08-06 21:30:32 +0800

    enhance: hide annotation properties

--- src/main/frontend/util/property.cljs
+++ src/main/frontend/util/property.cljs
@@ -20,7 +20,8 @@
 (defn built-in-properties
   []
   (set/union
-   #{:id :custom-id :background-color :heading :collapsed :created-at :updated-at :last-modified-at :created_at :last_modified_at :query-table :query-properties :query-sort-by :query-sort-desc}
+   #{:id :custom-id :background-color :heading :collapsed :created-at :updated-at :last-modified-at :created_at :last_modified_at :query-table :query-properties :query-sort-by :query-sort-desc
+     :ls-type :hl-type :hl-page :hl-stamp}
    (set (map keyword config/markers))
    (set (config/get-block-hidden-properties))
    @built-in-extended-properties))
