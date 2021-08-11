13eb3b347fbe4c29f50b4599c3dfd91ff39c2100
Tienson Qin <tiensonqin@gmail.com>
2021-08-12 00:20:56 +0800

    fix: date format order

--- src/main/frontend/state.cljs
+++ src/main/frontend/state.cljs
@@ -949,8 +949,10 @@
   []
   (or
    (when-let [repo (get-current-repo)]
-     (or (get-in @state [:config repo :date-formatter])
-         (get-in @state [:config repo :journal/page-title-format])))
+     (or
+      (get-in @state [:config repo :journal/page-title-format])
+      ;; for compatibility
+      (get-in @state [:config repo :date-formatter])))
    ;; TODO:
    (get-in @state [:me :settings :date-formatter])
    "MMM do, yyyy"))
