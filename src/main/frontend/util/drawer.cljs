02bdc0e8007fe87b198f38627b171cfd519d5643
leizhe <lzhes43@gmail.com>
2021-08-25 07:49:44 +0900

    fix(md): drawer works in md now

--- src/main/frontend/util/drawer.cljs
+++ src/main/frontend/util/drawer.cljs
@@ -25,19 +25,13 @@
    (vector? block)
    (= "Drawer" (first block))))
 
-(defn drawer-typ?
-  [block typ]
-  (and
-   (vector? block)
-   (drawer-block? block)
-   (= typ (second block))))
-
-
 (defn insert-drawer
   [{:block/keys [format] :as block} content typ value]
   (when (string? content)
     (let [ast (mldoc/->edn content (mldoc/default-config format))
-          has-typ-drawer? (mldoc/typ-drawer? (nth ast 2) typ)
+          has-properties? (mldoc/properties? (second ast))
+          has-typ-drawer? (mldoc/typ-drawer?
+                           (nth ast (if has-properties? 2 1)) typ)
           lines (string/split-lines content)
           title (first lines)
           body (string/join "\n" (rest lines))
@@ -47,14 +41,20 @@
           result  (cond
                     (not has-typ-drawer?)
                     (let [drawer (build-drawer-str typ value)]
-                      (println drawer)
-                      ;; (println "Body:\n" body)
-                      (if (mldoc/properties? (second ast))
-                        (let [prop-start-idx (.indexOf lines property/properties-start)
-                              prop-end-idx (.indexOf lines property/properties-end)
-                              properties (subvec lines prop-start-idx (inc prop-end-idx))
-                              after (subvec lines (inc prop-end-idx))]
-                          (string/join "\n" (concat [title] properties [drawer] after)))
+                      (if has-properties?
+                        (cond
+                          (= :org format)
+                          (let [prop-start-idx (.indexOf lines property/properties-start)
+                                prop-end-idx (.indexOf lines property/properties-end)
+                                properties (subvec lines prop-start-idx (inc prop-end-idx))
+                                after (subvec lines (inc prop-end-idx))]
+                            (string/join "\n" (concat [title] properties [drawer] after)))
+
+                          :else
+                          (let [properties-count (count (second (first (second ast))))
+                                before (subvec lines 0 (inc properties-count))
+                                after (rest lines)]
+                            (string/join "\n" (concat before [drawer] after))))
                         (str title "\n" drawer body)))
 
                     (and has-typ-drawer?
@@ -65,30 +65,7 @@
                                   value)
                           after (subvec lines (inc end-idx))
                           lines (concat before [(drawer-start typ)] middle [drawer-end] after)]
-                      ;; (println "Concat before: " before)
-                      ;; (println "Concat middle: " middle)
-                      ;; (println "Concat lines: " lines)
                       (string/join "\n" lines))
                     :else
                     content)]
-      ;; (println "Block ast:\n" ast)
-      (println "Logbook drawer exist: " has-typ-drawer?)
-      ;; (println "Logbook drawer starts at " start-idx)
-      ;; (println "Logbook drawer ends at " end-idx)
-      (println "Content before:\n" content)
-      (println "Content after:\n" result)
-      (println "-----------------------------------\n\n\n")
       (string/trimr result))))
-
-(defn get-drawer
-  [block typ]
-  (remove
-   (fn [x] (= x ""))
-   (map (fn [x]
-          (string/trim x))
-        (first (block-handler/walk-block
-                block
-                (fn [x]
-                  (and (drawer-block? x)
-                       (= typ (second x))))
-                #(next (next %)))))))
