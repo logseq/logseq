f544c57d1cd8371c40edd55ecd4a3519e4e13932
rcmerci <rcmerci@gmail.com>
2021-09-06 19:45:31 +0800

    delete range nodes

--- src/main/frontend/modules/outliner/yjs.cljs
+++ src/main/frontend/modules/outliner/yjs.cljs
@@ -122,7 +122,6 @@
                   (or (db-model/query-block-by-uuid id)
                       (first (block/extract-blocks (mldoc/->edn content (mldoc/default-config format))
                                                    content true format))))
-
         sibling? (not= parent-id left-id)]
     (outliner-core/insert-node new-node target-node sibling?)))
 
@@ -187,6 +186,70 @@
   ;; TODO
   )
 
+(defn- goto-innermost-struct-array [pos struct]
+  (loop [i 0 s struct]
+    (if (> i (- (count pos) 2))
+      s
+      (recur (inc i) (.get s (get pos i))))))
+
+(deftype Pos [pos-vec]
+
+  Object
+  (toString [_] pos-vec)
+
+  (inc-pos [_] (Pos. (conj (vec (butlast pos-vec)) (inc (last pos-vec)))))
+  (inc-level-pos [_] (Pos. (conj (Pos. (conj (vec (butlast pos-vec)) (inc (last pos-vec)))) 0)))
+  (add-next-level [_] (Pos. (conj pos-vec 0)))
+
+  (next-sibling-pos [_ struct]
+    (let [inner-struct (goto-innermost-struct-array pos-vec struct)
+          next-item (.get inner-struct (inc (last pos-vec)))]
+    (if (instance? y/Array next-item)
+      (Pos. (conj (vec (butlast pos-vec)) (+ 2 (last pos-vec))))
+      (Pos. (conj (vec (butlast pos-vec)) (+ 1 (last pos-vec)))))))
+
+  (next-non-sibling-pos! [this struct]
+    "create a y/Array when no child follows item at POS"
+    (let [inner-struct (goto-innermost-struct-array this struct)
+        next-item (.get inner-struct (inc (last this)))]
+    (when-not (instance? y/Array next-item)
+      (.insert inner-struct (inc (last this)) (clj->js [(y/Array.)])))
+    (.inc-level-pos this)))
+
+  ICounted
+  (-count [_] (count pos-vec))
+
+  ILookup
+  (-lookup [_ k] (get pos-vec k))
+  (-lookup [_ k not-found] (get pos-vec k not-found))
+
+  INext
+  (-next [_] (next pos-vec))
+
+  IComparable
+  (-compare [this other]
+    (let [pos1 (.-pos-vec this)
+          pos2 (.-pos-vec other)
+          len1 (count pos1)
+          len2 (count pos2)]
+      (loop [i 0]
+        (cond
+          (and (< i len1) (>= i len2))
+          -1
+          (and (< i len2) (>= i len1))
+          1
+          (and (>= i len1) (>= i len2))
+          0
+          :else
+          (let [nthi1 (nth pos1 i)
+                nthi2 (nth pos2 i)]
+            (cond
+              (< nthi1 nthi2)
+              -1
+              (> nthi1 nthi2)
+              1
+              (= nthi1 nthi2)
+              (recur (inc i)))))))))
 
 (defn find-pos [struct id]
   (let [toplevel (js->clj (.toArray struct))
@@ -199,15 +262,47 @@
           (let [sublevel (get toplevel i)]
             (if (instance? y/Array sublevel)
               (if-let [index (find-pos sublevel id)]
-                (vec (flatten [i index]))
+                (->Pos (vec (flatten [i index])))
                 (recur (+ i 1)))
               (recur (+ i 1)))))))))
 
-(defn inc-pos [pos]
-  (conj (vec (butlast pos)) (inc (last pos))))
+(defn- common-prefix [vec1 vec2]
+  (let [len1 (count vec1)
+        len2 (count vec2)]
+    (loop [i 0 r1 vec1 r2 vec2]
+      (cond
+        (or (>= i len1) (>= i len2))
+        [(subvec vec1 0 i) r1 r2]
 
-(defn inc-level-pos [pos]
-  (conj (inc-pos pos) 0))
+        (= (vec1 i) (vec2 i))
+        (recur (inc i) (vec (rest r1)) (vec (rest r2)))
+
+        :else
+        [(subvec vec1 0 i) r1 r2]))))
+
+(defn distance [pos-vec1 pos-vec2]
+    "(distance [1 2] [1 4]) => [[[1 2] 3]] => [[prefix-of-THIS-pos length]]
+(distance [1 2 3 1] [1 4 1]) => [[[1 2 3 1] :all] [[1 2 4] :all] [[1 3] 1] [[1 4 0] 2]]"
+  (let [[same-prefix-vec pos-vec1* pos-vec2*] (common-prefix pos-vec1 pos-vec2)
+        r (transient [])]
+    (when (> (count pos-vec1*) 1)
+      (conj! r [(vec (concat same-prefix-vec pos-vec1*)) :all]))
+    (loop [sublen (dec (count pos-vec1*))]
+      (when (> sublen 1)
+        (let [prefix (vec (concat same-prefix-vec (subvec pos-vec1* 0 sublen)))
+              prefix* (conj (vec (butlast prefix)) (inc (last prefix)))]
+          (conj! r [prefix* :all])
+          (recur (dec sublen)))))
+    (let [prefix (vec (concat same-prefix-vec (subvec pos-vec1* 0 1)))
+          prefix* (conj (vec (butlast prefix)) (inc (last prefix)))]
+      (if-let [pos-vec2*-first (pos-vec2 0 nil)]
+        (conj! r [])
+        )
+      )
+
+    (persistent! r)
+    ))
+
 
 ;;; outliner op + yjs op
 
@@ -218,10 +313,6 @@
 ;; (defn indent-outdent-nodes [nodes indent?]
 ;;   )
 
-
-;; (defn delete-nodes [start-node end-node block-ids]
-
-;;   )
 
 (defn- nodes-tree->struct&content [nodes-tree]
   (let [contents (atom {})
@@ -237,11 +328,7 @@
                     node)) nodes-tree)]
     [struct @contents]))
 
-(defn- goto-innermost-struct-array [pos struct]
-  (loop [i 0 s struct]
-    (if (> i (- (count pos) 2))
-      s
-      (recur (inc i) (.get s (get pos i))))))
+
 
 (defn- insert-nodes-aux [insert-structs pos struct]
   "insert INSERT-STRUCTS at POS"
@@ -251,58 +338,89 @@
             struct* (goto-innermost-struct-array pos struct)]
         (cond
           (vector? s)
-          (let [pos* (conj pos 0)]
+          (let [pos* (.add-next-level pos)]
             (.insert struct* (last pos) (clj->js [(y/Array.)]))
             (insert-nodes-aux s pos* struct)
-            (recur (inc i) (inc-pos pos)))
+            (recur (inc i) (.inc-pos pos)))
 
           :else
           (do
             (.insert struct* (last pos) (clj->js [s]))
-            (recur (inc i) (inc-pos pos))))))))
+            (recur (inc i) (.inc-pos pos))))))))
 
 
-(defn- next-sibling-pos [pos struct]
-  (let [inner-struct (goto-innermost-struct-array pos struct)
-        next-item (.get inner-struct (inc (last pos)))]
-    (if (instance? y/Array next-item)
-      (conj (vec (butlast pos)) (+ 2 (last pos)))
-      (conj (vec (butlast pos)) (+ 1 (last pos))))))
-
-(defn- next-non-sibling-pos! [pos struct]
-  "create a y/Array when no child follows item at POS"
-  (let [inner-struct (goto-innermost-struct-array pos struct)
-        next-item (.get inner-struct (inc (last pos)))]
-    (when-not (instance? y/Array next-item)
-      (.insert inner-struct (inc (last pos)) (clj->js [(y/Array.)])))
-    (inc-level-pos pos)))
-
-
-(defn insert-nodes [page-name new-nodes-tree target-uuid sibling?]
+(defn insert-nodes-yjs [page-name new-nodes-tree target-uuid sibling?]
   (let [[structs contents] (nodes-tree->struct&content new-nodes-tree)
         struct (structarray page-name)]
     (when-let [target-pos (find-pos (structarray page-name) (str target-uuid))]
       (let [pos (if sibling?
-                  (next-sibling-pos target-pos struct)
-                  (next-non-sibling-pos! target-pos struct))]
+                  (.next-sibling-pos target-pos struct)
+                  (.next-non-sibling-pos! target-pos struct))]
         (insert-nodes-aux structs pos (structarray page-name))
         (assoc-contents contents (contentmap))))))
 
+(defn insert-node-yjs [page-name new-node target-uuid sibling?]
+  (insert-nodes-yjs page-name [new-node] target-uuid sibling?))
+
+(defn- delete-range-nodes-prefix-part [prefix-vec start-pos-vec end-pos-vec struct]
+  (let [start-pos-vec-len (count start-pos-vec)]
+    ;; (when (> start-pos-vec-len 0))
+    (let [inner-struct (goto-innermost-struct-array (->Pos (vec (concat prefix-vec start-pos-vec))) struct)
+          start-index (last start-pos-vec)
+          len-to-remove (if (and (end-pos-vec 0 nil) (= start-pos-vec-len 1))
+                          (if (> (count end-pos-vec) 1)
+                            (- (end-pos-vec 0) start-index)
+                            (inc (- (end-pos-vec 0) start-index)))
+                          (- (.-length inner-struct) start-index))]
+      (.delete inner-struct start-index len-to-remove)
+      (if (>= start-pos-vec-len 2)
+        (delete-range-nodes-prefix-part [prefix-vec
+                                         (conj (subvec start-pos-vec 0 (- start-pos-vec-len 2))
+                                               (inc (start-pos-vec (- start-pos-vec-len 2) nil)))
+                                         end-pos-vec struct])
+        len-to-remove))))
+
+
+(defn- delete-range-nodes-suffix-part [prefix-vec end-pos-vec struct]
+  (let [end-pos-vec-len (count end-pos-vec)]
+    (when (> end-pos-vec-len 0)
+      (let [inner-struct (goto-innermost-struct-array (->Pos (vec (concat prefix-vec end-pos-vec))) struct)]
+        (if (<= (dec (.-length inner-struct)) (last end-pos-vec))
+          (delete-range-nodes-suffix-part prefix-vec (butlast end-pos-vec) struct)
+          (when (>= end-pos-vec-len 2)
+            (let [next-end-pos-vec (conj (subvec end-pos-vec 0 (- end-pos-vec-len 2))
+                                         (dec (end-pos-vec (- end-pos-vec-len 2))))]
+              (.delete inner-struct 0 (inc (last end-pos-vec)))
+              (delete-range-nodes-suffix-part prefix-vec next-end-pos-vec struct))))))))
+
+(defn delete-range-nodes [start-pos end-pos struct]
+  ;; {:pre [(<= (compare start-pos end-pos) 0)]}
+  (let [[same-prefix-vec pos-vec1* pos-vec2*] (common-prefix start-pos end-pos)]
+    (let [len-removed (delete-range-nodes-prefix-part same-prefix-vec pos-vec1* pos-vec2* struct)]
+      (if (>(count pos-vec2*) 0)
+        (let [pos-vec2*-after-delete-prefix-part (vec (cons (- (first pos-vec2*) len-removed) (rest pos-vec2*)))]
+          (delete-range-nodes-suffix-part same-prefix-vec pos-vec2*-after-delete-prefix-part struct))
+        (delete-range-nodes-suffix-part same-prefix-vec pos-vec2* struct)))))
+
+;; (defn delete-nodes-yjs [page-name start-uuid end-uuid block-ids]
+;;   (let [struct (structarray page-name)
+;;         start-pos (find-pos struct start-uuid)
+;;         end-pos (find-pos struct end-uuid)]
+
+;;     )
+;;   )
+
+
+
 (comment
-  (def page-blocks (db-model/get-page-blocks "page-name"))
-  (page-blocks->doc page-blocks "page-name")
-
-  (def test-struct (.getArray doc-local "page-name-struct"))
-  (.delete test-struct 0 (.-length test-struct))
-  (.insert test-struct 0 (clj->js ["1"]))
-  (.insert test-struct 1 (clj->js ["2"]))
-  (.insert test-struct 2 (clj->js [(y/Array.)]))
-  (def pos2 (find-pos test-struct "2"))
-  (assert (= [1] pos2))
-  (.insert (.get test-struct 2) 0 (clj->js ["3"]))
-  (assert (= [2 1] (next-sibling-pos [2 0] test-struct)))
-  (assert (= [2 1 0] (next-non-sibling-pos! [2 0] test-struct)))
-
-  ;; insert-nodes
-  (insert-nodes "page-name" (first xxxx) "61309f20-836c-4e47-894e-cc126050f039" true)
-  )
+ (def test-doc (y/Doc.))
+ (def test-struct (.getArray test-doc "test-struct"))
+ (.insert test-struct 0 (clj->js ["1"]))
+ (.insert test-struct 1 (clj->js ["2"]))
+ (.insert test-struct 2 (clj->js [(y/Array.)]))
+ (.insert (.get test-struct 2) 0 (clj->js ["3"]))
+ (.insert (.get test-struct 2) 1 (clj->js ["4"]))
+ (.insert (.get test-struct 2) 2 (clj->js [(y/Array.)]))
+ (.insert (.get (.get test-struct 2) 2) 0 (clj->js ["5"]))
+ (println (.toJSON test-struct))
+ )
