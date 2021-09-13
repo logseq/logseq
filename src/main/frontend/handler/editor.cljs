e978a8f972bfe7754f24dae0e7c7815fef47d2a9
Tienson Qin <tiensonqin@gmail.com>
2021-09-13 20:04:09 +0800

    fix: block parsing

--- src/main/frontend/handler/editor.cljs
+++ src/main/frontend/handler/editor.cljs
@@ -362,7 +362,9 @@
                              :else
                              (let [content' (str (config/get-block-pattern format) (if block-with-title? " " "\n") content)]
                                [content content']))
-        block (assoc block :block/content content')
+        block (assoc block
+                     :block/content content'
+                     :block/format format)
         block (apply dissoc block (remove #{:block/pre-block?} db-schema/retract-attributes))
         block (block/parse-block block)
         block (if (and first-block? (:block/pre-block? block))
