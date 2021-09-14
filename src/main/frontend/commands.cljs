593c48cd516e1f44ddd57d23906474ecd5fe48e9
Tienson Qin <tiensonqin@gmail.com>
2021-09-14 12:09:14 +0800

    fix: auto focus after typing /link

--- src/main/frontend/commands.cljs
+++ src/main/frontend/commands.cljs
@@ -31,7 +31,8 @@
 (def link-steps [[:editor/input (str slash "link")]
                  [:editor/show-input [{:command :link
                                        :id :link
-                                       :placeholder "Link"}
+                                       :placeholder "Link"
+                                       :autoFocus true}
                                       {:command :link
                                        :id :label
                                        :placeholder "Label"}]]])
