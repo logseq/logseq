39c35c71cb56711d3ee336c38b631582ba4427c2
clark-cui <cuirongchuan@163.com>
2021-09-22 20:11:48 +0800

    jscode enhance
    jscode enhance

--- src/main/frontend/exif.js
+++ src/main/frontend/exif.js
@@ -1,41 +1,41 @@
 // copied from https://stackoverflow.com/questions/7584794/accessing-jpeg-exif-rotation-data-in-javascript-on-the-client-side
 
-function objectURLToBlob(url, callback) {
-  var http = new XMLHttpRequest();
+const objectURLToBlob=(url, callback)=> {
+  const http = new XMLHttpRequest();
   http.open("GET", url, true);
   http.responseType = "blob";
-  http.onload = function(e) {
-    if (this.status == 200 || this.status === 0) {
-      callback(this.response);
+  http.onload = (e)=> {
+    if (http.status == 200 || http.status === 0) {
+      callback(http.response);
     }
   };
   http.send();
 }
 
-export var getEXIFOrientation = function (img, callback) {
-  var reader = new FileReader();
+export const getEXIFOrientation =  (img, callback)=> {
+  const reader = new FileReader();
   reader.onload = e => {
-    var view = new DataView(e.target.result)
+    const view = new DataView(e.target.result)
 
     if (view.getUint16(0, false) !== 0xFFD8) {
       return callback(-2)
     }
-    var length = view.byteLength
-    var offset = 2
+    const length = view.byteLength
+    let offset = 2
     while (offset < length) {
-      var marker = view.getUint16(offset, false)
+      const marker = view.getUint16(offset, false)
       offset += 2
       if (marker === 0xFFE1) {
         if (view.getUint32(offset += 2, false) !== 0x45786966) {
           return callback(-1)
         }
-        var little = view.getUint16(offset += 6, false) === 0x4949
+        const little = view.getUint16(offset += 6, false) === 0x4949
         offset += view.getUint32(offset + 4, little)
-        var tags = view.getUint16(offset, little)
+        const tags = view.getUint16(offset, little)
         offset += 2
-        for (var i = 0; i < tags; i++) {
+        for (let i = 0; i < tags; i++) {
           if (view.getUint16(offset + (i * 12), little) === 0x0112) {
-            var o = view.getUint16(offset + (i * 12) + 8, little);
+            const o = view.getUint16(offset + (i * 12) + 8, little);
             return callback(o)
           }
         }
@@ -48,7 +48,7 @@ export var getEXIFOrientation = function (img, callback) {
     return callback(-1)
   };
 
-  objectURLToBlob(img.src, function (blob) {
+  objectURLToBlob(img.src,  (blob)=> {
     reader.readAsArrayBuffer(blob.slice(0, 65536));
   });
 }
