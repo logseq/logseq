973040c8e3ab8cd09aac7bdac57149919db76e9a
Tienson Qin <tiensonqin@gmail.com>
2021-10-08 18:24:25 +0800

    fix: clean first empty line before the block content of src/quote

--- src/main/frontend/commands.cljs
+++ src/main/frontend/commands.cljs
@@ -493,7 +493,8 @@
           pos (if last-pattern
                 (string/last-index-of content last-pattern pos)
                 pos)
-          beginning-of-line? (cursor/beginning-of-line? input)
+          beginning-of-line? (or (cursor/beginning-of-line? input)
+                                 (= 1 (:pos @*angle-bracket-caret-pos)))
           value (if (and (contains? #{"block" "properties"} type)
                          (not beginning-of-line?))
                   (str "\n" value)
