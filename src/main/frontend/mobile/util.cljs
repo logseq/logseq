bf06dc3bf50341254b4fb67304e9ba870ded048e
leizhe <leizhe@leizhedeMacBook-Air.local>
2021-11-23 13:47:05 +0800

    add function for getting idevice info

--- src/main/frontend/mobile/util.cljs
+++ src/main/frontend/mobile/util.cljs
@@ -29,34 +29,58 @@
 
 (defonce idevice-info
   (atom
-   {:ipad-pro-12.9  {:width 1024 :height 1194 :statusbar 40}
-    :ipad-pro-11    {:width 834  :height 1194 :statusbar 40}
+   {:iPadPro-12.9  {:width 1024 :height 1194 :statusbar 40}
+    :iPadPro-11    {:width 834  :height 1194 :statusbar 40}
+    :iPadAir-10.5   {:width 834  :height 1112 :statusbar 40}
+    :iPad-10.2	    {:width 810  :height 1080 :statusbar 40}
     :iPadPro-9.7	{:width 768  :height 1024 :statusbar 40}
     :iPadmini-9.7   {:width 768  :height 1024 :statusbar 40}
-    :iPadAir-10.5   {:width 834  :height 1112 :statusbar 40}
     :iPadAir-9.7	{:width 768  :height 1024 :statusbar 40}
-    :iPad-10.2	    {:width 810  :height 1080 :statusbar 40}
     :iPad-9.7	    {:width 768  :height 1024 :statusbar 40}
     :iPhone12ProMax {:width 428  :height 926  :statusbar 47}
-    :iPhone12Pro	{:width 390  :height 844  :statusbar 47}
-    :iPhone12	    {:width 390  :height 844  :statusbar 47}
-    :iPhone12mini   {:width 375  :height 812  :statusbar 44}
     :iPhone11ProMax {:width 414  :height 896  :statusbar 44}
-    :iPhone11Pro	{:width 375  :height 812  :statusbar 44}
     :iPhone11	    {:width 414  :height 896  :statusbar 48}
     :iPhoneXSMax	{:width 414  :height 896  :statusbar 48}
-    :iPhoneXS	    {:width 375  :height 812  :statusbar 48}
     :iPhoneXR	    {:width 414  :height 896  :statusbar 48}
-    :iPhoneX	    {:width 375  :height 812  :statusbar 44}
     :iPhone8Plus	{:width 414  :height 736  :statusbar 20}
-    :iPhone8	    {:width 375  :height 667  :statusbar 20}
     :iPhone7Plus	{:width 414  :height 736  :statusbar 20}
-    :iPhone7	    {:width 375  :height 667  :statusbar 20}
     :iPhone6sPlus   {:width 414  :height 736  :statusbar 20}
-    :iPhone6s	    {:width 375  :height 667  :statusbar 20}
     :iPhone6Plus	{:width 414  :height 736  :statusbar 20}
+    :iPhone12	    {:width 390  :height 844  :statusbar 47}
+    :iPhone12Pro	{:width 390  :height 844  :statusbar 47}
+    :iPhoneX	    {:width 375  :height 812  :statusbar 44}
+    :iPhone12mini   {:width 375  :height 812  :statusbar 44}
+    :iPhone11Pro	{:width 375  :height 812  :statusbar 44}
+    :iPhoneXS	    {:width 375  :height 812  :statusbar 48}
+    :iPhone8	    {:width 375  :height 667  :statusbar 20}
+    :iPhone7	    {:width 375  :height 667  :statusbar 20}
+    :iPhone6s	    {:width 375  :height 667  :statusbar 20}
     :iPhone6	    {:width 375  :height 667  :statusbar 20}
     :iPhoneSE4.7	{:width 375  :height 667  :statusbar 20}
     :iPhoneSE4	    {:width 320  :height 568  :statusbar 20}
-    :iPodtouch5th   {:width 320  :height 568  :statusbar 20}}))
+    :iPodtouch5     {:width 320  :height 568  :statusbar 20}}))
 
+(defn get-idevice-model
+  []
+  (let [width (.-innerWidth js/window)
+        height (.-innerHeight js/window)]
+    (case [width height]
+      [320 568] "iPhoneSE4"
+      [375 667] "iPhone8"
+      [375 812] "iPhoneX"
+      [390 844] "iPhone12"
+      [414 736] "iPhone8Plus"
+      [414 896] "iPhone11"
+      [428 926] "iPhone12ProMax"
+      [768 1024] "iPad-9.7"
+      [810 1080] "iPad-10.2"
+      [834 1112] "iPadAir-10.5"
+      [834 1194] "iPadPro-11"
+      [1024 1194] "iPadPro-12.9"
+      "Not a known Apple device!")))
+
+(defn get-idevice-statusbar-height
+  []
+  (let [model (get-idevice-model)
+        model (keyword model)]
+    (:statusbar (model @idevice-info))))
