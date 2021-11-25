9cad54a2e05857f2baea2648b075e3ab86a68ad7
Andelf <andelf@gmail.com>
2021-11-25 21:38:31 +0800

    ci: refactor desktop app release ci, add nightly build

---
+++ scripts/get-pkg-version.js
@@ -0,0 +1,17 @@
+// This script file outputs the version in the package.json of project dir,
+// and optionally add nightly postfix.
+//
+// It is used as a helper by the continuous integration.
+const path = require('path')
+const process = require('process')
+
+const ver = require(path.join(__dirname, '../package.json')).version
+
+if (process.argv[2] === 'nightly') {
+  const today = new Date()
+  console.log(
+    ver + '+nightly.' + today.toISOString().split('T')[0].replaceAll('-', '')
+  )
+} else {
+  console.log(ver)
+}
