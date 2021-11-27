3e7091645f99fd9957f656e74ac831d401abddf0
Andelf <andelf@gmail.com>
2021-11-27 17:18:44 +0800

    ci: test

--- scripts/get-pkg-version.js
+++ scripts/get-pkg-version.js
@@ -2,8 +2,21 @@
 // It is used as a helper by the continuous integration
 const path = require('path')
 const process = require('process')
+const fs = require('fs')
 
-const ver = require(path.join(__dirname, '../package.json')).version
+const content = fs.readFileSync(
+  path.join(__dirname, '../src/main/frontend/version.cljs')
+)
+const pattern = /\(defonce version "(.*?)"\)/g
+
+const match = pattern.exec(content)
+let ver = '0.0.1'
+if (match) {
+  ver = match[1]
+} else {
+  console.error('Could not find version in version.cljs')
+  process.exit(1)
+}
 
 if (process.argv[2] === 'nightly') {
   const today = new Date()
