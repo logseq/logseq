fe419503c00adf78848b8dcdda64bc3b1a56616f
Junyi Du <junyidu.cn@gmail.com>
2021-12-20 16:21:07 +0800

    test(e2e): add page alias tests

--- e2e-tests/utils.ts
+++ e2e-tests/utils.ts
@@ -35,19 +35,36 @@ export async function createRandomPage(page: Page) {
     await page.waitForSelector(':nth-match(textarea, 1)', { state: 'visible' })
 }
 
-export async function lastBlock(page: Page): Promise<Locator> {
+/**
+* Locate the last block in the editor
+* @param page The Playwright Page object.
+* @param inner_only If true, only return the .page-inner-block which has no 
+extra blocks like linked references included. Defaults to false.
+* @returns The locator of the last block.
+*/
+export async function lastBlock(page: Page, inner_only: Boolean = false): Promise<Locator> {
     // discard any popups
     await page.keyboard.press('Escape')
     // click last block
-    await page.click('.ls-block >> nth=-1')
+    if (inner_only) 
+        await page.click('.page-blocks-inner .ls-block >> nth=-1')
+    else
+        await page.click('.ls-block >> nth=-1')
     // wait for textarea
     await page.waitForSelector(':nth-match(textarea, 1)', { state: 'visible' })
 
     return page.locator(':nth-match(textarea, 1)')
 }
 
-export async function newBlock(page: Page): Promise<Locator> {
-    await lastBlock(page)
+/**
+* Create and locate a new block at the end of the editor
+* @param page The Playwright Page object 
+* @param inner_only If true, only consider the .page-inner-block that no extra 
+blocks like linked references considered. Defaults to false.
+* @returns The locator of the last block
+*/
+export async function newBlock(page: Page, inner_only: Boolean = false): Promise<Locator> {
+    await lastBlock(page, inner_only)
     await page.press(':nth-match(textarea, 1)', 'Enter')
 
     return page.locator(':nth-match(textarea, 1)')
