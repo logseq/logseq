5d5811fb3809204a276e27858de2f67e4ec92ba8
shidenggui <longlyshidenggui@gmail.com>
2021-12-25 09:39:13 +0800

    fix: fix sometimes the top block doesn't expand when clicking the bullet of the block

--- src/main/frontend/components/block.cljs
+++ src/main/frontend/components/block.cljs
@@ -1336,20 +1336,15 @@
 
 (defn- bullet-on-click
   [e block uuid]
-  (let [block (if (-> block :block/properties :collapsed)
-                (do (editor-handler/expand-block! uuid)
-                    (update-in block [:block/properties] dissoc :collapsed))
-               block)]
-    (if (gobj/get e "shiftKey")
-      (do
-        (state/sidebar-add-block!
-          (state/get-current-repo)
-          (:db/id block)
-          :block
-          block)
-        (util/stop e))
-      (route-handler/redirect-to-page! uuid))
-    ))
+  (if (gobj/get e "shiftKey")
+    (do
+      (state/sidebar-add-block!
+       (state/get-current-repo)
+       (:db/id block)
+       :block
+       block)
+      (util/stop e))
+    (route-handler/redirect-to-page! uuid)))
 
 (defn- block-left-border-on-click
   [e children]
