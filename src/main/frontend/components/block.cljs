e416a23e1e0e9418fb991c2e7fc0d50277c97431
Peng Xiao <pengxiao@outlook.com>
2022-02-23 16:45:28 +0800

    fix: block ref id in block-reference

--- src/main/frontend/components/block.cljs
+++ src/main/frontend/components/block.cljs
@@ -879,7 +879,11 @@
               link-depth (or link-depth 0)]
           (if (> link-depth max-depth-of-links)
             [:p.warning.text-sm "Block ref nesting is too deep"]
-            (block-reference (assoc config :reference? true :link-depth (inc link-depth)) id label*)))
+            (block-reference (assoc config
+                                    :reference? true
+                                    :link-depth (inc link-depth) 
+                                    :block/uuid id)
+                             id label*)))
 
         ["Page_ref" page]
         (let [format (get-in config [:block :block/format])]
