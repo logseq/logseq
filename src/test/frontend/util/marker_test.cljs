31d6552481369d7937ee51d725db3dfb6c8f8730
llcc <lzhes43@gmail.com>
2022-03-02 10:43:46 +0800

    add test for set-marker

--- src/test/frontend/util/marker_test.cljs
+++ src/test/frontend/util/marker_test.cljs
@@ -1,6 +1,7 @@
 (ns frontend.util.marker-test
   (:require [cljs.test :refer [are deftest]]
-            [frontend.util.marker :as marker]))
+            [frontend.util.marker :as marker]
+            [clojure.string :as string]))
 
 (deftest add-or-update-marker-markdown
   (are [content marker expect] (= expect (marker/add-or-update-marker content :markdown marker))
@@ -22,5 +23,34 @@
     "todo" "TODO" "TODO todo"
     "TODO xxx" "DONE" "DONE xxx"
     "TODO" "DONE" "DONE "))
+
+(defn set-marker
+  [marker content format new-marker]
+  (let [old-header-marker (when (not= format :org)
+                            (re-find (marker/header-marker-pattern true marker) content))
+        new-header-marker (when old-header-marker
+                            (string/replace old-header-marker marker new-marker))
+        marker (or old-header-marker marker)
+        new-marker (or new-header-marker new-marker)
+        new-content (->
+                     (if marker
+                       (string/replace-first content (re-pattern (str "^" marker)) new-marker)
+                       (str new-marker " " content))
+                     (string/triml))]
+    new-content))
+
+(deftest set-marker-org
+  (are [marker content new-marker expect] (= expect (set-marker marker content :org new-marker))
+    nil "content" "TODO" "TODO content"
+    "TODO" "TODO content" "DOING" "DOING content"
+    "TODO" "## TODO content" "DOING" "## TODO content"
+    "DONE" "DONE content" nil "content"))
+
+(deftest set-marker-markdown
+  (are [marker content new-marker expect] (= expect (set-marker marker content :markdown new-marker))
+    nil "content" "TODO" "TODO content"
+    "TODO" "TODO content" "DOING" "DOING content"
+    "TODO" "## TODO content" "DOING" "## DOING content"
+    "DONE" "DONE content" nil "content"))
 
 #_(cljs.test/run-tests)
