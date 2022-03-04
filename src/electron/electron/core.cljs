a15df743c4ddc767438fbf3bb47adce40585b455
rcmerci <rcmerci@gmail.com>
2022-03-05 01:41:17 +0800

    fix(sync): rebase

--- src/electron/electron/core.cljs
+++ src/electron/electron/core.cljs
@@ -16,9 +16,7 @@
             [electron.git :as git]
             [electron.window :as win]
             [electron.exceptions :as exceptions]
-            ["/electron/utils" :as utils]
-            ["electron-context-menu" :as init-context-menu]
-            [goog.object :as gobj]))
+            ["/electron/utils" :as utils]))
 
 (defonce LSP_SCHEME "logseq")
 (defonce FILE_LSP_SCHEME "lsp")
