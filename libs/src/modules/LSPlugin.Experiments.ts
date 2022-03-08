6c329f529e6c4ba621035b1cfd28c6c8db688761
charlie <xyhp915@qq.com>
2022-03-08 10:54:18 +0800

    improve(plugin): types of expirements api

--- libs/src/modules/LSPlugin.Experiments.ts
+++ libs/src/modules/LSPlugin.Experiments.ts
@@ -11,23 +11,21 @@ export class LSPluginExperiments {
   ) {}
 
   get React (): unknown {
-    // @ts-ignore
-    return top.React
+    return this.ensureHostScope().React
   }
 
   get ReactDOM (): unknown {
-    // @ts-ignore
-    return top.ReactDOM
+    return this.ensureHostScope().ReactDOM
   }
 
   get pluginLocal (): PluginLocal {
-    return top.LSPluginCore.ensurePlugin(this.ctx.baseInfo.id)
+    return this.ensureHostScope().LSPluginCore.ensurePlugin(this.ctx.baseInfo.id)
   }
 
   private invokeExperMethod (type: string, ...args: Array<any>) {
+    const host = this.ensureHostScope()
     type = snakeCase(type)?.toLowerCase()
-    // @ts-ignore
-    return top.logseq.api['exper_' + type]?.apply(top, args)
+    return host.logseq.api['exper_' + type]?.apply(host, args)
   }
 
   async loadScripts (...scripts: Array<string>) {
@@ -51,9 +49,18 @@ export class LSPluginExperiments {
       subs?: Array<string>,
       render: (props: { content: string }) => any,
     }) {
-    // @ts-ignore
-    return top.logseq.api.exper_register_fenced_code_renderer(
+    return this.ensureHostScope().logseq.api.exper_register_fenced_code_renderer(
       this.ctx.baseInfo.id, type, opts
     )
   }
+
+  ensureHostScope (): any {
+    if (window === top) {
+      throw new Error('Can not access host scope!')
+    }
+
+    return top
+  }
 }
+
+
