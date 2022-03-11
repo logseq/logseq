1db0b41a6c2b5f490d37493f48e4c1a838225004
charlie <xyhp915@qq.com>
2022-03-11 15:01:50 +0800

    feat(ui): WIP setups of onboarding

--- src/main/frontend/components/onboarding/setups.cljs
+++ src/main/frontend/components/onboarding/setups.cljs
@@ -2,16 +2,33 @@
   (:require [frontend.state :as state]
             [rum.core :as rum]
             [frontend.ui :as ui]
+            [frontend.components.svg :as svg]
             [clojure.string :as string]))
 
-(rum/defc main
-  []
+(rum/defc setups-container
+  [flag content]
 
   [:div.cp__onboarding-setups.flex.items-center.justify-center
-   [:div.inner-card.flex.flex-col.items-center
-    [:h1.text-xl [:strong (ui/icon "heart")] "Welcome to " [:strong "Logseq!"]]
-    [:h2 "First you need to choose a folder where logseq will store your thoughts, ideas, notes."]
+   (let [picker? (= flag :picker)]
+     [:div.inner-card.flex.flex-col.items-center
 
+      [:h1.text-xl
+       (if picker?
+         [:span [:strong (ui/icon "heart")] "Welcome to " [:strong "Logseq!"]]
+         [:span [:strong (ui/icon "file-import")] "Import existing notes"])]
+
+      [:h2
+       (if picker?
+         "First you need to choose a folder where logseq will store your thoughts, ideas, notes."
+         "You can also do this later in the app.")]
+
+      content])])
+
+(rum/defc picker
+  []
+
+  (setups-container
+    :picker
     [:article.flex
      [:section.a
       [:strong "Let’s get you set up."]
@@ -19,7 +36,7 @@
       [:div.choose.flex.flex-col.items-center
        [:i]
        [:div.control
-        [:a.open-link.flex.items-center.justify-center.flex-col
+        [:label.action-input.flex.items-center.justify-center.flex-col
          [:strong "Choose a folder"]
          [:small "Open existing directory or Create a new one"]]]]]
      [:section.b.flex.items-center.flex-col
@@ -45,10 +62,31 @@
          (if-not title
            [:li.hr]
            [:li
-            {:key   title}
+            {:key title}
             [:i.as-flex-center
              {:class (when (string/ends-with? label ".edn") "is-file")}
              (when icon (ui/icon icon))]
             [:span
              [:strong.uppercase title]
-             [:small.opacity-50 label]]]))]]]]])
+             [:small.opacity-50 label]]]))]]]))
+
+(rum/defc importer
+  []
+  (setups-container
+    :importer
+    [:article.flex.flex-col.items-center.importer
+     [:section.c.text-center
+      [:h1 "Do you already have notes that you want to import?"]
+      [:h2 "If they are in a JSON or Markdown format logseq can work with them."]]
+     [:section.d.flex
+      [:label.action-input.flex.items-center
+       [:span.as-flex-center [:i (svg/roam-research 28)]]
+       [:span.flex.flex-col
+        [:strong "RoamResearch"]
+        [:small "Import a JSON Export of your Roam graph"]]]
+
+      [:label.action-input.flex.items-center
+       [:span.as-flex-center (ui/icon "sitemap" {:style {:fontSize "26px"}})]
+       [:span.flex.flex-col
+        [:strong "OPML"]
+        [:small " Import OPML files"]]]]]))
