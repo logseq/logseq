8454ec1ee520487f1c74126ad7cc19ac952b73fa
Tienson Qin <tiensonqin@gmail.com>
2022-03-18 13:48:27 +0800

    fix: sentry
    close #4613

--- src/main/frontend/modules/instrumentation/sentry.cljs
+++ src/main/frontend/modules/instrumentation/sentry.cljs
@@ -2,19 +2,20 @@
   (:require [frontend.version :refer [version]]
             [frontend.util :as util]
             [frontend.config :as cfg]
-            ["@sentry/browser" :as Sentry]
-            ["@sentry/electron" :as Sentry-electron]
+            ["@sentry/react" :as Sentry]
+            ["@sentry/tracing" :refer [BrowserTracing]]
             ["posthog-js" :as posthog]))
 
 (def config
   {:dsn "https://636e9174ffa148c98d2b9d3369661683@o416451.ingest.sentry.io/5311485"
    :release (util/format "logseq@%s" version)
    :environment (if cfg/dev? "development" "production")
-   :integrations [(new posthog/SentryIntegration posthog "logseq" 5311485)]
+   :platform (if (util/electron?) "electron" "web")
+   :integrations [(new posthog/SentryIntegration posthog "logseq" 5311485)
+                  (new BrowserTracing)]
    :debug cfg/dev?
    :tracesSampleRate 1.0})
 
 (defn init []
-  (let [config (clj->js config)
-        init-fn (if (util/electron?) Sentry-electron/init Sentry/init)]
-    (init-fn config)))
+  (let [config (clj->js config)]
+    (Sentry/init config)))
