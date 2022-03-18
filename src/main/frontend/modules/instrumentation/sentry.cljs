9669e43722c5fa9c74a92eff63d8884df8bcc298
llcc <lzhes43@gmail.com>
2022-03-18 18:00:45 +0800

    enhance: separate desktop and mobile in Sentry for better issue management (#4621)
    Co-authored-by: Andelf <andelf@gmail.com>

--- src/main/frontend/modules/instrumentation/sentry.cljs
+++ src/main/frontend/modules/instrumentation/sentry.cljs
@@ -4,11 +4,16 @@
             [frontend.config :as cfg]
             ["@sentry/react" :as Sentry]
             ["@sentry/tracing" :refer [BrowserTracing]]
-            ["posthog-js" :as posthog]))
+            ["posthog-js" :as posthog]
+            [frontend.mobile.util :as mobile-util]))
 
 (def config
   {:dsn "https://636e9174ffa148c98d2b9d3369661683@o416451.ingest.sentry.io/5311485"
-   :release (util/format "logseq@%s" version)
+   :release (util/format "logseq%s@%s" (cond
+                                         (mobile-util/native-android?) "-android"
+                                         (mobile-util/native-ios?) "-ios"
+                                         :else "")
+                         version)
    :environment (if cfg/dev? "development" "production")
    :platform (if (util/electron?) "electron" "web")
    :integrations [(new posthog/SentryIntegration posthog "logseq" 5311485)
