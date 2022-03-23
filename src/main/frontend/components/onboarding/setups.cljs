e765aae08154b53c63ddfbc8663f5c2def6089b9
llcc <lzhes43@gmail.com>
2022-03-23 15:28:05 +0800

    fix typo

--- src/main/frontend/components/onboarding/setups.cljs
+++ src/main/frontend/components/onboarding/setups.cljs
@@ -47,7 +47,7 @@
 
      (mobile-util/native-android?)
      [:div
-      "You can save them in your local storage, and use any third-party sync service to keep your notes sync with other devices."
+      "You can save them in your local storage, and use any third-party sync service to keep your notes sync with other devices. "
       "If you prefer to use Dropbox to sync your notes, you can use "
       [:a {:href "https://play.google.com/store/apps/details?id=com.ttxapps.dropsync"
            :target "_blank"}
