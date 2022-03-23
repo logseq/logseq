3a137f05806c124b8ff78d0b502c3a7eb6c8fbd5
charlie <xyhp915@qq.com>
2022-03-23 14:16:21 +0800

    chore(plugin): missing files & bump libs version

---
+++ libs/src/callable.apis.ts
@@ -0,0 +1,10 @@
+import { PluginLocal } from './LSPlugin.core'
+
+export function setSDKMetadata (
+  this: PluginLocal,
+  data: any
+) {
+  if (this?.sdk && data) {
+    this.sdk = Object.assign({}, this.sdk, data)
+  }
+}
