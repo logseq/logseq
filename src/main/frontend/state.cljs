019160f32a33efefcf59bcd8683206a750175c43
Tienson Qin <tiensonqin@gmail.com>
2022-03-31 20:34:12 +0800

    clear last saved cursor when pressing esc

--- src/main/frontend/state.cljs
+++ src/main/frontend/state.cljs
@@ -873,7 +873,8 @@
   []
   (swap! state merge {:editor/editing? nil
                       :editor/block    nil
-                      :cursor-range    nil}))
+                      :cursor-range    nil
+                      :editor/last-saved-cursor nil}))
 
 (defn into-code-editor-mode!
   []
