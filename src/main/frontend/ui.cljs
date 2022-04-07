8e8670854c659dc6ae959b128425d7c433bf9efa
Gabriel Horner <gabriel@logseq.com>
2022-04-07 17:04:20 -0400

    Users should use our bug reports
    Also added label so we can track where issues are coming from

--- src/main/frontend/ui.cljs
+++ src/main/frontend/ui.cljs
@@ -729,7 +729,7 @@
    [:div.flex.justify-between.items-center.px-1
     [:h5.text-red-600.pb-1 title]
     [:a.text-xs.opacity-50.hover:opacity-80
-     {:href "https://github.com/logseq/logseq/issues"
+     {:href "https://github.com/logseq/logseq/issues/new?labels=from:in-app&template=bug_report.yaml"
       :target "_blank"} "report issue"]]
    (when content [:pre.m-0.text-sm content])])
 
