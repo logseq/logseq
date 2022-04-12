8fb5796f81b2e358ded1a486005a447e13e92dbc
Tienson Qin <tiensonqin@gmail.com>
2022-04-13 07:28:53 +0800

    Remove unused code

--- src/main/frontend/handler/dnd.cljs
+++ src/main/frontend/handler/dnd.cljs
@@ -1,32 +1,12 @@
 (ns frontend.handler.dnd
-  (:require [frontend.db :as db]
-            [frontend.handler.editor :as editor-handler]
+  (:require [frontend.handler.editor :as editor-handler]
             [frontend.modules.outliner.core :as outliner-core]
             [frontend.modules.outliner.tree :as tree]
             [frontend.modules.outliner.transaction :as outliner-tx]
             [frontend.state :as state]
             [frontend.util :as util]))
 
-(defn- ancestor?
-  "Whether current-block is an ancestor of the target-block."
-  [current-block-uuid target-block]
-  (loop [loc target-block]
-    (if-let [parent (db/entity (:db/id (:block/parent loc)))]
-      (if (= (:block/uuid parent) current-block-uuid)
-        true
-        (recur parent))
-      false)))
-
 (defn move-blocks
-  "There can be two possible situations:
-  1. Move blocks in the same file (either top-to-bottom or bottom-to-top).
-  2. Move blocks between two different files.
-
-  move-to: :sibling :nested :top nil
-
-  Notes:
-  Sometimes we might need to move a parent block to it's own child.
-  "
   [^js event blocks target-block move-to]
   (let [blocks' (map #(dissoc % :block/level :block/children) blocks)
         first-block (first blocks')
