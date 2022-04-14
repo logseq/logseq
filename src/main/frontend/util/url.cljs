e1e7070ebbe6878a2196baa6edc733e49adb9c48
Junyi Du <junyidu.cn@gmail.com>
2022-04-15 01:36:17 +0800

    ux: page dropdown button for copy page URL

--- src/main/frontend/util/url.cljs
+++ src/main/frontend/util/url.cljs
@@ -8,28 +8,57 @@
 (def encode js/encodeURI)
 (def encode-param js/encodeURIComponent)
 
-(defn get-repo-identifier
-  "repo-path: output of `get-repo-name`"
+(defn get-local-repo-identifier
   [repo]
   (let [repo-path (db-conn/get-repo-name repo)]
     (db-conn/get-short-repo-name repo-path)))
 
-(defn get-logseq-repo-url
+(defn get-repoid-url
   "Get Logseq protocol URL, w/o param (v0.1).
    host: set to `nil` for local graph
-   protocol?: if true, returns with protocol prefix"
+   protocol?: if true, returns URL with protocol prefix"
   ([host action repo-identifier]
-   (get-logseq-repo-url host action repo-identifier true))
+   (get-repoid-url host action repo-identifier true))
   ([host action repo-identifier protocol?]
    (str (when protocol? (str LSP_SCHEME "://")) 
         (when host (str host "/")) 
         action "/" 
-        (encode repo-identifier)))
-)
+        (encode repo-identifier))))
 
-(defn get-local-logseq-entity-url-by-uuid
-  "The URL represents an entity in graph.
-   Ensure repo-identifier and uuid are valid string before hand.
-   Only the name of repo is required (not full path)"
-  [repo uuid]
-  (str (get-logseq-repo-url nil "graph" (get-repo-identifier repo)) "?block-id=" uuid))
+(defn get-logseq-graph-url
+  "The URL represents an graph, for example:
+   logseq://graph/abc
+   Ensure repo is valid before hand.
+   host: set to `nil` for local graph
+   protocol?: if true, returns URL with protocol prefix"
+  ([host repo]
+   (get-logseq-graph-url host repo true))
+  ([host repo protocol?]
+   (let [repo-identifier (if host
+                           repo ;; resolve remote repo identifier here
+                           (get-local-repo-identifier repo))]
+     (get-repoid-url host "graph" repo-identifier protocol?))))
+
+(defn get-logseq-graph-uuid-url
+  "The URL represents an entity in graph with uuid, for example:
+   logseq://graph/abc?block-id=<uuid>
+   Ensure repo and uuid are valid before hand.
+   host: set to `nil` for local graph
+   protocol?: if true, returns URL with protocol prefix"
+  ([host repo uuid]
+   (get-logseq-graph-uuid-url host repo uuid true))
+  ([host repo uuid protocol?]
+   (str (get-logseq-graph-url host repo protocol?)
+        "?block-id=" uuid)))
+
+(defn get-logseq-graph-page-url
+  "The URL represents an page in graph with pagename, for example:
+   logseq://graph/abc?page=<page-name>
+   Ensure repo and page-name are valid before hand.
+   host: set to `nil` for local graph
+   protocol?: if true, returns URL with protocol prefix"
+  ([host repo page-name]
+   (get-logseq-graph-page-url host repo page-name true))
+  ([host repo page-name protocol?]
+   (str (get-logseq-graph-url host repo protocol?)
+        "?page=" (encode-param page-name))))
