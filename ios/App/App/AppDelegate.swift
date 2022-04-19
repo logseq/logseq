83465a6446f8b218ea8ad61e16fc5649bdf852e7
llcc <lzhes43@gmail.com>
2022-04-19 18:01:42 +0800

    enhance(iOS): receive share content after cold launch

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -10,6 +10,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        DispatchQueue.global().asyncAfter(deadline: .now() + 3) {
+            NotificationCenter.default
+                .post(name: Notification.Name("triggerSendIntent"), object: nil )
+        }
         return true
     }
     
@@ -65,8 +69,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
             
             store.processed = false
-            let nc = NotificationCenter.default
-            nc.post(name: Notification.Name("triggerSendIntent"), object: nil )
+            
+            NotificationCenter.default.post(name: Notification.Name("triggerSendIntent"), object: nil )
             
             return success
         }
