d00030b04a7179d4810b9287049b50d45a1edc15
llcc <lzhes43@gmail.com>
2022-04-26 16:56:55 +0800

    feat(gesture): more commands on action-bar

--- src/main/frontend/mobile/action_sheet.cljs
+++ src/main/frontend/mobile/action_sheet.cljs
@@ -1,24 +1,45 @@
 (ns frontend.mobile.action-sheet 
   (:require
+   [frontend.extensions.srs :as srs]
    [frontend.handler.editor :as editor-handler]
    [frontend.state :as state]
    [frontend.ui :as ui]
    [rum.core :as rum]))
 
-(rum/defc action-bar < rum/reactive
-  []
+(defonce font-size 23)
+
+(rum/defcs action-bar < rum/reactive
   (when-let [block (state/sub :mobile/actioned-block)]
-    (prn :action-bar :block block)
-    [:div.fixed.action-bar
-     [:button.bottom-action.flex-row
-      {:on-click (fn [_event]
-                   (editor-handler/delete-block-aux! block true)
-                   (state/set-state! :mobile/show-action-bar? false))}
-      (ui/icon "trash" {:style {:fontSize 25}})
-      [:div "Delete"]]
-     [:button.bottom-action.flex-row
-      (ui/icon "calendar-plus" {:style {:fontSize 25}})
-      [:div "Schedule"]]
-     [:button.bottom-action.flex-row
-      (ui/icon "calendar-minus" {:style {:fontSize 25}})
-      [:div "Deadline"]]]))
+    (let [block-id   (:block/uuid block)
+          properties (:block/properties block)
+          heading?   (true? (:heading properties))]
+     [:div.fixed.action-bar
+      [:button.bottom-action.flex-row
+       {:on-click (fn [_event]
+                    (editor-handler/delete-block-aux! block true)
+                    (state/set-state! :mobile/show-action-bar? false))}
+       (ui/icon "trash" {:style {:fontSize font-size}})
+       [:div.description "Delete"]]
+      
+      [:button.bottom-action.flex-row
+       {:on-click (fn [_event]
+                    (editor-handler/copy-selection-blocks)
+                    (state/set-state! :mobile/show-action-bar? false))}
+       (ui/icon "copy" {:style {:fontSize font-size}})
+       [:div.description "Copy"]]
+      
+      [:button.bottom-action.flex-row
+       {:on-click (fn [_event]
+                    (srs/make-block-a-card! (:block/uuid block))
+                    (state/set-state! :mobile/show-action-bar? false))}
+       (ui/icon "infinity" {:style {:fontSize font-size}})
+       [:div.description "Card"]]
+      
+      [:button.bottom-action.flex-row
+       {:on-click (fn [_event]
+                    (if heading?
+                      (editor-handler/remove-block-property! block-id :heading)
+                      (editor-handler/set-block-property! block-id :heading true))
+                    (state/set-state! :mobile/show-action-bar? false))}
+       (ui/icon "replace" {:style {:fontSize font-size}})
+       [:div.description "Heading"]]])))
