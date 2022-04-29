d728481d7ab15121cb832ed227022d130b756a05
charlie <xyhp915@qq.com>
2022-04-29 14:43:13 +0800

    improve(plugin): add api of query element rect data from host

--- src/main/logseq/api.cljs
+++ src/main/logseq/api.cljs
@@ -796,8 +796,13 @@
 ;; helpers
 (defn ^:export query_element_by_id
   [id]
-  (let [^js el (gdom/getElement id)]
+  (when-let [^js el (gdom/getElement id)]
     (if el (str (.-tagName el) "#" id) false)))
+
+(defn ^:export query_element_rect
+  [selector]
+  (when-let [^js el (js/document.querySelector selector)]
+    (bean/->js (.toJSON (.getBoundingClientRect el)))))
 
 (defn ^:export set_focused_settings
   [pid]
