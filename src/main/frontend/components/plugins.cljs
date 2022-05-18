f497a05c51aefa8a949b55028846eeb6ea4b169e
Yue Yang <g1enyy0ung@gmail.com>
2022-05-18 22:38:47 +0800

    fix: remove first <hr />
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- src/main/frontend/components/plugins.cljs
+++ src/main/frontend/components/plugins.cljs
@@ -81,7 +81,7 @@
               group-first?      (:group-first opt)
               plg               (get (:plugin/installed-plugins @state/state) (keyword (:pid opt)))]
           [:div
-           (when group-first? [:hr.my-2])
+           (when (and group-first? (not= idx 0)) [:hr.my-2])
            [:div.it.flex.px-3.py-1.5.rounded-sm.justify-between
             {:key      (str idx (:url opt))
              :title    (:description opt)
