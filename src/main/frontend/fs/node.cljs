b025ce71d25f31a66ec7ad563cc8ffa48199a743
Tienson Qin <tiensonqin@gmail.com>
2022-06-06 18:33:37 +0800

    enhance: backup db file when not matched

--- src/main/frontend/fs/node.cljs
+++ src/main/frontend/fs/node.cljs
@@ -69,6 +69,8 @@
         (->
          (p/let [result (ipc/ipc "writeFile" repo path content)
                  mtime (gobj/get result "mtime")]
+           (when-not contents-matched?
+             (ipc/ipc "backupDbFile" (config/get-local-dir repo) path db-content content))
            (db/set-file-last-modified-at! repo path mtime)
            (p/let [content (if (encrypt/encrypted-db? (state/get-current-repo))
                              (encrypt/decrypt content)
