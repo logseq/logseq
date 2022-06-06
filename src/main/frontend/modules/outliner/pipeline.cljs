c3801d5aaf4d680c55e5b84821024da1e793e2cd
Tienson Qin <tiensonqin@gmail.com>
2022-06-06 14:35:07 +0800

    fix: slow re-index

--- src/main/frontend/modules/outliner/pipeline.cljs
+++ src/main/frontend/modules/outliner/pipeline.cljs
@@ -9,13 +9,12 @@
 
 (defn invoke-hooks
   [tx-report]
-  (let [{:keys [pages blocks]} (ds-report/get-blocks-and-pages tx-report)]
-    (when-not (:from-disk? (:tx-meta tx-report))
-      (doseq [p (seq pages)] (updated-page-hook tx-report p)))
-    (when (and state/lsp-enabled? (seq blocks))
-      (state/pub-event! [:plugin/hook-db-tx
-                         {:blocks  blocks
-                          :tx-data (:tx-data tx-report)
-                          :tx-meta (:tx-meta tx-report)}]))
-    ;; TODO: Add blocks to hooks
-    #_(doseq [b (seq blocks)])))
+  (when (and (not (:from-disk? (:tx-meta tx-report)))
+             (not (:new-graph? (:tx-meta tx-report))))
+    (let [{:keys [pages blocks]} (ds-report/get-blocks-and-pages tx-report)]
+      (doseq [p (seq pages)] (updated-page-hook tx-report p))
+      (when (and state/lsp-enabled? (seq blocks))
+        (state/pub-event! [:plugin/hook-db-tx
+                           {:blocks  blocks
+                            :tx-data (:tx-data tx-report)
+                            :tx-meta (:tx-meta tx-report)}])))))
