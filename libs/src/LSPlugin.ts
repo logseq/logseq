09775ea9d8b99195f4b1b1e526d1032760443cd7
charlie <xyhp915@qq.com>
2022-06-13 17:40:09 +0800

    improve(plugin): add theme changed hook

--- libs/src/LSPlugin.ts
+++ libs/src/LSPlugin.ts
@@ -410,6 +410,7 @@ export interface IAppProxy {
   // hook events
   onCurrentGraphChanged: IUserHook
   onThemeModeChanged: IUserHook<{ mode: 'dark' | 'light' }>
+  onThemeChanged: IUserHook<Partial<{name: string, mode: string, pid: string, url: string}>>
   onBlockRendererSlotted: IUserSlotHook<{ uuid: BlockUUID }>
 
   /**
