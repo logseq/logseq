25b914d23a23d037b3fe5180d9cdd3d9affba284
charlie <xyhp915@qq.com>
2022-06-30 11:14:14 +0800

    feat(sites): downloads page

--- sites/src/pages/Downloads/index.tsx
+++ sites/src/pages/Downloads/index.tsx
@@ -1,11 +1,15 @@
 import './index.css'
 import {
-  AppleLogo, AppStoreLogo, GooglePlayLogo,
-  LinuxLogo, WindowsLogo
+  AppleLogo, AppStoreLogo, DownloadSimple, GooglePlayLogo,
+  LinuxLogo, QrCode, WindowsLogo
 } from 'phosphor-react'
 import { useState } from 'react'
 import cx from 'classnames'
 import { LSButton } from '../../components/Buttons'
+import { IconsIntel } from '../../components/Icons'
+
+const headImageBg: any = new URL('assets/dl_head_bg.jpg', import.meta.url)
+const headImagePhone: any = new URL('assets/dl_head_bg_2.png', import.meta.url)
 
 const releases = [
   ['MacOS', <AppleLogo/>],
@@ -20,10 +24,55 @@ export function HeadDownloadLinks () {
 
   const resolvePanel = function ([label, icon]: [string, any]) {
     switch (label) {
-      case '':
+      case 'iOS':
+        return (
+          <div className="flex flex-col items-center">
+            <LSButton
+              className={'bg-logseq-400 px-6 py-4'}
+              leftIcon={icon}
+              rightIcon={<QrCode className="opacity-50"/>}
+            >
+              Download on the App Store
+            </LSButton>
+          </div>
+        )
+      case 'MacOS':
+        return (
+          <div className="flex space-x-6">
+            <div className="flex flex-col items-center">
+              <LSButton
+                className={'bg-logseq-400 px-6 py-4'}
+                leftIcon={<IconsIntel size={26} color={'white'}/>}
+                rightIcon={<DownloadSimple className="opacity-50"/>}
+              >
+                Download for Intel chip
+              </LSButton>
+              <span className="text-xs opacity-60 py-2">
+               Most common in Macs
+             </span>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <LSButton
+                className={'bg-logseq-600 px-6 py-4'}
+                leftIcon={<AppleLogo size={24} color={'white'}/>}
+                rightIcon={<DownloadSimple className="opacity-50"/>}
+              >
+                Download for Apple chip
+              </LSButton>
+              <span className="text-xs opacity-60 py-2">
+                Macs from November 2020 and later
+             </span>
+            </div>
+          </div>
+        )
       default:
         return (
-          <LSButton leftIcon={icon}>
+          <LSButton
+            leftIcon={icon}
+            rightIcon={<DownloadSimple className="opacity-50"/>}
+            className="bg-logseq-600 px-6 py-4"
+          >
             Download {label} release
           </LSButton>
         )
@@ -32,7 +81,7 @@ export function HeadDownloadLinks () {
 
   return (
     <div className="app-head-downloads">
-      <div className="flex flex-col items-center pt-28 pb-12 text-slogan">
+      <div className="flex flex-col items-center pt-20 pb-12 text-slogan">
         <h1 className="text-6xl">
           <strong className="font-semibold tracking-wide">Download </strong>
           <span className="opacity-60">the apps.</span>
@@ -68,7 +117,22 @@ export function HeadDownloadLinks () {
         </ul>
 
         <div className="panels flex py-10 justify-center">
-          {resolvePanel(activeRelease)}
+          {resolvePanel(activeRelease as any)}
+        </div>
+      </div>
+
+      <div className="screen-shot">
+        <div className="img-wrap mx-24 relative overflow-hidden">
+          <img alt="Image"
+               src={headImageBg}
+               className="opacity-80 translate-y-28 rounded-md overflow-hidden -mt-20 img-bg"
+          />
+
+          <img alt="Image"
+               src={headImagePhone}
+               className="opacity-90 absolute -right-20 -bottom-16 w-[380px]
+               animate-in duration-1000 slide-in-from-right-40 fade-in-0"
+          />
         </div>
       </div>
     </div>
@@ -78,8 +142,16 @@ export function HeadDownloadLinks () {
 export function DownloadsPage () {
   return (
     <div className="app-page">
+      <div className="page-inner-full-wrap dl-a">
+        <div className="page-inner">
+          <HeadDownloadLinks/>
+        </div>
+      </div>
+
       <div className="page-inner">
-        <HeadDownloadLinks/>
+        <h1 className="text-6xl text-center my-60">
+          Another modules
+        </h1>
       </div>
     </div>
   )
