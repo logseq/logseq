a2ae2f3b6c1e281c887976dd08874d976e764859
llcc <lzhes43@gmail.com>
2022-07-01 19:31:00 +0800

    fix unit tests

--- src/test/frontend/util/page_property_test.cljs
+++ src/test/frontend/util/page_property_test.cljs
@@ -6,99 +6,99 @@
   (testing "add org page property"
     (are [x y] (= x y)
       (property/insert-property :org "" :title "title")
-      "#+title: title"
+      "#+TITLE: title"
 
       (property/insert-property :org "hello" :title "title")
-      "#+title: title\nhello"
+      "#+TITLE: title\nhello"
 
-      (property/insert-property :org "#+title: title\nhello" :title "new title")
-      "#+title: new title\nhello"
+      (property/insert-property :org "#+TITLE: title\nhello" :title "new title")
+      "#+TITLE: new title\nhello"
 
-      (property/insert-property :org "#+title: title\nhello" :alias "alias1")
-      "#+alias: alias1\n#+title: title\nhello"
+      (property/insert-property :org "#+TITLE: title\nhello" :alias "alias1")
+      "#+ALIAS: alias1\n#+TITLE: title\nhello"
 
-      (property/insert-property :org "#+title: title\n#+alias: alias1\nhello" :alias "alias2")
-      "#+title: title\n#+alias: alias2\nhello"
+      (property/insert-property :org "#+TITLE: title\n#+ALIAS: alias1\nhello" :alias "alias2")
+      "#+TITLE: title\n#+ALIAS: alias2\nhello"
 
-      (property/insert-property :org "#+title: title\n#+alias: alias1, alias2\nhello" :alias "alias3")
-      "#+title: title\n#+alias: alias3\nhello"))
+      (property/insert-property :org "#+TITLE: title\n#+ALIAS: alias1, alias2\nhello" :alias "alias3")
+      "#+TITLE: title\n#+ALIAS: alias3\nhello"))
 
   (testing "add markdown page property"
     (are [x y] (= x y)
       (property/insert-property :markdown "" :title "title")
-      "title:: title"
+      "TITLE:: title"
 
       (property/insert-property :markdown "hello" :title "title")
-      "title:: title\nhello"
+      "TITLE:: title\nhello"
 
-      (property/insert-property :markdown "title:: title\nhello" :title "new title")
-      "title:: new title\nhello"
+      (property/insert-property :markdown "TITLE:: title\nhello" :title "new title")
+      "TITLE:: new title\nhello"
 
-      (property/insert-property :markdown "title:: title\nhello" :alias "alias1")
-      "alias:: alias1\ntitle:: title\nhello"
+      (property/insert-property :markdown "TITLE:: title\nhello" :alias "alias1")
+      "ALIAS:: alias1\nTITLE:: title\nhello"
 
-      (property/insert-property :markdown "title:: title\nalias:: alias1\nhello" :alias "alias2")
-      "title:: title\nalias:: alias2\nhello"
+      (property/insert-property :markdown "TITLE:: title\nALIAS:: alias1\nhello" :alias "alias2")
+      "TITLE:: title\nALIAS:: alias2\nhello"
 
-      (property/insert-property :markdown "title:: title\nalias:: alias1, alias2\nhello" :alias "alias3")
-      "title:: title\nalias:: alias3\nhello"
+      (property/insert-property :markdown "TITLE:: title\nALIAS:: alias1, alias2\nhello" :alias "alias3")
+      "TITLE:: title\nALIAS:: alias3\nhello"
 
-      (property/insert-property :markdown "title:: title\nalias:: alias1, alias2\nhello" :aliases "aliases1")
-      "aliases:: aliases1\ntitle:: title\nalias:: alias1, alias2\nhello"
+      (property/insert-property :markdown "TITLE:: title\nALIAS:: alias1, alias2\nhello" :aliases "aliases1")
+      "ALIASES:: aliases1\nTITLE:: title\nALIAS:: alias1, alias2\nhello"
 
-      (property/insert-property :markdown "title:: title\nalias:: alias1, alias2\naliases:: aliases1\nhello" :aliases "aliases2")
-      "title:: title\nalias:: alias1, alias2\naliases:: aliases2\nhello")))
+      (property/insert-property :markdown "TITLE:: title\nALIAS:: alias1, alias2\nALIASES:: aliases1\nhello" :aliases "aliases2")
+      "TITLE:: title\nALIAS:: alias1, alias2\nALIASES:: aliases2\nhello")))
 
 (deftest test-insert-properties
   (testing "add org page properties"
     (are [x y] (= x y)
 
       (property/insert-properties :org "" {:title "title"})
-      "#+title: title"
+      "#+TITLE: title"
 
       (property/insert-properties :org "hello" {:title "title"})
-      "#+title: title\nhello"
+      "#+TITLE: title\nhello"
 
-      (property/insert-properties :org "#+title: title\nhello"
+      (property/insert-properties :org "#+TITLE: title\nhello"
                                   {:title "new title"
                                    :alias "alias1"})
-      "#+alias: alias1\n#+title: new title\nhello"
+      "#+ALIAS: alias1\n#+TITLE: new title\nhello"
 
-      (property/insert-properties :org "#+title: title\n#+alias: alias1\nhello"
+      (property/insert-properties :org "#+TITLE: title\n#+ALIAS: alias1\nhello"
                                   {:title "new title"
                                    :alias "alias2"
                                    :aliases "aliases1"})
-      "#+aliases: aliases1\n#+title: new title\n#+alias: alias2\nhello"
+      "#+ALIASES: aliases1\n#+TITLE: new title\n#+ALIAS: alias2\nhello"
 
-      (property/insert-properties :org "#+title: title\n#+alias: alias1, alias2\n#+aliases: aliases1\nhello"
+      (property/insert-properties :org "#+TITLE: title\n#+ALIAS: alias1, alias2\n#+ALIASES: aliases1\nhello"
                                   {:title "new title"
                                    :alias "alias2"
                                    :aliases "aliases1"})
-      "#+title: new title\n#+alias: alias2\n#+aliases: aliases1\nhello"))
+      "#+TITLE: new title\n#+ALIAS: alias2\n#+ALIASES: aliases1\nhello"))
 
   (testing "add markdown page properties"
     (are [x y] (= x y)
       (property/insert-properties :markdown "" {:title "title"})
-      "title:: title"
+      "TITLE:: title"
 
       (property/insert-properties :markdown "hello" {:title "title"})
-      "title:: title\nhello"
+      "TITLE:: title\nhello"
 
-      (property/insert-properties :markdown "title:: title\nhello"
+      (property/insert-properties :markdown "TITLE:: title\nhello"
                                   {:title "new title"
                                    :alias "alias1"})
-      "alias:: alias1\ntitle:: new title\nhello"
+      "ALIAS:: alias1\nTITLE:: new title\nhello"
 
-      (property/insert-properties :markdown "title:: title\nalias:: alias1\nhello"
+      (property/insert-properties :markdown "TITLE:: title\nalias:: alias1\nhello"
                                   {:title "new title"
                                    :alias "alias2"
                                    :aliases "aliases1"})
-      "aliases:: aliases1\ntitle:: new title\nalias:: alias2\nhello"
+      "ALIASES:: aliases1\nTITLE:: new title\nALIAS:: alias2\nhello"
 
-      (property/insert-properties :markdown "title:: title\nalias:: alias1, alias2\naliases:: aliases1\nhello"
+      (property/insert-properties :markdown "TITLE:: title\nALIAS:: alias1, alias2\nALIASES:: aliases1\nhello"
                                   {:title "new title"
                                    :alias "alias2"
                                    :aliases "aliases1"})
-      "title:: new title\nalias:: alias2\naliases:: aliases1\nhello")))
+      "TITLE:: new title\nALIAS:: alias2\nALIASES:: aliases1\nhello")))
 
 #_(cljs.test/run-tests)
