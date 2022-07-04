1b2ba255216987e6c769e0f7f31d7d2a0a2418c7
Tienson Qin <tiensonqin@gmail.com>
2022-07-04 20:12:46 +0800

    fix: rename namespace page results in lower-case page

--- deps/graph-parser/src/logseq/graph_parser/block.cljs
+++ deps/graph-parser/src/logseq/graph_parser/block.cljs
@@ -251,8 +251,7 @@
           [original-page-name page-name journal-day] (convert-page-if-journal original-page-name date-formatter)
           namespace? (and (not (boolean (text/get-nested-page-name original-page-name)))
                           (text/namespace-page? original-page-name))
-          page-entity (some-> db (d/entity [:block/name page-name]))
-          original-page-name (or (:block/original-name page-entity) original-page-name)]
+          page-entity (some-> db (d/entity [:block/name page-name]))]
       (merge
        {:block/name page-name
         :block/original-name original-page-name}
