b0fac4e2d7e6e1d26f69e33faa693224e7f43925
8N9KT <84566861+8N9KT@users.noreply.github.com>
2022-07-16 10:22:10 +0200

    Update query_table.cljs

--- src/main/frontend/components/query_table.cljs
+++ src/main/frontend/components/query_table.cljs
@@ -168,10 +168,9 @@
                            (let [vals (for [item value]
                                         (page-cp {} {:block/name item}))]
                              (interpose [:span ", "] vals))
-                           (if (not (string? value))
-                              (if (boolean? value) 
-                                (str value)
-                                value)
-                             (if-let [page (db/entity [:block/name (util/page-name-sanity-lc value)])]
-                               (page-cp {} page)
-                               (inline-text format value)))))))]))]))]]])))
+                           (cond
+                             (boolean? value) (str value)
+                             (not (string? value)) value
+                             :else (if-let [page (db/entity [:block/name (util/page-name-sanity-lc value)])]
+                                     (page-cp {} page)
+                                     (inline-text format value)))))))]))]))]]])))
