9b15fa0e639b8c3b673fb6adf84a4b285329ef81
Peng Xiao <pengxiao@outlook.com>
2022-08-12 14:07:04 +0800

    fix: change logseq portal's shortcut

--- tldraw/apps/tldraw-logseq/src/lib/tools/LogseqPortalTool/LogseqPortalTool.tsx
+++ tldraw/apps/tldraw-logseq/src/lib/tools/LogseqPortalTool/LogseqPortalTool.tsx
@@ -9,7 +9,7 @@ export class LogseqPortalTool extends TLTool<
   TLApp<Shape, TLReactEventMap>
 > {
   static id = 'logseq-portal'
-  static shortcut = ['i']
+  static shortcut = ['l']
   static states = [IdleState, CreatingState]
   static initial = 'idle'
 
