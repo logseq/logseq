220a55b75642a909bdd4688969d11ed908464eb7
Tienson Qin <tiensonqin@gmail.com>
2022-08-16 11:21:59 +0800

    fix: collapsing multi-line block
    close #6374

--- src/main/frontend/handler/editor.cljs
+++ src/main/frontend/handler/editor.cljs
@@ -2916,9 +2916,9 @@
 
               :else
               nil)))
-        
+
         (close-autocomplete-if-outside input)
-        
+
         (when-not (or (= k "Shift") is-processed?)
           (state/set-last-key-code! {:key-code key-code
                                      :code code
