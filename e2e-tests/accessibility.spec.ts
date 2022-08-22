54e43c71dd51a27615cc797a3495c036ecec3407
Konstantinos Kaloutas <konstantinos@logseq.com>
2022-08-22 15:53:59 +0300

    enhance: add basic a11y test

---
+++ e2e-tests/accessibility.spec.ts
@@ -0,0 +1,10 @@
+import { injectAxe, checkA11y, getViolations, reportViolations } from 'axe-playwright'
+import { test } from './fixtures'
+import { createRandomPage } from './utils'
+
+
+test('check a11y for the whole page', async ({ page }) => {
+    await injectAxe(page)
+    await createRandomPage(page)
+    await checkA11y(page)
+})
