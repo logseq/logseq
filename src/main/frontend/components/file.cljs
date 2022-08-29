a47361e8ab4ce62a86e60ac0b46a33e8d79f7396
charlie <xyhp915@qq.com>
2022-08-29 10:48:32 +0800

    fix: custom css not reload when switching graph

--- src/main/frontend/components/file.cljs
+++ src/main/frontend/components/file.cljs
@@ -103,14 +103,14 @@
        [:img {:src path}]
 
        (and format (contains? (gp-config/text-formats) format))
-       (when-let [file-content (db/get-file path)]
+       (when-let [file-content (or (db/get-file path) "")]
          (let [content (string/trim file-content)
                mode (util/get-file-ext path)]
-           (lazy-editor/editor {:file? true
-                                :file-path path}
-                               (str "file-edit-" random-id)
-                               {:data-lang mode}
-                               content
+            (lazy-editor/editor {:file?     true
+                                 :file-path path}
+                                (str "file-edit-" random-id)
+                                {:data-lang mode}
+                                content
                                {})))
 
        :else
