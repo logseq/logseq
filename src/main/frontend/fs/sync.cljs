332b8de5a1f97aa318702464ac3f66541c463c0b
rcmerci <rcmerci@gmail.com>
2022-09-06 13:14:45 +0800

    fix: es maybe nil

--- src/main/frontend/fs/sync.cljs
+++ src/main/frontend/fs/sync.cljs
@@ -2147,7 +2147,7 @@
   "filter out FileChangeEvents checksum changed,
   compare checksum in FileChangeEvent and checksum calculated now"
   [es]
-  {:pre [(coll? es)
+  {:pre [(or (nil? es) (coll? es))
          (every? #(instance? FileChangeEvent %) es)]}
   (go
     (when (seq es)
@@ -2180,7 +2180,7 @@
 (defn- filter-too-huge-files
   "filter out files > `file-size-limit`"
   [es]
-  {:pre [(coll? es)
+  {:pre [(or (nil? es) (coll? es))
          (every? #(instance? FileChangeEvent %) es)]}
   (filterv filter-too-huge-files-aux es))
 
