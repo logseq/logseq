logseq/logseq

commit 6e3f784626b2739d682dc578ac3b34e13bbbe5ef

Fix global directory watcher not working for multiple windows
Fix also simplifies global config component and generalizes file watcher
to handle any global directory. Also fixed intermittent, annoying "object could not
be cloned" error

src/main/frontend/handler/global_config.cljs
@@ -2,8 +2,7 @@
   "This ns is a system component that encapsulates global config functionality.
   Unlike repo config, this also manages a directory for configuration. This
   component depends on a repo."
-  (:require [frontend.config :as config]
-            [frontend.fs :as fs]
+  (:require [frontend.fs :as fs]
             [frontend.handler.common.file :as file-common-handler]
             [frontend.state :as state]
             [cljs.reader :as reader]
@@ -51,30 +50,16 @@
     (p/let [config-content (fs/read-file config-dir config-path)]
       (set-global-config-state! config-content))))
 
-(defn- watch-dir!
-  "Watches global config dir for given repo/db"
-  [repo]
-  (fs/watch-dir! (global-config-dir)
-                 ;; Global dir needs to know it's current graph in order to send
-                 ;; change events to the right window and graph db
-                 {:current-repo-dir (config/get-repo-dir repo)}))
-
-(defn re-watch-dir!
-  "Rewatch global config dir for given repo/db. Unwatches dir first as we don't
-  want multiple file watchers, especially when switching graphs"
-  [repo]
-  (fs/unwatch-dir! (global-config-dir))
-  (watch-dir! repo))
-
 (defn start
   "This component has four responsibilities on start:
 - Fetch root-dir for later use with config paths
 - Manage ui state of global config
 - Create a global config dir and file if it doesn't exist
-- Start a file watcher for global config dir"
+- Start a file watcher for global config dir if it's not already started.
+  Watcher ensures client db is seeded with correct file data."
   [{:keys [repo]}]
   (p/let [root-dir' (ipc/ipc "getLogseqDotDirRoot")
           _ (reset! root-dir root-dir')
           _ (restore-global-config!)
           _ (create-global-config-file-if-not-exists repo)
-          _ (watch-dir! repo)]))
+          _ (fs/watch-dir! (global-config-dir) {:global-dir true})]))

src/electron/electron/fs_watcher.cljs
@@ -24,8 +24,14 @@
                         (send file-watcher-chan
                               (bean/->js {:type type :payload payload})))
                     true))
-        wins (window/get-graph-all-windows (or (:current-repo-dir payload) dir))]
-    (if (contains? #{"unlinkDir" "addDir"} type)
+        wins (if (:global-dir payload)
+               (window/get-all-windows)
+               (window/get-graph-all-windows dir))]
+    (if (or (contains? #{"unlinkDir" "addDir"} type)
+            ;; Only change events to a global dir are emitted to all windows.
+            ;; Add* events are not emitted to all since each client adds
+            ;; files at different times.
+            (and (:global-dir payload) (= "change" type)))
       ;; notify every windows
       (doseq [win wins] (send-fn win))
 
@@ -45,66 +51,100 @@
         stat (when (and (not= event "unlink")
                         (not dir-path?))
                (fs/statSync path))]
-    (send-file-watcher! dir event (merge options
-                                         {:dir (utils/fix-win-path! dir)
+    (send-file-watcher! dir event (merge {:dir (utils/fix-win-path! dir)
                                           :path (utils/fix-win-path! path)
                                           :content content
-                                          :stat stat}))))
+                                          :stat stat}
+                                         (select-keys options [:global-dir])))))
+(defn- create-dir-watcher
+  [dir options]
+  (let [watcher-opts (clj->js
+                      {:ignored (fn [path]
+                                  (utils/ignored-path? dir path))
+                       :ignoreInitial false
+                       :ignorePermissionErrors true
+                       :interval polling-interval
+                       :binaryInterval polling-interval
+                       :persistent true
+                       :disableGlobbing true
+                       :usePolling false
+                       :awaitWriteFinish true})
+        dir-watcher (.watch watcher dir watcher-opts)]
+    ;; TODO: batch sender
+    (.on dir-watcher "unlinkDir"
+         (fn [path]
+           (when (= dir path)
+             (publish-file-event! dir dir "unlinkDir" options))))
+    (.on dir-watcher "addDir"
+         (fn [path]
+           (when (= dir path)
+             (publish-file-event! dir dir "addDir" options))))
+    (.on dir-watcher "add"
+         (fn [path]
+           (publish-file-event! dir path "add" options)))
+    (.on dir-watcher "change"
+         (fn [path]
+           (publish-file-event! dir path "change" options)))
+    (.on dir-watcher "unlink"
+         ;; delay 500ms for syncing disks
+         (fn [path]
+           (js/setTimeout #(when (not (fs/existsSync path))
+                             (publish-file-event! dir path "unlink" options))
+                          500)))
+    (.on dir-watcher "error"
+         (fn [path]
+           (.warn utils/logger "Watch error happened: " (str {:path path}))))
+
+    dir-watcher))
+
+(defn- seed-client-with-initial-global-dir-data
+  "Ensures that secondary clients initialize their databases efficiently and in
+  the same way as the primary client. This fn achieves this by creating a
+  temporary watcher whose sole purpose is to seed the db and then close  when
+  its done seeding a.k.a. ready event fires."
+  [dir options]
+  (let [dir-watcher (create-dir-watcher dir options)]
+    (.on dir-watcher "ready" (fn []
+                               (.close dir-watcher)))))
+
+(defn- create-and-save-watcher
+  [dir options]
+  (let [dir-watcher (create-dir-watcher dir options)
+        watcher-del-f #(.close dir-watcher)]
+    (swap! *file-watcher assoc dir [dir-watcher watcher-del-f])
+    ;; electron app extends `EventEmitter`
+    ;; TODO check: duplicated with the logic in "window-all-closed" ?
+    (.on app "quit" watcher-del-f)))
+
+(defn- watch-global-dir!
+  "Only one watcher exists per global dir so only create the watcher for the
+  primary client. Secondary clients only seed their client database."
+  [dir options]
+  (if (get @*file-watcher dir)
+    (seed-client-with-initial-global-dir-data dir options)
+    (create-and-save-watcher dir options)))
 
 (defn watch-dir!
-  "Watch a directory if no such file watcher exists. Has the following options:
-* :current-repo-dir - Provides current repo-dir for global directories. Needed as watch events need to take place in a repo context in order for window and db to function correctly"
+  "Watches a directory and emits file events. In addition to file
+  watching, clients rely on watchers to initially seed their database with
+  the file contents of a dir. This is done with the ignoreInitial option
+  set to false, https://github.com/paulmillr/chokidar#path-filtering. The
+  watcher emits addDir and add file events which then seed the client database.
+  This fn has the following options:
+
+* :global-dir - Boolean that indicates the watched directory is global. This
+  type of directory has different behavior then a normal watcher as it
+  broadcasts its change events to all clients. This option needs to be passed to
+  clients in order for them to identify the correct db"
   [dir options]
-  (when-not (get @*file-watcher dir)
-    (if (fs/existsSync dir)
-      (let [watcher-opts (clj->js
-                          {:ignored (fn [path]
-                                      (utils/ignored-path? dir path))
-                           :ignoreInitial false
-                           :ignorePermissionErrors true
-                           :interval polling-interval
-                           :binaryInterval polling-interval
-                           :persistent true
-                           :disableGlobbing true
-                           :usePolling false
-                           :awaitWriteFinish true})
-            dir-watcher (.watch watcher dir watcher-opts)
-            watcher-del-f #(.close dir-watcher)]
-        (swap! *file-watcher assoc dir [dir-watcher watcher-del-f])
-        ;; TODO: batch sender
-        (.on dir-watcher "unlinkDir"
-             (fn [path]
-               (when (= dir path)
-                 (publish-file-event! dir dir "unlinkDir" options))))
-        (.on dir-watcher "addDir"
-             (fn [path]
-               (when (= dir path)
-                 (publish-file-event! dir dir "addDir" options))))
-        (.on dir-watcher "add"
-             (fn [path]
-               (publish-file-event! dir path "add" options)))
-        (.on dir-watcher "change"
-             (fn [path]
-               (publish-file-event! dir path "change" options)))
-        (.on dir-watcher "unlink"
-             ;; delay 500ms for syncing disks
-             (fn [path]
-               (js/setTimeout #(when (not (fs/existsSync path))
-                                 (publish-file-event! dir path "unlink" options))
-                              500)))
-        (.on dir-watcher "error"
-             (fn [path]
-               (.warn utils/logger "Watch error happened: "
-                      (str {:path path}))))
-
-        ;; electron app extends `EventEmitter`
-        ;; TODO check: duplicated with the logic in "window-all-closed" ?
-        (.on app "quit" watcher-del-f)
-
-        true)
-      ;; retry if the `dir` not exists, which is useful when a graph's folder is
-      ;; back after refreshing the window
-      (js/setTimeout #(watch-dir! dir {}) 5000))))
+  (if (:global-dir options)
+    (watch-global-dir! dir options)
+    (when-not (get @*file-watcher dir)
+      (if (fs/existsSync dir)
+        (create-and-save-watcher dir options)
+        ;; retry if the `dir` not exists, which is useful when a graph's folder is
+        ;; back after refreshing the window
+        (js/setTimeout #(watch-dir! dir options) 5000)))))
 
 (defn close-watcher!
   "If no `dir` provided, close all watchers;

src/electron/electron/handler.cljs
@@ -479,11 +479,13 @@
   ;;    1. there is no one window on the same dir
   ;;    2. reset file watcher to resend `add` event on window refreshing
   (when dir
-    (watcher/watch-dir! dir options)))
+    (watcher/watch-dir! dir options)
+    nil))
 
 (defmethod handle :unwatchDir [^js _window [_ dir]]
   (when dir
-    (watcher/close-watcher! dir)))
+    (watcher/close-watcher! dir)
+    nil))
 
 (defn open-new-window!
   "Persist db first before calling! Or may break db persistency"
@@ -603,6 +605,10 @@
              (fn [^js event args-js]
                (try
                  (let [message (bean/->clj args-js)]
+                   ;; Be careful with the return values of `handle` defmethods.
+                   ;; Values that are not non-JS objects will cause this
+                   ;; exception -
+                   ;; https://www.electronjs.org/docs/latest/breaking-changes#behavior-changed-sending-non-js-objects-over-ipc-now-throws-an-exception
                    (bean/->js (handle (or (utils/get-win-from-sender event) window) message)))
                  (catch js/Error e
                    (when-not (contains? #{"mkdir" "stat"} (nth args-js 0))

src/main/frontend/fs/watcher_handler.cljs
@@ -45,10 +45,12 @@
     (db/set-file-last-modified-at! repo path mtime)))
 
 (defn handle-changed!
-  [type {:keys [dir path content stat current-repo-dir] :as payload}]
+  [type {:keys [dir path content stat global-dir] :as payload}]
   (when dir
     (let [path (gp-util/path-normalize path)
-          repo (config/get-local-repo (or current-repo-dir dir))
+          ;; Global directory events don't know their originating repo so we rely
+          ;; on the client to correctly identify it
+          repo (if global-dir (state/get-current-repo) (config/get-local-repo dir))
           pages-metadata-path (config/get-pages-metadata-path)
           {:keys [mtime]} stat
           db-content (or (db/get-file repo path) "")]

src/main/frontend/handler/events.cljs
@@ -32,7 +32,6 @@
             [frontend.handler.page :as page-handler]
             [frontend.handler.plugin :as plugin-handler]
             [frontend.handler.repo :as repo-handler]
-            [frontend.handler.global-config :as global-config-handler]
             [frontend.handler.repo-config :as repo-config-handler]
             [frontend.handler.route :as route-handler]
             [frontend.handler.search :as search-handler]
@@ -121,7 +120,6 @@
          (route-handler/redirect-to-home!))
        (when-let [dir-name (config/get-repo-dir graph)]
          (fs/watch-dir! dir-name))
-       (global-config-handler/re-watch-dir! graph)
        (srs/update-cards-due-count!)
        (state/pub-event! [:graph/ready graph])
        (repo-handler/refresh-repos!)

commit 992c534020d6a2cfb9314db1223c0f319d4257a4

3 fixes where global-config as incorrectly called for non desktop
These issues were introduced with #6531. The global-config component
should not be called if not in a supported platform. This fixes
re-index, switching graphs and editing config.edn for mobile.  Switching
graphs was partially fixed in #6664 but didn't address the root cause
which is the global-handler component shouldn't be called if not enabled
for a platform

src/main/frontend/handler/file.cljs
@@ -95,7 +95,9 @@
   (let [original-content (db/get-file repo path)
         write-file! (if from-disk?
                       #(p/resolved nil)
-                      #(let [path-dir (if (= (path/dirname path) (global-config-handler/global-config-dir))
+                      #(let [path-dir (if (and
+                                            (config/global-config-enabled?)
+                                            (= (path/dirname path) (global-config-handler/global-config-dir)))
                                         (global-config-handler/global-config-dir)
                                         (config/get-repo-dir repo))]
                          (fs/write-file! repo path-dir path content
@@ -122,7 +124,7 @@
                        (p/let [_ (repo-config-handler/restore-repo-config! repo)]
                          (state/pub-event! [:shortcut/refresh]))
 
-                       (= path (global-config-handler/global-config-path))
+                       (and (config/global-config-enabled?) (= path (global-config-handler/global-config-path)))
                        (p/let [_ (global-config-handler/restore-global-config!)]
                          (state/pub-event! [:shortcut/refresh]))
 
@@ -131,7 +133,8 @@
 
                      (when re-render-root? (ui-handler/re-render-root!)))
                    (fn [error]
-                     (when (= path (global-config-handler/global-config-path))
+                     (when (and (config/global-config-enabled?)
+                                (= path (global-config-handler/global-config-path)))
                        (state/pub-event! [:notification/show
                                          {:content (str "Failed to write to file " path)
                                           :status :error}]))

src/main/frontend/handler/global_config.cljs
@@ -34,24 +34,21 @@
 
 (defn- create-global-config-file-if-not-exists
   [repo-url]
-  (when (not-empty @root-dir)
-   (let [config-dir (global-config-dir)
+  (let [config-dir (global-config-dir)
         config-path (global-config-path)]
     (p/let [_ (fs/mkdir-if-not-exists config-dir)
             file-exists? (fs/create-if-not-exists repo-url config-dir config-path default-content)]
            (when-not file-exists?
              (file-common-handler/reset-file! repo-url config-path default-content)
-             (set-global-config-state! default-content))))))
+             (set-global-config-state! default-content)))))
 
 (defn restore-global-config!
   "Sets global config state from config file"
   []
-  (if (not-empty @root-dir)
-    (let [config-dir (global-config-dir)
-          config-path (global-config-path)]
-      (p/let [config-content (fs/read-file config-dir config-path)]
-        (set-global-config-state! config-content)))
-    (set-global-config-state! default-content)))
+  (let [config-dir (global-config-dir)
+        config-path (global-config-path)]
+    (p/let [config-content (fs/read-file config-dir config-path)]
+      (set-global-config-state! config-content))))
 
 (defn start
   "This component has four responsibilities on start:

src/main/frontend/handler/repo.cljs
@@ -408,7 +408,8 @@
    (state/set-db-restoring! true)
    (db/restore-graph! repo)
    (repo-config-handler/restore-repo-config! repo)
-   (global-config-handler/restore-global-config!)
+   (when (config/global-config-enabled?)
+     (global-config-handler/restore-global-config!))
     ;; Don't have to unlisten the old listener, as it will be destroyed with the conn
    (db/listen-and-persist! repo)
    (state/pub-event! [:shortcut/refresh])

src/main/frontend/handler/web/nfs.cljs
@@ -338,14 +338,16 @@
                                        (fn [path handle]
                                          (when nfs?
                                            (swap! path-handles assoc path handle))))
-                         global-dir (global-config-handler/global-config-dir)
-                         global-files-result (if (config/global-config-enabled?)
-                                               (fs/get-files global-dir (constantly nil))
-                                               [])
                          new-local-files (-> (->db-files mobile-native? electron? dir-name local-files-result)
                                              (remove-ignore-files dir-name nfs?))
-                         new-global-files (-> (->db-files mobile-native? electron? global-dir global-files-result)
-                                              (remove-ignore-files global-dir nfs?))
+                         new-global-files (if (config/global-config-enabled?)
+                                            (p/let [global-files-result (fs/get-files
+                                                                          (global-config-handler/global-config-dir)
+                                                                          (constantly nil))
+                                                    global-files (-> (->db-files mobile-native? electron? (global-config-handler/global-config-dir) global-files-result)
+                                                  (remove-ignore-files (global-config-handler/global-config-dir) nfs?))]
+                                              global-files)
+                                            (p/resolved []))
                          new-files (concat new-local-files new-global-files)
 
                          _ (when nfs?