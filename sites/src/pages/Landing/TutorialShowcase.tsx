01d3e390e30235d9cdc99f5c6342a1207a625a4e
charlie <xyhp915@qq.com>
2022-09-13 15:29:59 +0800

    improve(sites): module layout on mobile

--- sites/src/pages/Landing/TutorialShowcase.tsx
+++ sites/src/pages/Landing/TutorialShowcase.tsx
@@ -1,8 +1,8 @@
-import { FrameCorners } from 'phosphor-react'
+import { FrameCorners, CaretLeft, CaretDown } from 'phosphor-react'
 import { FloatGlassButton, imageS1 } from './common'
 import { AnimateInTurnBox } from '../../components/Animations'
 import cx from 'classnames'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useAppState } from '../../state'
 
 const featuresSlideItems = [
@@ -28,20 +28,54 @@ const featuresSlideItems = [
   }
 ]
 
+export function TutorialFeaturesPanel (
+  props: Partial<{ activeItem: typeof featuresSlideItems[number] }>
+) {
+  return (
+    <article className="app-tutorial-features-panel relative">
+      <div className="hd">
+        <strong>
+          <i>1</i>
+          <i>2</i>
+        </strong>
+
+        <h1 className="flex text-lg sm:text-3xl justify-center">
+          Capturing and structuring class notes
+        </h1>
+      </div>
+      <div className="bd">
+        <div className="wrap flex">
+          <img src={imageS1} alt="images"/>
+        </div>
+      </div>
+      <div className="ft absolute bottom-6 right-6">
+        <FloatGlassButton>
+          <FrameCorners
+            className={'font-bold cursor-pointer'}
+            size={26}
+            weight={'duotone'}
+          />
+        </FloatGlassButton>
+      </div>
+    </article>
+  )
+}
+
 export function TutorialFeaturesSlide () {
-  const [activeTab, setActiveTab] = useState(featuresSlideItems[0].label)
+  const [activeIndex, setActiveIndex] = useState(0)
+  const activeItem = featuresSlideItems[activeIndex]
 
   return (
     <div className="app-tutorial-features-slide">
       <div className="inner px-14">
         {/* Tabs */}
         <ul className="tabs flex flex space-x-8 justify-around">
-          {featuresSlideItems.map(it => {
+          {featuresSlideItems.map((it, idx) => {
             return (
               <li
                 key={it.label}
-                className={cx({ active: (it.label === activeTab) })}
-                onClick={() => setActiveTab(it.label)}
+                className={cx({ active: (idx === activeIndex) })}
+                onClick={() => setActiveIndex(idx)}
               >
                 <span>{it.icon}</span><strong>{it.label}</strong>
               </li>)
@@ -49,45 +83,46 @@ export function TutorialFeaturesSlide () {
         </ul>
 
         {/* Panel */}
-        <article className="panels relative">
-          <div className="hd">
-            <strong>
-              <i>1</i>
-              <i>2</i>
-            </strong>
-
-            <h1 className="flex text-3xl justify-center">
-              Capturing and structuring class notes
-            </h1>
-          </div>
-          <div className="bd">
-            <div className="wrap flex">
-              <img src={imageS1} alt="images"/>
-            </div>
-          </div>
-          <div className="ft absolute bottom-6 right-6">
-            <FloatGlassButton>
-              <FrameCorners
-                className={'font-bold cursor-pointer'}
-                size={26}
-                weight={'duotone'}
-              />
-            </FloatGlassButton>
-          </div>
-        </article>
+        <TutorialFeaturesPanel activeItem={activeItem}/>
       </div>
     </div>
   )
 }
 
 export function TutorialFeaturesSelect () {
-  const [activeTab, setActiveTab] = useState(featuresSlideItems[0].label)
+  const [activeIndex, setActiveIndex] = useState(0)
+  const activeItem = featuresSlideItems[activeIndex]
 
   return (
-    <div className="app-tutorial-features-select">
-      <select className={'app-form-select w-full'}>
-        <option>{activeTab}</option>
-      </select>
+    <div className={cx('app-tutorial-features-select', `index-of-${activeIndex}`)}>
+      <div className="app-form-select-wrap">
+        <span className="icon">
+          {activeItem.icon}
+        </span>
+
+        <select className={'app-form-select w-full'}
+                onChange={(e) => {
+                  setActiveIndex(
+                    e.target.selectedIndex
+                  )
+                }}
+                value={activeIndex}
+        >
+          {featuresSlideItems.map((it, idx) => {
+            return (<option
+              key={it.label}
+              value={idx}
+            >{it.label}</option>)
+          })}
+        </select>
+
+        <span className="arrow">
+          <CaretDown weight={'bold'}/>
+        </span>
+      </div>
+
+      {/* Panel */}
+      <TutorialFeaturesPanel activeItem={activeItem}/>
     </div>
   )
 }
