a0b37fa20e1fbf9b48297fa6f8945c0bc38283ef
Konstantinos Kaloutas <konstantinos@logseq.com>
2022-09-15 17:59:26 +0300

    fix: restore zoom level

--- src/electron/electron/window.cljs
+++ src/electron/electron/window.cljs
@@ -146,7 +146,7 @@
         (.on "new-window" new-win-handler)
         (.on "will-navigate" will-navigate-handler)
         (.on "did-start-navigation" #(.send web-contents "persist-zoom-level" (.getZoomLevel web-contents)))
-        (.on "did-navigate-in-page" #(.send web-contents "restore-zoom-level")))
+        (.on "page-title-updated" #(.send web-contents "restore-zoom-level")))
 
       (doto win
         (.on "enter-full-screen" #(.send web-contents "full-screen" "enter"))
