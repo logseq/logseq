3f500101a7c9cab480dc79429969c69fa2c70777
Peng Xiao <pengxiao@freewheel.tv>
2022-09-17 20:52:06 +0800

    fix: enable editing when clicking a selected shape

--- tldraw/packages/core/src/lib/tools/TLSelectTool/states/PointingSelectedShapeState.ts
+++ tldraw/packages/core/src/lib/tools/TLSelectTool/states/PointingSelectedShapeState.ts
@@ -1,13 +1,19 @@
 import { Vec } from '@tldraw/vec'
-import type * as types from '../../../../types'
-import type * as shapes from '../../../shapes'
+import {
+  type TLEventMap,
+  type TLEventShapeInfo,
+  type TLEvents,
+  TLTargetType,
+} from '../../../../types'
+import { PointUtils } from '../../../../utils'
+import { type TLShape, TLBoxShape } from '../../../shapes'
 import type { TLApp } from '../../../TLApp'
 import { TLToolState } from '../../../TLToolState'
 import type { TLSelectTool } from '../TLSelectTool'
 
 export class PointingSelectedShapeState<
-  S extends shapes.TLShape,
-  K extends types.TLEventMap,
+  S extends TLShape,
+  K extends TLEventMap,
   R extends TLApp<S, K>,
   P extends TLSelectTool<S, K, R>
 > extends TLToolState<S, K, R, P> {
@@ -15,7 +21,7 @@ export class PointingSelectedShapeState<
 
   private pointedSelectedShape?: S
 
-  onEnter = (info: types.TLEventShapeInfo<S>) => {
+  onEnter = (info: TLEventShapeInfo<S>) => {
     this.pointedSelectedShape = info.shape
   }
 
@@ -23,32 +29,45 @@ export class PointingSelectedShapeState<
     this.pointedSelectedShape = undefined
   }
 
-  onWheel: types.TLEvents<S>['wheel'] = (info, e) => {
+  onWheel: TLEvents<S>['wheel'] = (info, e) => {
     this.onPointerMove(info, e)
   }
 
-  onPointerMove: types.TLEvents<S>['pointer'] = () => {
+  onPointerMove: TLEvents<S>['pointer'] = () => {
     const { currentPoint, originPoint } = this.app.inputs
     if (Vec.dist(currentPoint, originPoint) > 5) {
       this.tool.transition('translating')
     }
   }
 
-  onPointerUp: types.TLEvents<S>['pointer'] = () => {
-    const { shiftKey } = this.app.inputs
+  onPointerUp: TLEvents<S>['pointer'] = () => {
+    const { shiftKey, currentPoint } = this.app.inputs
+    const { selectedShapesArray } = this.app
     if (!this.pointedSelectedShape) throw Error('Expected a pointed selected shape')
     if (shiftKey) {
       const { selectedIds } = this.app
       const next = Array.from(selectedIds.values())
       next.splice(next.indexOf(this.pointedSelectedShape.id), 1)
       this.app.setSelectedShapes(next)
+    } else if (
+      selectedShapesArray.length === 1 &&
+      this.pointedSelectedShape.canEdit &&
+      this.pointedSelectedShape instanceof TLBoxShape &&
+      PointUtils.pointInBounds(currentPoint, this.pointedSelectedShape.bounds)
+    ) {
+      this.tool.transition('editingShape', {
+        shape: this.pointedSelectedShape,
+        order: 0,
+        type: TLTargetType.Shape,
+      })
+      return
     } else {
       this.app.setSelectedShapes([this.pointedSelectedShape.id])
     }
     this.tool.transition('idle')
   }
 
-  onPinchStart: types.TLEvents<S>['pinch'] = (info, event) => {
+  onPinchStart: TLEvents<S>['pinch'] = (info, event) => {
     this.tool.transition('pinching', { info, event })
   }
 }
