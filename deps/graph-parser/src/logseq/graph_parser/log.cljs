a368cab5e40b5c5cf81d8be46fc82dac7e0a45e9
Gabriel Horner <gabriel@logseq.com>
2022-09-19 11:24:19 -0400

    Add ns linter to deps libs and then add+update ns docstrings

--- deps/graph-parser/src/logseq/graph_parser/log.cljs
+++ deps/graph-parser/src/logseq/graph_parser/log.cljs
@@ -1,6 +1,6 @@
 (ns logseq.graph-parser.log
-  "Minimal logging ns that implements basic lambdaisland.glogi fns. May use
-  glogi later if this ns is used more")
+  "Minimal, logging ns that shims lambdaisland.glogi fns for nbb. Could port
+  glogi to nbb later if this shim gets too big")
 
 (defn error [& msgs]
   (apply js/console.error (map clj->js msgs)))
