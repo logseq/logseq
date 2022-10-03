b8a60822f764a15b7d7cac6a3206aede00245328
Seth <sethyuan@gmail.com>
2022-10-03 18:31:56 +0800

    feat: support locale sensitive string comparisons for table sorting

--- src/main/frontend/components/query_table.cljs
+++ src/main/frontend/components/query_table.cljs
@@ -39,9 +39,23 @@
     (:block/name item)
     (get-in item [:block/properties sort-by-column])))
 
+(defn- locale-compare
+  "Use locale specific comparison for strings and general comparison for others."
+  [x y]
+  (if (and (string? x) (string? y))
+    (.localeCompare x y)
+    (< x y)))
+
+(defn- locale-compare-desc
+  "Use locale specific descedent comparison for strings and general comparison for others."
+  [x y]
+  (if (and (string? x) (string? y))
+    (.localeCompare y x)
+    (> x y)))
+
 (defn- sort-result [result {:keys [sort-by-column sort-desc?]}]
   (if (some? sort-by-column)
-    (let [comp (if sort-desc? > <)]
+    (let [comp (if sort-desc? locale-compare-desc locale-compare)]
       (sort-by (fn [item]
                  (block/normalize-block (sort-by-fn sort-by-column item)))
                comp
