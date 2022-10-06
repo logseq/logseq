ec7f622989d1ad1794a41883182aae7bc5c20be5
Konstantinos Kaloutas <konstantinos@logseq.com>
2022-10-06 15:30:33 +0300

    fix: remove attributes and protocols

--- src/main/frontend/security.cljs
+++ src/main/frontend/security.cljs
@@ -2,14 +2,7 @@
   "Provide security focused fns like preventing XSS attacks"
   (:require ["dompurify" :as DOMPurify]))
 
-(def sanitization-options (clj->js {:ADD_TAGS ["iframe"]
-                                    :ALLOW_UNKNOWN_PROTOCOLS true
-                                    :ADD_ATTR ["allow"
-                                               "src"
-                                               "allowfullscreen"
-                                               "frameborder"
-                                               "scrolling"
-                                               "target"]}))
+(def sanitization-options (clj->js {:ADD_TAGS ["iframe"]}))
 
 (defn sanitize-html
   [html]
