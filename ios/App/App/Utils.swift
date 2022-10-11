a8e35db725d774dd756836754e730dabf083b944
Andelf <andelf@gmail.com>
2022-10-11 15:30:10 +0800

    chore: run swiftlint

--- ios/App/App/Utils.swift
+++ ios/App/App/Utils.swift
@@ -9,14 +9,14 @@ import Foundation
 import Capacitor
 
 @objc(Utils)
-public class Utils: CAPPlugin  {
-    
+public class Utils: CAPPlugin {
+
     @objc func isZoomed(_ call: CAPPluginCall) {
-        
+
         var isZoomed: Bool {
             return UIScreen.main.scale < UIScreen.main.nativeScale
         }
 
         call.resolve(["isZoomed": isZoomed])
     }
- }
+}
