1d9350e9a3ec4f9c6351295ddde43d0e074d17ce
Matt Lilley <m.k.lilley@gmail.com>
2022-11-14 22:22:28 +0000

    Fix shift+click on all pages launching a new window

--- src/main/frontend/components/page.cljs
+++ src/main/frontend/components/page.cljs
@@ -1050,6 +1050,7 @@
                                              (swap! *checks update idx not))})]
 
                 [:td.name [:a {:on-click (fn [e]
+                                          (.preventDefault e)
                                            (let [repo (state/get-current-repo)]
                                              (when (gobj/get e "shiftKey")
                                                (state/sidebar-add-block!
