b3d3ceae31318bc5d3b59de297e33bbdc8715f5b
Darwis <darwis.e03@gmail.com>
2022-11-16 23:28:02 +0800

    - expose goog.string, goog.dom, goog.obj for helper to interact with javascript - revert evel-string arguments into single arguments, current block are not passed into `eval-string` - use metadata to detect if returned eval-result are hiccup.

--- src/main/frontend/extensions/sci.cljs
+++ src/main/frontend/extensions/sci.cljs
@@ -1,6 +1,9 @@
 (ns frontend.extensions.sci
   (:require [sci.core :as sci]
-            [frontend.util :as util]))
+            [frontend.util :as util]
+            [goog.dom]
+            [goog.object]
+            [goog.string]))
 
 ;; Some helpers
 (def sum (partial apply +))
@@ -9,9 +12,7 @@
   (/ (reduce + coll) (count coll)))
 
 (defn eval-string
-  ([s]
-   (eval-string s {}))
-  ([s ns]
+  [s]
   (try
     (sci/eval-string s {:bindings {'sum sum
                                    'average average
@@ -20,12 +21,10 @@
                                    'isNaN js/isNaN
                                    'log js/console.log
                                    'pprint util/pp-str}
-                        :namespaces ns
-                        :classes {'logseq-api js/logseq.api 'logseq-gp js/logseq.graph_parser :allow :all}
-                        })
+                        :classes {'gstring goog.string 'gdom goog.dom 'gobj goog.object 'logseq-api js/logseq.api 'logseq-gp js/logseq.graph_parser :allow :all}})
     (catch :default e
       (println "Query: sci eval failed:")
-      (js/console.error e)))))
+      (js/console.error e))))
 
 (defn call-fn
   [f & args]
@@ -36,6 +35,7 @@
   [:div
    [:code "Results:"]
    [:div.results.mt-1
-    (let [editor-ns {'block (sci/new-var 'block block)}
-          result (eval-string code {'editor editor-ns})]
+    (let [fn-code (str "(fn [block] " code ")")
+          f (eval-string fn-code)
+          result (call-fn f block)]
       (if (and (vector? result) (:hiccup (meta result))) result [:pre.code (str result)]))]])
