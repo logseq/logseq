cd0be06d479ba34c041915dd1a0241269b07a72f
Peng Xiao <pengxiao@freewheel.tv>
2022-11-16 15:53:29 +0800

    dev: tldraw preview button in demo

--- tldraw/apps/tldraw-logseq/src/components/StatusBar/StatusBar.tsx
+++ tldraw/apps/tldraw-logseq/src/components/StatusBar/StatusBar.tsx
@@ -32,7 +32,7 @@ export const StatusBar = observer(function StatusBar() {
     <div className="tl-statusbar">
       {app.selectedTool.id} | {app.selectedTool.currentState.id}
       <div style={{ flex: 1 }} />
-      <div id="tl-statusbar-anchor" style={{ display: 'flex' }} />
+      <div id="tl-statusbar-anchor" className='flex gap-1' />
     </div>
   )
 })
