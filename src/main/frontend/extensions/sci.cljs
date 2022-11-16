037ec33b403e972abf6782a7aea577aca1459a22
Gabriel Horner <gabriel@logseq.com>
2022-11-16 14:38:36 -0500

    Address review feedback
    - eval-result's additional sci options should not impact other features
    - remove custom-js binding to allow for more general purpose solutions

--- src/main/frontend/extensions/sci.cljs
+++ src/main/frontend/extensions/sci.cljs
@@ -12,30 +12,37 @@
   (/ (reduce + coll) (count coll)))
 
 (defn eval-string
-  [s]
-  (try
-    (sci/eval-string s {:bindings {'sum sum
-                                   'average average
-                                   'parseFloat js/parseFloat
-                                   'custom-js (if (exists? js/customJs) js/customJs (Empty.))
-                                   'isNaN js/isNaN
-                                   'log js/console.log
-                                   'pprint util/pp-str}
-                        :classes {'gstring goog.string 'gdom goog.dom 'gobj goog.object 'logseq-api js/logseq.api 'logseq-gp js/logseq.graph_parser :allow :all}})
-    (catch :default e
-      (println "Query: sci eval failed:")
-      (js/console.error e))))
+  "Second arg is a map of options for sci/eval-string"
+  ([s]
+   (eval-string s {}))
+  ([s options]
+   (try
+     (sci/eval-string s (merge-with merge
+                                    {:bindings {'sum sum
+                                                'average average
+                                                'parseFloat js/parseFloat
+                                                'isNaN js/isNaN
+                                                'log js/console.log
+                                                'pprint util/pp-str}}
+                                    options))
+     (catch :default e
+       (println "Query: sci eval failed:")
+       (js/console.error e)))))
 
 (defn call-fn
   [f & args]
   (apply f args))
 
 (defn eval-result
+  "Evaluate code with sci in a block context"
   [code block]
   [:div
    [:code "Results:"]
    [:div.results.mt-1
-    (let [fn-code (str "(fn [block] " code ")")
-          f (eval-string fn-code)
-          result (call-fn f block)]
-      (if (and (vector? result) (:hiccup (meta result))) result [:pre.code (str result)]))]])
+    (let [result (eval-string code {:bindings {'block block}
+                                    :classes {'logseq-api js/logseq.api
+                                              'logseq-gp js/logseq.graph_parser
+                                              :allow :all}})]
+      (if (and (vector? result) (:hiccup (meta result)))
+        result
+        [:pre.code (str result)]))]])
