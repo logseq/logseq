3fcd53bd959ca061a2e8dff65e0409dc8da139eb
Gabriel Horner <gabriel@logseq.com>
2022-12-05 17:18:32 -0500

    First pass at schema for global config

--- scripts/src/logseq/tasks/malli.clj
+++ scripts/src/logseq/tasks/malli.clj
@@ -3,6 +3,7 @@
   (:require [malli.core :as m]
             [malli.error :as me]
             [frontend.schema.handler.plugin-config :as plugin-config-schema]
+            [frontend.schema.handler.global-config :as global-config-schema]
             [clojure.pprint :as pprint]
             [clojure.edn :as edn]))
 
@@ -13,6 +14,19 @@
                        slurp
                        edn/read-string
                        (m/explain plugin-config-schema/Plugins-edn)
+                       me/humanize)]
+    (do
+      (println "Found errors:")
+      (pprint/pprint errors))
+    (println "Valid!")))
+
+(defn validate-global-config-edn
+  "Validate a global config.edn file"
+  [file]
+  (if-let [errors (->> file
+                       slurp
+                       edn/read-string
+                       (m/explain global-config-schema/Config-edn)
                        me/humanize)]
     (do
       (println "Found errors:")
