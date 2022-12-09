cb4072ba7cfb8010dd1ec92918054138177c2124
charlie <xyhp915@qq.com>
2022-12-09 22:10:22 +0800

    feat(plugins): WIP set up a http server for logseq apis

---
+++ src/electron/electron/server.cljs
@@ -0,0 +1,41 @@
+(ns electron.server
+  (:require ["fastify" :as Fastify]
+            [promesa.core :as p]
+            [electron.logger :as logger]))
+
+(defonce ^:private *win (atom nil))
+(defonce ^:private *server (atom nil))
+
+(defonce HOST "0.0.0.0")
+(defonce PORT 3333)
+
+(defn api-handler
+  [^js req ^js rep]
+  (.send rep #js {:method (.-method req) :msg "Hello Logseq!"}))
+
+(defn close!
+  []
+  (when @*server
+    (logger/debug "[server] closing ...")
+    (-> (.close @*server)
+        (p/then #(reset! *server nil)))))
+
+(defn start!
+  []
+  (-> (p/let [_     (close!)
+              ^js s (Fastify. #js {:logger true})
+              ;; routes
+              _     (doto s
+                      (.get "/apis/:action" api-handler)
+                      (.post "/apis/:action" api-handler))
+              ;; listen port
+              _     (.listen s #js {:host HOST :port PORT})]
+        (reset! *server s))
+      (p/then #(logger/debug "[server] start successfully! :" PORT))
+      (p/catch #(logger/error "[server] start error #" %))))
+
+(defn setup!
+  [^js win]
+  (reset! *win win)
+  (start!)
+  #())
