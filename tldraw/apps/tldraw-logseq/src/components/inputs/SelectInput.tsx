109b4910427cd5e7e8fc9838a57d7c2d722d0d85
Konstantinos <konstantinos@logseq.com>
2022-12-12 12:11:03 +0200

    Feat: Use tooltips instead of titles on whiteboard buttons (#7643)
     Move select and pan to main toolbar
     Create tooltip component
     Add tooltip support to button, toggle, toggle groups, select and color input
     Style shortcuts on primary tools

--- tldraw/apps/tldraw-logseq/src/components/inputs/SelectInput.tsx
+++ tldraw/apps/tldraw-logseq/src/components/inputs/SelectInput.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as Select from '@radix-ui/react-select'
 import { TablerIcon } from '../icons'
+import { Tooltip } from '../Tooltip'
 
 export interface SelectOption {
   value: string
@@ -10,10 +11,11 @@ export interface SelectOption {
 interface SelectInputProps extends React.HTMLAttributes<HTMLElement> {
   options: SelectOption[]
   value: string
+  tooltip?: React.ReactNode
   onValueChange: (value: string) => void
 }
 
-export function SelectInput({ options, value, onValueChange, ...rest }: SelectInputProps) {
+export function SelectInput({ options, tooltip, value, onValueChange, ...rest }: SelectInputProps) {
   const [isOpen, setIsOpen] = React.useState(false)
   return (
     <div {...rest} className="tl-select-input">
@@ -23,14 +25,16 @@ export function SelectInput({ options, value, onValueChange, ...rest }: SelectIn
         value={value}
         onValueChange={onValueChange}
       >
-        <Select.Trigger className="tl-select-input-trigger">
-          <div className="tl-select-input-trigger-value">
-            <Select.Value />
-          </div>
-          <Select.Icon style={{ lineHeight: 1 }}>
-            <TablerIcon name={isOpen ? 'chevron-up' : 'chevron-down'} />
-          </Select.Icon>
-        </Select.Trigger>
+        <Tooltip content={tooltip}>
+          <Select.Trigger className="tl-select-input-trigger">
+            <div className="tl-select-input-trigger-value">
+              <Select.Value />
+            </div>
+            <Select.Icon style={{ lineHeight: 1 }}>
+              <TablerIcon name={isOpen ? 'chevron-up' : 'chevron-down'} />
+            </Select.Icon>
+          </Select.Trigger>
+        </Tooltip>
 
         <Select.Portal className="tl-select-input-portal">
           <Select.Content className="tl-select-input-content">
--- tldraw/apps/tldraw-logseq/src/lib/tools/TextTool.tsx
+++ tldraw/apps/tldraw-logseq/src/lib/tools/TextTool.tsx
@@ -4,6 +4,6 @@ import { TextShape, type Shape } from '../shapes'
 
 export class TextTool extends TLTextTool<TextShape, Shape, TLReactEventMap> {
   static id = 'text'
-  static shortcut = ['6', 't']
+  static shortcut = ['8', 't']
   Shape = TextShape
 }
