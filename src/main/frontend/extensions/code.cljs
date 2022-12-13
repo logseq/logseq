993084666681eccdadf05047388cd7c33a4aadf4
sid597 <siddharthdv77@gmail.com>
2022-12-13 13:28:31 +0530

    use ->> for better readability

--- src/main/frontend/extensions/code.cljs
+++ src/main/frontend/extensions/code.cljs
@@ -260,8 +260,13 @@
                 state)
    :did-update (fn [state]
                  (reset! (:code-options state) (last (:rum/args state)))
-                 (.setValue @(:editor-atom state)
-                            (string/join "" (butlast (:lines (last (:rum/args state))))))
+                 (->> state
+                      :rum/args
+                      last
+                      :lines
+                      butlast
+                      (string/join "")
+                      (.setValue @(:editor-atom state)))
                  state)}
   [state _config id attr code _theme _options]
   [:div.extensions__code
