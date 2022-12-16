c26e4ca4d78113d003a392e2adfbe0f732f70035
Gabriel Horner <gabriel@logseq.com>
2022-12-16 14:53:53 -0500

    PoC that a markdown+edn file can persist
    This works just for a page named 'progress' that exists as progress.md

--- src/main/frontend/handler/property.cljs
+++ src/main/frontend/handler/property.cljs
@@ -3,6 +3,7 @@
   (:require [frontend.state :as state]
             [frontend.db :as db]
             [clojure.string :as string]
+            [frontend.handler.file :as file-handler]
             [logseq.graph-parser.util :as gp-util]
             [logseq.graph-parser.util.page-ref :as page-ref]))
 
@@ -13,10 +14,22 @@
 ;; TODO spec
 (defn set-property-schema!
   [entity key value]
+  ;; schema is a partial schema that is only good enough for diffs/txs
   (let [schema (assoc (:block/property-schema entity) key value)]
     (db/transact! (state/get-current-repo)
-      [{:db/id (:db/id entity)
-        :block/property-schema schema}])))
+                  [{:db/id (:db/id entity)
+                    :block/property-schema schema}])
+    ;; Persist schema here since this is the only to update edn file
+    (let [db-ent (db/entity (:db/id entity))
+          file (get-in db-ent [:block/file :file/path])
+          full-schema (assoc (:block/property-schema db-ent) key value)
+          ;; TODO: Update this so that property entities that don't have pages work
+          edn-file (string/replace-first file #"\.(.*)$" ".edn")
+          repo (state/get-current-repo)
+          file-content (pr-str {:page
+                                {:block/property-schema full-schema}})
+          files [[edn-file file-content]]]
+      (file-handler/alter-files-handler! repo files {} {}))))
 
 (defn validate
   "Check whether the `value` validate against the `schema`."
