7f9ade39319ed6b6b873ab6ccdb1a385b2a268be
Konstantinos Kaloutas <konstantinos@logseq.com>
2022-12-22 19:13:34 +0200

    fix: remove unused imports

--- tldraw/apps/tldraw-logseq/src/lib/shapes/LogseqPortalShape.tsx
+++ tldraw/apps/tldraw-logseq/src/lib/shapes/LogseqPortalShape.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {
-  BoundsUtils,
   delay,
   getComputedColor,
-  TLBounds,
   TLBoxShape,
   TLBoxShapeProps,
   TLResetBoundsInfo,
