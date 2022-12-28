7c6aa5cc28e5d0da636978463d904e81ee95bf24
situ2001 <yongcong2001@outlook.com>
2022-12-28 22:56:42 +0800

    fix: full search result should not be only displayed on electron app

--- src/main/frontend/components/search.cljs
+++ src/main/frontend/components/search.cljs
@@ -378,7 +378,7 @@
        :on-shift-chosen #(search-on-shift-chosen repo search-q %)
        :item-render #(search-item-render search-q %)
        :on-chosen-open-link #(search-on-chosen-open-link repo search-q %)})
-     (when (and has-more? (util/electron?) (not all?))
+     (when (and has-more? (not all?))
        [:div.px-2.py-4.search-more
         [:a.text-sm.font-medium {:href (rfe/href :search {:q search-q})
                                  :on-click (fn []
