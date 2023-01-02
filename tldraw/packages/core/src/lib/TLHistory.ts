245a52f87d58b25e1759615f13e7b9b8965657b6
Tienson Qin <tiensonqin@gmail.com>
2023-01-02 12:23:01 +0800

    add deserialize back

--- tldraw/packages/core/src/lib/TLHistory.ts
+++ tldraw/packages/core/src/lib/TLHistory.ts
@@ -57,4 +57,82 @@ export class TLHistory<S extends TLShape = TLShape, K extends TLEventMap = TLEve
     const ShapeClass = this.app.getShapeClass(serializedShape.type)
     return new ShapeClass(serializedShape)
   }
+
+  @action deserialize = (snapshot: TLDocumentModel) => {
+    transaction(() => {
+      const { pages } = snapshot
+      const wasPaused = this.isPaused
+      this.pause()
+
+      const newSelectedIds = [...this.app.selectedIds]
+
+      try {
+        const pagesMap = new Map(this.app.pages)
+        const pagesToAdd: TLPage<S, K>[] = []
+
+        for (const serializedPage of pages) {
+          const page = pagesMap.get(serializedPage.id)
+          if (page !== undefined) {
+            // Update the page
+            const shapesMap = new Map(page.shapes.map(shape => [shape.props.id, shape]))
+            const shapesToAdd: S[] = []
+            for (const serializedShape of serializedPage.shapes) {
+              const shape = shapesMap.get(serializedShape.id)
+              if (shape !== undefined) {
+                // Update the shape
+                if (shape.nonce !== serializedShape.nonce) {
+                  shape.update(serializedShape, true)
+                  shape.nonce = serializedShape.nonce!
+                  shape.setLastSerialized(serializedShape)
+                }
+                shapesMap.delete(serializedShape.id)
+              } else {
+                // Create the shape
+                shapesToAdd.push(this.instantiateShape(serializedShape))
+              }
+            }
+
+            // Do not remove shapes if currently state is creating or editing
+            // Any shapes remaining in the shapes map need to be removed
+            if (shapesMap.size > 0 && !this.app.selectedTool.isInAny('creating', 'editingShape')) {
+              page.removeShapes(...shapesMap.values())
+            }
+            // Add any new shapes
+            if (shapesToAdd.length > 0) page.addShapes(...shapesToAdd)
+            // Remove the page from the map
+            pagesMap.delete(serializedPage.id)
+            page.updateBindings(serializedPage.bindings)
+            page.nonce = serializedPage.nonce ?? 0
+          } else {
+            // Create the page
+            const { id, name, shapes, bindings, nonce } = serializedPage
+            pagesToAdd.push(
+              new TLPage(this.app, {
+                id,
+                name,
+                nonce,
+                bindings,
+                shapes: shapes.map(serializedShape => {
+                  return this.instantiateShape(serializedShape)
+                }),
+              })
+            )
+          }
+        }
+
+        // Add any new pages
+        if (pagesToAdd.length > 0) this.app.addPages(pagesToAdd)
+
+        // Any pages remaining in the pages map need to be removed
+        if (pagesMap.size > 0) this.app.removePages(Array.from(pagesMap.values()))
+
+        this.app.setSelectedShapes(newSelectedIds).setErasingShapes([])
+      } catch (e) {
+        console.warn(e)
+      }
+
+      // Resume the history if not originally paused
+      if (!wasPaused) this.resume()
+    })
+  }
 }
