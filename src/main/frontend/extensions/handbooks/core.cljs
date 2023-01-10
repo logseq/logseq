11a94cc2edbf111416304e31e55298917737dfed
charlie <xyhp915@qq.com>
2023-01-10 15:10:03 +0800

    feat(ui): WIP handbooks pane

--- src/main/frontend/extensions/handbooks/core.cljs
+++ src/main/frontend/extensions/handbooks/core.cljs
@@ -2,6 +2,38 @@
   (:require [rum.core :as rum]
             [frontend.ui :as ui]))
 
+(rum/defc pane-dashboard
+  []
+
+  [:div.pane.dashboard-pane
+   [:h2 "Popular topics"]
+   [:div.topics-list
+    (take
+     3 (repeat
+        [:div.topic-card.flex
+         [:div.l "Cover"]
+         [:div.r.flex.flex-col
+          [:strong "Switching your notetaking process"]
+          [:span "What makes Logseq different from your previous tools?"]]]))]
+
+   [:h2 "Help categories"]
+   [:div.categories-list
+    [:div.category-card.bg-red-600
+     [:strong "Get started here"]
+     [:span "5 onboarding articles"]]
+
+    [:div.category-card.bg-green-600
+     [:strong "Get started here"]
+     [:span "5 onboarding articles"]]
+
+    [:div.category-card.bg-yellow-600
+     [:strong "Get started here"]
+     [:span "5 onboarding articles"]]
+
+    [:div.category-card.bg-indigo-600
+     [:strong "Get started here"]
+     [:span "5 onboarding articles"]]]])
+
 (rum/defc content
   []
 
@@ -11,38 +43,12 @@
 
    [:div.search.relative
     [:span.icon.absolute.opacity-90
-     {:style {:top 7 :left 7}}
+     {:style {:top 6 :left 7}}
      (ui/icon "search" {:size 12})]
     [:input {:placeholder "Search"
-             :auto-focus   true}]]
+             :auto-focus  true}]]
 
-   [:div.bd
-    [:h2 "Popular topics"]
-    [:div.topics-list
-     (take
-      3 (repeat
-         [:div.topic-card.flex
-          [:div.l "Cover"]
-          [:div.r.flex.flex-col
-           [:strong "Switching your notetaking process"]
-           [:span "What makes Logseq different from your previous tools?"]]]))]
-
-    [:h2 "Help categories"]
-    [:div.categories-list
-     [:div.category-card.bg-red-600
-      [:strong "Get started here"]
-      [:span "5 onboarding articles"]]
-
-     [:div.category-card.bg-green-600
-      [:strong "Get started here"]
-      [:span "5 onboarding articles"]]
-
-     [:div.category-card.bg-yellow-600
-      [:strong "Get started here"]
-      [:span "5 onboarding articles"]]
-
-     [:div.category-card.bg-indigo-600
-      [:strong "Get started here"]
-      [:span "5 onboarding articles"]]]]])
+   ;; entry pane
+   (pane-dashboard)])
 
 
