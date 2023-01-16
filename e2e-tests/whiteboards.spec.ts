e2384af7d1b9b0397fd626a556cb44c80503e32c
Konstantinos Kaloutas <konstantinos@logseq.com>
2023-01-16 16:33:15 +0200

    Revert "test e2e"
    This reverts commit 897a78701328fd7ff634388753d03c627513aca0.

--- e2e-tests/whiteboards.spec.ts
+++ e2e-tests/whiteboards.spec.ts
@@ -42,47 +42,34 @@ test('newly created whiteboard should have a default title', async ({ page }) =>
   )
 })
 
-test('set whiteboard title', async ({ page }) => {
-  const title = 'my-whiteboard'
+// test('set whiteboard title', async ({ page }) => {
+//   const title = 'my-whiteboard'
 
-  await page.click('.whiteboard-page-title')
-  await page.fill('.whiteboard-page-title input', title)
-  const canvas = await page.waitForSelector('.logseq-tldraw')
-  await canvas.click({
-    position: {
-      x: 100,
-      y: 100,
-    },
-  })
+//   await page.click('.whiteboard-page-title')
+//   await page.fill('.whiteboard-page-title input', title)
+//   await page.keyboard.press('Enter')
+//   await expect(page.locator('.whiteboard-page-title .title')).toContainText(
+//     title
+//   )
+// })
 
-  await expect(page.locator('.whiteboard-page-title .title')).toContainText(
-    title
-  )
-})
+// test('update whiteboard title', async ({ page }) => {
+//   const title = 'my-whiteboard'
 
-test('update whiteboard title', async ({ page }) => {
-  const title = 'my-whiteboard'
+//   await page.click('.whiteboard-page-title')
+//   await page.fill('.whiteboard-page-title input', title + '-2')
+//   await page.keyboard.press('Enter')
 
-  await page.click('.whiteboard-page-title')
-  await page.fill('.whiteboard-page-title input', title + '-2')
-  const canvas = await page.waitForSelector('.logseq-tldraw')
-  await canvas.click({
-    position: {
-      x: 100,
-      y: 100,
-    },
-  })
+//   // Updating non-default title should pop up a confirmation dialog
+//   await expect(page.locator('.ui__confirm-modal >> .headline')).toContainText(
+//     `Do you really want to change the page name to “${title}-2”?`
+//   )
 
-  // Updating non-default title should pop up a confirmation dialog
-  await expect(page.locator('.ui__confirm-modal >> .headline')).toContainText(
-    `Do you really want to change the page name to “${title}-2”?`
-  )
-
-  await page.click('.ui__confirm-modal button')
-  await expect(page.locator('.whiteboard-page-title .title')).toContainText(
-    title + '-2'
-  )
-})
+//   await page.click('.ui__confirm-modal button')
+//   await expect(page.locator('.whiteboard-page-title .title')).toContainText(
+//     title + '-2'
+//   )
+// })
 
 test('draw a rectangle', async ({ page }) => {
   const canvas = await page.waitForSelector('.logseq-tldraw')
@@ -146,14 +133,9 @@ test('quick add another whiteboard', async ({ page }) => {
 
   await page.click('.whiteboard-page-title')
   await page.fill('.whiteboard-page-title input', 'my-whiteboard-3')
-  const canvas = await page.waitForSelector('.logseq-tldraw')
-  await canvas.click({
-    position: {
-      x: 100,
-      y: 100,
-    },
-  })
+  await page.keyboard.press('Enter')
 
+  const canvas = await page.waitForSelector('.logseq-tldraw')
   await canvas.dblclick({
     position: {
       x: 100,
