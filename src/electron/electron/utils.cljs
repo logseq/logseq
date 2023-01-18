63ec9800ffab14cec77cb594ee477c660007eaee
sallto <thomas@saller.com.de>
2023-01-18 10:53:08 +0100

    fix: move primary directory to ~/.config/Logseq

--- src/electron/electron/utils.cljs
+++ src/electron/electron/utils.cljs
@@ -37,10 +37,11 @@
 
 (defn get-ls-dotdir-root
   []
-  (let [lg-dir (path/join (.getPath app "home") ".logseq")]
-    (if-not (fs/existsSync lg-dir)
-      (do (fs/mkdirSync lg-dir) lg-dir)
-      lg-dir)))
+  (let [legacy-dir (path/join (.getPath app "home") ".logseq")
+        new-dir (path/join (.getPath app "userData") "Logseq")
+        exists? #(when (fs/existsSync %1) %1)]
+    (or (exists? legacy-dir) (exists? new-dir) 
+        (do (fs/mkdirSync new-dir) new-dir))))
 
 (defn get-ls-default-plugins
   []
