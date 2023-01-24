d2aab9f4f61dfb7384c2e106508ea2ab5bc2971b
sallto <thomas@saller.com.de>
2023-01-24 17:12:39 +0100

    fix: Copy button correctly copies images

--- src/main/frontend/components/block.cljs
+++ src/main/frontend/components/block.cljs
@@ -272,7 +272,8 @@
                    (reset! *resizing-image? false)
                    state)}
   [state config title src metadata full_text local?]
-  (let [size (get state ::size)]
+  (let [size (get state ::size)
+        image-src (string/replace src #"^assets://" "")]
     (ui/resize-provider
      (ui/resize-consumer
       (if-not (mobile-util/native-ios?)
@@ -301,11 +302,10 @@
        [:img.rounded-sm.relative
         (merge
          {:loading "lazy"
-          :src     src
+          :src     image-src
           :title   title}
          metadata)]
        [:.asset-overlay]
-       (let [image-src (string/replace src #"^assets://" "")]
          [:.asset-action-bar {:aria-hidden "true"}
           ;; the image path bar
           (when (util/electron?)
@@ -361,7 +361,7 @@
              :on-mouse-down util/stop
              :on-click open-lightbox}
 
-            (ui/icon "maximize")]]])]))))
+            (ui/icon "maximize")]]]]))))
 
 (rum/defc audio-cp [src]
   [:audio {:src src
