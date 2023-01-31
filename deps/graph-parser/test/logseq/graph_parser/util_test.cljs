ec068fb9217b8a49b8e5e3b9d87e114411c1383f
sallto <thomas@saller.com.de>
2023-01-31 22:27:01 +0100

    feat: add tests for extraction of file extension

--- deps/graph-parser/test/logseq/graph_parser/util_test.cljs
+++ deps/graph-parser/test/logseq/graph_parser/util_test.cljs
@@ -12,3 +12,16 @@
        "4"         false
        "foo bar"   false
        "`property" false))
+
+(deftest extract-file-extension? 
+  (are [x y]
+       (= (gp-util/path->file-ext x) y)
+    "foo.bar" "bar"
+    "foo"     nil
+    "foo.bar.baz" "baz"
+    "../assets/audio.mp3" "mp3"
+    "/root/Documents/audio.mp3" "mp3"
+    "C:\\Users\\foo\\Documents\\audio.mp3" "mp3"
+    "/root/Documents/audio" nil
+    "/root/Documents/audio." nil
+    "special/characters/aäääöüß.7z" "7z"))
