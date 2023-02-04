ca31cc7381ea5b8b1d6ddce2209af4da2047ac7e
situ2001 <yongcong2001@outlook.com>
2023-02-04 21:44:39 +0800

    Revert "fix(android): theme "system" not working"
    This reverts commit 6fca332dd5e6745cfac1f88cd24daca5bcd3ef2e.

--- android/app/src/main/java/com/logseq/app/MainActivity.java
+++ android/app/src/main/java/com/logseq/app/MainActivity.java
@@ -1,11 +1,8 @@
 package com.logseq.app;
 
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.os.Build;
 import android.os.Bundle;
 import android.webkit.ValueCallback;
-import android.webkit.WebSettings;
 
 import com.getcapacitor.BridgeActivity;
 
@@ -13,21 +10,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends BridgeActivity {
-    void setDarkModeToWebView() {
-        // Android "fix" for enabling dark mode
-        // Reference: https://github.com/ionic-team/capacitor/discussions/1978
-        int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-        WebSettings webSettings = this.bridge.getWebView().getSettings();
-        // As of Android 10, you can simply force the dark mode
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
-                webSettings.setForceDark(WebSettings.FORCE_DARK_ON);
-            } else {
-                webSettings.setForceDark(WebSettings.FORCE_DARK_OFF);
-            }
-        }
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(FolderPicker.class);
@@ -46,24 +28,6 @@ public class MainActivity extends BridgeActivity {
             }
         }, 5000);
 
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        setDarkModeToWebView();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        setDarkModeToWebView();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        setDarkModeToWebView();
     }
 
     @Override
