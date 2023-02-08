39a50c312c42496a1e811940797d172fa497e1e1
Konstantinos Kaloutas <konstantinos@logseq.com>
2023-02-08 13:49:34 +0200

    chore: add more heading tests

--- e2e-tests/headings.spec.ts
+++ e2e-tests/headings.spec.ts
@@ -1,22 +1,37 @@
 import { expect } from '@playwright/test'
 import { test } from './fixtures'
-import { createRandomPage, enterNextBlock, editFirstBlock } from './utils'
+import { createRandomPage, editFirstBlock, newInnerBlock } from './utils'
 
-test('set heading to 1 using context menu', async ({ page }) => {
+test('set heading to 1 using', async ({ page }) => {
     await createRandomPage(page)
 
     await page.type('textarea >> nth=0', 'foo')
 
-    await enterNextBlock(page)
+    await page.keyboard.press('Escape', { delay: 50 })
 
     await page.locator('span.bullet-container >> nth=0').click({button: "right"})
 
     await page.locator('#custom-context-menu .to-heading-button[title="Heading 1"]').click()
 
+    await editFirstBlock(page)
+    await page.waitForTimeout(500)
+
+    expect(await page.inputValue('textarea >> nth=0')).toBe('# foo')
+
+    await page.keyboard.press('Escape', { delay: 50 })
+
     expect(await page.locator('.ls-block .block-content >> nth=0').innerHTML()).toContain('<h1>foo</h1>')
 })
 
-test('set heading to 2 using context menu', async ({ page }) => {
+test('remove heading', async ({ page }) => {
+  await page.locator('span.bullet-container >> nth=0').click({button: "right"})
+
+  await page.locator('#custom-context-menu .to-heading-button[title="Remove heading"]').click()
+
+  expect(await page.locator('.ls-block .block-content >> nth=0').innerHTML()).toContain('foo')
+})
+
+test('set heading to 2', async ({ page }) => {
   await page.locator('span.bullet-container >> nth=0').click({button: "right"})
 
   await page.locator('#custom-context-menu .to-heading-button[title="Heading 2"]').click()
@@ -24,18 +39,40 @@ test('set heading to 2 using context menu', async ({ page }) => {
   expect(await page.locator('.ls-block .block-content >> nth=0').innerHTML()).toContain('<h2>foo</h2>')
 })
 
-test('set heading to auto using context menu', async ({ page }) => {
+test('switch to auto heading', async ({ page }) => {
   await page.locator('span.bullet-container >> nth=0').click({button: "right"})
 
   await page.locator('#custom-context-menu .to-heading-button[title="Auto heading"]').click()
 
+  await editFirstBlock(page)
+  await page.waitForTimeout(500)
+
+  expect(await page.inputValue('textarea >> nth=0')).toBe('foo')
+
+  await page.keyboard.press('Escape', { delay: 50 })
+
   expect(await page.locator('.ls-block .block-content >> nth=0').innerHTML()).toContain('<h2>foo</h2>')
 })
 
-test('remove heading using context menu', async ({ page }) => {
-  await page.locator('span.bullet-container >> nth=0').click({button: "right"})
+test('set heading of nested block to auto', async ({ page }) => {
+  await newInnerBlock(page)
+  await page.waitForTimeout(500)
 
-  await page.locator('#custom-context-menu .to-heading-button[title="Remove heading"]').click()
+  await page.type('textarea >> nth=0', 'bar')
 
-  expect(await page.locator('.ls-block .block-content >> nth=0').innerHTML()).toContain('foo')
+  await page.keyboard.press("Tab")
+
+  await page.keyboard.press('Escape', { delay: 50 })
+
+  await page.locator('span.bullet-container >> nth=1').click({button: "right"})
+
+  await page.locator('#custom-context-menu .to-heading-button[title="Auto heading"]').click()
+
+  expect(await page.locator('.ls-block .block-content >> nth=1').innerHTML()).toContain('<h3>bar</h3>')
+})
+
+test('view nested block on a dedicated page', async ({ page }) => {
+  await page.locator('span.bullet-container >> nth=1').click()
+
+  expect(await page.locator('.ls-block .block-content >> nth=0').innerHTML()).toContain('<h1>bar</h1>')
 })
