9d2055f8b94018c9d3e55b7ca43b8a00e6ee448b
Junyi Du <junyidu.cn@gmail.com>
2023-02-10 10:54:42 +0800

    enhance: re-index performance

--- deps/graph-parser/src/logseq/graph_parser/block.cljs
+++ deps/graph-parser/src/logseq/graph_parser/block.cljs
@@ -248,7 +248,7 @@
                               (assoc :repeated? true))))))]
     (apply merge m)))
 
-(defn convert-page-if-journal
+(defn- convert-page-if-journal-impl
   "Convert journal file name to user' custom date format"
   [original-page-name date-formatter]
   (when original-page-name
@@ -258,6 +258,8 @@
        (let [original-page-name (date-time-util/int->journal-title day date-formatter)]
          [original-page-name (gp-util/page-name-sanity-lc original-page-name) day])
        [original-page-name page-name day]))))
+
+(def convert-page-if-journal (memoize convert-page-if-journal-impl))
 
 ;; TODO: refactor
 (defn page-name->map
