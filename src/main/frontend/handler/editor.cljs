ebea4aa0113ae937ce6ad110351ff744b30dc26a
ksqsf <i@ksqsf.moe>
2023-02-13 13:30:02 +0900

    enhance: recognize refs as links when inserting links from selection

--- src/main/frontend/handler/editor.cljs
+++ src/main/frontend/handler/editor.cljs
@@ -131,7 +131,7 @@
      (let [{:keys [selection-start selection-end format selection value edit-id input]} m
            cur-pos (cursor/pos input)
            empty-selection? (= selection-start selection-end)
-           selection-link? (and selection (gp-util/url? selection))
+           selection-link? (and selection (gp-util/link? selection))
            [content forward-pos] (cond
                                    empty-selection?
                                    (config/get-empty-link-and-forward-pos format)
