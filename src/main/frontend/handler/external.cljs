8fcf1885e28af4cd497e07bd90c924d6622bd7ed
Gabriel Horner <gabriel@logseq.com>
2023-02-20 14:58:52 -0500

    Remove redundant uses of :db option

--- src/main/frontend/handler/external.cljs
+++ src/main/frontend/handler/external.cljs
@@ -88,7 +88,7 @@
           ;; add empty pos metadata
           parsed-blocks (map (fn [b] [b {}]) parsed-blocks)
           parsed-blocks (->>
-                         (block/extract-blocks parsed-blocks "" :markdown {:db (db/get-db repo)})
+                         (block/extract-blocks parsed-blocks "" :markdown {})
                          (mapv editor/wrap-parse-block))
           page-name (:title headers)]
       (when (not (db/page-exists? page-name))
