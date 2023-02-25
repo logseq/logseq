7731659cd4472fee9c8119379965a927e984d521
sallto <thomas@saller.com.de>
2023-02-25 22:51:03 +0100

    fix(dev): remove import to pass lint checks

--- src/main/frontend/components/page_menu.cljs
+++ src/main/frontend/components/page_menu.cljs
@@ -10,7 +10,6 @@
             [frontend.state :as state]
             [frontend.ui :as ui]
             [frontend.util :as util]
-            [frontend.util.url :as url-util]
             [frontend.util.page :as page-util]
             [frontend.handler.shell :as shell]
             [frontend.mobile.util :as mobile-util]
