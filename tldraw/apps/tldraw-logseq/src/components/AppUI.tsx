01200cc4285a7e6204b9422d87d016732bd91750
Konstantinos Kaloutas <konstantinos@logseq.com>
2023-03-09 16:23:57 +0200

    refactor isDev

--- tldraw/apps/tldraw-logseq/src/components/AppUI.tsx
+++ tldraw/apps/tldraw-logseq/src/components/AppUI.tsx
@@ -3,14 +3,13 @@ import { ActionBar } from './ActionBar'
 import { DevTools } from './Devtools'
 import { PrimaryTools } from './PrimaryTools'
 import { StatusBar } from './StatusBar'
-
-const isDev = window?.logseq?.api?.get_state_from_store?.('ui/developer-mode?') || process.env.NODE_ENV === 'development'
+import { isDev } from '@tldraw/core'
 
 export const AppUI = observer(function AppUI() {
   return (
     <>
-      {isDev && <StatusBar />}
-      {isDev && <DevTools />}
+      {isDev() && <StatusBar />}
+      {isDev() && <DevTools />}
       <PrimaryTools />
       <ActionBar />
     </>
