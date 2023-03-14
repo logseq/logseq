caa4bfa027fab2b685770578ab996beeceb64685
Konstantinos Kaloutas <konstantinos@logseq.com>
2023-03-14 12:40:28 +0200

    fix: get editing id

--- src/main/frontend/util/page.cljs
+++ src/main/frontend/util/page.cljs
@@ -25,8 +25,8 @@
   []
   (if (state/editing?)
     (get-in (first (state/get-editor-args)) [:block :block/page :db/id])
-    (when (not (state/get-edit-input-id))
-      (get-current-page-id))))
+    (or (state/get-edit-input-id) ; if there is an edit-input-id set, we are probably still on editing mode 
+        (get-current-page-id))))
 
 (defn get-page-file-path
   "Gets the file path of a page. If no page is given, detects the current page.
