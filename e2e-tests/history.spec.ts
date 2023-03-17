db44e5c5f797e88dfa87474a0fe9d3b884ae10ed
Konstantinos Kaloutas <konstantinos@logseq.com>
2023-03-17 11:26:18 +0200

    chore: add tests

--- e2e-tests/history.spec.ts
+++ e2e-tests/history.spec.ts
@@ -2,7 +2,23 @@ import { expect } from '@playwright/test'
 import { test } from './fixtures'
 import { createRandomPage, modKey, searchAndJumpToPage } from './utils'
 
-test('undo/redo stack should be different for each page', async ({ page, block }) => {
+test('undo/redo on a page should work as expected', async ({ page, block }) => {
+  const page1 = await createRandomPage(page)
+
+  await block.mustType('text 1')
+  await page.waitForTimeout(500) // Wait for 500ms autosave period to expire
+  await expect(page.locator('text="text 1"')).toHaveCount(1)
+
+  await page.keyboard.press(modKey + '+z')
+  await page.waitForTimeout(100)
+  await expect(page.locator('text="text 1"')).toHaveCount(0)
+
+  await page.keyboard.press(modKey + '+Shift+z')
+  await page.waitForTimeout(100)
+  await expect(page.locator('text="text 1"')).toHaveCount(1)
+})
+
+test('should have an isolated undo/redo stack for each page', async ({ page, block }) => {
   const page1 = await createRandomPage(page)
 
   await block.mustType('text 1')
@@ -37,7 +53,7 @@ test('undo/redo of a page should be not be allowed on block context', async ({ p
   await expect(page.locator('text="text 1"')).toHaveCount(1)
 })
 
-test('undo/redo of a block should allowed on page context', async ({ page, block }) => {
+test('undo/redo of a block should be allowed on page context', async ({ page, block }) => {
   const page1 = await createRandomPage(page)
 
   await page.locator('span.bullet-container >> nth=0').click()
