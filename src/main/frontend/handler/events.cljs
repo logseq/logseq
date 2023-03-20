527b1544a4ce822b2e94efe985d5fcd598f5986b
charlie <xyhp915@qq.com>
2023-03-20 15:16:32 +0800

    enhance(login): polish modal ui for mobile

--- src/main/frontend/handler/events.cljs
+++ src/main/frontend/handler/events.cljs
@@ -476,7 +476,8 @@
       (reset! util/keyboard-height keyboard-height)
       (set! (.. main-node -style -marginBottom) (str keyboard-height "px"))
       (when-let [^js html (js/document.querySelector ":root")]
-        (.setProperty (.-style html) "--ls-native-kb-height" (str keyboard-height "px")))
+        (.setProperty (.-style html) "--ls-native-kb-height" (str keyboard-height "px"))
+        (.add (.-classList html) "has-mobile-keyboard"))
       (when-let [left-sidebar-node (gdom/getElement "left-sidebar")]
         (set! (.. left-sidebar-node -style -bottom) (str keyboard-height "px")))
       (when-let [right-sidebar-node (gdom/getElementByClass "sidebar-item-list")]
@@ -498,7 +499,8 @@
       (state/set-state! :mobile/show-recording-bar? false))
     (when (mobile-util/native-ios?)
       (when-let [^js html (js/document.querySelector ":root")]
-        (.removeProperty (.-style html) "--ls-native-kb-height"))
+        (.removeProperty (.-style html) "--ls-native-kb-height")
+        (.remove (.-classList html) "has-mobile-keyboard"))
       (when-let [card-preview-el (js/document.querySelector ".cards-review")]
         (set! (.. card-preview-el -style -marginBottom) "0px"))
       (when-let [card-preview-el (js/document.querySelector ".encryption-password")]
