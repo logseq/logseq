19a1aa1e34ab3e9be9004f583106c83f66a06212
Konstantinos Kaloutas <konstantinos@logseq.com>
2023-04-19 19:17:44 +0300

    Revert "fix: portal height"
    This reverts commit 5a7895fbc5a34640e135252a811270168e9ba38b.

--- tldraw/apps/tldraw-logseq/src/components/Button/CircleButton.tsx
+++ tldraw/apps/tldraw-logseq/src/components/Button/CircleButton.tsx
@@ -1,22 +1,40 @@
+import React from 'react'
 import { TablerIcon } from '../icons'
 
 export const CircleButton = ({
+  active,
   style,
   icon,
+  otherIcon,
   onClick,
 }: {
+  active?: boolean
   style?: React.CSSProperties
   icon: string
+  otherIcon?: string
   onClick: () => void
 }) => {
+  const [recentlyChanged, setRecentlyChanged] = React.useState(false)
+
+  React.useEffect(() => {
+    setRecentlyChanged(true)
+    const timer = setTimeout(() => {
+      setRecentlyChanged(false)
+    }, 500)
+    return () => clearTimeout(timer)
+  }, [active])
+
   return (
     <button
+      data-active={active}
+      data-recently-changed={recentlyChanged}
       data-html2canvas-ignore="true"
       style={style}
       className="tl-circle-button"
       onPointerDown={onClick}
     >
-      <div className="tl-circle-button-icons-wrapper">
+      <div className="tl-circle-button-icons-wrapper" data-icons-count={otherIcon ? 2 : 1}>
+        {otherIcon && <TablerIcon name={otherIcon} />}
         <TablerIcon name={icon} />
       </div>
     </button>
