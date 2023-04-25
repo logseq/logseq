bcdc4fc803c1c8aa6efac09ada9cde0ff62e0118
Konstantinos Kaloutas <konstantinos@logseq.com>
2023-04-25 15:18:33 +0300

    Fix: search in page position

--- src/main/frontend/components/container.cljs
+++ src/main/frontend/components/container.cljs
@@ -511,9 +511,6 @@
       {:tabIndex "-1"
        :data-is-margin-less-pages margin-less-pages?}
 
-      (when (util/electron?)
-        (find-in-page/search))
-
       (when show-action-bar?
         (action-bar/action-bar))
 
@@ -782,6 +779,9 @@
                         :route-match    route-match
                         :default-home   default-home
                         :new-block-mode new-block-mode})
+
+        (when (util/electron?)
+          (find-in-page/search))
 
         (main {:route-match         route-match
                :margin-less-pages?  margin-less-pages?
