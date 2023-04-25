8ed3004732140f2dd6d9e5f38bd4d4a2dc9275c4
Sergey Kolesnik <sergey.s.kolesnik@gmail.com>
2023-04-25 23:21:27 +0300

    rework without new signature

--- src/main/frontend/components/block.cljs
+++ src/main/frontend/components/block.cljs
@@ -933,14 +933,12 @@
 
 (defn inline-text
   ([format v]
-   (inline-text {} format v false))
+   (inline-text {} format v))
   ([config format v]
-   (inline-text {} format v false))
-  ([config format v no-margin?]
    (when (string? v)
      (let [inline-list (gp-mldoc/inline->edn v (gp-mldoc/default-config format))]
        [:div.inline
-         (when (not no-margin?) {:class "mr-1"})
+         (when (get config :add-margin? true) {:class "mr-1"})
          (map-inline config inline-list)]))))
 
 (defn- render-macro
@@ -955,7 +953,7 @@
        (if (and (not paragraph?)
                 (mldoc/block-with-title? (ffirst ast)))
          (markup-elements-cp (assoc config :block/format format) ast)
-         (inline-text {} format macro-content true)))
+         (inline-text {:add-margin? false} format macro-content)))
      [:span.warning {:title (str "Unsupported macro name: " name)}
       (macro->text name arguments)])])
 
