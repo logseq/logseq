e54098b1ce7a3a073b4d3a3a7b62df58373a39b1
Bad3r <bad3r@pm.me>
2023-04-26 09:47:18 -0400

    Revert "enhance: Use similar syntax for consistency"
    This reverts commit f85d380ad9afd6f1103157da6f17e9761b59902e.

--- src/main/frontend/config.cljs
+++ src/main/frontend/config.cljs
@@ -211,9 +211,9 @@
   (let [format (or format (keyword (state/get-preferred-format)))]
     (case format
       :org
-      "_"
-      :markdown
-      ["<u>" "</u>"])))
+      {:open "_" :close "_"}
+      :markdown ;; no underline for markdown by default (use HTML syntax instead)
+      {:open "<u>" :close "</u>"})))
 
 (defn get-strike-through
   [format]
