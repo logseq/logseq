7f9c8ad3aeb07f8e5923f1f5e3b7d3d5b2b1f6e3
Junyi Du <junyidu.cn@gmail.com>
2023-05-12 14:31:56 +0800

    ui: version control text box update

--- src/main/frontend/components/settings.cljs
+++ src/main/frontend/components/settings.cljs
@@ -667,14 +667,20 @@
   []
   [:div.panel-wrap
    [:div.text-sm.my-4
+    (ui/admonition
+     :tip
+     [:p "If you have Logseq Sync enabled, you can view a page's edit history directly. This section is for tech-savvy only."])
+    [:span.text-sm.opacity-50.my-4 
+     "To view page's edit history, click the three horizontal dots in the top-right corner and select \"View page history\"."]
+    [:br][:br]
     [:span.text-sm.opacity-50.my-4
-     "You can view a page's edit history by clicking the three horizontal dots "
-     "in the top-right corner and selecting \"View page history\". "
-     "Logseq uses "]
+     "For professional users, Logseq also supports using "]
     [:a {:href "https://git-scm.com/" :target "_blank"}
      "Git"]
     [:span.text-sm.opacity-50.my-4
-     " for version control."]]
+     " for version control."]
+    [:span.text-sm.opacity-50.my-4
+     "General issues about Git usage is not supported by the Logseq team and should be used at own risk"]]
    [:br]
    (switch-git-auto-commit-row t)
    (git-auto-commit-seconds t)
