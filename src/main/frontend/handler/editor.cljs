945a5326316a8f9c73be0ceec3df5cf65681cabb
Tienson Qin <tiensonqin@gmail.com>
2023-05-15 15:31:44 +0800

    remove unnecessary save-current-block

--- src/main/frontend/handler/editor.cljs
+++ src/main/frontend/handler/editor.cljs
@@ -2448,7 +2448,6 @@
             (profile
              "Insert block"
              (outliner-tx/transact! {:outliner-op :insert-blocks}
-               (save-current-block!)
                (insert-new-block! state)))))))))
 
 (defn- inside-of-single-block
