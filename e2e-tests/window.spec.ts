8c51ed214f96e79e968b90f0aa922a52f9af80ea
Konstantinos Kaloutas <konstantinos@logseq.com>
2023-05-22 12:41:31 +0300

    fluxbox test

--- e2e-tests/window.spec.ts
+++ e2e-tests/window.spec.ts
@@ -1,21 +1,20 @@
 import { expect } from '@playwright/test'
 import { test } from './fixtures'
-
 import { IsMac } from './utils';
 
 if (!IsMac) {
-    test('Window should not be maximized on first launch', async ({ page }) => {
+    test('Window should not be maximized on first launch', async ({ page, app }) => {
         await expect(page.locator('.window-controls .maximize-toggle.maximize')).toHaveCount(1)
     })
 
     test('Window should be maximized and icon should change on maximize-toggle click', async ({ page }) => {
-        await page.click('.window-controls .maximize-toggle')
+        await page.click('.window-controls .maximize-toggle.maximize')
 
         await expect(page.locator('.window-controls .maximize-toggle.restore')).toHaveCount(1)
     })
 
     test('Window should be restored and icon should change on maximize-toggle click', async ({ page }) => {
-        await page.click('.window-controls .maximize-toggle')
+        await page.click('.window-controls .maximize-toggle.restore')
 
         await expect(page.locator('.window-controls .maximize-toggle.maximize')).toHaveCount(1)
     })
