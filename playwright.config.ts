f260e9c370e3648a8421889dccf3ac32c8a949bf
Bad3r <bad3r@protonmail.com>
2023-05-23 14:20:01 +0000

    Revert playwright config change

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,6 @@ const config: PlaywrightTestConfig = {
   testDir: './e2e-tests',
   maxFailures: 1,
   workers: 1, // NOTE: must be 1 for now, otherwise tests will fail.
-  retries: process.env.CI ? 2 : 0, // Retry on CI only.
   use: {
     screenshot: 'only-on-failure',
   },
