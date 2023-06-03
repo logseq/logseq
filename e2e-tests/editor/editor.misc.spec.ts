9ecf84bd14981956128b540009a12839ffc7d362
Bad3r <bad3r@pm.me>
2023-06-03 14:13:03 -0400

    enhance: handle test timeout in CI

--- e2e-tests/editor/editor.misc.spec.ts
+++ e2e-tests/editor/editor.misc.spec.ts
@@ -212,18 +212,37 @@ test('copy and paste block after editing new block #5962', async ({
   await block.waitForBlocks(3)
 })
 
-test('press escape when link/image dialog is open, should restore focus to input', async ({ page, block }) => {
+test('press escape when link/image dialog is open, should restore focus to input', async ({
+  page,
+  block,
+}) => {
   for (const [commandTrigger, modalName] of [['/link', 'commands']]) {
     await createRandomPage(page)
 
     // Open the action modal
     await block.mustFill('')
     await page.keyboard.type(commandTrigger, { delay: STD_DELAY })
-    await page.waitForSelector(`[data-modal-name="${modalName}"]`, {timeout: STD_DELAY * 10})
+    const MAX_ATTEMPTS = 3
+    let attempts = 0
+    while (true) {
+      try {
+        await page.waitForSelector(`[data-modal-name="${modalName}"]`, {
+          timeout: STD_DELAY * 10,
+        })
+        break // if waitForSelector succeeds, break the loop
+      } catch (err) {
+        attempts++
+        if (attempts >= MAX_ATTEMPTS) {
+          throw err // if it fails MAX_ATTEMPTS times, re-throw the error
+        }
+      }
+    }
 
     // Press enter to open the link dialog
     await page.keyboard.press('Enter')
-    await page.waitForSelector(`[data-modal-name="input"]`, {timeout: STD_DELAY * 10})
+    await page.waitForSelector(`[data-modal-name="input"]`, {
+      timeout: STD_DELAY * 10,
+    })
 
     // Press escape; should close link dialog and restore focus to the block textarea
     await page.keyboard.press('Escape', { delay: STD_DELAY })
