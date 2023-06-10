ed72dcc7c4e5dd0ff1e4eb11b78da8611627def9
Bad3r <bad3r@pm.me>
2023-06-10 00:03:09 -0400

    enhance(e2e): update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,34 @@
 import { PlaywrightTestConfig } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
+  name: 'E2E 🧪',
+
+  // The directory where the tests are located
+  // The order of the tests is determined by the file names alphabetically.
   testDir: './e2e-tests',
+
+  // The number of retries before marking a test as failed.
   maxFailures: 1,
-  workers: 1, // NOTE: must be 1 for now, otherwise tests will fail.
+
+  // The number of Logseq instances to run in parallel.
+  // NOTE: must be 1 for now, otherwise tests will fail.
+  workers: 1,
+
+  // 'github' for GitHub Actions CI to generate annotations, plus a concise 'dot'.
+  // default 'list' when running locally.
+  reporter: process.env.CI ? 'github' : 'list',
+
+  // Retry on CI only.
+  // This will reduce the risk of false negatives when running on CI.
+  retries: process.env.CI ? 2 : 0,
+
+  // Fail the build on CI if you accidentally left test.only in the source code.
+  forbidOnly: !!process.env.CI,
+
   use: {
+    // SCapture screenshot after each test failure.
     screenshot: 'only-on-failure',
-  }
+  },
 }
 
 export default config
