ca34564c619e381b648dc48f149b2323a4127acb
Tienson Qin <tiensonqin@gmail.com>
2023-06-14 16:20:56 +0800

    fix: db wrap-parse-block

--- src/main/frontend/handler/editor/impl/db.cljs
+++ src/main/frontend/handler/editor/impl/db.cljs
@@ -13,20 +13,19 @@
                    (nil? x))) refs))
 
 (defn wrap-parse-block
-  [{:block/keys [content format left uuid level] :as block}]
-  (let [block (if (string/blank? content)
+  [{:block/keys [content left level] :as block}]
+  (let [block (or (and (:db/id block) (db/pull (:db/id block))) block)
+        block (if (string/blank? content)
                 block
-                (let [block (or (and (:db/id block) (db/pull (:db/id block))) block)
-                      block (merge block
-                                   (block/parse-title-and-body uuid format false content))
-                      block (merge block
-                                   (block/parse-block block))]
+                (let [block (merge block
+                                   (block/parse-block (assoc block :block/content content)))]
                   (update block :block/refs remove-non-existed-refs!)))
         block (if (and left (not= (:block/left block) left)) (assoc block :block/left left) block)]
     (-> block
         (dissoc
          :block.temp/top?
          :block.temp/bottom?
-         :block/pre-block?)
+         :block/pre-block?
+         :block/unordered?)
         (assoc :block/content content)
         (merge (if level {:block/level level} {})))))
