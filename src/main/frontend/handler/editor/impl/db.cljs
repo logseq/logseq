3a20a147e62e42f025931b11c9f79fdc660e85f2
Tienson Qin <tiensonqin@gmail.com>
2023-06-14 15:40:13 +0800

    fix: parse block

--- src/main/frontend/handler/editor/impl/db.cljs
+++ src/main/frontend/handler/editor/impl/db.cljs
@@ -1,6 +1,7 @@
 (ns frontend.handler.editor.impl.db
   "DB-based graph implementation"
   (:require [frontend.db :as db]
+            [clojure.string :as string]
             [frontend.format.block :as block]))
 
 (defn- remove-non-existed-refs!
@@ -13,14 +14,19 @@
 
 (defn wrap-parse-block
   [{:block/keys [content format left uuid level] :as block}]
-  (let [block (or (and (:db/id block) (db/pull (:db/id block))) block)
-        block (merge block
-                     (block/parse-title-and-body uuid format false content))
-        block (block/parse-block block)
-        block (update block :block/refs remove-non-existed-refs!)
+  (let [block (if (string/blank? content)
+                block
+                (let [block (or (and (:db/id block) (db/pull (:db/id block))) block)
+                      block (merge block
+                                   (block/parse-title-and-body uuid format false content))
+                      block (merge block
+                                   (block/parse-block block))]
+                  (update block :block/refs remove-non-existed-refs!)))
         block (if (and left (not= (:block/left block) left)) (assoc block :block/left left) block)]
     (-> block
-        (dissoc :block.temp/top?
-                :block.temp/bottom?)
+        (dissoc
+         :block.temp/top?
+         :block.temp/bottom?
+         :block/pre-block?)
         (assoc :block/content content)
         (merge (if level {:block/level level} {})))))
