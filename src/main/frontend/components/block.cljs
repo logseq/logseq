1fa9450f2dd88bf1ff9389fa306af5090496c0f0
Mikhail Smelov <32079260+maxboy05@users.noreply.github.com>
2023-06-24 05:18:34 +0300

    Add asset key

--- src/main/frontend/components/block.cljs
+++ src/main/frontend/components/block.cljs
@@ -352,7 +352,7 @@
                 :on-click      (fn [e]
                                  (util/stop e)
                                  (-> (util/copy-image-to-clipboard image-src)
-                                     (p/then #(notification/show! "Copied!" :success))))}
+                                     (p/then #(notification/show! (t :asset/copy-notif) :success))))}
                (ui/icon "copy")]
 
               [:button.asset-action-btn
