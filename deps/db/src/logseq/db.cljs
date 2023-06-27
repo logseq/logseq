c11e94ed59d8e3b3596997311ce4c6f5fd3ca89b
Gabriel Horner <gabriel@logseq.com>
2023-06-27 14:39:31 -0400

    Fix default-page creation for start-conn

--- deps/db/src/logseq/db.cljs
+++ deps/db/src/logseq/db.cljs
@@ -5,15 +5,17 @@
             [datascript.core :as d]))
 
 (defn create-default-pages!
+  "Creates default pages if one of the default pages does not exist. This
+   fn is idempotent"
   [db-conn]
   (when-not (d/entity @db-conn [:block/name "card"])
     (d/transact! db-conn
-      (concat
-       [{:block/name "card"
-         :block/original-name "card"
-         :block/uuid (d/squuid)}
-        {:schema/version db-schema/version}]
-       default-db/built-in-pages))))
+                 (concat
+                  [{:block/name "card"
+                    :block/original-name "card"
+                    :block/uuid (d/squuid)}
+                   {:schema/version db-schema/version}]
+                  default-db/built-in-pages))))
 
 (defn start-conn
   "Create datascript conn with schema and default data"
@@ -21,5 +23,5 @@
       :or {create-default-pages? true}}]
   (let [db-conn (d/create-conn (db-schema/get-schema))]
     (when create-default-pages?
-      (create-default-pages? db-conn))
+      (create-default-pages! db-conn))
     db-conn))
