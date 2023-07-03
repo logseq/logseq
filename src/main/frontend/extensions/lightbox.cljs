5fb3842c2b2e5f78ed9001b113a4615e80edd1df
Konstantinos Kaloutas <konstantinos@logseq.com>
2023-07-03 12:48:40 +0300

    refactor lightbox

--- src/main/frontend/extensions/lightbox.cljs
+++ src/main/frontend/extensions/lightbox.cljs
@@ -3,15 +3,10 @@
             [cljs-bean.core :as bean]
             [frontend.util :as util]))
 
-(defn load-base-assets$
-  []
-  (util/js-load$ (str util/JS_ROOT "/photoswipe.umd.min.js"))
-  (util/js-load$ (str util/JS_ROOT "/photoswipe-lightbox.umd.min.js")))
-
 (defn preview-images!
   [images]
-
-  (p/let [_ (load-base-assets$)]
+  (p/let [_ (util/js-load$ (str util/JS_ROOT "/photoswipe.umd.min.js"))
+          _ (util/js-load$ (str util/JS_ROOT "/photoswipe-lightbox.umd.min.js"))]
     (let [options {:dataSource images :pswpModule js/window.PhotoSwipe :showHideAnimationType "fade"}
           ^js lightbox (js/window.PhotoSwipeLightbox. (bean/->js options))]
       (doto lightbox
