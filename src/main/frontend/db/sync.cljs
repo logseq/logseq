f1422c282736fda4c14a5062e7bd157bffe6a30f
rcmerci <rcmerci@gmail.com>
2023-07-11 14:28:33 +0800

    add frontend.db.sync

---
+++ src/main/frontend/db/sync.cljs
@@ -0,0 +1,19 @@
+(ns frontend.db.sync
+  (:require [datascript.core :as d]
+            [frontend.db.conn :as conn]
+            [frontend.state :as state]))
+
+
+
+(defn- export-as-blocks
+  [repo]
+  (let [db (conn/get-db repo)
+        datoms (d/datoms db :eavt)]
+    (->> datoms
+         (partition-by :e)
+         (keep (fn [datoms]
+                 (when (seq datoms)
+                   (reduce
+                    (fn [r datom] (assoc r (:a datom) (:v datom)))
+                    {:db/id (:e (first datoms))}
+                    datoms)))))))
