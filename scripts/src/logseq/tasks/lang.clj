1d30efe47fe6262bb7b4a2fb4efdde150711d45c
Inge <ipetter@gmail.com>
2023-07-21 08:01:59 +0200

    Update Norwegian translation

--- scripts/src/logseq/tasks/lang.clj
+++ scripts/src/logseq/tasks/lang.clj
@@ -174,7 +174,7 @@
    :nb-NO #{:port :type :whiteboard :right-side-bar/flashcards :right-side-bar/whiteboards 
             :search-item/whiteboard :settings-page/enable-flashcards :settings-page/enable-whiteboards 
             :settings-page/tab-editor :shortcut.category/whiteboard :whiteboard/medium 
-            :whiteboard/twitter-url :whiteboard/youtube-url}
+            :whiteboard/twitter-url :whiteboard/youtube-url :right-side-bar/history-global}
    :tr #{:help/awesome-logseq}
    })
 
