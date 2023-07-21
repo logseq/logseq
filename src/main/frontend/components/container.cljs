1239c3d859c060a768c634993151ee92a1b3cfd5
Konstantinos Kaloutas <konstantinos@logseq.com>
2023-07-21 11:52:26 +0300

    add flashcards shortcut

--- src/main/frontend/components/container.cljs
+++ src/main/frontend/components/container.cljs
@@ -201,8 +201,9 @@
                    (state/pub-event! [:modal/show-cards]))}
      (ui/icon "infinity")
      [:span.flex-1 (t :right-side-bar/flashcards)]
+     [:span.ml-1 (ui/render-keyboard-shortcut (ui/keyboard-shortcut-from-config :go/flashcards))]
      (when (and num (not (zero? num)))
-       [:span.ml-3.inline-block.py-0.5.px-3.text-xs.font-medium.rounded-full.fade-in num])]))
+       [:span.ml-1.inline-block.py-0.5.px-3.text-xs.font-medium.rounded-full.fade-in num])]))
 
 (defn get-default-home-if-valid
   []
