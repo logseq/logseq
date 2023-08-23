0e698f1545b8313ee6fde6adf54ad4c30f051438
YU <111847207+YU000jp@users.noreply.github.com>
2023-08-23 10:12:47 +0900

    Revert "fix(bug): Main scrollbar jumps #5961 set initially visible"
    This reverts commit be47536ab119361b9eb2221e4bf5f15306da99bb.

--- src/main/frontend/ui.cljs
+++ src/main/frontend/ui.cljs
@@ -1136,7 +1136,6 @@
   ([content-fn {:keys [trigger-once? _debug-id]
                 :or {trigger-once? false}}]
    (let [[visible? set-visible!] (rum/use-state false)
-         [initially-visible? set-initially-visible!] (rum/use-state false) ;; set initially visible
          root-margin 100
          inViewState (useInView #js {:rootMargin (str root-margin "px")
                                      :triggerOnce trigger-once?
@@ -1145,8 +1144,7 @@
                                                    (when (or (and (not visible?) in-view?)
                                                              ;; hide only the components below the current top for better ux
                                                              (and visible? (not in-view?) (> self-top root-margin)))
-                                                     (set-visible! in-view?)
-                                                       (set-initially-visible! true))))}) ;; set initially visible
+                                                     (set-visible! in-view?))))})
          ref (.-ref inViewState)]
      (lazy-visible-inner visible? content-fn ref))))
 
