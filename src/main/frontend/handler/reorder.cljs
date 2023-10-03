0fa171b942313c77a630202548913a10be20decb
Tienson Qin <tiensonqin@gmail.com>
2023-10-03 20:03:09 +0800

    enhance: ensure reorder doesn't result in data-loss

--- src/main/frontend/handler/reorder.cljs
+++ src/main/frontend/handler/reorder.cljs
@@ -8,15 +8,23 @@
   [items {:keys [target to]}]
   (let [items (vec items)]
     (when (and target to (not= target to))
-      (->>
-       (if (< target to)
-         [(util/safe-subvec items 0 target)
-          (util/safe-subvec items (inc target) (inc to))
-          [(nth items target)]
-          (util/safe-subvec items (inc to) (count items))]
-         [(util/safe-subvec items 0 to)
-          [(nth items target)]
-          (util/safe-subvec items to target)
-          (util/safe-subvec items (inc target) (count items))])
-       (apply concat)
-       (vec)))))
+      (let [result (->>
+                    (if (< target to)
+                      [(util/safe-subvec items 0 target)
+                       (util/safe-subvec items (inc target) (inc to))
+                       [(nth items target)]
+                       (util/safe-subvec items (inc to) (count items))]
+                      [(util/safe-subvec items 0 to)
+                       [(nth items target)]
+                       (util/safe-subvec items to target)
+                       (util/safe-subvec items (inc target) (count items))])
+                    (apply concat)
+                    (vec))]
+        (if (= (count items) (count result))
+          result
+          (do
+            (js/console.error "Reorder failed:")
+            (prn :reorder-data {:items items
+                                :target target
+                                :to to})
+            items))))))
