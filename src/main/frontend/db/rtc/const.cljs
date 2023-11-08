192bc01249701407cadf828671459cdb2550568b
Tienson Qin <tiensonqin@gmail.com>
2023-11-08 14:54:43 +0800

    fix: merge conflicts

--- src/main/frontend/db/rtc/const.cljs
+++ src/main/frontend/db/rtc/const.cljs
@@ -18,51 +18,6 @@
 (def general-attr-set
   (into #{} (map first) general-attrs-schema-coll))
 
-(def data-from-ws-schema
-  [:map {:closed true}
-   [:req-id :string]
-   [:t {:optional true} :int]
-   [:t-before {:optional true} :int]
-   [:affected-blocks {:optional true}
-    [:map-of :string
-     [:multi {:dispatch :op :decode/string #(update % :op keyword)}
-      [:move
-       (apply conj
-              [:map {:closed true}
-               [:op :keyword]
-               [:self :string]
-               [:parents [:sequential :string]]
-               [:left :string]
-               [:content {:optional true} :string]]
-              general-attrs-schema-coll)]
-      [:remove
-       [:map {:closed true}
-        [:op :keyword]
-        [:block-uuid :string]]]
-      [:update-attrs
-       (apply conj
-              [:map {:closed true}
-               [:op :keyword]
-               [:self :string]
-               [:parents {:optional true} [:sequential :string]]
-               [:left {:optional true} :string]
-               [:content {:optional true} :string]]
-              general-attrs-schema-coll)]
-      [:update-page
-       (apply conj
-              [:map {:closed true}
-               [:op :keyword]
-               [:self :string]
-               [:page-name :string]
-               [:original-name :string]]
-              general-attrs-schema-coll)]
-      [:remove-page
-       [:map {:closed true}
-        [:op :keyword]
-        [:block-uuid :string]]]]]]])
-(def data-from-ws-decoder (m/decoder data-from-ws-schema mt/string-transformer))
-(def data-from-ws-validator (m/validator data-from-ws-schema))
-
 (def block-type-schema [:enum "property" "class" "whiteboard" "object" "hidden" "closed value"])
 
 (def op-schema
@@ -158,10 +113,6 @@
         [:block-uuid :uuid]]]]]]])
 (def data-from-ws-decoder (m/decoder data-from-ws-schema mt/string-transformer))
 (def data-from-ws-validator (m/validator data-from-ws-schema))
-
-
-
-
 
 (def data-to-ws-schema
   (mu/closed-schema
