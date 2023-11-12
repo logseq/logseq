40955a025c05c02c4aa40d86153bfa287e90e403
Tienson Qin <tiensonqin@gmail.com>
2023-11-12 21:20:33 +0800

    enhance: replace help font with svg

--- src/main/frontend/components/container.cljs
+++ src/main/frontend/components/container.cljs
@@ -757,7 +757,10 @@
       [:div.inner
        {:title    (t :help-shortcut-title)
         :on-click #(state/toggle! :ui/help-open?)}
-       "?"]]
+       [:svg.scale-125 {:stroke "currentColor", :fill "none", :stroke-linejoin "round", :width "24", :viewbox "0 0 24 24", :xmlns "http://www.w3.org/2000/svg", :stroke-linecap "round", :stroke-width "2", :class "icon icon-tabler icon-tabler-help-small", :height "24"}
+        [:path {:stroke "none", :d "M0 0h24v24H0z", :fill "none"}]
+        [:path {:d "M12 16v.01"}]
+        [:path {:d "M12 13a2 2 0 0 0 .914 -3.782a1.98 1.98 0 0 0 -2.414 .483"}]]]]
 
      (when help-open?
        (help-menu-popup))
