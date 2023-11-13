88fc95a8eab2fc37e42ced7bbd27a9e52071ecef
Tienson Qin <tiensonqin@gmail.com>
2023-11-13 13:48:18 +0800

    fix: right sidebar background color

--- src/main/frontend/components/right_sidebar.cljs
+++ src/main/frontend/components/right_sidebar.cljs
@@ -430,7 +430,7 @@
                                                                        (state/sidebar-add-block! repo "history" :history))}
             (t :right-side-bar/history)]])]]
 
-      [:.sidebar-item-list.flex-1.scrollbar-spacing.ml-2.pr-3
+      [:.sidebar-item-list.flex-1.scrollbar-spacing.ml-2
        (if @*anim-finished?
          (for [[idx [repo db-id block-type]] (medley/indexed blocks)]
             (rum/with-key
