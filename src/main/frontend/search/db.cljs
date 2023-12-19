c0be3b5b1ffa5e6cc0a8f82975735e6240fa88f1
Tienson Qin <tiensonqin@gmail.com>
2023-12-20 01:48:17 +0800

    fix: file db full page content search

--- src/main/frontend/search/db.cljs
+++ src/main/frontend/search/db.cljs
@@ -65,23 +65,25 @@
         page? (some? name)
         block? (nil? name)
         db-based? (config/db-based-graph? repo)]
-    (when-not (and block? (> (count content) (max-len)))
-      (when-not (and (empty? properties)
-                     (or (and block? (string/blank? content))
-                         (and db-based? page?)))        ; empty page or block
-        (let [content (if block?
-                        (if db-based? content (property-util/remove-built-in-properties format content))
+    (when-not (or
+               (and page? name (model/whiteboard-page? name))
+               (and block? (> (count content) (max-len)))
+               (and (empty? properties)
+                    (or (and block? (string/blank? content))
+                        (and db-based? page?))))        ; empty page or block
+      (let [content (if block?
+                      (if db-based? content (property-util/remove-built-in-properties format content))
                         ;; File based page content
-                        (if db-based?
-                          ""            ; empty page content
-                          (some-> (:block/file (db/entity (:db/id block))) :file/content)))
-              content' (if (and db-based? (seq properties))
-                         (str content (when (not= content "") "\n") (get-db-properties-str properties))
-                         content)]
-          (when-not (string/blank? content')
-            {:id (str uuid)
-             :page (str (:block/uuid page))
-             :content (sanitize content')}))))))
+                      (if db-based?
+                        ""            ; empty page content
+                        (some-> (:block/file (db/entity (:db/id block))) :file/content)))
+            content' (if (and db-based? (seq properties))
+                       (str content (when (not= content "") "\n") (get-db-properties-str properties))
+                       content)]
+        (when-not (string/blank? content')
+          {:id (str uuid)
+           :page (str (:block/uuid page))
+           :content (sanitize content')})))))
 
 (defn original-page-name->index
   [p]
