b4757dad0e8d99f441cc1bed1afc77babf46b5f2
Tienson Qin <tiensonqin@gmail.com>
2024-01-10 17:29:11 +0800

    enhance: get worker runtime id automatically

--- src/dev-cljs/shadow/user.clj
+++ src/dev-cljs/shadow/user.clj
@@ -13,6 +13,13 @@
 
 ;; Get the runtime id from http://localhost:9630/runtimes, pick the one which shows `browser-worker`
 (defn worker-repl
-  [runtime-id]
-  (assert runtime-id "runtime-id shouldn't be empty")
-  (api/repl :app {:runtime-id runtime-id}))
+  ([]
+   (when-let [runtime-id (->> (api/repl-runtimes :app)
+                              (filter (fn [runtime] (= :browser-worker (:host runtime))))
+                              first
+                              :client-id)]
+     (prn :worker-runtime-id runtime-id)
+     (worker-repl runtime-id)))
+  ([runtime-id]
+   (assert runtime-id "runtime-id shouldn't be empty")
+   (api/repl :app {:runtime-id runtime-id})))
