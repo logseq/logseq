095894b255b770d64cce923cd3cf29b72133913c
Gabriel Horner <gabriel@logseq.com>
2024-01-16 15:29:39 -0500

    Revert most of "fix: new db graph couldn't be created"
    Last commit removed built-in pages and was noticed on imports.
    Previous bug of new db graphs leading to a blank screen no longer
    occurred after I printed transact! transactions

--- deps/db/src/logseq/db/sqlite/create_graph.cljs
+++ deps/db/src/logseq/db/sqlite/create_graph.cljs
@@ -5,7 +5,8 @@
             [logseq.db.frontend.property :as db-property]
             [logseq.db.frontend.property.util :as db-property-util]
             [logseq.common.util :as common-util]
-            [datascript.core :as d]))
+            [datascript.core :as d]
+            [logseq.db :as ldb]))
 
 (defn build-db-initial-data
   [config-content]
@@ -23,6 +24,7 @@
                         :file/path (str "logseq/" "custom.js")
                         :file/content ""
                         :file/last-modified-at (js/Date.)}]
+        default-pages (ldb/build-default-pages-tx)
         default-properties (mapcat
                             (fn [[k-keyword {:keys [schema original-name closed-values]}]]
                               (let [k-name (name k-keyword)]
@@ -36,5 +38,6 @@
                                      :block/original-name (or original-name k-name)
                                      :block/name (common-util/page-name-sanity-lc k-name)
                                      :block/uuid (d/squuid)})])))
-                            db-property/built-in-properties)]
-    (concat initial-data initial-files default-properties)))
+                            db-property/built-in-properties)
+        ]
+    (concat initial-data initial-files default-pages default-properties)))
