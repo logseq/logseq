baa12d76dd432fddef9c8cbe2a9a6f43bc0ad4f7
rcmerci <rcmerci@gmail.com>
2024-01-19 00:20:46 +0800

    test(rtc): add basic testcase

--- src/test/frontend/worker/rtc/asset_sync_effects_test.cljs
+++ src/test/frontend/worker/rtc/asset_sync_effects_test.cljs
@@ -4,10 +4,18 @@
   (:require [cljs.core.async :as async :refer [<! >! go timeout]]
             [clojure.test :as t :refer [deftest is use-fixtures]]
             [frontend.test.helper :include-macros true :as test-helper]
-            [frontend.worker.rtc.fixture :as rtc-fixture]))
+            [frontend.worker.rtc.fixture :as rtc-fixture]
+            [spy.core :as spy]))
 
 (use-fixtures :each
   test-helper/start-and-destroy-db-map-fixture
   rtc-fixture/listen-test-db-fixture
   rtc-fixture/start-and-stop-asset-sync-loop-fixture
   rtc-fixture/clear-op-mem-stores-fixture)
+
+
+(deftest asset-sync-loop-init-test
+  (let [ws @(:*ws @rtc-fixture/*test-asset-sync-state)
+        handler-fn (:handler-fn ws)
+        ws-msg (first (spy/last-call handler-fn))]
+    (is (= "list-graphs" (:action ws-msg)))))
