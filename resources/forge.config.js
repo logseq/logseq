01eb66b8468e2efee953c98a14b86b8cec8492f6
Andelf <andelf@gmail.com>
2024-01-18 17:04:27 +0800

    chore(release): bump version 0.10.5

--- resources/forge.config.js
+++ resources/forge.config.js
@@ -4,7 +4,7 @@ module.exports = {
   packagerConfig: {
     name: 'Logseq',
     icon: './icons/logseq_big_sur.icns',
-    buildVersion: 78,
+    buildVersion: 79,
     protocols: [
       {
         "protocol": "logseq",
