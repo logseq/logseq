23d557e6cb43944130345eceab2fe1fdea470288
Frederick Morlock <frederickgeek8@gmail.com>
2024-01-20 04:12:34 -0500

    Make linter happy & add more complete tests
    I also removed some left behind debug statements
    and incorrectly named/commented tests.

--- src/test/frontend/handler/page_test.cljs
+++ src/test/frontend/handler/page_test.cljs
@@ -8,8 +8,7 @@
             [frontend.state :as state]
             [frontend.mobile.util :as mobile-util]
             [frontend.handler.page :as page-handler]
-            [frontend.test.helper :as test-helper]
-            [frontend.config :as config]))
+            [frontend.test.helper :as test-helper]))
 
 (defn- replace-page-ref!
   [content old-name new-name]
@@ -51,7 +50,127 @@
 (defn- add-block-page
   [block]
   (assoc block :block/page block))
-  ;; block)
+
+;; Test resolution of files in the journal folder
+(defn- test-get-page-ref-text-org-journals
+  []
+  (testing "Testing resolution of paths from within the journals folder."
+    ;; journals/failure.org
+    (with-redefs [state/get-edit-block (constantly (add-block-page (db-utils/pull [:block/name "failure"])))]
+      ;; check non-existent file resolution (platform-dependent) with space in name
+      (is (= "[[file:../pages/test case.org][Test Case]]" (frontend.handler.page/get-page-ref-text "Test Case")))
+      ;; check existing file resolution (not platform-dependent)
+      (is (= "[[file:../pages/foo.org][foo]]" (frontend.handler.page/get-page-ref-text "foo")))
+      ;; check existing file resolution created with absolute path
+      (is (= "[[file:/test-db/fooart.org][fooart]]" (frontend.handler.page/get-page-ref-text "fooart")))
+      ;; check existing journal file resolution (not platform-dependent)
+      (is (= "[[file:./2024-01-16.org][2024-01-16]]" (frontend.handler.page/get-page-ref-text "2024-01-16")))
+      ;; check non-existent journal file resolution (platform dependent)
+      (is (= "[[file:./2024-01-15.org][2024-01-15]]" (frontend.handler.page/get-page-ref-text "2024-01-15")))
+      ;; check existent journal file outside of journal folder
+      (is (= "[[file:../2024-01-13.org][2024-01-13]]" (frontend.handler.page/get-page-ref-text "2024-01-13")))
+      ;; check existent journal file with absolute path
+      (is (= "[[file:/test-db/2024-01-01.org][2024-01-01]]" (frontend.handler.page/get-page-ref-text "2024-01-01")))
+      ;; Check resolution of existing file in non-standard location
+      (is (= "[[file:../nonstandard/location.org][location]]" (frontend.handler.page/get-page-ref-text "location")))
+      ;; Check resolution of existing file at root of graph
+      (is (= "[[file:../root.org][root]]" (frontend.handler.page/get-page-ref-text "root")))
+      ;;
+      )
+
+    ;; journals/2024-01-16.org
+    (with-redefs [state/get-edit-block (constantly (add-block-page (db-utils/pull [:block/name "2024-01-16"])))]
+      ;; check non-existent file resolution (platform-dependent) with space in name
+      (is (= "[[file:../pages/test case.org][Test Case]]" (frontend.handler.page/get-page-ref-text "Test Case")))
+      ;; check existing file resolution (not platform-dependent)
+      (is (= "[[file:../pages/foo.org][foo]]" (frontend.handler.page/get-page-ref-text "foo")))
+      ;; check existing file resolution created with absolute path
+      (is (= "[[file:/test-db/fooart.org][fooart]]" (frontend.handler.page/get-page-ref-text "fooart")))
+      ;; check existing journal file resolution (not platform-dependent)
+      (is (= "[[file:./2024-01-16.org][2024-01-16]]" (frontend.handler.page/get-page-ref-text "2024-01-16")))
+      ;; check non-existent journal file resolution (platform dependent)
+      (is (= "[[file:./2024-01-15.org][2024-01-15]]" (frontend.handler.page/get-page-ref-text "2024-01-15")))
+      ;; check existent journal file outside of journal folder
+      (is (= "[[file:../2024-01-13.org][2024-01-13]]" (frontend.handler.page/get-page-ref-text "2024-01-13")))
+      ;; check existent journal file with absolute path
+      (is (= "[[file:/test-db/2024-01-01.org][2024-01-01]]" (frontend.handler.page/get-page-ref-text "2024-01-01")))
+      ;; Check resolution of existing file in non-standard location
+      (is (= "[[file:../nonstandard/location.org][location]]" (frontend.handler.page/get-page-ref-text "location")))
+      ;; Check resolution of existing file at root of graph
+      (is (= "[[file:../root.org][root]]" (frontend.handler.page/get-page-ref-text "root")))
+      ;;
+      )))
+
+;; Test resolution of files at non-standard locations
+(defn- test-get-page-ref-text-org-nonstandard
+  []
+  (testing "Resolution of files from within non-standard folders."
+    ;; root.org
+    (with-redefs [state/get-edit-block (constantly (add-block-page (db-utils/pull [:block/name "root"])))]
+      ;; check non-existent file resolution (platform-dependent) with space in name
+      (is (= "[[file:./pages/test case.org][Test Case]]" (frontend.handler.page/get-page-ref-text "Test Case")))
+      ;; check existing file resolution (not platform-dependent)
+      (is (= "[[file:./pages/foo.org][foo]]" (frontend.handler.page/get-page-ref-text "foo")))
+      ;; check existing file resolution created with absolute path
+      (is (= "[[file:/test-db/fooart.org][fooart]]" (frontend.handler.page/get-page-ref-text "fooart")))
+      ;; check existing journal file resolution (not platform-dependent)
+      (is (= "[[file:./journals/2024-01-16.org][2024-01-16]]" (frontend.handler.page/get-page-ref-text "2024-01-16")))
+      ;; check non-existent journal file resolution (platform dependent)
+      (is (= "[[file:./journals/2024-01-15.org][2024-01-15]]" (frontend.handler.page/get-page-ref-text "2024-01-15")))
+      ;; check existent journal file outside of journal folder
+      (is (= "[[file:./2024-01-13.org][2024-01-13]]" (frontend.handler.page/get-page-ref-text "2024-01-13")))
+      ;; check existent journal file with absolute path
+      (is (= "[[file:/test-db/2024-01-01.org][2024-01-01]]" (frontend.handler.page/get-page-ref-text "2024-01-01")))
+      ;; Check resolution of existing file in non-standard location
+      (is (= "[[file:./nonstandard/location.org][location]]" (frontend.handler.page/get-page-ref-text "location")))
+      ;;
+      )
+
+    ;; nonstandard/location.org
+    (with-redefs [state/get-edit-block (constantly (add-block-page (db-utils/pull [:block/name "location"])))]
+      ;; check non-existent file resolution (platform-dependent) with space in name
+      (is (= "[[file:../pages/test case.org][Test Case]]" (frontend.handler.page/get-page-ref-text "Test Case")))
+      ;; check existing file resolution (not platform-dependent)
+      (is (= "[[file:../pages/foo.org][foo]]" (frontend.handler.page/get-page-ref-text "foo")))
+      ;; check existing file resolution created with absolute path
+      (is (= "[[file:/test-db/fooart.org][fooart]]" (frontend.handler.page/get-page-ref-text "fooart")))
+      ;; check existing journal file resolution (not platform-dependent)
+      (is (= "[[file:../journals/2024-01-16.org][2024-01-16]]" (frontend.handler.page/get-page-ref-text "2024-01-16")))
+      ;; check non-existent journal file resolution (platform dependent)
+      (is (= "[[file:../journals/2024-01-15.org][2024-01-15]]" (frontend.handler.page/get-page-ref-text "2024-01-15")))
+      ;; check existent journal file outside of journal folder
+      (is (= "[[file:../2024-01-13.org][2024-01-13]]" (frontend.handler.page/get-page-ref-text "2024-01-13")))
+      ;; check existent journal file with absolute path
+      (is (= "[[file:/test-db/2024-01-01.org][2024-01-01]]" (frontend.handler.page/get-page-ref-text "2024-01-01")))
+      ;; Check resolution of existing file at root of graph
+      (is (= "[[file:../root.org][root]]" (frontend.handler.page/get-page-ref-text "root")))
+      ;;
+      )))
+
+;; Test resolution of files in pages folder
+(defn- test-get-page-ref-text-org-pages
+  []
+  (testing "Resolution of files from within the pages/ folder."
+ ;; /pages/foo.org
+    (with-redefs [state/get-edit-block (constantly (add-block-page (db-utils/pull [:block/name "foo"])))]
+      ;; check non-existent file resolutino (platform-dependent) with space in name
+      (is (= "[[file:./test case.org][Test Case]]" (frontend.handler.page/get-page-ref-text "Test Case")))
+      ;; check existing file resolution created with absolute path
+      (is (= "[[file:/test-db/fooart.org][fooart]]" (frontend.handler.page/get-page-ref-text "fooart")))
+      ;; check existing file resolution (not platform-dependent) for journal
+      (is (= "[[file:../journals/2024-01-16.org][2024-01-16]]" (frontend.handler.page/get-page-ref-text "2024-01-16")))
+      ;; check non-existent journal file resolution (platform dependent)
+      (is (= "[[file:../journals/2024-01-15.org][2024-01-15]]" (frontend.handler.page/get-page-ref-text "2024-01-15")))
+      ;; check existent journal file outside of journal folder
+      (is (= "[[file:../2024-01-13.org][2024-01-13]]" (frontend.handler.page/get-page-ref-text "2024-01-13")))
+      ;; check existent journal file with absolute path
+      (is (= "[[file:/test-db/2024-01-01.org][2024-01-01]]" (frontend.handler.page/get-page-ref-text "2024-01-01")))
+      ;; Check resolution of existing file at root of graph
+      (is (= "[[file:../root.org][root]]" (frontend.handler.page/get-page-ref-text "root")))
+      ;; Check resolution of existing file in non-standard location
+      (is (= "[[file:../nonstandard/location.org][location]]" (frontend.handler.page/get-page-ref-text "location")))
+      ;;
+      )))
 
 ;; we want the same resolution of these pasts /not/ dependent on platform
 (defn- test-get-page-ref-text-org
@@ -64,15 +183,17 @@
                               :journal/file-name-format "yyyy-MM-dd"}
 
       (test-helper/load-test-files [{:file/path "pages/foo.org"
-                                     :file/content "* Test!"}
+                                     :file/content "* Normal page in the pages folder."}
                                     {:file/path "2024-01-13.org"
-                                     :file/content "* eh?"}
+                                     :file/content "* Journal file at root of the graph."}
                                     {:file/path "journals/2024-01-16.org"
-                                     :file/content "* Example entry."}
+                                     :file/content "* Journal file in journal folder."}
                                     {:file/path "journals/failure.org"
-                                     :file/content "* Example entry."}
+                                     :file/content "* Non-standard journal file in the journal folder."}
                                     {:file/path "root.org"
-                                     :file/content "* Example entry."}
+                                     :file/content "* This file is at the root of the graph."}
+                                    {:file/path "nonstandard/location.org"
+                                     :file/content "* This file is in a non-standard location."}
                                     ;; hopefully these don't happen in practice, but we should at
                                     ;; least parse them in a sane way instead of giving an incorrect
                                     ;; parsed file:// url
@@ -81,100 +202,25 @@
                                     {:file/path "/test-db/2024-01-01.org"
                                      :file/content "* Absolute Paths?"}])
 
-      (print (config/get-repo-dir (state/get-current-repo)))
-
-        ;; journals/failure.org
-      (with-redefs [state/get-edit-block (constantly (add-block-page (db-utils/pull [:block/name "failure"])))]
-          ;; check non-existent file resolutino (platform-dependent) with space in name
-        (is (= "[[file:../pages/test case.org][Test Case]]" (frontend.handler.page/get-page-ref-text "Test Case")))
-          ;; check existing file resolution (not platform-dependent)
-        (is (= "[[file:../pages/foo.org][foo]]" (frontend.handler.page/get-page-ref-text "foo")))
-          ;; check existing file resolution created with absolute path
-        (is (= "[[file:/test-db/fooart.org][fooart]]" (frontend.handler.page/get-page-ref-text "fooart")))
-          ;; check existing file resolution (not platform-dependent) for journal
-        (is (= "[[file:./2024-01-16.org][2024-01-16]]" (frontend.handler.page/get-page-ref-text "2024-01-16")))
-          ;; check non-existent journal file resolution (platform dependent)
-        (is (= "[[file:./2024-01-15.org][2024-01-15]]" (frontend.handler.page/get-page-ref-text "2024-01-15")))
-          ;; check existent journal file outside of journal folder
-        (is (= "[[file:../2024-01-13.org][2024-01-13]]" (frontend.handler.page/get-page-ref-text "2024-01-13")))
-           ;; check existent journal file with absolute path
-        (is (= "[[file:/test-db/2024-01-01.org][2024-01-01]]" (frontend.handler.page/get-page-ref-text "2024-01-01")))
-          ;;
-        )
-
-      ;; journals/2024-01-16.org
-      (with-redefs [state/get-edit-block (constantly (add-block-page (db-utils/pull [:block/name "2024-01-16"])))]
-          ;; check non-existent file resolutino (platform-dependent) with space in name
-        (is (= "[[file:../pages/test case.org][Test Case]]" (frontend.handler.page/get-page-ref-text "Test Case")))
-          ;; check existing file resolution (not platform-dependent)
-        (is (= "[[file:../pages/foo.org][foo]]" (frontend.handler.page/get-page-ref-text "foo")))
-          ;; check existing file resolution created with absolute path
-        (is (= "[[file:/test-db/fooart.org][fooart]]" (frontend.handler.page/get-page-ref-text "fooart")))
-          ;; check existing file resolution (not platform-dependent) for journal
-        (is (= "[[file:./2024-01-16.org][2024-01-16]]" (frontend.handler.page/get-page-ref-text "2024-01-16")))
-          ;; check non-existent journal file resolution (platform dependent)
-        (is (= "[[file:./2024-01-15.org][2024-01-15]]" (frontend.handler.page/get-page-ref-text "2024-01-15")))
-          ;; check existent journal file outside of journal folder
-        (is (= "[[file:../2024-01-13.org][2024-01-13]]" (frontend.handler.page/get-page-ref-text "2024-01-13")))
-           ;; check existent journal file with absolute path
-        (is (= "[[file:/test-db/2024-01-01.org][2024-01-01]]" (frontend.handler.page/get-page-ref-text "2024-01-01")))
-          ;;
-        )
-
-      ;; /root.org
-      (with-redefs [state/get-edit-block (constantly (add-block-page (db-utils/pull [:block/name "root"])))]
-          ;; check non-existent file resolutino (platform-dependent) with space in name
-        (is (= "[[file:./pages/test case.org][Test Case]]" (frontend.handler.page/get-page-ref-text "Test Case")))
-          ;; check existing file resolution (not platform-dependent)
-        (is (= "[[file:./pages/foo.org][foo]]" (frontend.handler.page/get-page-ref-text "foo")))
-          ;; check existing file resolution created with absolute path
-        (is (= "[[file:/test-db/fooart.org][fooart]]" (frontend.handler.page/get-page-ref-text "fooart")))
-          ;; check existing file resolution (not platform-dependent) for journal
-        (is (= "[[file:./journals/2024-01-16.org][2024-01-16]]" (frontend.handler.page/get-page-ref-text "2024-01-16")))
-          ;; check non-existent journal file resolution (platform dependent)
-        (is (= "[[file:./journals/2024-01-15.org][2024-01-15]]" (frontend.handler.page/get-page-ref-text "2024-01-15")))
-          ;; check existent journal file outside of journal folder
-        (is (= "[[file:./2024-01-13.org][2024-01-13]]" (frontend.handler.page/get-page-ref-text "2024-01-13")))
-           ;; check existent journal file with absolute path
-        (is (= "[[file:/test-db/2024-01-01.org][2024-01-01]]" (frontend.handler.page/get-page-ref-text "2024-01-01")))
-          ;;
-        )
-      ;; This test is here, but fails, but also should never happen in practice (it would require being on a absolute-path page)
-      ;; /test-db/fooart.org
-      ;; (with-redefs [state/get-edit-block (constantly (add-block-page (db-utils/pull [:block/name "fooart"])))]
-      ;;     ;; check non-existent file resolutino (platform-dependent) with space in name
-      ;;   (is (= "[[file:/test-db/pages/test case.org][Test Case]]" (frontend.handler.page/get-page-ref-text "Test Case")))
-      ;;     ;; check existing file resolution (not platform-dependent)
-      ;;   (is (= "[[file:./pages/foo.org][foo]]" (frontend.handler.page/get-page-ref-text "foo")))
-      ;;     ;; check existing file resolution created with absolute path
-      ;;   (is (= "[[file:/test-db/fooart.org][fooart]]" (frontend.handler.page/get-page-ref-text "fooart")))
-      ;;     ;; check existing file resolution (not platform-dependent) for journal
-      ;;   (is (= "[[file:./journals/2024-01-16.org][2024-01-16]]" (frontend.handler.page/get-page-ref-text "2024-01-16")))
-      ;;     ;; check non-existent journal file resolution (platform dependent)
-      ;;   (is (= "[[file:/test-db/journals/2024-01-15.org][2024-01-15]]" (frontend.handler.page/get-page-ref-text "2024-01-15")))
-      ;;     ;; check existent journal file outside of journal folder
-      ;;   (is (= "[[file:./2024-01-13.org][2024-01-13]]" (frontend.handler.page/get-page-ref-text "2024-01-13")))
-      ;;      ;; check existent journal file with absolute path
-      ;;   (is (= "[[file:/test-db/2024-01-01.org][2024-01-01]]" (frontend.handler.page/get-page-ref-text "2024-01-01")))
-      ;;     ;;
-      ;;   )
-
-;;
+      (test-get-page-ref-text-org-journals)
+      (test-get-page-ref-text-org-pages)
+      (test-get-page-ref-text-org-nonstandard)
+      ;;
       )))
 
 (deftest ^:focus test-get-page-ref-text-mobile
-  (testing "test page reference resolution for mobile-native platform"
+  (testing "test page reference resolution for mobile-native platform."
 
     (with-redefs  [mobile-util/native-platform? (constantly true)]
       (test-get-page-ref-text-org))))
 
 (deftest ^:focus test-get-page-ref-text-native
-  (testing "test page reference resolution for org on native"
+  (testing "test page reference resolution for org on native."
     (test-get-page-ref-text-org)))
 
 ;; In theory this test should work, but electron does seem to work with unit tests..
 ;; (deftest ^:focus test-get-page-ref-text-electron
-;;   (testing "test page reference resolution for org on electron"
+;;   (testing "test page reference resolution for org on electron."
 
 ;;     (with-redefs [util/electron? (constantly true)]
 ;;       (test-get-page-ref-text-org))))
