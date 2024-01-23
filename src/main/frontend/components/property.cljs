3700592832a3df2049c13e63d647b598e4da02cf
Tienson Qin <tiensonqin@gmail.com>
2024-01-24 02:18:25 +0800

    Remove page properties background for db-based graphs

--- src/main/frontend/components/property.cljs
+++ src/main/frontend/components/property.cljs
@@ -721,7 +721,13 @@
                    (not (:page-configure? opts)))
       [:div.ls-properties-area (cond-> (if in-block-container?
                                          {}
-                                         {:class [(if class-schema? "class-properties" "page-properties")]})
+                                         {:class [(cond
+                                                    class-schema?
+                                                    "class-properties"
+                                                    (config/db-based-graph? (state/get-current-repo))
+                                                    nil
+                                                    :else
+                                                    "page-properties")]})
                                  (:selected? opts)
                                  (update :class conj "select-none"))
        (properties-section block (if class-schema? properties own-properties) opts)
