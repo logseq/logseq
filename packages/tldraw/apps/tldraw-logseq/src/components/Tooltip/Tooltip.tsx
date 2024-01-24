28ce29cf6d2768519fa3b58ed34bd59cd4265490
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-01-24 12:51:33 +0200

    replace radix with LSUI tooltip

--- packages/tldraw/apps/tldraw-logseq/src/components/Tooltip/Tooltip.tsx
+++ packages/tldraw/apps/tldraw-logseq/src/components/Tooltip/Tooltip.tsx
@@ -1,30 +1,21 @@
-import * as ReactTooltip from '@radix-ui/react-tooltip'
-import type { Side } from '@radix-ui/react-popper'
-export interface TooltipProps extends ReactTooltip.TooltipContentProps {
-  children: React.ReactNode
-  side?: Side
-  sideOffset?: number
-  content?: React.ReactNode
-}
+// @ts-ignore
+const LSUI = window.LSUI
 
-export function Tooltip({ side, content, sideOffset = 10, ...rest }: TooltipProps) {
+export function Tooltip({ side, content, sideOffset = 10, ...rest }) {
   return content ? (
-    <ReactTooltip.Provider delayDuration={300}>
-      <ReactTooltip.Root>
-        <ReactTooltip.Trigger asChild>{rest.children}</ReactTooltip.Trigger>
-        <ReactTooltip.Portal>
-          <ReactTooltip.Content
-            className="tl-tooltip-content"
+    <LSUI.TooltipProvider delayDuration={300}>
+      <LSUI.Tooltip>
+        <LSUI.TooltipTrigger asChild>{rest.children}</LSUI.TooltipTrigger>
+          <LSUI.TooltipContent
             sideOffset={sideOffset}
             side={side}
             {...rest}
           >
             {content}
-            <ReactTooltip.Arrow className="tl-tooltip-arrow" />
-          </ReactTooltip.Content>
-        </ReactTooltip.Portal>
-      </ReactTooltip.Root>
-    </ReactTooltip.Provider>
+            <LSUI.TooltipArrow className="tl-tooltip-arrow" />
+          </LSUI.TooltipContent>
+      </LSUI.Tooltip>
+    </LSUI.TooltipProvider>
   ) : (
     <>{rest.children}</>
   )
