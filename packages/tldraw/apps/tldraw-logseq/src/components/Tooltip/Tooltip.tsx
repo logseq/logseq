ad0eae9aaf0530a6577c8c85ca03a0beee0dc5cd
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-01-26 18:06:59 +0200

    reintroduce tooltip props interface

--- packages/tldraw/apps/tldraw-logseq/src/components/Tooltip/Tooltip.tsx
+++ packages/tldraw/apps/tldraw-logseq/src/components/Tooltip/Tooltip.tsx
@@ -1,7 +1,16 @@
+import type { Side } from '@radix-ui/react-popper'
+
 // @ts-ignore
 const LSUI = window.LSUI
 
-export function Tooltip({ side, content, sideOffset = 10, ...rest }) {
+export interface TooltipProps {
+  children: React.ReactNode
+  side?: Side
+  sideOffset?: number
+  content?: React.ReactNode
+}
+
+export function Tooltip({ side, content, sideOffset = 10, ...rest }: TooltipProps) {
   return content ? (
     <LSUI.TooltipProvider delayDuration={300}>
       <LSUI.Tooltip>
