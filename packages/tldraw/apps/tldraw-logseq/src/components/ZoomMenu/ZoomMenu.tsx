e715b3596e957058083c3996f18c19679c855b08
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-01-29 09:37:02 +0200

    replace dropdown component

--- packages/tldraw/apps/tldraw-logseq/src/components/ZoomMenu/ZoomMenu.tsx
+++ packages/tldraw/apps/tldraw-logseq/src/components/ZoomMenu/ZoomMenu.tsx
@@ -1,9 +1,9 @@
-import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
 import { useApp } from '@tldraw/react'
 import { KeyboardShortcut } from '../KeyboardShortcut'
-import { MOD_KEY } from '@tldraw/core'
 import { observer } from 'mobx-react-lite'
-import * as React from 'react'
+
+// @ts-ignore
+const LSUI = window.LSUI
 
 export const ZoomMenu = observer(function ZoomMenu(): JSX.Element {
   const app = useApp()
@@ -12,59 +12,53 @@ export const ZoomMenu = observer(function ZoomMenu(): JSX.Element {
   }
 
   return (
-    <DropdownMenuPrimitive.Root>
-      <DropdownMenuPrimitive.Trigger className="tl-button text-sm px-2 important" id="tl-zoom">
+    <LSUI.DropdownMenu>
+      <LSUI.DropdownMenuTrigger className="tl-button text-sm px-2 important" id="tl-zoom">
         {(app.viewport.camera.zoom * 100).toFixed(0) + '%'}
-      </DropdownMenuPrimitive.Trigger>
-      <DropdownMenuPrimitive.Content
+      </LSUI.DropdownMenuTrigger>
+      <LSUI.DropdownMenuContent
         onCloseAutoFocus={e => e.preventDefault()}
-        className="tl-menu"
         id="zoomPopup"
         sideOffset={12}
       >
-        <DropdownMenuPrimitive.Item
-          className="tl-menu-item"
+        <LSUI.DropdownMenuItem
           onSelect={preventEvent}
           onClick={app.api.zoomToFit}
         >
           Zoom to drawing
           <KeyboardShortcut action="whiteboard/zoom-to-fit" />
-        </DropdownMenuPrimitive.Item>
-        <DropdownMenuPrimitive.Item
-          className="tl-menu-item"
+        </LSUI.DropdownMenuItem>
+        <LSUI.DropdownMenuItem
           onSelect={preventEvent}
           onClick={app.api.zoomToSelection}
           disabled={app.selectedShapesArray.length === 0}
         >
           Zoom to fit selection
           <KeyboardShortcut action="whiteboard/zoom-to-selection" />
-        </DropdownMenuPrimitive.Item>
-        <DropdownMenuPrimitive.Item
-          className="tl-menu-item"
+        </LSUI.DropdownMenuItem>
+        <LSUI.DropdownMenuItem
           onSelect={preventEvent}
           onClick={app.api.zoomIn}
         >
           Zoom in
           <KeyboardShortcut action="whiteboard/zoom-in" />
-        </DropdownMenuPrimitive.Item>
-        <DropdownMenuPrimitive.Item
-          className="tl-menu-item"
+        </LSUI.DropdownMenuItem>
+        <LSUI.DropdownMenuItem
           onSelect={preventEvent}
           onClick={app.api.zoomOut}
         >
           Zoom out
           <KeyboardShortcut action="whiteboard/zoom-out" />
-        </DropdownMenuPrimitive.Item>
-        <DropdownMenuPrimitive.Item
-          className="tl-menu-item"
+        </LSUI.DropdownMenuItem>
+        <LSUI.DropdownMenuItem
           onSelect={preventEvent}
           onClick={app.api.resetZoom}
         >
           Reset zoom
           <KeyboardShortcut action="whiteboard/reset-zoom" />
-        </DropdownMenuPrimitive.Item>
-      </DropdownMenuPrimitive.Content>
-    </DropdownMenuPrimitive.Root>
+        </LSUI.DropdownMenuItem>
+      </LSUI.DropdownMenuContent>
+    </LSUI.DropdownMenu>
   )
 })
 
