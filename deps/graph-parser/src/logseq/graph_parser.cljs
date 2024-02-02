b6a952fd7d4553702289da9d26482a76cd40d5fa
Gabriel Horner <gabriel@logseq.com>
2024-02-02 16:16:18 -0500

    fix: assets not copied for import
    Caused by a commit yesterday. Also removed unused
    import-file-to-db-graph options

--- deps/graph-parser/src/logseq/graph_parser.cljs
+++ deps/graph-parser/src/logseq/graph_parser.cljs
@@ -199,9 +199,7 @@ Options available:
 
 (defn import-file-to-db-graph
   "Parse file and save parsed data to the given db graph."
-  [conn file content {:keys [delete-blocks-fn extract-options skip-db-transact?]
-                      :or {delete-blocks-fn (constantly [])
-                           skip-db-transact? false}
+  [conn file content {:keys [extract-options]
                       :as options}]
   (let [format (common-util/get-format file)
         {:keys [tx ast]}
@@ -227,7 +225,6 @@ Options available:
               ;; remove file path relative
               pages (map #(dissoc % :block/file :block/properties) pages)
               block-ids (map (fn [block] {:block/uuid (:block/uuid block)}) blocks)
-              delete-blocks (delete-blocks-fn @conn (first pages) file block-ids)
               block-refs-ids (->> (mapcat :block/refs blocks)
                                   (filter (fn [ref] (and (vector? ref)
                                                          (= :block/uuid (first ref)))))
@@ -263,12 +260,10 @@ Options available:
               blocks (map #(update-imported-block conn %) blocks)
               pages-index (map #(select-keys % [:block/name]) pages)]
 
-          {:tx (concat refs whiteboard-pages pages-index delete-blocks pages block-ids blocks)
+          {:tx (concat refs whiteboard-pages pages-index pages block-ids blocks)
            :ast ast})
         tx' (common-util/fast-remove-nils tx)
-        result (if skip-db-transact?
-                 tx'
-                 (d/transact! conn tx' (select-keys options [:new-graph? :from-disk?])))]
+        result (d/transact! conn tx' (select-keys options [:new-graph? :from-disk?]))]
     {:tx-report result
      :ast ast}))
 
--- src/main/frontend/components/imports.cljs
+++ src/main/frontend/components/imports.cljs
@@ -344,9 +344,11 @@
         original-graph-name (string/replace (.-webkitRelativePath (first file-objs)) #"/.*" "")
         import-graph-fn (fn [graph-name]
                           (let [files (->> file-objs
-                                           (remove #(fs-util/ignored-path? original-graph-name (.-webkitRelativePath %)))
                                            (map #(hash-map :file-object %
-                                                           :rpath (path/trim-dir-prefix original-graph-name (.-webkitRelativePath %)))))]
+                                                           :rpath (path/trim-dir-prefix original-graph-name (.-webkitRelativePath %))))
+                                           (remove #(and (not (string/starts-with? (:rpath %) "assets/"))
+                                                         ;; TODO: Update this when supporting more formats as this agressively excludes most formats
+                                                         (fs-util/ignored-path? original-graph-name (.-webkitRelativePath (:file-object %))))))]
                             (if-let [config-file (first (filter #(= (:rpath %) "logseq/config.edn") files))]
                               (import-file-graph files graph-name config-file)
                               (notification/show! "Import failed as the file 'logseq/config.edn' was not found for a Logseq graph."
