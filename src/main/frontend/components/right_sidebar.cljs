00f98be4157f03a6e2fc5867f0cb5bcf05458952
rcmerci <rcmerci@gmail.com>
2024-02-06 17:12:43 +0800

    fix: add rtc-debug-ui in right-sidebar
    maybe removed by a merge operation :(

--- src/main/frontend/components/right_sidebar.cljs
+++ src/main/frontend/components/right_sidebar.cljs
@@ -22,7 +22,8 @@
             [frontend.modules.editor.undo-redo :as undo-redo]
             [medley.core :as medley]
             [reitit.frontend.easy :as rfe]
-            [rum.core :as rum]))
+            [rum.core :as rum]
+            [frontend.db.rtc.debug-ui :as rtc-debug-ui]))
 
 (rum/defc toggle
   []
@@ -180,6 +181,9 @@
     :shortcut-settings
     [[:.flex.items-center (ui/icon "command" {:class "text-md mr-2"}) (t :help/shortcuts)]
      (shortcut-settings)]
+    :rtc
+    [[:.flex.items-center (ui/icon "cloud" {:class "text-md mr-2"}) "(Dev) RTC"]
+     (rtc-debug-ui/rtc-debug-ui)]
 
     ["" [:span]]))
 
@@ -453,15 +457,21 @@
         (when (and config/dev? (state/sub [:ui/developer-mode?]))
           [:div.text-sm
            [:button.button.cp__right-sidebar-settings-btn {:on-click (fn [_e]
+                                                                       (state/sidebar-add-block! repo "rtc" :rtc))}
+            "(Dev) RTC"]])
+
+        (when (and config/dev? (state/sub [:ui/developer-mode?]))
+          [:div.text-sm
+           [:button.button.cp__right-sidebar-settings-btn {:on-click (fn [_e]
                                                                        (state/sidebar-add-block! repo "history" :history))}
             (t :right-side-bar/history)]])]]
 
       [:.sidebar-item-list.flex-1.scrollbar-spacing.px-2
        (if @*anim-finished?
          (for [[idx [repo db-id block-type]] (medley/indexed blocks)]
-            (rum/with-key
-              (sidebar-item repo idx db-id block-type block-count)
-              (str "sidebar-block-" db-id)))
+           (rum/with-key
+             (sidebar-item repo idx db-id block-type block-count)
+             (str "sidebar-block-" db-id)))
          [:div.p-4
           [:span.font-medium.opacity-50 "Loading ..."]])]]]))
 
