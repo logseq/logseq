721217447899c312226d1e67a59985c9503d1c91
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-02-08 19:04:02 +0200

    add glidejs theme

--- gulpfile.js
+++ gulpfile.js
@@ -79,6 +79,7 @@ const common = {
       () => gulp.src([
         'node_modules/@glidejs/glide/dist/glide.min.js',
         'node_modules/@glidejs/glide/dist/css/glide.core.min.css',
+        'node_modules/@glidejs/glide/dist/css/glide.theme.min.css',
       ]).pipe(gulp.dest(path.join(outputPath, 'js', 'glide'))),
       () => gulp.src([
         'node_modules/pdfjs-dist/build/pdf.js',
