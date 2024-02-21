32626de329c969e62028c689f287b23cd25eb50c
Gabriel Horner <gabriel@logseq.com>
2024-02-21 09:25:50 -0500

    fix: lint

--- src/test/frontend/worker/rtc/rtc_fns_test.cljs
+++ src/test/frontend/worker/rtc/rtc_fns_test.cljs
@@ -200,8 +200,7 @@
         page-name "apply-remote-move-ops-test"
         [page-uuid
          uuid1-client uuid2-client
-         uuid1-remote
-         uuid1-not-exist] (repeatedly random-uuid)]
+         uuid1-remote] (repeatedly random-uuid)]
     (page-handler/create! page-name {:redirect? false :create-first-block? false :uuid page-uuid})
     (outliner-tx/transact!
      opts
