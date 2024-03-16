fa13452608e5410639649c4c4d49bdfd346ee4f7
Yannick Armand <yannick.armand@protonmail.com>
2024-03-16 22:20:05 +0100

    dev: remove obsolete "@tailwindcss/line-clamp" dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -140,7 +140,6 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/line-clamp'),
     require('tailwind-capitalize-first-letter'),
     require('tailwindcss-animate'),
     exposeColorsToCssVars
