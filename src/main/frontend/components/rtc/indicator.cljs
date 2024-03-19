fbb970e65c1598d96c2f6fc150387da36ac06864
Tienson Qin <tiensonqin@gmail.com>
2024-03-19 14:56:52 +0800

    fix: don't show sync indicator for local graphs

--- src/main/frontend/components/rtc/indicator.cljs
+++ src/main/frontend/components/rtc/indicator.cljs
@@ -4,10 +4,12 @@
             [frontend.ui :as ui]
             [logseq.shui.ui :as shui]
             [frontend.state :as state]
-            [frontend.util :as util]))
+            [frontend.util :as util]
+            [logseq.db :as ldb]
+            [frontend.db :as db]))
 
 (rum/defc details
-  [{:keys [unpushed-block-update-count]} uploading? downloading?]
+  [{:keys [unpushed-block-update-count]} uploading?]
   [:div.cp__rtc-sync-details.text-sm.p-1
    (cond
      uploading?
@@ -31,16 +33,17 @@
          {:variant :ghost
           :size :sm}
          "Downloading...")
-        [:div.cp__rtc-sync
-         [:div.cp__rtc-sync-indicator
-          [:a.button.cloud
-           {:on-click #(shui/popup-show! (.-target %)
-                                         (details state uploading? downloading?)
-                                         {:align "end"})
-            :class    (util/classnames [{:on (and online? (= :open rtc-state))
-                                         :idle (and online? (= :open rtc-state) (zero? unpushed-block-update-count)
-                                                    (not uploading?)
-                                                    (not downloading?))
-                                         :queuing (or uploading? downloading? (pos? unpushed-block-update-count))}])}
-           [:span.flex.items-center
-            (ui/icon "cloud" {:size ui/icon-size})]]]]))))
+        (when (and graph-uuid (= graph-uuid (ldb/get-graph-rtc-uuid (db/get-db))))
+          [:div.cp__rtc-sync
+           [:div.cp__rtc-sync-indicator
+            [:a.button.cloud
+             {:on-click #(shui/popup-show! (.-target %)
+                                           (details state uploading?)
+                                           {:align "end"})
+              :class    (util/classnames [{:on (and online? (= :open rtc-state))
+                                           :idle (and online? (= :open rtc-state) (zero? unpushed-block-update-count)
+                                                      (not uploading?)
+                                                      (not downloading?))
+                                           :queuing (or uploading? downloading? (pos? unpushed-block-update-count))}])}
+             [:span.flex.items-center
+              (ui/icon "cloud" {:size ui/icon-size})]]]])))))
