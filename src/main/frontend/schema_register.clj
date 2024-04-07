02cdd211fedc4308b114d52f8f82a9301f02a32c
rcmerci <rcmerci@gmail.com>
2024-04-07 16:37:50 +0800

    fix: don't need to add spec def

--- src/main/frontend/schema_register.clj
+++ src/main/frontend/schema_register.clj
@@ -7,9 +7,6 @@
   [kw docstring & [optional-malli-schema]]
   (assert (keyword? kw) "must be keyword")
   (assert (some? docstring) "must have 'docstring' arg")
-  (let [register-schema (when optional-malli-schema
-                          `[(assert (frontend.schema-register/not-register-yet? ~kw) (str "Already registered: " ~kw))
-                            (frontend.schema-register/register! ~kw ~optional-malli-schema)])]
-    `(do
-       (cljs.spec.alpha/def ~kw any?)
-       ~@register-schema)))
+  (when optional-malli-schema
+    `(do (assert (frontend.schema-register/not-register-yet? ~kw) (str "Already registered: " ~kw))
+         (frontend.schema-register/register! ~kw ~optional-malli-schema))))
