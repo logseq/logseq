d8f98c2733dd509077d9072ee7c7d407c9f99150
charlie <xyhp915@qq.com>
2024-04-09 11:22:52 +0800

    enhance(ui): polish details for the input style

--- packages/ui/@/components/ui/input.tsx
+++ packages/ui/@/components/ui/input.tsx
@@ -6,14 +6,15 @@ import { cn } from '@/lib/utils'
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+const Input = React.forwardRef<HTMLInputElement, InputProps & any>(
+  ({ className, type, small, ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
           'ui__input',
-          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background ' +
+          (small ? 'h-8 py-1 px-2' : 'h-10 px-3 py-2'),
+          'flex w-full rounded-md border border-input bg-background text-sm ring-offset-background ' +
           'file:border-0 file:bg-transparent file:text-sm file:font-medium focus:border-input ' +
           'placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring ' +
           'focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50',
