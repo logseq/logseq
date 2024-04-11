5d28129f81e11b3e0ad7bfa95e11ed3ceeb348ed
rcmerci <rcmerci@gmail.com>
2024-04-11 14:40:45 +0800

    fix(lang): remove non-exist key

--- scripts/src/logseq/tasks/lang.clj
+++ scripts/src/logseq/tasks/lang.clj
@@ -161,19 +161,19 @@
   "Allows certain keys in a language to have the same translation
    as English. Happens more in romance languages but pretty rare otherwise"
   {:fr #{:port :type :help/docs :search-item/page :shortcut.category/navigating :text/image
-         :settings-of-plugins :code :on-boarding/section-pages :paginates/pages :right-side-bar/history-global
+         :settings-of-plugins :code :on-boarding/section-pages :paginates/pages
          :shortcut.category/plugins :whiteboard/rectangle :whiteboard/triangle}
    :de #{:graph :host :plugins :port :right-side-bar/whiteboards
          :settings-of-plugins :search-item/whiteboard :shortcut.category/navigating
          :settings-page/enable-tooltip :settings-page/enable-whiteboards :settings-page/plugin-system}
-   :es #{:settings-page/tab-general :settings-page/tab-editor :whiteboard/color :right-side-bar/history-global}
+   :es #{:settings-page/tab-general :settings-page/tab-editor :whiteboard/color}
    :it #{:home :handbook/home :host :help/awesome-logseq :on-boarding/section-computer
          :settings-page/tab-account :settings-page/tab-editor :whiteboard/link}
    :nl #{:plugins :type :left-side-bar/nav-recent-pages :plugin/update}
    :pl #{:port}
    :pt-BR #{:plugins :right-side-bar/flashcards :settings-page/enable-flashcards :page/backlinks
             :host :settings-page/tab-editor :shortcut.category/plugins :whiteboard/link :settings-of-plugins :whiteboard
-            :whiteboards :on-boarding/quick-tour-journal-page-desc-2 :plugin/downloads :right-side-bar/history-global
+            :whiteboards :on-boarding/quick-tour-journal-page-desc-2 :plugin/downloads
             :right-side-bar/whiteboards :search-item/whiteboard :settings-page/enable-whiteboards :settings-page/plugin-system
             :shortcut.category/whiteboard :command.whiteboard/zoom-in :command.whiteboard/zoom-out}
    :pt-PT #{:plugins :settings-of-plugins :plugin/downloads :right-side-bar/flashcards
@@ -181,9 +181,9 @@
    :nb-NO #{:port :type :whiteboard :right-side-bar/flashcards :right-side-bar/whiteboards
             :search-item/whiteboard :settings-page/enable-flashcards :settings-page/enable-whiteboards
             :settings-page/tab-editor :shortcut.category/whiteboard :whiteboard/medium
-            :whiteboard/twitter-url :whiteboard/youtube-url :right-side-bar/history-global :linked-references/filter-heading}
+            :whiteboard/twitter-url :whiteboard/youtube-url :linked-references/filter-heading}
    :tr #{:help/awesome-logseq}
-   :id #{:host :port :on-boarding/section-app :right-side-bar/history-global}})
+   :id #{:host :port :on-boarding/section-app}})
 
 (defn- validate-languages-dont-have-duplicates
   "Looks up duplicates for all languages"
