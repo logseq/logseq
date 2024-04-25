cb22dca5e0da2888223c7b1ca2ec972b066508b4
Gabriel Horner <gabriel@logseq.com>
2024-04-25 09:39:02 -0400

    fix: db tests

--- deps/db/test/logseq/db/frontend/rules_test.cljs
+++ deps/db/test/logseq/db/frontend/rules_test.cljs
@@ -20,10 +20,11 @@
 
 (deftest has-page-property-rule
   (let [conn (new-db-conn)
+        page (sqlite-util/build-new-page "Page")
         _ (d/transact! conn [(sqlite-util/build-new-property :user.property/foo {})
                              (sqlite-util/build-new-property :user.property/foo2 {})
-                             (sqlite-util/build-new-page "Page")
-                             {:block/original-name "Page" :block/properties {:user.property/foo "bar"}}])]
+                             page
+                             {:block/uuid (:block/uuid page) :block/properties {:user.property/foo "bar"}}])]
     (is (= ["Page"]
            (->> (q-with-rules '[:find (pull ?b [:block/original-name]) :where (has-page-property ?b :user.property/foo)]
                               @conn)
@@ -42,16 +43,18 @@
 
 (deftest page-property-rule
   (let [conn (new-db-conn)
+        page (sqlite-util/build-new-page "Page")
+        page-a (sqlite-util/build-new-page "Page A")
         _ (d/transact! conn [(sqlite-util/build-new-property :user.property/foo {})
                              (sqlite-util/build-new-property :user.property/foo2 {})
                              (sqlite-util/build-new-property :user.property/number-many {:type :number :cardinality :many})
                              (sqlite-util/build-new-property :user.property/page-many {:type :page :cardinality :many})
-                             (sqlite-util/build-new-page "Page")
-                             (sqlite-util/build-new-page "Page A")
-                             {:block/original-name "Page" :block/properties {:user.property/foo "bar"}}
-                             {:block/original-name "Page" :block/properties {:user.property/number-many #{5 10}}}
-                             {:block/original-name "Page" :block/properties {:user.property/page-many #{[:block/original-name "Page A"]}}}
-                             {:block/original-name "Page A" :block/properties {:user.property/foo "bar A"}}])]
+                             page
+                             page-a
+                             {:block/uuid (:block/uuid page) :block/properties {:user.property/foo "bar"}}
+                             {:block/uuid (:block/uuid page) :block/properties {:user.property/number-many #{5 10}}}
+                             {:block/uuid (:block/uuid page) :block/properties {:user.property/page-many #{[:block/uuid (:block/uuid page-a)]}}}
+                             {:block/uuid (:block/uuid page-a) :block/properties {:user.property/foo "bar A"}}])]
 
     (testing "cardinality :one property"
       (is (= ["Page"]
