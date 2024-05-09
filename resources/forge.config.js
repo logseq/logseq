2d6f2932140503929e82496ee4481b4d0ac49823
Andelf <andelf@gmail.com>
2024-05-09 12:57:34 +0800

    ci(build): try add msi build

--- resources/forge.config.js
+++ resources/forge.config.js
@@ -39,6 +39,13 @@ module.exports = {
       }
     },
     {
+      'name': '@electron-forge/maker-wix',
+      'config': {
+        'language': 1033,
+        'manufacturer': 'Logseq, Inc.',
+      }
+    },
+    {
       name: '@electron-forge/maker-dmg',
       config: {
         format: 'ULFO',
