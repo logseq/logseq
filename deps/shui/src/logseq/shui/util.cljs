ad9dc70b9ce03eaa8d5ef7a7546aa2b42aa3378b
Gabriel Horner <gabriel@logseq.com>
2024-05-15 10:08:47 -0400

    fix: finish removing table v2 from shui dep
    Followup to 82a1a187622d8afdee4b0498254a91e4bd7f70a0.
    Copied more useful intro from shui-graph into README

--- deps/shui/src/logseq/shui/util.cljs
+++ deps/shui/src/logseq/shui/util.cljs
@@ -11,61 +11,6 @@
 
 (goog-define NODETEST false)
 
-(defn el->clj-rect [el]
-  (let [rect (.getBoundingClientRect el)]
-    {:top    (.-top rect)
-     :left   (.-left rect)
-     :bottom (.-bottom rect)
-     :right  (.-right rect)
-     :width  (.-width rect)
-     :height (.-height rect)
-     :x      (.-x rect)
-     :y      (.-y rect)}))
-
-(defn clj-rect-observer [update!]
-  (js/ResizeObserver.
-    (fn [entries]
-      (when (.-contentRect (first (js->clj entries)))
-        (update!)))))
-
-(defn use-dom-bounding-client-rect
-  ([el] (use-dom-bounding-client-rect el nil))
-  ([el tick]
-   (let [[rect set-rect] (rum/use-state nil)]
-     (rum/use-effect!
-       (if el
-         (fn []
-           (let [update! #(set-rect (el->clj-rect el))
-                 observer (clj-rect-observer update!)]
-             (update!)
-             (.observe observer el)
-             #(.disconnect observer)))
-         #())
-       [el tick])
-     rect)))
-
-(defn use-ref-bounding-client-rect
-  ([] (use-ref-bounding-client-rect nil))
-  ([tick]
-   (let [[ref set-ref] (rum/use-state nil)
-         rect (use-dom-bounding-client-rect ref tick)]
-     [set-ref rect ref]))
-  ([ref tick] [nil (use-dom-bounding-client-rect ref tick)]))
-
-(defn rem->px [rem]
-  (-> js/document.documentElement
-    js/getComputedStyle
-    (.-fontSize)
-    (js/parseFloat)
-    (* rem)))
-
-(defn px->rem [px]
-  (->> js/document.documentElement
-    js/getComputedStyle
-    (.-fontSize)
-    (js/parseFloat)
-    (/ px)))
-
 (defn kebab-case->camel-case
   "Converts from kebab case to camel case, eg: on-click to onClick"
   [input]
