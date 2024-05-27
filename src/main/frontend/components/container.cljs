2b41fd1654731132379888ac380ae6c780629cf6
Tienson Qin <tiensonqin@gmail.com>
2024-05-28 02:24:09 +0800

    fix: can't use property shortcuts on pages

--- src/main/frontend/components/container.cljs
+++ src/main/frontend/components/container.cljs
@@ -896,11 +896,11 @@
                           (state/close-modal!)
                           (hide-context-menu-and-clear-selection e))
 
-                        (and (seq (state/get-selection-block-ids))
-                             (not (or (.-ctrlKey e) (.-metaKey e) (.-altKey e)))
-                             (not (util/input? (.-target e)))
-                             (not (seq @jump-handler/*jump-data))
-                             (not= (shui-dialog/get-last-modal-id) :property-dialog))
+                        (and
+                         (not (or (.-ctrlKey e) (.-metaKey e) (.-altKey e)))
+                         (not (util/input? (.-target e)))
+                         (not (seq @jump-handler/*jump-data))
+                         (not= (shui-dialog/get-last-modal-id) :property-dialog))
                         (let [shift? (.-shiftKey e)
                               shortcut (if shift? (str "shift+" (.-key e)) (.-key e))]
                           (db-property-handler/set-property-by-shortcut! shortcut)))))))
