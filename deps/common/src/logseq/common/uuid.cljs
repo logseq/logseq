be41c8bae76244a29d4c0c4db6fb82dec4ad1b4b
rcmerci <rcmerci@gmail.com>
2024-06-20 17:00:37 +0800

    chore: add some comments

--- deps/common/src/logseq/common/uuid.cljs
+++ deps/common/src/logseq/common/uuid.cljs
@@ -3,6 +3,9 @@
   (:require [datascript.core :as d]))
 
 (defn- gen-journal-page-uuid
+  "00000001-2024-0620-0000-000000000000
+first 8 chars as type, currently only '00000001' for journal-day-page.
+the remaining chars for data of this type"
   [journal-day]
   {:pre [(int? journal-day)]}
   (let [journal-day-str  (str journal-day)
