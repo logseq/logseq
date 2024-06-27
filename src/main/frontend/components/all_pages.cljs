3809d58b21c6fbe50d9c6cad31ab3bffc90e9e8d
Gabriel Horner <gabriel@logseq.com>
2024-06-27 12:05:16 -0400

    fix: typos

--- src/main/frontend/components/all_pages.cljs
+++ src/main/frontend/components/all_pages.cljs
@@ -101,7 +101,7 @@
        (map (fn [p] (assoc p :id (:db/id p))))))
 
 (rum/defc columns-select
-  [columns {:keys [column-visible? column-toggle-visiblity]}]
+  [columns {:keys [column-visible? column-toggle-visibility]}]
   (shui/dropdown-menu
    (shui/dropdown-menu-trigger
     {:asChild true}
@@ -117,7 +117,7 @@
        {:key (str (:id column))
         :className "capitalize"
         :checked (column-visible? column)
-        :onCheckedChange #(column-toggle-visiblity column %)}
+        :onCheckedChange #(column-toggle-visibility column %)}
        (:name column))))))
 
 (defn table-header
