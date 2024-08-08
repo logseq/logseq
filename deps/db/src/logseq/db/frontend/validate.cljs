d132f41cab9c2b93d18639ab7914f885d5791c92
rcmerci <rcmerci@gmail.com>
2024-08-08 17:11:13 +0800

    enhance: validate :block/type change

--- deps/db/src/logseq/db/frontend/validate.cljs
+++ deps/db/src/logseq/db/frontend/validate.cljs
@@ -16,11 +16,32 @@
     closed-schema?
     mu/closed-schema))
 
+(defn validate-ents-before-after!
+  [changed-ids db-before db-after tx-data tx-meta]
+  (let [id->ent-before (into {}
+                             (keep (fn [id] (when-let [ent (d/entity db-before id)] [id ent])))
+                             changed-ids)
+        id->ent-after (keep (fn [id] (when-let [ent (d/entity db-after id)] [id ent])) changed-ids)
+        ent-before+ent-after-coll
+        (reduce
+         (fn [acc [id ent-after]]
+           (if-let [ent-before (id->ent-before id)]
+             (conj acc [ent-before ent-after])
+             acc))
+         [] id->ent-after)]
+    (doseq [[ent-before ent-after] ent-before+ent-after-coll]
+      (let [[type-before type-after] [(:block/type ent-before) (:block/type ent-after)]]
+        (when (and (some? type-before)
+                   (nil? type-after))
+          (js/console.error "Illeagal :block/type change, entity id:" (:db/id ent-after))
+          (prn :ent-before ent-before :ent-after ent-after :tx-data tx-data :tx-meta tx-meta))))))
+
 (defn validate-tx-report!
   "Validates the datascript tx-report for entities that have changed. Returns
   boolean indicating if db is valid"
-  [{:keys [db-after tx-data tx-meta]} validate-options]
-  (let [changed-ids (->> tx-data (map :e) distinct)
+  [{:keys [db-before db-after tx-data tx-meta]} validate-options]
+  (let [changed-ids (->> tx-data (keep :e) distinct)
+        _ (validate-ents-before-after! changed-ids db-before db-after tx-data tx-meta)
         tx-datoms (mapcat #(d/datoms db-after :eavt %) changed-ids)
         ent-maps* (map (fn [[db-id m]]
                          ;; Add :db/id for debugging
