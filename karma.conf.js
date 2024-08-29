98502d7cde0866ea568298fb3cdec6e6a7fe416a
rcmerci <rcmerci@gmail.com>
2024-08-29 12:50:50 +0800

    test(rtc): add karma target

---
+++ karma.conf.js
@@ -0,0 +1,17 @@
+module.exports = function (config) {
+    config.set({
+        browsers: ['Chrome'],
+        // The directory where the output file lives
+        basePath: 'static/rtc-e2e-test-karma',
+        // The file itself
+        files: ['main.js'],
+        frameworks: ['cljs-test'],
+        plugins: ['karma-cljs-test', 'karma-chrome-launcher'],
+        colors: true,
+        logLevel: config.LOG_INFO,
+        client: {
+            args: ["shadow.test.karma.init"],
+            singleRun: true
+        }
+    })
+};
