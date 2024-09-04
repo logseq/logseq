ad0968ba71de89d53e435135ab4482433c4a25a8
rcmerci <rcmerci@gmail.com>
2024-09-05 02:12:23 +0800

    test(rtc): add helpers for syncing messages between test-clients

--- src/rtc_e2e_test/helper.cljs
+++ src/rtc_e2e_test/helper.cljs
@@ -1,10 +1,18 @@
 (ns helper
-  (:require [const]
+  (:require [cljs.test :as t :refer [is]]
+            [cognitect.transit :as transit]
+            [const]
+            [datascript.core :as d]
             [frontend.common.missionary-util :as c.m]
+            [frontend.worker.rtc.client-op :as client-op]
             [frontend.worker.rtc.core :as rtc.core]
-            [missionary.core :as m]
+            [frontend.worker.rtc.log-and-state :as rtc-log-and-state]
             [frontend.worker.state :as worker-state]
-            [meander.epsilon :as me]))
+            [logseq.db :as ldb]
+            [logseq.db.frontend.order :as db-order]
+            [logseq.outliner.batch-tx :as batch-tx]
+            [meander.epsilon :as me]
+            [missionary.core :as m]))
 
 (def new-task--upload-example-graph
   (rtc.core/new-task--upload-graph const/test-token const/test-repo const/test-graph-name))
@@ -57,17 +65,17 @@
 (defn new-task--download-graph
   [graph-uuid graph-name]
   (m/sp
-   (let [download-info-uuid (m/? (rtc.core/new-task--request-download-graph const/test-token graph-uuid))
-         result (m/? (rtc.core/new-task--wait-download-info-ready const/test-token download-info-uuid graph-uuid 60000))
-         {:keys [_download-info-uuid
-                 download-info-s3-url
-                 _download-info-tx-instant
-                 _download-info-t
-                 _download-info-created-at]} result]
-     (when (= result :timeout)
-       (throw (ex-info "wait download-info-ready timeout" {})))
-     (m/? (rtc.core/new-task--download-graph-from-s3
-           graph-uuid graph-name download-info-s3-url)))))
+    (let [download-info-uuid (m/? (rtc.core/new-task--request-download-graph const/test-token graph-uuid))
+          result (m/? (rtc.core/new-task--wait-download-info-ready const/test-token download-info-uuid graph-uuid 60000))
+          {:keys [_download-info-uuid
+                  download-info-s3-url
+                  _download-info-tx-instant
+                  _download-info-t
+                  _download-info-created-at]} result]
+      (when (= result :timeout)
+        (throw (ex-info "wait download-info-ready timeout" {})))
+      (m/? (rtc.core/new-task--download-graph-from-s3
+            graph-uuid graph-name download-info-s3-url)))))
 
 (defn get-downloaded-test-conn
   []
@@ -83,3 +91,54 @@
 
     [?op-type _ {:block-uuid ?block-uuid}]
     [?op-type ?block-uuid]))
+
+(defn new-task--wait-all-client-ops-sent
+  [& {:keys [timeout] :or {timeout 10000}}]
+  (m/sp
+    (let [r (m/? (m/timeout
+                  (m/reduce (fn [_ v]
+                              (when (and (= :rtc.log/push-local-update (:type v))
+                                         (empty? (client-op/get-all-ops const/downloaded-test-repo)))
+                                (is (nil? (:ex-data v)))
+                                (reduced v)))
+                            rtc-log-and-state/rtc-log-flow)
+                  timeout :timeout))]
+      (is (not= :timeout r)))))
+
+(defn new-task--send-message-to-other-client
+  [message]
+  (m/sp
+    (let [conn (get-downloaded-test-conn)
+          _ (assert (some? conn))
+          content-page-id (:db/id (ldb/get-page @conn "contents"))
+          _ (assert content-page-id)
+          sorted-blocks (ldb/sort-by-order (ldb/get-page-blocks @conn content-page-id))
+          min-order (db-order/gen-key nil (:block/order (first sorted-blocks)))
+          tx-data [{:block/uuid (random-uuid)
+                    :block/parent content-page-id
+                    :block/order min-order
+                    :block/title (transit/write (transit/writer :json) message)
+                    :block/page content-page-id
+                    :block/format :markdown
+                    :block/updated-at 1724836490810
+                    :block/created-at 1724836490810}]]
+      (batch-tx/with-batch-tx-mode conn {:e2e-test const/downloaded-test-repo :skip-store-conn true}
+        (d/transact! conn tx-data))
+      (m/? (new-task--wait-all-client-ops-sent)))))
+
+(defn new-task--wait-message-from-other-client
+  "Return a task that return message from other client"
+  [block-title-pred-fn & {:keys [retry-message retry-count] :or {retry-count 4}}]
+  (c.m/backoff
+   (take retry-count c.m/delays)
+   (m/sp
+     (let [conn (get-downloaded-test-conn)
+           _ (assert (some? conn))
+           content-page-id (:db/id (ldb/get-page @conn "contents"))
+           _ (assert content-page-id)
+           first-block (first (ldb/sort-by-order (ldb/get-page-blocks @conn content-page-id)))
+           first-block-title (some->> (:block/title first-block) (transit/read (transit/reader :json)))]
+       (when-not (and (some? first-block-title)
+                      (block-title-pred-fn first-block-title))
+         (throw (ex-info (str "wait message from other client " retry-message) {:missionary/retry true})))
+       first-block-title))))
