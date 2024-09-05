992e00ed16747ccf2855a1dd781cf123242b1f72
Gabriel Horner <gabriel@logseq.com>
2024-09-05 14:42:26 -0400

    fix: ignore dev lint failing

--- src/rtc_e2e_test/client_steps.cljs
+++ src/rtc_e2e_test/client_steps.cljs
@@ -123,6 +123,7 @@
    :client2
    (helper/new-task--wait-message-from-other-client #(= "test-send-message-to-other-client" %))})
 
+#_:clj-kondo/ignore
 (def ^:private step5
   "client1:
   - insert some blocks in page2
