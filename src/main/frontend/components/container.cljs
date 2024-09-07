78dcb625cbfdf87f63b615a27b71d730f8bfc1f0
YU <111847207+YU000jp@users.noreply.github.com>
2024-09-07 17:47:53 +0900

    Add favorite button in left menu

--- src/main/frontend/components/container.cljs
+++ src/main/frontend/components/container.cljs
@@ -162,7 +162,9 @@
 (rum/defc favorites < rum/reactive
   [t]
   (let [_favorites-updated? (state/sub :favorites/updated?)
-        favorite-entities (page-handler/get-favorites)]
+        favorite-entities (page-handler/get-favorites)
+        current-page (state/get-current-page)
+        favorited? (page-handler/favorited? current-page)]
     (nav-content-item
       [:a.flex.items-center.text-sm.font-medium.rounded-md.wrap-th
       (ui/icon "star" {:size 16})
@@ -174,18 +176,28 @@
       (fn [e]
         (rfe/push-state :page {:name "Favorites"})
         (util/stop e))}
-     (when (seq favorite-entities)
-       (let [favorite-items (map
-                             (fn [e]
-                               (let [icon (icon/get-node-icon e {})]
-                                 {:id (str (:db/id e))
-                                  :value (:block/uuid e)
-                                  :content [:li.favorite-item (page-name e icon false)]}))
-                             favorite-entities)]
-         (dnd-component/items favorite-items
-                              {:on-drag-end (fn [favorites']
-                                              (page-handler/<reorder-favorites! favorites'))
-                               :parent-node :ul.favorites.text-sm}))))))
+     [(when current-page
+        (when-not favorited?
+          [:div.favorite-item.flex.items-center.cursor.text-sm
+           {:on-click (fn []
+                        (page-handler/<favorite-page! current-page))}
+           [[:button.button.icon
+             (ui/icon "star" {:class "icon" :size 12})]
+            [:span
+             {:style {:font-size "0.8em"}}
+             (t :page/add-to-favorites)]]]))
+      (when (seq favorite-entities)
+        (let [favorite-items (map
+                              (fn [e]
+                                (let [icon (icon/get-node-icon e {})]
+                                  {:id (str (:db/id e))
+                                   :value (:block/uuid e)
+                                   :content [:li.favorite-item (page-name e icon false)]}))
+                              favorite-entities)]
+          (dnd-component/items favorite-items
+                               {:on-drag-end (fn [favorites']
+                                               (page-handler/<reorder-favorites! favorites'))
+                                :parent-node :ul.favorites.text-sm})))])))
 
 (rum/defc recent-pages < rum/reactive db-mixins/query
   [t]
