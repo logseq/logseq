e3555f524d4501e279a9d5efc99dd334821bb2a4
Tienson Qin <tiensonqin@gmail.com>
2024-10-14 04:23:25 +0800

    fix: order

--- deps/db/src/logseq/db/frontend/malli_schema.cljs
+++ deps/db/src/logseq/db/frontend/malli_schema.cljs
@@ -117,8 +117,8 @@
                                       ;; This allows block types like property-value-block to require properties in
                                       ;; their schema that they depend on
                                       (not= :logseq.property/created-from-property k)
-                                      (d/entity db k)
-                                      (not (db-property/db-attribute-properties k)))]
+                                      (not (db-property/db-attribute-properties k))
+                                      (d/entity db k))]
                  (update m :block/properties (fnil conj [])
                          ;; use explicit call to be nbb compatible
                          [(let [closed-values (entity-plus/lookup-kv-then-entity property :property/closed-values)]
