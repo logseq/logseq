79f2691eca6119b7b7e3400cdd60ea11e500d932
Gabriel Horner <gabriel@logseq.com>
2024-11-11 17:17:09 -0500

    fix: new file graphs keep failing on migration 44
    File graphs fail on it b/c of d/datoms lookup on db graph properties

--- src/main/frontend/worker/db/migrate.cljs
+++ src/main/frontend/worker/db/migrate.cljs
@@ -146,41 +146,42 @@
 
 (defn- update-hl-color-and-page
   [conn _search-db]
-  (let [db @conn
-        hl-color (d/entity db :logseq.property.pdf/hl-color)
-        hl-page (d/entity db :logseq.property.pdf/hl-page)
-        existing-colors (d/datoms db :avet :logseq.property.pdf/hl-color)
-        color-update-tx (mapcat
+  (when (ldb/db-based-graph? @conn)
+   (let [db @conn
+         hl-color (d/entity db :logseq.property.pdf/hl-color)
+         hl-page (d/entity db :logseq.property.pdf/hl-page)
+         existing-colors (d/datoms db :avet :logseq.property.pdf/hl-color)
+         color-update-tx (mapcat
+                          (fn [datom]
+                            (let [block (d/entity db (:v datom))
+                                  color-ident (keyword "logseq.property" (str "color." (:block/title block)))]
+                              (if block
+                                [[:db/add (:e datom) :logseq.property.pdf/hl-color color-ident]
+                                 [:db/retractEntity (:db/id block)]]
+                                [[:db/retract (:e datom) :logseq.property.pdf/hl-color]])))
+                          existing-colors)
+         page-datoms (d/datoms db :avet :logseq.property.pdf/hl-page)
+         page-update-tx (mapcat
                          (fn [datom]
                            (let [block (d/entity db (:v datom))
-                                 color-ident (keyword "logseq.property" (str "color." (:block/title block)))]
-                             (if block
-                               [[:db/add (:e datom) :logseq.property.pdf/hl-color color-ident]
+                                 value (db-property/property-value-content block)]
+                             (if (integer? value)
+                               [[:db/add (:e datom) :logseq.property.pdf/hl-page value]
                                 [:db/retractEntity (:db/id block)]]
-                               [[:db/retract (:e datom) :logseq.property.pdf/hl-color]])))
-                         existing-colors)
-        page-datoms (d/datoms db :avet :logseq.property.pdf/hl-page)
-        page-update-tx (mapcat
-                        (fn [datom]
-                          (let [block (d/entity db (:v datom))
-                                value (db-property/property-value-content block)]
-                            (if (integer? value)
-                              [[:db/add (:e datom) :logseq.property.pdf/hl-page value]
-                               [:db/retractEntity (:db/id block)]]
-                              [[:db/retract (:e datom) :logseq.property.pdf/hl-page]])))
-                        page-datoms)]
+                               [[:db/retract (:e datom) :logseq.property.pdf/hl-page]])))
+                         page-datoms)]
     ;; update schema first
-    (d/transact! conn
-                 (concat
-                  [{:db/ident :logseq.property.pdf/hl-page
-                    :block/schema {:type :raw-number}}
-                   [:db/retract (:db/id hl-page) :db/valueType]
-                   {:db/ident :logseq.property.pdf/hl-color
-                    :block/schema {:type :default}}]
-                  (db-property-build/closed-values->blocks
-                   (assoc hl-color :closed-values (get-in db-property/built-in-properties [:logseq.property.pdf/hl-color :closed-values])))))
+     (d/transact! conn
+                  (concat
+                   [{:db/ident :logseq.property.pdf/hl-page
+                     :block/schema {:type :raw-number}}
+                    [:db/retract (:db/id hl-page) :db/valueType]
+                    {:db/ident :logseq.property.pdf/hl-color
+                     :block/schema {:type :default}}]
+                   (db-property-build/closed-values->blocks
+                    (assoc hl-color :closed-values (get-in db-property/built-in-properties [:logseq.property.pdf/hl-color :closed-values])))))
     ;; migrate data
-    (concat color-update-tx page-update-tx)))
+     (concat color-update-tx page-update-tx))))
 
 (defn- store-url-value-in-block-title
   [conn _search-db]
