2fe80396446b9a69f85ad0aa21722ffc08417bcd
charlie <xyhp915@qq.com>
2024-11-15 14:53:00 +0800

    enhance(ui): polish class page sections

--- src/main/frontend/components/page.cljs
+++ src/main/frontend/components/page.cljs
@@ -224,34 +224,35 @@
           db-based? (config/db-based-graph? repo)]
       [:<>
        (when (and db-based? (or (ldb/class? block) (ldb/property? block)))
-         [:div.font-medium.mt-8.pb-3.opacity-50.ml-2
-          "Notes"])
+         [:div.mt-4.ml-2.-mb-1
+          (ui/foldable
+            [:div.font-medium.opacity-50 {:class "pl-0.5"} "Notes"]
+            [:div.ml-1.-mb-2
+             (cond
+               (and
+                 (not block?)
+                 (empty? children) page-e)
+               (dummy-block page-e)
 
-       [:div.ml-1
-        (cond
-          (and
-           (not block?)
-           (empty? children) page-e)
-          (dummy-block page-e)
-
-          :else
-          (let [document-mode? (state/sub :document/mode?)
-                hiccup-config (merge
-                               {:id (if block? (str block-id) page-name)
-                                :db/id (:db/id block)
-                                :block? block?
-                                :editor-box editor/box
-                                :document/mode? document-mode?}
-                               config)
-                config (common-handler/config-with-document-mode hiccup-config)
-                blocks (if block? [block] (db/sort-by-order children block))]
-            [:div
-             (page-blocks-inner page-e blocks config sidebar? whiteboard? block-id)
-             (when-not config/publishing?
-               (let [args (if block-id
-                            {:block-uuid block-id}
-                            {:page page-name})]
-                 (add-button args (:container-id config))))]))]])))
+               :else
+               (let [document-mode? (state/sub :document/mode?)
+                     hiccup-config (merge
+                                     {:id (if block? (str block-id) page-name)
+                                      :db/id (:db/id block)
+                                      :block? block?
+                                      :editor-box editor/box
+                                      :document/mode? document-mode?}
+                                     config)
+                     config (common-handler/config-with-document-mode hiccup-config)
+                     blocks (if block? [block] (db/sort-by-order children block))]
+                 [:div
+                  (page-blocks-inner page-e blocks config sidebar? whiteboard? block-id)
+                  (when-not config/publishing?
+                    (let [args (if block-id
+                                 {:block-uuid block-id}
+                                 {:page page-name})]
+                      (add-button args (:container-id config))))]))]
+            {:disable-on-pointer-down? true})])])))
 
 (rum/defc today-queries < rum/reactive
   [repo today? sidebar?]
