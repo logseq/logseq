b1cbc4aa351bfd2af14c799f9cba48a87ef1e8dd
Gabriel Horner <gabriel@logseq.com>
2024-11-20 10:39:21 -0500

    fix: docs graph fails to import with both property*
    options enabled

--- deps/graph-parser/test/logseq/graph_parser/exporter_test.cljs
+++ deps/graph-parser/test/logseq/graph_parser/exporter_test.cljs
@@ -609,14 +609,18 @@
 
 (deftest-async export-files-with-property-parent-classes-option
   (p/let [file-graph-dir "test/resources/exporter-test-graph"
-          files (mapv #(node-path/join file-graph-dir %) ["pages/CreativeWork.md" "pages/Movie.md" "pages/type.md"])
+          files (mapv #(node-path/join file-graph-dir %) ["pages/CreativeWork.md" "pages/Movie.md" "pages/type.md"
+                                                          "pages/Whiteboard___Tool.md" "pages/Whiteboard___Arrow_head_toggle.md"])
           conn (db-test/create-conn)
-          _ (import-files-to-db files conn {:property-parent-classes ["parent"]})]
+          _ (import-files-to-db files conn {:property-parent-classes ["parent"]
+                                            ;; Also add this option to trigger some edge cases with namespace pages
+                                            :property-classes ["type"]})]
 
     (is (empty? (map :entity (:errors (db-validate/validate-db! @conn))))
         "Created graph has no validation errors")
 
-    (is (= #{:user.class/Movie :user.class/CreativeWork :user.class/Thing}
+    (is (= #{:user.class/Movie :user.class/CreativeWork :user.class/Thing
+             :user.class/Class :user.class/Tool :user.class/Whiteboard___Tool}
            (->> @conn
                 (d/q '[:find [?ident ...]
                        :where [?b :block/type "class"] [?b :db/ident ?ident] (not [?b :logseq.property/built-in?])])
