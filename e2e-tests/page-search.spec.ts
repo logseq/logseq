911b373e04a1a987a8c8b3b388b03da1d30412e5
Tienson Qin <tiensonqin@gmail.com>
2024-12-11 01:34:47 +0800

    fix: e2e tests

--- e2e-tests/page-search.spec.ts
+++ e2e-tests/page-search.spec.ts
@@ -39,7 +39,7 @@ test('Search page and blocks (diacritics)', async ({ page, block }) => {
 
   // check if diacritics are indexed
   const results = await searchPage(page, 'Einführung in die Allgemeine Sprachwissenschaft' + rand)
-  await expect(results.length).toEqual(6) // 1 page + 2 block + 2 page content + 1 current page
+  await expect(results.length).toEqual(4) // 1 page + 2 block + 1 current page
   await closeSearchBox(page)
 })
 
