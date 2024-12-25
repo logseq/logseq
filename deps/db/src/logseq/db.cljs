bee0e27d19a8774864cea3883ef7d406a9916fd7
Tienson Qin <tiensonqin@gmail.com>
2024-12-25 15:41:00 +0800

    fix: tests

--- deps/db/src/logseq/db.cljs
+++ deps/db/src/logseq/db.cljs
@@ -57,9 +57,8 @@
   ([repo-or-conn tx-data]
    (transact! repo-or-conn tx-data nil))
   ([repo-or-conn tx-data tx-meta]
-   (when (or goog.DEBUG
-             ;; test
-             (exists? js/process))
+   (when (or (exists? js/process)
+             goog.DEBUG)
      (assert-no-entities tx-data))
    (let [tx-data (map (fn [m]
                         (if (map? m)
