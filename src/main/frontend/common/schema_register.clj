f5ffcf1d04cd88bd70e570c06bf24b44cddf9b62
rcmerci <rcmerci@gmail.com>
2025-01-08 17:16:54 +0800

    chore: update docstring of defkeyword

--- src/main/frontend/common/schema_register.clj
+++ src/main/frontend/common/schema_register.clj
@@ -2,9 +2,11 @@
   "Macro 'defkeyword' to def keyword with docstring and malli-schema.
    Used by frontend and worker namespaces")
 
-
 (defmacro defkeyword
-  "Define keyword with docstring and malli-schema"
+  "Define keyword with docstring and malli-schema.
+  How 'find keyword definition' works?
+  clojure-lsp treat keywords defined by `cljs.spec.alpha/def` as keyword-definition.
+  Adding a :lint-as `defkeyword` -> `cljs.spec.alpha/def` in clj-kondo config make it works."
   [kw docstring & [optional-malli-schema]]
   (assert (keyword? kw) "must be keyword")
   (assert (some? docstring) "must have 'docstring' arg")
