ddf921f267d047ed0e1fb675338ae0a3d03a3a56
Gabriel Horner <gabriel@logseq.com>
2025-01-22 17:01:35 -0500

    fix: mention 'task' on scheduled property
    as it also belongs to tasks. Also tweak language so that it also starts
    with 'Repeat'. Using consistent language helps users understand behavior
    that changes depending on configuration

--- src/main/frontend/components/property/value.cljs
+++ src/main/frontend/components/property/value.cljs
@@ -253,8 +253,8 @@
                                                                                                (:db/id property))
                                                       (db-property-handler/remove-block-property! (:db/id block)
                                                                                                   :logseq.task/scheduled-on-property)))))]
-       (if (= :logseq.task/deadline (:db/ident property))
-         [:div "Set as repeated task"]
+       (if (#{:logseq.task/deadline :logseq.task/scheduled} (:db/ident property))
+         [:div "Repeat task"]
          [:div "Repeat " (if (= :date (get-in property [:block/schema :type])) "date" "datetime")])]]
      [:div.flex.flex-row.gap-2
       [:div.flex.text-muted-foreground.mr-4
