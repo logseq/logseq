5983bd047378357888e6ea06b954b1416feb1c80
Gabriel Horner <gabriel@logseq.com>
2025-01-28 15:34:26 -0500

    enhance: update sqlite.build EDN to generate tx
    for existing blocks and pages e.g. importing

--- deps/db/test/logseq/db/sqlite/build_test.cljs
+++ deps/db/test/logseq/db/sqlite/build_test.cljs
@@ -1,8 +1,10 @@
 (ns logseq.db.sqlite.build-test
-  (:require [cljs.test :refer [deftest is]]
-            [logseq.db.sqlite.build :as sqlite-build]
+  (:require [cljs.test :refer [deftest is testing]]
+            [datascript.core :as d]
             [logseq.db.frontend.property :as db-property]
-            [logseq.db.test.helper :as db-test]))
+            [logseq.db.sqlite.build :as sqlite-build]
+            [logseq.db.test.helper :as db-test]
+            [datascript.impl.entity :as de]))
 
 (deftest build-tags
   (let [conn (db-test/create-conn)
@@ -64,3 +66,75 @@
                 :logseq.property/background-image
                 db-property/property-value-content))
         "built-in :default property is created and correctly associated to a block")))
+
+(defn- readable-properties
+  [ent]
+  (->> (db-property/properties ent)
+       (mapv (fn [[k v]]
+               [k
+                (cond
+                  (= :block/tags k)
+                  (mapv :db/ident v)
+                  (and (set? v) (every? de/entity? v))
+                  (set (map db-property/property-value-content v))
+                  (de/entity? v)
+                  (or (:db/ident v) (db-property/property-value-content v))
+                  :else
+                  v)]))
+       (into {})))
+
+(deftest build-for-existing-blocks
+  (let [conn (db-test/create-conn)
+        _ (sqlite-build/create-blocks
+           conn
+           {:properties {:p1 {}}
+            :classes {:MyClass {}}
+            :pages-and-blocks
+            [{:page {:block/title "page1"}
+              :blocks [{:block/title "block 1"}
+                       {:block/title "block 2"}]}]})
+        block (db-test/find-block-by-content @conn "block 1")
+        block2 (db-test/find-block-by-content @conn "block 2")
+        {:keys [init-tx block-props-tx]}
+        (sqlite-build/build-blocks-tx
+         {:pages-and-blocks [{:page (select-keys (:block/page block) [:block/uuid :block/title])
+                              :blocks [(merge {:block/title "imported task" :block/uuid (:block/uuid block)}
+                                              {:build/properties {:logseq.task/status :logseq.task/status.todo}
+                                               :build/tags [:logseq.class/Task]})]}]
+          :build-existing-tx? true})
+        _ (d/transact! conn init-tx)
+        _ (d/transact! conn block-props-tx)
+        updated-block (d/entity @conn [:block/uuid (:block/uuid block)])
+        {init-tx2 :init-tx block-props-tx2 :block-props-tx :as _tx}
+        (sqlite-build/build-blocks-tx
+         {:pages-and-blocks [{:page (select-keys (:block/page block2) [:block/uuid :block/title])
+                              :blocks [(merge {:block/title "imported block" :block/uuid (:block/uuid block2)}
+                                              {:build/properties {:user.property/p1 "foo"}
+                                               :build/tags [:user.class/MyClass]})]}]
+          :properties {:user.property/p1 {:logseq.property/type :default}}
+          :build-existing-tx? true})
+        _ (d/transact! conn init-tx2)
+        _ (d/transact! conn block-props-tx2)
+        updated-block2 (d/entity @conn [:block/uuid (:block/uuid block2)])]
+;;     (cljs.pprint/pprint _tx)
+    (testing "block with built-in properties and tags"
+      (is (= []
+             (filter #(or (:db/id %) (:db/ident %))
+                     (concat init-tx block-props-tx)))
+          "Tx doesn't try to create new blocks or modify existing idents")
+      (is (= "imported task" (:block/title updated-block)))
+      (is (= {:block/tags [:logseq.class/Task]
+              :logseq.task/status :logseq.task/status.todo}
+             (readable-properties updated-block))
+          "Block's properties and tags are updated"))
+
+    (testing "block with existing user properties and tags"
+      (is (= []
+             (filter #(or (:db/id %) (:db/ident %))
+                     (concat init-tx2 block-props-tx2)))
+          "Tx doesn't try to create new blocks or modify existing idents")
+      (is (= "imported block" (:block/title updated-block2)))
+      (is (= {:block/tags [:user.class/MyClass]
+              :user.property/p1 "foo"}
+             (readable-properties updated-block2))
+          "Block's properties and tags are updated"))))
