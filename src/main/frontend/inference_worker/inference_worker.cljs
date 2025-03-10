691e121261bf85c114d6adbf33d5bad339915758
rcmerci <rcmerci@gmail.com>
2025-03-10 14:46:42 +0800

    upgrade shadowcljs

--- src/main/frontend/inference_worker/inference_worker.cljs
+++ src/main/frontend/inference_worker/inference_worker.cljs
@@ -1,6 +1,6 @@
 (ns frontend.inference-worker.inference-worker
   "Worker used for text embedding and vector-db"
-  (:require
+  (:require ["@huggingface/transformers" :as transformers]
             [lambdaisland.glogi.console :as glogi-console]
             [lambdaisland.glogi :as log]))
 
