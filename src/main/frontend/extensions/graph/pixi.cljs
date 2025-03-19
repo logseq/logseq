8efc8406a63311e906b97390e85924e7f290b871
lhewett946 <lhewett946@gmail.com>
2025-03-19 23:57:07 +0000

    Fix lint error with unused variable colors

--- src/main/frontend/extensions/graph/pixi.cljs
+++ src/main/frontend/extensions/graph/pixi.cljs
@@ -15,18 +15,6 @@
 
 (def Graph (gobj/get graphology "Graph"))
 
-(defonce colors
-  ["#1f77b4"
-   "#ff7f0e"
-   "#2ca02c"
-   "#d62728"
-   "#9467bd"
-   "#8c564b"
-   "#e377c2"
-   "#7f7f7f"
-   "#bcbd22"
-   "#17becf"])
-
 (defn default-style
   [dark?]
   {:node {:size   (fn [node]
