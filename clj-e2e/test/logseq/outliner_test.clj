b236964ed6fe357c44944c0de204ca1a8f795380
Tienson Qin <tiensonqin@gmail.com>
2025-04-21 18:48:26 +0800

    add indent outdent test

--- clj-e2e/test/logseq/outliner_test.clj
+++ clj-e2e/test/logseq/outliner_test.clj
@@ -12,6 +12,7 @@
   (w/with-page-open
     (w/make-page {:headless headless
                   :persistent false})
+    (w/navigate "http://localhost:3001")
     (f)))
 
 (use-fixtures :once open-page)
@@ -24,12 +25,12 @@
   [text]
   (w/fill "*:focus" text))
 
-(defn- search
-  [text]
+(defn- new-page
+  [title]
   (w/wait-for :#search-button {})
   (w/click :#search-button)
   (wait-timeout 100)
-  (input text)
+  (input title)
   (wait-timeout 100)
   (w/click [(ws/text "Create page") (ws/nth= "0")])
   ;; FIXME: Enter doesn't work
@@ -50,18 +51,43 @@
   [q]
   (w/count* (w/-query q)))
 
+(defn- blocks-count
+  []
+  (count-elements ".ls-block"))
+
 (deftest create-test-page-and-insert-blocks
-  ;; Navigate to a page.
-  (w/navigate "http://localhost:3001")
-  (search "Test")
+  (new-page "Test")
   ;; a page block and a child block
-  (is (= 2 (count-elements ".ls-block")))
+  (is (= 2 (blocks-count)))
   (new-block {:current-text "first block"
               :next-text "second block"})
   (exit-edit)
-  (is (= 3 (w/count* (w/-query ".ls-block"))))
+  (is (= 3 (blocks-count)))
   ;; Pause to use the repl, e.g. example in the comment below
-  (repl/pause))
+  ;; (repl/pause)
+  )
+
+(defn- bounding-xy
+  [locator]
+  (let [box (.boundingBox locator)]
+    [(.-x box) (.-y box)]))
+
+(defn- indent-outdent
+  [indent?]
+  (let [editor (w/-query ".editor-wrapper textarea")
+        [x1 _] (bounding-xy editor)
+        _ (w/keyboard-press (if indent? "Tab" "Shift+Tab"))
+        [x2 _] (bounding-xy editor)]
+    (if indent?
+      (is (< x1 x2))
+      (is (> x1 x2)))))
+
+(deftest indent-and-outdent
+  (new-page "indent outdent test")
+  (new-block {:current-text "b1"
+              :next-text "b2"})
+  (indent-outdent true)
+  (indent-outdent false))
 
 (comment
 
