7f9517c7d8260a3e80f1d66a558af4f68659d1ee
Tienson Qin <tiensonqin@gmail.com>
2025-04-22 19:02:28 +0800

    fix: (rseq nil) throws error

--- deps/db/src/logseq/db/common/sqlite.cljs
+++ deps/db/src/logseq/db/common/sqlite.cljs
@@ -285,13 +285,14 @@
 
 (defn get-recent-updated-pages
   [db]
-  (->> (d/datoms db :avet :block/updated-at)
-       rseq
-       (keep (fn [datom]
-               (let [e (d/entity db (:e datom))]
-                 (when (and (common-entity-util/page? e) (not (entity-util/hidden? e)))
-                   e))))
-       (take 30)))
+  (some->>
+   (d/datoms db :avet :block/updated-at)
+   rseq
+   (keep (fn [datom]
+           (let [e (d/entity db (:e datom))]
+             (when (and (common-entity-util/page? e) (not (entity-util/hidden? e)))
+               e))))
+   (take 30)))
 
 (defn get-initial-data
   "Returns current database schema and initial data.
