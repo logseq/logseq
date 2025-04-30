dcb6c4fb3a2acc4fccc6d436c0ea314c49720ebe
rcmerci <rcmerci@gmail.com>
2025-04-30 18:10:19 +0800

    test(e2e): update rtc-basic-test

--- clj-e2e/src/logseq/e2e/page.clj
+++ clj-e2e/src/logseq/e2e/page.clj
@@ -1,6 +1,5 @@
 (ns logseq.e2e.page
-  (:require [logseq.e2e.keyboard :as k]
-            [logseq.e2e.util :as util]
+  (:require [logseq.e2e.util :as util]
             [wally.main :as w]
             [wally.selectors :as ws]))
 
@@ -12,14 +11,3 @@
   (util/search title)
   (w/click [(ws/text "Create page") (ws/nth= "0")])
   (util/wait-editor-visible))
-
-(defn wait-for-page-created
-  [page-name]
-  (loop [i 3]
-    (util/search page-name)
-    (util/wait-timeout 1000)
-    (when (and (pos? i)
-               (not (.isVisible (.first (w/-query (format "[data-testid='%s']" page-name))))))
-      (k/esc)
-      (recur (dec i))))
-  (k/esc))
--- clj-e2e/test/logseq/e2e/rtc_basic_test.clj
+++ clj-e2e/test/logseq/e2e/rtc_basic_test.clj
@@ -5,16 +5,15 @@
    [logseq.e2e.fixtures :as fixtures :refer [*page1 *page2]]
    [logseq.e2e.graph :as graph]
    [logseq.e2e.page :as page]
+   [logseq.e2e.rtc :as rtc]
    [logseq.e2e.util :as util]
    [wally.main :as w]
-   [wally.repl :as repl]
-   [logseq.e2e.assert :as assert]))
+   [wally.repl :as repl]))
 
 (use-fixtures :once fixtures/open-2-pages)
 
 (deftest rtc-basic-test
   (let [graph-name (str "rtc-graph-" (.toEpochMilli (java.time.Instant/now)))
-        page-name "rtc-test-page1"
         page-names (map #(str "rtc-test-page" %) (range 4))]
     (testing "open 2 app instances, add a rtc graph, check this graph available on other instance"
       (cp/prun!
@@ -29,11 +28,13 @@
         (graph/switch-graph graph-name true)))
     (testing "do some operations on logseq pages"
       (doseq [page-name page-names]
-        (w/with-page @*page1
-          (page/new-page page-name))
-        (w/with-page @*page2
-          (page/wait-for-page-created page-name)
-          (util/search-and-click page-name))))
+        (let [{:keys [_local-tx remote-tx]}
+              (w/with-page @*page1
+                (rtc/with-wait-tx-updated
+                  (page/new-page page-name)))]
+          (w/with-page @*page2
+            (rtc/wait-tx-update-to remote-tx)
+            (util/search-and-click page-name)))))
 
     (testing "cleanup"
       (w/with-page @*page2
