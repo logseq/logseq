logseq/logseq

commit aa2bf82b086e3a340942a4f39549a63253375d3c

fix: blank journals page

src/main/frontend/components/container.cljs
@@ -708,7 +708,6 @@
   (let [default-home (get-default-home-if-valid)
         current-repo (state/sub :git/current-repo)
         loading-files? (when current-repo (state/sub [:repo/loading-files? current-repo]))
-        latest-journals (db/get-latest-journals (state/get-current-repo) 1)
         graph-parsing-state (state/sub [:graph/parsing-state current-repo])]
     (cond
       (or
@@ -727,20 +726,17 @@
               (:page default-home))
          (route-handler/redirect-to-page! (:page default-home))
 
-         (and config/publishing?
-              (not default-home)
-              (empty? latest-journals))
+         (let [latest-journals (db/get-latest-journals (state/get-current-repo) 1)]
+           (and config/publishing?
+                (not default-home)
+                (empty? latest-journals)))
          (route-handler/redirect! {:to :all-pages})
 
          loading-files?
          (ui/loading (t :loading-files))
 
-         (seq latest-journals)
-         (journal/all-journals)
-
-         ;; FIXME: why will this happen?
          :else
-         [:div])])))
+         (journal/all-journals))])))
 
 (defn- hide-context-menu-and-clear-selection
   [e]

commit 121312b8ef9bf32c529f7a506a89e3fed4a42323

fix: journals toggle still displayed journals
A regression probably from aa2bf82b086e3a340942a4f39549a63253375d3c.
Also fix journals still showing up in left sidebar

src/main/frontend/components/container.cljs
@@ -248,8 +248,9 @@
              (shui/tabler-icon "filter-edit" {:size 14})]}
      [:div.sidebar-navigations.flex.flex-col.mt-1
        ;; required custom home page
-      (let [page (:page default-home)]
-        (if (and page (not (state/enable-journals? (state/get-current-repo))))
+      (let [page (:page default-home)
+            enable-journals? (state/enable-journals? (state/get-current-repo))]
+        (if (and page (not enable-journals?))
           (sidebar-item
            {:class "home-nav"
             :title page
@@ -260,17 +261,18 @@
             :icon "home"
             :shortcut :go/home})
 
-          (sidebar-item
-           {:class "journals-nav"
-            :active (and (not srs-open?)
-                         (or (= route-name :all-journals) (= route-name :home)))
-            :title (t :left-side-bar/journals)
-            :on-click-handler (fn [e]
-                                (if (gobj/get e "shiftKey")
-                                  (route-handler/sidebar-journals!)
-                                  (route-handler/go-to-journals!)))
-            :icon "calendar"
-            :shortcut :go/journals})))
+          (when enable-journals?
+           (sidebar-item
+            {:class "journals-nav"
+             :active (and (not srs-open?)
+                          (or (= route-name :all-journals) (= route-name :home)))
+             :title (t :left-side-bar/journals)
+             :on-click-handler (fn [e]
+                                 (if (gobj/get e "shiftKey")
+                                   (route-handler/sidebar-journals!)
+                                   (route-handler/go-to-journals!)))
+             :icon "calendar"
+             :shortcut :go/journals}))))
 
       (for [nav checked-navs]
         (cond
@@ -725,10 +727,11 @@
               (:page default-home))
          (route-handler/redirect-to-page! (:page default-home))
 
-         (let [latest-journals (db/get-latest-journals (state/get-current-repo) 1)]
-           (and config/publishing?
-                (not default-home)
-                (empty? latest-journals)))
+         (or (not (state/enable-journals? current-repo))
+             (let [latest-journals (db/get-latest-journals (state/get-current-repo) 1)]
+               (and config/publishing?
+                    (not default-home)
+                    (empty? latest-journals))))
          (route-handler/redirect! {:to :all-pages})
 
          loading-files?