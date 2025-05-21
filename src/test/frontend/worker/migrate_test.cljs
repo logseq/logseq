b55c55831f9c55e757c4ba4b1e8012bf6315b692
Gabriel Horner <gabriel@logseq.com>
2025-05-21 16:11:53 -0400

    chore: add test for migration bug
    in f4d3dca19c33f2facd7d8608e590055234ff8bab. Put the fix in feat/db
    since it should be deploy asap and test in this branch
    since only this branch has the ability to test migrations

--- src/test/frontend/worker/migrate_test.cljs
+++ src/test/frontend/worker/migrate_test.cljs
@@ -87,3 +87,19 @@
               {:db/id 158,
                ;; :block/order "a9"
                }])))))
+
+(deftest ensure-built-in-data-exists!
+  (let [db-transit (str (fs-node/readFileSync "src/test/migration/64.8.transit"))
+        db (ldb/read-transit-str db-transit)
+        conn (d/conn-from-db db)
+        initial-version (:kv/value (d/entity @conn :logseq.kv/graph-initial-schema-version))
+        graph-created-at (:kv/value (d/entity @conn :logseq.kv/graph-created-at))
+        _ (assert (= {:major 64 :minor 8} initial-version))
+        _ (assert (some? graph-created-at))
+        _ (db-migrate/ensure-built-in-data-exists! conn)]
+    (is (= initial-version
+           (:kv/value (d/entity @conn :logseq.kv/graph-initial-schema-version)))
+        "Initial version not changed by fn")
+    (is (= graph-created-at
+           (:kv/value (d/entity @conn :logseq.kv/graph-created-at)))
+        "Graph created at not changed by fn")))
