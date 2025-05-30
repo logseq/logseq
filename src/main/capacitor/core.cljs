478783558f24a0d8dd1ec00f303e235cf50c8fd7
rcmerci <rcmerci@gmail.com>
2025-05-30 14:48:14 +0800

    fix(mobile): require ns frontend.background-tasks

--- src/main/capacitor/core.cljs
+++ src/main/capacitor/core.cljs
@@ -2,6 +2,7 @@
   (:require ["react-dom/client" :as rdc]
             [capacitor.components.app :as app]
             [capacitor.nav :as nav]
+            [frontend.background-tasks]
             [frontend.components.page :as page]
             [frontend.components.user.login :as login]
             [frontend.handler :as fhandler]
