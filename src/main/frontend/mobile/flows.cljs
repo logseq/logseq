3843394d4242b4ab0e294d5d9afb3f4718def585
rcmerci <rcmerci@gmail.com>
2025-05-31 17:50:52 +0800

    enhance(mobile): update network-online-event-flow

--- src/main/frontend/mobile/flows.cljs
+++ src/main/frontend/mobile/flows.cljs
@@ -6,7 +6,15 @@
 (def *mobile-app-state (atom nil))
 
 (def mobile-network-status-flow
-  (m/watch *mobile-network-status))
+  (->> (m/watch *mobile-network-status)
+       (m/eduction (map #(js->clj % :keywordize-keys true)))))
 
 (def mobile-app-state
   (m/watch *mobile-app-state))
+
+(comment
+  (c.m/run-background-task
+   ::test
+   (m/reduce (fn [_ x]
+               (prn ::xxx x))
+             (m/latest vector mobile-network-status-flow mobile-app-state))))
