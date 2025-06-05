20ec61641df7f05739a02a7f4dd049a8e611ddc8
Tienson Qin <tiensonqin@gmail.com>
2025-06-06 03:55:40 +0800

    enhance(ux): put pages before blocks when auto-complete node refs
    We can improve it further to display horizontal split views for both
    pages and blocks on desktop.

--- src/main/frontend/components/cmdk/core.cljs
+++ src/main/frontend/components/cmdk/core.cljs
@@ -852,6 +852,7 @@
       {:class "text-xl bg-transparent border-none w-full outline-none px-3 py-3"
        :auto-focus true
        :autoComplete "off"
+       :autoCapitalize false
        :placeholder (input-placeholder false)
        :ref #(when-not @input-ref (reset! input-ref %))
        :on-change debounced-on-change
