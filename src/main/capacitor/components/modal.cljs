e78d630bbb87148c69940bcaa45a988796df177a
charlie <xyhp915@qq.com>
2025-06-11 14:55:59 +0800

    enhance(mobile): add action header for the page block modal

--- src/main/capacitor/components/modal.cljs
+++ src/main/capacitor/components/modal.cljs
@@ -21,9 +21,17 @@
        :mode "ios" ;; force card modal for android
        :expand "block"}
 
-      (ion/content {:class "ion-padding scrolling"}
-        (ui/classic-app-container-wrap
-          (page/page-cp (db/entity [:block/uuid (:block/uuid block)])))
-                  (mobile-bar/mobile-bar)
-                  (when show-action-bar?
-                    (action-bar/action-bar))))))
+      (ion/page
+        {:class "block-modal-page"}
+        (ion/header
+          [:span.opacity-40.active:opacity-60
+           {:on-click #(swap! state/*modal-data assoc :open? false)}
+           (ion/tabler-icon "chevron-down" {:size 16 :stroke 3})]
+          [:span.opacity-40.active:opacity-60 (ion/tabler-icon "dots-vertical" {:size 18 :stroke 2})])
+
+        (ion/content {:class "ion-padding scrolling"}
+          (ui/classic-app-container-wrap
+            (page/page-cp (db/entity [:block/uuid (:block/uuid block)])))
+          (mobile-bar/mobile-bar)
+          (when show-action-bar?
+            (action-bar/action-bar)))))))
