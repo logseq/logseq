d0c54bb2b682de87062edccb0ccd73df239ce8fd
Tienson Qin <tiensonqin@gmail.com>
2025-07-01 23:05:25 +0800

    fix: lint

--- deps/db/src/logseq/db/common/reference.cljs
+++ deps/db/src/logseq/db/common/reference.cljs
@@ -87,7 +87,6 @@
         (let [e (d/entity db eid)]
           (recur (:db/id (:block/parent e)) (conj parents' eid)))))))
 
-
 (defn get-linked-references
   [db id]
   (let [entity (d/entity db id)
