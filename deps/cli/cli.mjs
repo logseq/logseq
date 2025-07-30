75517ea4cc27dcdc2fb0a669f31c61028c793c71
Gabriel Horner <gabriel@logseq.com>
2025-07-30 17:37:24 -0400

    enhance(dev): task to vendor nbb deps for release

--- deps/cli/cli.mjs
+++ deps/cli/cli.mjs
@@ -6,6 +6,7 @@ import { dirname, resolve } from 'path';
 
 const __dirname = fileURLToPath(dirname(import.meta.url));
 addClassPath(resolve(__dirname, 'src'));
+addClassPath(resolve(__dirname, 'vendor/src'));
 const { main } = await loadFile(resolve(__dirname, 'src/logseq/cli.cljs'));
 
 // Expects to be called as node X.js ...
