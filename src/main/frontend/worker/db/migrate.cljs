8c402676a9b0c91b9e3d848b5b3de779c0b4a00e
Tienson Qin <tiensonqin@gmail.com>
2025-08-01 00:09:31 +0800

    fix: page references with same name in one block (#12017)
    * fix: page references with the same name in one block
    
    * fix: empty :block/name for Library pages
    
    * fix: invalid call to uuid-string?
    
    trace suddenly showing up in multiple CLI calls
    
    ---------
    
    Co-authored-by: Gabriel Horner <gabriel@logseq.com>

--- src/main/frontend/worker/db/migrate.cljs
+++ src/main/frontend/worker/db/migrate.cljs
@@ -343,6 +343,17 @@
                      sqlite-create-graph/mark-block-as-built-in))]
     [page]))
 
+(defn- add-missing-page-name
+  [db]
+  (let [pages (d/datoms db :avet :block/name "")]
+    (keep
+     (fn [d]
+       (let [page (d/entity db (:e d))]
+         (when-not (string/blank? (:block/title page))
+           {:db/id (:db/id page)
+            :block/name (common-util/page-name-sanity-lc (:block/title page))})))
+     pages)))
+
 (def schema-version->updates
   "A vec of tuples defining datascript migrations. Each tuple consists of the
    schema version integer and a migration map. A migration map can have keys of :properties, :classes
@@ -354,7 +365,8 @@
    ["65.4" {:fix fix-using-properties-as-tags}]
    ["65.5" {:fix remove-block-order-for-tags}]
    ["65.6" {:fix update-extends-to-cardinality-many}]
-   ["65.7" {:fix add-quick-add-page}]])
+   ["65.7" {:fix add-quick-add-page}]
+   ["65.8" {:fix add-missing-page-name}]])
 
 (let [[major minor] (last (sort (map (comp (juxt :major :minor) db-schema/parse-schema-version first)
                                      schema-version->updates)))]
