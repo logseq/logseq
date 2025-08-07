10054b8213ac30a7d6ce8c86e98baff585476af6
charlie <xyhp915@qq.com>
2025-08-07 17:06:18 +0800

    fix(mobile): sidebar link item

--- src/main/mobile/components/left_sidebar.cljs
+++ src/main/mobile/components/left_sidebar.cljs
@@ -9,19 +9,22 @@
   []
   (let [open? (rum/react mobile-state/*left-sidebar-open?)]
     (silkhq/sidebar-sheet
-     {:presented (boolean open?)
-      :onPresentedChange (fn [v]
-                           (when (false? v)
-                             (mobile-state/close-left-sidebar!)))}
-     (silkhq/sidebar-sheet-portal
-      (silkhq/sidebar-sheet-view
-       {:class "app-silk-sidebar-sheet-view"}
-       (silkhq/sidebar-sheet-backdrop)
-       (silkhq/sidebar-sheet-content
-        {:class "flex flex-col items-center p-2"}
-        (silkhq/sidebar-sheet-handle)
-        [:div.w-full.app-silk-popup-content-inner.p-2
-         [:div.left-sidebar-inner
-          [:div.sidebar-contents-container.mt-8
-           (container/sidebar-favorites)
-           (container/sidebar-recent-pages)]]]))))))
+      {:presented (boolean open?)
+       :onPresentedChange (fn [v]
+                            (when (false? v)
+                              (mobile-state/close-left-sidebar!)))}
+      (silkhq/sidebar-sheet-portal
+        (silkhq/sidebar-sheet-view
+          {:class "app-silk-sidebar-sheet-view"}
+          (silkhq/sidebar-sheet-backdrop)
+          (silkhq/sidebar-sheet-content
+            {:class "flex flex-col items-center p-2"}
+            (silkhq/sidebar-sheet-handle)
+            [:div.w-full.app-silk-popup-content-inner.p-2
+             [:div.left-sidebar-inner
+              [:div.sidebar-contents-container.mt-8
+               {:on-click (fn [^js e]
+                            (when (some-> (.-target e) (.closest ".link-item"))
+                              (mobile-state/toggle-left-sidebar!)))}
+               (container/sidebar-favorites)
+               (container/sidebar-recent-pages)]]]))))))
