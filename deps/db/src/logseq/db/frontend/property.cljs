5cbf2efe0605a50aac31eca86cece03270d8f401
Tienson Qin <tiensonqin@gmail.com>
2025-08-11 08:56:43 +0800

    Add two properties for groups sorting
    1. :logseq.property.table/sort-groups-by
    2. :logseq.property.table/sort-groups-desc?

--- deps/db/src/logseq/db/frontend/property.cljs
+++ deps/db/src/logseq/db/frontend/property.cljs
@@ -12,6 +12,14 @@
 ;; Main property vars
 ;; ==================
 
+;; Ignore this property when rtc,
+;; since users frequently click the sort button to view table content temporarily,
+;; but this action does not need to be synchronized with other clients.
+(def property-ignore-rtc
+  {:rtc/ignore-attr-when-init-upload true
+   :rtc/ignore-attr-when-init-download true
+   :rtc/ignore-attr-when-syncing true})
+
 (def ^:large-vars/data-var built-in-properties
   "Map of built in properties for db graphs with their :db/ident as keys.
    Each property has a config map with the following keys:
@@ -425,18 +433,26 @@
                                      {:type :coll
                                       :hide? true
                                       :public? false}
-                                     ;; ignore this property when rtc,
-                                     ;; since users frequently click the sort button to view table content temporarily,
-                                     ;; but this action does not need to be synchronized with other clients.
-                                     :rtc {:rtc/ignore-attr-when-init-upload true
-                                           :rtc/ignore-attr-when-init-download true
-                                           :rtc/ignore-attr-when-syncing true}}
+                                     :rtc property-ignore-rtc}
 
      :logseq.property.table/filters {:title "View filters"
                                      :schema
                                      {:type :map
                                       :hide? true
                                       :public? false}}
+     :logseq.property.table/sort-groups-by {:title "View sort groups by"
+                                            :schema
+                                            {:type :property
+                                             :hide? true
+                                             :public? false}
+                                            :rtc property-ignore-rtc}
+     :logseq.property.table/sort-groups-desc? {:title "View sort groups DESC"
+                                               :schema
+                                               {:type :boolean
+                                                :hide? true
+                                                :public? false}
+                                               :properties {:logseq.property/scalar-default-value true}
+                                               :rtc property-ignore-rtc}
 
      :logseq.property.table/hidden-columns {:title "View hidden columns"
                                             :schema
@@ -485,17 +501,13 @@
                                              :schema {:type :raw-number
                                                       :hide? true
                                                       :public? false}
-                                             :rtc {:rtc/ignore-attr-when-init-upload true
-                                                   :rtc/ignore-attr-when-init-download true
-                                                   :rtc/ignore-attr-when-syncing true}}
+                                             :rtc property-ignore-rtc}
      :logseq.property.asset/remote-metadata {:title "File remote metadata"
                                              :schema
                                              {:type :map
                                               :hide? true
                                               :public? false}
-                                             :rtc {:rtc/ignore-attr-when-init-upload true
-                                                   :rtc/ignore-attr-when-init-download true
-                                                   :rtc/ignore-attr-when-syncing true}}
+                                             :rtc property-ignore-rtc}
      :logseq.property.asset/resize-metadata {:title "Asset resize metadata"
                                              :schema {:type :map
                                                       :hide? true
@@ -561,9 +573,7 @@
                                                        :schema {:type :datetime
                                                                 :public? false
                                                                 :hide? true}
-                                                       :rtc {:rtc/ignore-attr-when-init-upload true
-                                                             :rtc/ignore-attr-when-init-download true
-                                                             :rtc/ignore-attr-when-syncing true}})))
+                                                       :rtc property-ignore-rtc})))
 
 (def db-attribute-properties
   "Internal properties that are also db schema attributes"
