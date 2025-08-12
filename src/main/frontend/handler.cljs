061528d08229aa8a00467aa2db705360e3d64fcb
Tienson Qin <tiensonqin@gmail.com>
2025-08-12 12:52:16 +0800

    fix: slow switch to large graph with local embedding enabled

--- src/main/frontend/handler.cljs
+++ src/main/frontend/handler.cljs
@@ -168,13 +168,7 @@
                repo (or (state/get-current-repo) (:url (first repos)))
                _ (if (empty? repos)
                    (repo-handler/new-db! config/demo-repo)
-                   (restore-and-setup! repo))
-               webgpu-available? (db-browser/<check-webgpu-available?)]
-         (log/info :webgpu-available? webgpu-available?)
-         (when webgpu-available?
-           (p/do! (db-browser/start-inference-worker!)
-                  (db-browser/<connect-db-worker-and-infer-worker!)
-                  (reset! vector-search-flows/*infer-worker-ready true)))
+                   (restore-and-setup! repo))]
          (set-network-watcher!)
 
          (when (util/electron?)
@@ -184,7 +178,13 @@
        (p/catch (fn [e]
                   (js/console.error "Error while restoring repos: " e)))
        (p/finally (fn []
-                    (state/set-db-restoring! false))))
+                    (state/set-db-restoring! false)
+                    (p/let [webgpu-available? (db-browser/<check-webgpu-available?)]
+                      (log/info :webgpu-available? webgpu-available?)
+                      (when webgpu-available?
+                        (p/do! (db-browser/start-inference-worker!)
+                               (db-browser/<connect-db-worker-and-infer-worker!)
+                               (reset! vector-search-flows/*infer-worker-ready true)))))))
 
    (util/<app-wake-up-from-sleep-loop (atom false))
 
