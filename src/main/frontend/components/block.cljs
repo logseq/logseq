84506133a8ef1e95d2371294098ca4648963461f
charlie <xyhp915@qq.com>
2025-09-02 15:01:48 +0800

    fix(ux): edit block for the asset ref block

--- src/main/frontend/components/block.cljs
+++ src/main/frontend/components/block.cljs
@@ -373,7 +373,8 @@
                                               [:div
                                                {:on-click #(shui/popup-hide!)}
                                                (shui/dropdown-menu-item
-                                                {:on-click #(some-> (db/entity [:block/uuid (get-blockid)]) (editor-handler/edit-block! :max))}
+                                                {:on-click #(some-> (db/entity [:block/uuid (get-blockid)])
+                                                              (editor-handler/edit-block! :max {:container-id :unknown-container}))}
                                                 [:span.flex.items-center.gap-1
                                                  (ui/icon "edit") (t :asset/edit-block)])
                                                (shui/dropdown-menu-item
