7c068b4680e9019ebb0fd7a8b0dd81303c4c291e
Tienson Qin <tiensonqin@gmail.com>
2025-09-04 13:45:06 +0800

    chore: remove comment

--- deps/db/src/logseq/db/common/view.cljs
+++ deps/db/src/logseq/db/common/view.cljs
@@ -456,11 +456,8 @@
                             (keep (fn [id]
                                     (let [e (d/entity db id)]
                                       (when-not (= :logseq.property/query (:db/ident (:logseq.property/created-from-property e)))
-                                        e))
-                                    ;; (let [e (d/entity db id)]
-                                    ;;   (when-not (contains? query-entity-ids (:db/id (:block/parent e)))
-                                    ;;     e))
-                                    )query-entity-ids)
+                                        e)))
+                                  query-entity-ids)
                             (get-view-entities db view-id opts))
           entities (if (= feat-type :linked-references)
                      (:ref-blocks entities-result)
