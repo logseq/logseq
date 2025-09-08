86ca24513449a131ed08add0f2684311d045ddca
charlie <xyhp915@qq.com>
2025-09-08 11:20:20 +0800

    enhance(android): adjust soft input mode and set navigation bar color based on theme

--- android/app/src/main/java/com/logseq/app/MainActivity.java
+++ android/app/src/main/java/com/logseq/app/MainActivity.java
@@ -1,8 +1,11 @@
 package com.logseq.app;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.webkit.ValueCallback;
 import android.webkit.WebView;
 
@@ -22,6 +25,8 @@ public class MainActivity extends BridgeActivity {
         webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
         webView.getSettings().setUseWideViewPort(true);
         webView.getSettings().setLoadWithOverviewMode(true);
+
+        setNavigationBarColorBasedOnTheme();
 
         new Timer().schedule(new TimerTask() {
             @Override
@@ -56,5 +61,23 @@ public class MainActivity extends BridgeActivity {
                 }
             });
         }
+    }
+
+    private void setNavigationBarColorBasedOnTheme() {
+        Window window = getWindow();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { // API 26及以上支持
+            // 根据主题选择颜色
+            int navBarColor = isDarkMode()
+                    ? getResources().getColor(R.color.colorPrimaryDark, getTheme())
+                    : getResources().getColor(R.color.colorPrimary, getTheme());
+
+            // 设置导航栏颜色
+            window.setNavigationBarColor(navBarColor);
+        }
+    }
+
+    private boolean isDarkMode() {
+        int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        return nightModeFlags == Configuration.UI_MODE_NIGHT_YES;
     }
 }
