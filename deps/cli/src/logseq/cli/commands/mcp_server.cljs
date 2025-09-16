c1d2b0b9157bc48d555423a3cd0384a894aa15fb
Gabriel Horner <gabriel@logseq.com>
2025-09-16 10:02:50 -0400

    enhance: Add HTTP Streamable mode to MCP server
    and default to it

--- deps/cli/src/logseq/cli/commands/mcp_server.cljs
+++ deps/cli/src/logseq/cli/commands/mcp_server.cljs
@@ -2,6 +2,9 @@
   "Command to run a MCP server"
   (:require ["@modelcontextprotocol/sdk/server/mcp.js" :refer [McpServer]]
             ["@modelcontextprotocol/sdk/server/stdio.js" :refer [StdioServerTransport]]
+            ["@modelcontextprotocol/sdk/server/streamableHttp.js" :refer [StreamableHTTPServerTransport]]
+            ["@modelcontextprotocol/sdk/types.js" :refer [isInitializeRequest]]
+            ["express$default" :as express]
             ["fs" :as fs]
             ["zod/v3" :as z]
             [logseq.cli.common.mcp.tools :as cli-common-mcp-tools]
@@ -92,7 +95,71 @@
     :listProperties {:fn local-list-properties}
     :listTags {:fn local-list-tags}}))
 
-(defn start [{{:keys [debug-tool graph] :as opts} :opts :as m}]
+(def ^:private transports
+  "Stores transports by session ID"
+  (atom {}))
+
+(defn- handle-post-session-request [mcp-server {:keys [port]} req res]
+  (let [session-id (aget (.-headers req) "mcp-session-id")]
+    (js/console.log "POST /mcp request" session-id)
+    (cond
+      (and session-id (@transports session-id))
+      (.handleRequest (@transports session-id) req res (.-body req))
+
+      (and (not session-id)
+           (isInitializeRequest (.-body req)))
+      (let [transport (StreamableHTTPServerTransport.
+                       #js {:sessionIdGenerator (comp str random-uuid)
+                            :enableDnsRebindingProtection true
+                            :allowedHosts #js [(str "localhost:" port)]})]
+        (set! (.-onclose transport)
+              (fn []
+                (js/console.log "Transport closed")
+                (when (.-sessionId transport)
+                  (swap! transports dissoc (.-sessionId transport)))))
+        (.connect mcp-server transport)
+        (.handleRequest transport req res (.-body req))
+        (js/console.log "Initialize sessionId" (.-sessionId transport))
+        (if (.-sessionId transport)
+          (swap! transports assoc (.-sessionId transport) transport)
+          (js/console.error "No sessionId to initialize!")))
+
+      :else
+      (do
+        (.status res 400)
+        (.json res #js {:jsonrpc "2.0"
+                        :error #js {:code -32000
+                                    :message "Bad Request: No valid session ID provided"}
+                        :id nil})))))
+
+(defn- handle-session-request
+  "Reusable handler for GET and DELETE"
+  [req res]
+  (let [session-id (aget (.-headers req) "mcp-session-id")]
+    (js/console.log (.-method req) "/mcp" session-id)
+    (if-let [transport (and session-id (@transports session-id))]
+      (.handleRequest transport req res)
+      (-> res (.status 400) (.send res "Invalid or missing session ID")))))
+
+(defn- create-http-server [mcp-server opts]
+  (let [app (express)]
+    (.use app (.json express))
+    (.post app "/mcp" #(handle-post-session-request mcp-server opts %1 %2))
+    (.get app "/mcp" handle-session-request)
+    (.delete app "/mcp" handle-session-request)
+    app))
+
+(defn- start-http-server [mcp-server {:keys [port] :as opts}]
+  (let [app (create-http-server mcp-server opts)]
+    (.listen app port
+             (fn [error]
+               (if error
+                 (do (js/console.error "Failed to start server:" error)
+                     (js/process.exit 1))
+                 (js/console.log
+                  (str "MCP Streamable HTTP Server started on port " port)))))))
+
+(defn start [{{:keys [debug-tool graph stdio] :as opts} :opts :as m}]
   (when (and graph (not (fs/existsSync (cli-util/get-graph-dir graph))))
     (cli-util/error "Graph" (pr-str graph) "does not exist"))
   (if debug-tool
@@ -106,13 +173,14 @@
         (p/let [resp ((:fn tool-m) m (clj->js (dissoc opts :debug-tool)))]
           (js/console.log resp))
         (cli-util/error "Tool" (pr-str debug-tool) "not found")))
-    (let [server (McpServer. #js {:name "Logseq MCP Server"
-                                  :version "0.1.0"})
-          transport (StdioServerTransport.)
-          conn (when graph (apply sqlite-cli/open-db! (cli-util/->open-db-args graph)))]
+    (let [mcp-server (McpServer. #js {:name "Logseq MCP Server"
+                                      :version "0.1.0"})]
       (if graph
-        (doseq [[k v] local-tools]
-          (.registerTool server (name k) (:config v) (partial (:fn v) conn)))
+        (let [conn (apply sqlite-cli/open-db! (cli-util/->open-db-args graph))]
+          (doseq [[k v] local-tools]
+            (.registerTool mcp-server (name k) (:config v) (partial (:fn v) conn))))
         (doseq [[k v] api-tools]
-          (.registerTool server (name k) (:config v) (partial (:fn v) m))))
-      (nbb/await (.connect server transport)))))
+          (.registerTool mcp-server (name k) (:config v) (partial (:fn v) m))))
+      (if stdio
+        (nbb/await (.connect mcp-server (StdioServerTransport.)))
+        (start-http-server mcp-server (select-keys opts [:port]))))))
