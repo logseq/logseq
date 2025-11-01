d1d1ca30edd620142fc9b281341eb7cf3bd0bc91
rcmerci <rcmerci@gmail.com>
2025-11-01 20:39:55 +0800

    feat(rtc,e2ee): basic e2ee-password ui

---
+++ src/main/frontend/components/e2ee.cljs
@@ -0,0 +1,53 @@
+(ns frontend.components.e2ee
+  (:require [frontend.common.crypt :as crypt]
+            [logseq.shui.hooks :as hooks]
+            [logseq.shui.ui :as shui]
+            [promesa.core :as p]
+            [rum.core :as rum]))
+
+(rum/defc e2ee-request-new-password
+  [password-promise]
+  (let [[password set-password!] (hooks/use-state "")]
+    [:div.e2ee-password-modal-overlay
+     [:div.e2ee-password-modal-content
+      [:h3 "Enter new E2EE Password"]
+      [:input {:type "password"
+               :value password
+               :on-change (fn [e] (set-password! (-> e .-target .-value)))}]
+      (shui/button
+       {:on-click (fn []
+                    (p/resolve! password-promise password)
+                    (shui/dialog-close!))}
+       "OK")
+      (shui/button
+       {:on-click (fn []
+                    (p/reject! password-promise :cancelled)
+                    (shui/dialog-close!))}
+       "Cancel")]]))
+
+(rum/defc e2ee-password-to-decrypt-private-key
+  [encrypted-private-key private-key-promise]
+  (let [[password set-password!] (hooks/use-state "")
+        [decrypt-fail? set-decrypt-fail] (hooks/use-state false)]
+    [:div.e2ee-password-modal-overlay
+     [:div.e2ee-password-modal-content
+      [:h3 "Enter E2EE Password"]
+      (when decrypt-fail? [:p "Wrong Password"])
+      [:input {:type "password"
+               :value password
+               :on-change (fn [e] (set-password! (-> e .-target .-value)))}]
+      (shui/button
+       {:on-click (fn []
+                    (->
+                     (p/let [private-key (crypt/<decrypt-private-key password encrypted-private-key)]
+                       (p/resolve! private-key-promise private-key)
+                       (shui/dialog-close!))
+                     (p/catch (fn [e]
+                                (when (= "decrypt-private-key" (ex-message e))
+                                  (set-decrypt-fail true))))))}
+       "OK")
+      (shui/button
+       {:on-click (fn []
+                    (p/reject! private-key-promise :cancelled)
+                    (shui/dialog-close!))}
+       "Cancel")]]))
--- src/main/frontend/handler/crypt.cljs
+++
@@ -1,6 +0,0 @@
-(ns frontend.handler.crypt
-  (:require [frontend.common.thread-api :refer [def-thread-api]]))
-
-(def-thread-api :thread-api/request-e2ee-password
-  []
-  {:password "test-password"})
