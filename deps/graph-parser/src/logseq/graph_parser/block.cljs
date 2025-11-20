43b09095acb1de9d44600488c28cda368d81c4b3
Gabriel Horner <gabriel@logseq.com>
2025-11-20 12:12:00 -0500

    fix: graph-parser lint

--- deps/graph-parser/src/logseq/graph_parser/block.cljs
+++ deps/graph-parser/src/logseq/graph_parser/block.cljs
@@ -14,7 +14,6 @@
             [logseq.common.util.page-ref :as page-ref]
             [logseq.common.uuid :as common-uuid]
             [logseq.db :as ldb]
-            [logseq.db.common.entity-plus :as entity-plus]
             [logseq.db.common.order :as db-order]
             [logseq.db.frontend.class :as db-class]
             [logseq.graph-parser.mldoc :as gp-mldoc]
