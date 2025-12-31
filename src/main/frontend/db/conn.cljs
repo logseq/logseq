152171fb20197798d109941833674681ba81c2b6
Gabriel Horner <gabriel@logseq.com>
2025-12-30 22:10:27 -0500

    fix: frontend and graph-parser tests and lints
    Removed file specific test

--- src/main/frontend/db/conn.cljs
+++ src/main/frontend/db/conn.cljs
@@ -10,6 +10,7 @@
             [frontend.util.text :as text-util]
             [logseq.common.util :as common-util]
             [logseq.db :as ldb]
+            [logseq.db.file-based.schema :as file-schema]
             [logseq.db.frontend.schema :as db-schema]
             [logseq.graph-parser.text :as text]))
 
@@ -80,7 +81,9 @@
    (start! repo {}))
   ([repo {:keys [listen-handler]}]
    (let [db-name (db-conn-state/get-repo-path repo)
-         db-conn (d/create-conn db-schema/schema)]
+         db-conn (if (config/db-based-graph? repo)
+                   (d/create-conn db-schema/schema)
+                   (d/create-conn file-schema/schema))]
      (destroy-all!)
      (swap! conns assoc db-name db-conn)
      (when listen-handler
