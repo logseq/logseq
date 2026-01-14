0da1fac18dd0b5ced594940ec36da21ebc478a37
rcmerci <rcmerci@gmail.com>
2026-01-14 22:21:10 +0800

    add :logseq-cli build
    see also docs/cli/logseq-cli.md

---
+++ src/main/logseq/cli/main.cljs
@@ -0,0 +1,65 @@
+(ns logseq.cli.main
+  (:refer-clojure :exclude [run!])
+  (:require [clojure.string :as string]
+            [logseq.cli.commands :as commands]
+            [logseq.cli.config :as config]
+            [logseq.cli.format :as format]
+            [promesa.core :as p]))
+
+(defn- usage
+  [summary]
+  (string/join "\n"
+               ["logseq-cli <command> [options]"
+                ""
+                "Commands: ping, status, query, export, graph-list, graph-create, graph-switch, graph-remove, graph-validate, graph-info, add, remove, search, tree"
+                ""
+                "Options:"
+                summary]))
+
+(defn run!
+  ([args] (run! args {:exit? true}))
+  ([args {:keys [exit?] :or {exit? true}}]
+   (let [parsed (commands/parse-args args)]
+     (cond
+       (:help? parsed)
+       (p/resolved {:exit-code 0
+                    :output (usage (:summary parsed))})
+
+       (not (:ok? parsed))
+       (p/resolved {:exit-code 1
+                    :output (format/format-result {:status :error
+                                                   :error (:error parsed)}
+                                                  {:json? false})})
+
+       :else
+       (let [cfg (config/resolve-config (:options parsed))
+             action-result (commands/build-action parsed cfg)]
+         (if-not (:ok? action-result)
+           (p/resolved {:exit-code 1
+                        :output (format/format-result {:status :error
+                                                       :error (:error action-result)}
+                                                      cfg)})
+           (-> (commands/execute (:action action-result) cfg)
+               (p/then (fn [result]
+                         (let [opts (cond-> cfg
+                                       (:output-format result)
+                                       (assoc :output-format (:output-format result)))]
+                           {:exit-code 0
+                            :output (format/format-result result opts)})))
+               (p/catch (fn [error]
+                          (let [message (or (some-> (ex-data error) :message)
+                                            (.-message error)
+                                            (str error))]
+                            {:exit-code 1
+                             :output (format/format-result {:status :error
+                                                            :error {:code :exception
+                                                                    :message message}}
+                                                           cfg)}))))))))))
+
+(defn main
+  [& args]
+  (-> (run! args)
+      (p/then (fn [{:keys [exit-code output]}]
+                (when (seq output)
+                  (println output))
+                (.exit js/process exit-code)))))
---
+++ src/test/logseq/cli/transport_test.cljs
@@ -0,0 +1,64 @@
+(ns logseq.cli.transport-test
+  (:require [cljs.test :refer [deftest is async testing]]
+            [promesa.core :as p]
+            [logseq.cli.transport :as transport]))
+
+(defn- start-server
+  [handler]
+  (p/create
+   (fn [resolve reject]
+     (let [http (js/require "http")
+           server (.createServer http handler)]
+       (.on server "error" reject)
+       (.listen server 0 "127.0.0.1"
+                (fn []
+                  (let [address (.address server)
+                        port (.-port address)
+                        stop! (fn []
+                                (p/create (fn [resolve _]
+                                            (.close server (fn [] (resolve true))))))]
+                    (resolve {:url (str "http://127.0.0.1:" port)
+                              :stop! stop!}))))))))
+
+(deftest test-request-retries
+  (async done
+    (let [calls (atom 0)]
+      (-> (p/let [{:keys [url stop!]} (start-server
+                                       (fn [_req res]
+                                         (let [attempt (swap! calls inc)]
+                                           (if (= attempt 1)
+                                             (do
+                                               (.writeHead res 500 #js {"Content-Type" "text/plain"})
+                                               (.end res "boom"))
+                                             (do
+                                               (.writeHead res 200 #js {"Content-Type" "text/plain"})
+                                               (.end res "ok"))))))
+                 response (transport/request {:method "GET"
+                                              :url (str url "/retry")
+                                              :retries 1
+                                              :timeout-ms 1000})]
+            (is (= 200 (:status response)))
+            (is (= 2 @calls))
+            (p/let [_ (stop!)]
+              (done)))
+          (p/catch (fn [e]
+                     (is false (str "unexpected error: " e))
+                     (done)))))))
+
+(deftest test-request-timeout
+  (async done
+    (-> (p/let [{:keys [url stop!]} (start-server
+                                     (fn [_req _res]
+                                       nil))]
+          (p/catch
+           (transport/request {:method "GET"
+                               :url (str url "/hang")
+                               :timeout-ms 10
+                               :retries 0})
+           (fn [e]
+             (is (= :timeout (-> (ex-data e) :code)))
+             (p/let [_ (stop!)]
+               (done)))))
+        (p/catch (fn [e]
+                   (is false (str "unexpected error: " e))
+                   (done))))))
