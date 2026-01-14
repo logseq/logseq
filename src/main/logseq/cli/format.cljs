91aa2e5e79cf2bef970019ec4a5b60b29e5644bc
rcmerci <rcmerci@gmail.com>
2026-01-14 22:21:10 +0800

    add :logseq-cli build
    see also docs/cli/logseq-cli.md

---
+++ src/main/logseq/cli/format.cljs
@@ -0,0 +1,54 @@
+(ns logseq.cli.format
+  (:require [clojure.string :as string]
+            [clojure.walk :as walk]))
+
+(defn- normalize-json
+  [value]
+  (walk/postwalk (fn [entry]
+                   (if (uuid? entry)
+                     (str entry)
+                     entry))
+                 value))
+
+(defn- ->json
+  [{:keys [status data error]}]
+  (let [obj (js-obj)]
+    (set! (.-status obj) (name status))
+    (cond
+      (= status :ok)
+      (set! (.-data obj) (clj->js (normalize-json data)))
+
+      (= status :error)
+      (set! (.-error obj) (clj->js (normalize-json (update error :code name)))))
+    (js/JSON.stringify obj)))
+
+(defn- ->human
+  [{:keys [status data error]}]
+  (case status
+    :ok
+    (if (and (map? data) (contains? data :message))
+      (:message data)
+      (pr-str data))
+
+    :error
+    (str "error: " (:message error))
+
+    (pr-str {:status status :data data :error error})))
+
+(defn- ->edn
+  [{:keys [status data error]}]
+  (pr-str (cond-> {:status status}
+            (= status :ok) (assoc :data data)
+            (= status :error) (assoc :error error))))
+
+(defn format-result
+  [result {:keys [json? output-format]}]
+  (let [format (cond
+                 (= output-format :edn) :edn
+                 (= output-format :json) :json
+                 json? :json
+                 :else :human)]
+    (case format
+      :json (->json result)
+      :edn (->edn result)
+      (->human result))))
