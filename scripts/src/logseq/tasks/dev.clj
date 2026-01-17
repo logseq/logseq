d5082ecc9b595fe67318d5b32bdbe1b35b81310f
rcmerci <rcmerci@gmail.com>
2026-01-17 23:25:11 +0800

    fix: bb process/shell throws 'Operations not permitted(sysctl fail)' in agent sandbox

--- scripts/src/logseq/tasks/dev.clj
+++ scripts/src/logseq/tasks/dev.clj
@@ -17,9 +17,9 @@
 (defn test
   "Run tests. Pass args through to cmd 'yarn cljs:run-test'"
   [& args]
-  (shell "yarn cljs:test")
+  (shell {:shutdown nil} "yarn cljs:test")
   (let [args* (or (seq args) ["-e" "long" "-e" "fix-me"])]
-    (apply shell "yarn cljs:run-test" args*)))
+    (apply shell {:shutdown nil} "yarn cljs:run-test" args*)))
 
 (defn lint-and-test
   "Run all lint tasks, then run tests(exclude testcases tagged by :long).
@@ -45,11 +45,12 @@
   (let [config-edn ".clj-kondo/metosin/malli-types/config.edn"
         compile-cmd "clojure -M:cljs compile gen-malli-kondo-config"]
     (println compile-cmd)
-    (shell compile-cmd)
+    (shell {:shutdown nil} compile-cmd)
     (println "generate kondo config: " config-edn)
     (io/make-parents config-edn)
     (let [config (with-out-str
-                   (pp/pprint (edn/read-string (:out (shell {:out :string} "node ./static/gen-malli-kondo-config.js")))))]
+                   (pp/pprint (edn/read-string (:out (shell {:out :string :shutdown nil}
+                                                            "node ./static/gen-malli-kondo-config.js")))))]
       (spit config-edn config))))
 
 (defn diff-datoms
@@ -76,19 +77,19 @@
                                                     (fs/glob "." "{src/main,deps/graph-parser/src}/**")))))]
     (do
       (println "Building publishing js asset...")
-      (shell "clojure -M:cljs release publishing db-worker inference-worker"))
+      (shell {:shutdown nil} "clojure -M:cljs release publishing db-worker inference-worker"))
     (println "Publishing js asset is up to date")))
 
 (defn publishing-backend
   "Builds publishing backend and copies over supporting frontend assets"
   [& args]
-  (apply shell {:dir "deps/publishing" :extra-env {"ORIGINAL_PWD" (fs/cwd)}}
+  (apply shell {:dir "deps/publishing" :extra-env {"ORIGINAL_PWD" (fs/cwd)} :shutdown nil}
          "yarn -s nbb-logseq -cp src:../graph-parser/src script/publishing.cljs"
          (into ["static"] args)))
 
 (defn watch-publishing-frontend
   [& _args]
-  (shell "npx shadow-cljs watch publishing"))
+  (shell {:shutdown nil} "npx shadow-cljs watch publishing"))
 
 (defn watch-publishing-backend
   "Builds publishing backend once watch-publishing-frontend has built initial frontend"
@@ -110,4 +111,4 @@
     (doseq [file-graph file-graphs]
       (let [db-graph (fs/path parent-graph-dir (fs/file-name file-graph))]
         (println "Importing" (str db-graph) "...")
-        (apply shell "bb" "dev:import" file-graph db-graph (concat import-options ["--validate"]))))))
+        (apply shell {:shutdown nil} "bb" "dev:db-import" file-graph db-graph (concat import-options ["--validate"]))))))
