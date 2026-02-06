2ea618b76fae2c68235817299637c2b25f5f10fd
Tienson Qin <tiensonqin@gmail.com>
2026-02-06 15:55:55 +0800

    fix: don't notify source client with presence update

--- deps/db-sync/test/logseq/db_sync/test_runner.cljs
+++ deps/db-sync/test/logseq/db_sync/test_runner.cljs
@@ -3,6 +3,7 @@
             [logseq.db-sync.node-adapter-test]
             [logseq.db-sync.node-config-test]
             [logseq.db-sync.platform-test]
+            [logseq.db-sync.worker-handler-ws-test]
             [shadow.test :as st]
             [shadow.test.env :as env]))
 
