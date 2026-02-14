84b0b4d9904395d043ab662581ed98eb91e190df
TechnoHouse (deephbz) <13776377+deephbz@users.noreply.github.com>
2026-02-14 22:12:10 +0800

    fix(fs): ensure assets dir exists and silence read-file-raw ENOENT

--- src/test/frontend/fs_test.cljs
+++ src/test/frontend/fs_test.cljs
@@ -2,10 +2,13 @@
   (:require ["fs" :as fs-node]
             ["path" :as node-path]
             [cljs.test :refer [is]]
+            [frontend.config :as config]
             [frontend.fs :as fs]
             [frontend.test.helper :as test-helper :include-macros true :refer [deftest-async]]
             [frontend.test.node-fixtures :as node-fixtures]
             [frontend.test.node-helper :as test-node-helper]
+            [frontend.util :as util]
+            [logseq.common.config :as common-config]
             [promesa.core :as p]))
 
 (deftest-async create-if-not-exists-creates-correctly
@@ -46,3 +49,44 @@
        (fn []
          (fs-node/unlinkSync some-file)
          (fs-node/rmdirSync dir))))))
+
+(deftest-async write-asset-file-ensures-db-assets-dir-test
+  (let [repo-dir (node-path/resolve (test-node-helper/create-tmp-dir))
+        file-name "asset.txt"
+        payload "asset payload"
+        calls (atom [])
+        old-window (.-window js/globalThis)
+        had-window? (.call (.-hasOwnProperty (.-prototype js/Object))
+                           js/globalThis
+                           "window")
+        window #js {:apis #js {:writeFileBytes
+                               (fn [file-path data]
+                                 (swap! calls conj [:write file-path data])
+                                 (p/resolved true))}}]
+    (js/Object.defineProperty js/globalThis
+                              "window"
+                              #js {:value window
+                                   :configurable true
+                                   :writable true})
+    (p/with-redefs [config/get-repo-dir (constantly repo-dir)
+                    util/electron? (constantly true)
+                    fs/mkdir-recur! (fn [dir]
+                                      (swap! calls conj [:mkdir dir])
+                                      (p/resolved nil))]
+      (-> (p/do!
+           (fs/write-asset-file! "repo" file-name payload)
+           (let [assets-dir (node-path/join repo-dir common-config/local-assets-dir)
+                 asset-path (node-path/join assets-dir file-name)]
+             (is (= [[:mkdir assets-dir]
+                     [:write asset-path payload]]
+                    @calls))))
+          (p/finally
+            (fn []
+              (if had-window?
+                (js/Object.defineProperty js/globalThis
+                                          "window"
+                                          #js {:value old-window
+                                               :configurable true
+                                               :writable true})
+                (js/Reflect.deleteProperty js/globalThis "window"))
+              (fs-node/rmdirSync repo-dir)))))))
