f4b9d9ba69067051f88fd75dfcf80ce8637a17e9
Mega Yu <yuhg2310@gmail.com>
2026-03-18 09:49:04 +0800

    chore(deps): remove deprecated aws-amplify-react dependency and import the local i18n dictionary

--- packages/amplify/src/amplify.ts
+++ packages/amplify/src/amplify.ts
@@ -1,49 +1,7 @@
 import './styles.css'
 import { Amplify, Auth, Hub, I18n } from 'aws-amplify'
 import { LSAuthenticator, LSAuthenticatorChangePassword } from './LSAuthenticator'
-import { dict } from 'aws-amplify-react/lib-esm/AmplifyI18n'
-
-// fix i18n
-dict.zh['Reset Password'] = '重置密码'
-dict.zh['Enter your email'] = '请输入邮箱'
-dict.zh['Enter your password'] = '请输入密码'
-dict.zh['Enter your Username'] = '请输入用户名'
-dict.zh['Confirm Password'] = '确认密码'
-dict.zh['Please confirm your Password'] = '请确认密码'
-dict.zh['Incorrect username or password.'] = '用户名或者密码不正确。如果您的邮箱未验证，请尝试使用用户名(非邮箱)登录，以保证再次邮箱验证流程。'
-dict.zh['User already exists'] = '用户名已经存在'
-dict.zh['Username or Email'] = '用户名或邮箱'
-dict.zh['Enter your Username or Email'] = '请输入用户名或邮箱'
-
-// @ts-ignore attach defaults
-dict.en = {
-  'Incorrect username or password.': 'Incorrect username or password!   ' +
-    'For unconfirmed users, please input your username instead of Email to receive the code.',
-  'User already exists': 'Username already exists'
-}
-
-const fixesMapping = {
-  'Sign Up': ['Sign up', 'Create Account'],
-  'Sign In': ['Sign in'],
-  'Sign Out': 'Sign out',
-  'Send Code': 'Send code',
-  'Forgot Password': ['Forgot your password?'],
-  'Enter your email': ['Enter your Email'],
-  'Enter your password': ['Enter your Password'],
-  'Enter your username': ['Enter your Username or Email']
-}
-
-Object.keys(dict).forEach((k) => {
-  const target = dict[k]
-  Object.entries(fixesMapping).forEach(([k1, v1]) => {
-    if (target?.hasOwnProperty(k1)) {
-      const vs = Array.isArray(v1) ? v1 : [v1]
-      vs.forEach(it => {
-        target[it] = target[k1]
-      })
-    }
-  })
-})
+import { dict } from './amplify-i18n-dict'
 
 I18n.putVocabularies(dict)
 
