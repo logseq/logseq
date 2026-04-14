968fca916c08572fe446afeefedfc989011be63d
Developer <dev@example.com>
2026-04-14 21:54:03 +0800

    feat(memo): register commands and ribbon icons

--- src/main/frontend/modules/memo/core.cljs
+++ src/main/frontend/modules/memo/core.cljs
@@ -4,6 +4,7 @@
             [frontend.modules.memo.parser :as parser]
             [frontend.modules.memo.index :as index]
             [frontend.state :as state]
+            [frontend.components.left-sidebar :as left-sidebar]
             [logseq.memo.schema :as schema]))
 
 (defn init!
@@ -17,9 +18,37 @@
   (commands/register!
    {:id "logseq.memo/create-setting"
     :name "创建设定"
-    :action (fn [] (println "Create setting"))}))
+    :callback (fn [] (println "Open create setting modal"))})
+
+  (commands/register!
+   {:id "logseq.memo/open-graph"
+    :name "打开设定图谱"
+    :callback (fn [] (println "Open memo graph view"))})
+
+  (commands/register!
+   {:id "logseq.memo/open-inspiration"
+    :name "打开灵感建议"
+    :callback (fn [] (println "Open inspiration panel"))})
+
+  (commands/register!
+   {:id "logseq.memo/check-consistency"
+    :name "检查一致性"
+    :callback (fn [] (println "Run consistency check"))}))
+
+(defn register-ribbon!
+  []
+  (left-sidebar/add-icon!
+   {:icon "📖"
+    :title "设定面板"
+    :action (fn [] (state/toggle-panel! :memo-sidebar))})
+
+  (left-sidebar/add-icon!
+   {:icon "🕸️"
+    :title "设定图谱"
+    :action (fn [] (state/open-view! :memo-graph))}))
 
 (defn register!
   []
   (init! (state/get-current-repo))
-  (register-commands!))
+  (register-commands!)
+  (register-ribbon!))
