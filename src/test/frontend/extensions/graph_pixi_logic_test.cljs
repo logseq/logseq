1b2f9068e935ad9d6e4c7376203414f65c2d474b
Tienson Qin <tiensonqin@gmail.com>
2026-05-09 17:34:42 +0800

    test: filter graph lines on highlight

--- src/test/frontend/extensions/graph_pixi_logic_test.cljs
+++ src/test/frontend/extensions/graph_pixi_logic_test.cljs
@@ -154,7 +154,7 @@
     (is (= :dimmed (logic/node-emphasis one-hop "d")))
     (is (= :connected (logic/node-emphasis two-hop "d")))))
 
-(deftest highlighted-links-keep-default-graph-lines
+(deftest highlighted-links-keep-default-lines-and-filter-selected-neighborhood
   (let [links [{:source "a" :target "b"}
                {:source "a" :target "c"}
                {:source "b" :target "d"}
@@ -166,7 +166,10 @@
                       "e" ["d"]}
         state (logic/highlight-state #{"a"} neighbor-map 2)]
     (is (= links (logic/highlight-visible-links links (logic/highlight-state #{} neighbor-map))))
-    (is (= links (logic/highlight-visible-links links state)))))
+    (is (= #{{:source "a" :target "b"}
+             {:source "a" :target "c"}
+             {:source "b" :target "d"}}
+           (set (logic/highlight-visible-links links state))))))
 
 (deftest node-click-action-distinguishes-highlight-unhighlight-and-open
   (is (= {:action :highlight
