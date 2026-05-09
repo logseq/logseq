6a6056ecc9e21c0f1b1f0164625fa8e4feb5a0fc
Tienson Qin <tiensonqin@gmail.com>
2026-05-09 17:41:08 +0800

    test: keep graph depth incremental

--- src/test/frontend/extensions/graph_test.cljs
+++ src/test/frontend/extensions/graph_test.cljs
@@ -7,3 +7,23 @@
          (graph/canvas-style {})))
   (is (= {:width "640px" :height "480px"}
          (graph/canvas-style {:width 640 :height 480}))))
+
+(deftest render-container-deps-ignore-incremental-depth
+  (let [opts {:nodes []
+              :links []
+              :dark? true
+              :view-mode :tags-and-objects
+              :width 640
+              :height 480
+              :aria-label "Graph"
+              :depth 1
+              :arrow-mode :both
+              :link-distance 72
+              :show-edge-labels? true
+              :on-node-activate identity
+              :on-selection-change identity
+              :on-rendered identity}
+        deps (graph/render-container-deps opts)]
+    (is (not (some #{1} deps)))
+    (is (some #{:both} deps))
+    (is (some #{72} deps))))
