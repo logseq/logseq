ec0c01fd974059806a162ea2b61efbafa3c0f624
Tienson Qin <tiensonqin@gmail.com>
2026-05-21 14:52:28 +0800

    enhance: use graph identity in URLs (#12693)
    * enhance: use graph identity in URLs
    
    * fix: resolve graph identity URLs
    
    * fix: satisfy graph url lint
    
    * fix: resolve graph id from hash urls
    
    * fix: preserve tab graph on reload
    
    * fix: initialize tab graph before render
    
    * enhance: open graph in another tab
    
    * fix: open graph tabs by graph id
    
    * fix: open local graph tabs by registry id
    
    * fix: avoid duplicate rtc graph id
    
    * fix: open electron graph window on shift click
    
    * fix: address graph identity review

--- src/main/frontend/components/repo.cljs
+++ src/main/frontend/components/repo.cljs
@@ -125,6 +125,34 @@
         (p/then (fn []
                   (repo-handler/remove-repo! repo))))))
 
+(defn graph-open-new-window-target
+  [{:keys [url] :as repo}]
+  (when-let [graph-id (:graph-id (graph/repo-summary->registry-entry repo))]
+    {:repo url
+     :graph-id graph-id}))
+
+(defn- <graph-open-new-window-target
+  [{:keys [url] :as repo}]
+  (if-let [target (graph-open-new-window-target repo)]
+    (p/resolved target)
+    (p/let [registry (graph/<get-graph-registry)
+            entry (graph/resolve-registry-target registry {:graph-identifier url})]
+      (when entry
+        {:repo (:repo entry)
+         :graph-id (:graph-id entry)}))))
+
+(defn open-in-another-tab-action?
+  [{:keys [root]}]
+  (boolean (and util/web-platform? root)))
+
+(defn open-graph-in-another-tab!
+  [repo]
+  (if-let [target (graph-open-new-window-target repo)]
+    (state/pub-event! [:graph/open-new-window target])
+    (p/let [target (<graph-open-new-window-target repo)]
+      (when target
+        (state/pub-event! [:graph/open-new-window target])))))
+
 (rum/defc ^:large-vars/cleanup-todo repos-inner
   "Graph list in `All graphs` page"
   [repos]
@@ -171,6 +199,13 @@
             (ui/icon "dots" {:size 15})))
           (shui/dropdown-menu-content
            {:align "end"}
+           (when (open-in-another-tab-action? repo)
+             (shui/dropdown-menu-item
+              {:key "open-in-another-tab"
+               :class "open-in-another-tab-menu-item"
+               :on-click #(open-graph-in-another-tab! repo)}
+              (t :graph/open-in-another-tab-action)))
+
            (when root
              (shui/dropdown-menu-item
               {:key "delete-locally"
@@ -330,9 +365,14 @@
                                        (when (and ready-for-use? (not downloading?))
                                          (when-let [on-click (:on-click opts)]
                                            (on-click e))
-                                         (if (and (gobj/get e "shiftKey")
-                                                  (not (and rtc-graph? remote?)))
-                                           (state/pub-event! [:graph/open-new-window url])
+                                         (if (and (gobj/get e "shiftKey") (:root graph))
+                                           (if (util/electron?)
+                                             (state/pub-event! [:graph/open-new-window url])
+                                             (if-let [target (graph-open-new-window-target graph)]
+                                               (state/pub-event! [:graph/open-new-window target])
+                                               (p/let [target (<graph-open-new-window-target graph)]
+                                                 (when target
+                                                   (state/pub-event! [:graph/open-new-window target])))))
                                            (cond
                                                   ;; exists locally?
                                              (or (:root graph) (not rtc-graph?))
--- src/main/mobile/deeplink.cljs
+++ src/main/mobile/deeplink.cljs
@@ -4,90 +4,134 @@
             [frontend.config :as config]
             [frontend.context.i18n :refer [t]]
             [frontend.db.async :as db-async]
+            [frontend.handler.graph :as graph-handler]
             [frontend.handler.notification :as notification]
             [frontend.handler.route :as route-handler]
             [frontend.mobile.intent :as intent]
             [frontend.state :as state]
             [frontend.util.text :as text-util]
+            [frontend.util.url :as url-util]
             [goog :refer [Uri]]
             [logseq.common.util :as common-util]
             [promesa.core :as p]))
 
 (def *link-to-another-graph (atom false))
 
+(defn- redirect-url-target-route!
+  [url-target link-to-another-graph?]
+  (when-let [{:keys [to page-id block-id]} (:route url-target)]
+    (js/setTimeout
+     (fn []
+       (case to
+         :page
+         (route-handler/redirect-to-page! page-id)
+
+         :block
+         (route-handler/redirect-to-page! block-id)
+
+         nil)
+       (reset! *link-to-another-graph false))
+     (if link-to-another-graph?
+       1000
+       0))))
+
+(defn- handle-web-url-target!
+  [url-target]
+  (when (seq url-target)
+    (p/let [registry (graph-handler/<get-graph-registry)
+            repos (->> (state/sub [:me :repos])
+                       (remove #(= (:url %) config/demo-repo)))
+            target-repo (:repo (graph-handler/resolve-registry-target
+                                (concat registry
+                                        (graph-handler/registry-from-repo-summaries repos))
+                                url-target))]
+      (if target-repo
+        (let [link-to-another-graph? (not= target-repo (state/get-current-repo))]
+          (when link-to-another-graph?
+            (state/pub-event! [:graph/switch target-repo])
+            (reset! *link-to-another-graph true))
+          (redirect-url-target-route! url-target link-to-another-graph?))
+        (when-let [graph-id (:graph-id url-target)]
+          (notification/show! (t :deeplink/open-graph-error graph-id) :error false))))))
+
 (defn deeplink [url]
-  (let [url (string/replace url "logseq.com/" "")
-        ^js/Uri parsed-url (.parse Uri url)
-        hostname (.getDomain parsed-url)
-        pathname (.getPath parsed-url)
-        search-params (.getQueryData parsed-url)
-        current-repo-url (state/get-current-repo)
-        get-graph-name-fn #(-> (text-util/get-graph-name-from-path %)
-                               (string/split "/")
-                               last
-                               string/lower-case)
-        current-graph-name (get-graph-name-fn current-repo-url)
-        repos (->> (state/sub [:me :repos])
-                   (remove #(= (:url %) config/demo-repo))
-                   (map :url))
-        repo-names (map #(get-graph-name-fn %) repos)]
-    (cond
-      (and (= hostname "mobile") (= pathname "/go/audio"))
-      (state/pub-event! [:mobile/start-audio-record])
-      (and (= hostname "mobile") (= pathname "/go/quick-add"))
-      (state/pub-event! [:mobile/set-tab "capture"])
-      (= hostname "graph")
-      (let [graph-name (some-> pathname
-                               (string/replace "/" "")
-                               string/lower-case)
-            [page-name block-uuid] (map #(.get search-params %)
-                                        ["page" "block-id"])]
+  (let [url-target (url-util/parse-web-url-target url)]
+    (if (seq url-target)
+      (handle-web-url-target! url-target)
+      (let [url (string/replace url "logseq.com/" "")
+            ^js/Uri parsed-url (.parse Uri url)
+            hostname (.getDomain parsed-url)
+            pathname (.getPath parsed-url)
+            search-params (.getQueryData parsed-url)
+            current-repo-url (state/get-current-repo)
+            get-graph-name-fn #(-> (text-util/get-graph-name-from-path %)
+                                   (string/split "/")
+                                   last
+                                   string/lower-case)
+            current-graph-name (get-graph-name-fn current-repo-url)
+            repos (->> (state/sub [:me :repos])
+                       (remove #(= (:url %) config/demo-repo))
+                       (map :url))
+            repo-names (map #(get-graph-name-fn %) repos)]
+        (cond
+          (and (= hostname "mobile") (= pathname "/go/audio"))
+          (state/pub-event! [:mobile/start-audio-record])
 
-        (when-not (string/blank? graph-name)
-          (when-not (= graph-name current-graph-name)
-            (let [graph-idx (.indexOf repo-names graph-name)
-                  graph-url (when (not= graph-idx -1)
-                              (nth repos graph-idx))]
-              (if graph-url
-                (do (state/pub-event! [:graph/switch graph-url])
-                    (reset! *link-to-another-graph true))
-                (notification/show! (t :deeplink/open-graph-error graph-name) :error false))))
+          (and (= hostname "mobile") (= pathname "/go/quick-add"))
+          (state/pub-event! [:mobile/set-tab "capture"])
 
-          (when (or (= graph-name current-graph-name)
-                    @*link-to-another-graph)
-            (js/setTimeout
-             (fn []
-               (cond
-                 page-name
-                 (p/let [block (db-async/<get-block (state/get-current-repo) page-name {:children? false})]
-                   (if block
-                     (route-handler/redirect-to-page! block-uuid)
-                     (notification/show! (t :deeplink/open-page-error page-name) :error false)))
+          (= hostname "graph")
+          (let [graph-name (some-> pathname
+                                   (string/replace "/" "")
+                                   string/lower-case)
+                [page-name block-uuid] (map #(.get search-params %)
+                                            ["page" "block-id"])]
 
-                 block-uuid
-                 (p/let [block (db-async/<get-block (state/get-current-repo) block-uuid {:children? false})]
-                   (if block
-                     (route-handler/redirect-to-page! block-uuid)
-                     (notification/show! (t :deeplink/open-block-error block-uuid) :error false)))
+            (when-not (string/blank? graph-name)
+              (when-not (= graph-name current-graph-name)
+                (let [graph-idx (.indexOf repo-names graph-name)
+                      graph-url (when (not= graph-idx -1)
+                                  (nth repos graph-idx))]
+                  (if graph-url
+                    (do (state/pub-event! [:graph/switch graph-url])
+                        (reset! *link-to-another-graph true))
+                    (notification/show! (t :deeplink/open-graph-error graph-name) :error false))))
 
-                 :else
-                 nil)
-               (reset! *link-to-another-graph false))
-             (if @*link-to-another-graph
-               1000
-               0)))))
+              (when (or (= graph-name current-graph-name)
+                        @*link-to-another-graph)
+                (js/setTimeout
+                 (fn []
+                   (cond
+                     page-name
+                     (p/let [block (db-async/<get-block (state/get-current-repo) page-name {:children? false})]
+                       (if block
+                         (route-handler/redirect-to-page! block-uuid)
+                         (notification/show! (t :deeplink/open-page-error page-name) :error false)))
 
-      (= hostname "shared")
-      (let [result (into {} (map (fn [key]
-                                   [(keyword key) (.get search-params key)])
-                                 ["title" "url" "type" "payload"]))]
-        (if (:payload result)
-          (let [raw (common-util/safe-decode-uri-component (:payload result))
-                payload (-> raw
-                            js/JSON.parse
-                            (js->clj :keywordize-keys true))]
-            (intent/handle-payload payload))
-          (intent/handle-result result)))
+                     block-uuid
+                     (p/let [block (db-async/<get-block (state/get-current-repo) block-uuid {:children? false})]
+                       (if block
+                         (route-handler/redirect-to-page! block-uuid)
+                         (notification/show! (t :deeplink/open-block-error block-uuid) :error false)))
 
-      :else
-      nil)))
+                     :else
+                     nil)
+                   (reset! *link-to-another-graph false))
+                 (if @*link-to-another-graph
+                   1000
+                   0)))))
+
+          (= hostname "shared")
+          (let [result (into {} (map (fn [key]
+                                       [(keyword key) (.get search-params key)])
+                                     ["title" "url" "type" "payload"]))]
+            (if (:payload result)
+              (let [raw (common-util/safe-decode-uri-component (:payload result))
+                    payload (-> raw
+                                js/JSON.parse
+                                (js->clj :keywordize-keys true))]
+                (intent/handle-payload payload))
+              (intent/handle-result result)))
+
+          :else
+          nil)))))
