19ff7e6f7b72606448f96f3f78d17f8d7d3fd683
Mega Yu <yuhg2310@gmail.com>
2026-05-28 10:06:04 +0800

    fix lint

--- deps/common/src/logseq/common/log.cljs
+++ deps/common/src/logseq/common/log.cljs
@@ -2,11 +2,6 @@
   "Minimal, logging ns that shims lambdaisland.glogi fns for nbb. Could port
   glogi to nbb later if this shim gets too big")
 
-(defn info
-  "Logs one or more values at info level."
-  [& msgs]
-  (apply js/console.info (map clj->js msgs)))
-
 (defn error
   "Logs one or more values at error level."
   [& msgs]
