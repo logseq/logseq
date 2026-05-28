5e50933111c45eb8a7d1dfc7fd65e568d9173e62
Mega Yu <yuhg2310@gmail.com>
2026-05-28 08:56:02 +0800

    fix(log): improve function signatures for clarity

--- deps/common/src/logseq/common/log.cljs
+++ deps/common/src/logseq/common/log.cljs
@@ -2,8 +2,12 @@
   "Minimal, logging ns that shims lambdaisland.glogi fns for nbb. Could port
   glogi to nbb later if this shim gets too big")
 
-(defn info [& msgs]
+(defn info
+  "Logs one or more values at info level."
+  [& msgs]
   (apply js/console.info (map clj->js msgs)))
 
-(defn error [& msgs]
+(defn error
+  "Logs one or more values at error level."
+  [& msgs]
   (apply js/console.error (map clj->js msgs)))
