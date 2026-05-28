04e302c67c00e5fd6143b2685e9333c48da66ef3
Tienson Qin <tiensonqin@gmail.com>
2026-05-28 23:39:55 +0800

    fix: align db worker cors origin

--- src/main/frontend/worker/db_worker_node.cljs
+++ src/main/frontend/worker/db_worker_node.cljs
@@ -26,7 +26,7 @@
   (server-list/path root-dir))
 
 (def ^:private cors-headers
-  #js {"Access-Control-Allow-Origin" "*"
+  #js {"Access-Control-Allow-Origin" "lsp://logseq.io"
        "Access-Control-Allow-Methods" "GET,POST,OPTIONS"
        "Access-Control-Allow-Headers" "Content-Type,Authorization"})
 
