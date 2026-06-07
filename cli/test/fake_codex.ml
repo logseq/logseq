90edb473cec91601bc639c6b2d248f0022c7dd6b
rcmerci <rcmerci@gmail.com>
2026-06-07 22:21:37 +0800

    test with melange-jest

---
+++ cli/test/fake_codex.ml
@@ -0,0 +1,22 @@
+external set_timeout : ((unit -> unit)[@u]) -> int -> unit = "setTimeout"
+
+let argv_contains value =
+  Array.exists (( = ) value) Node.Process.argv
+
+let () =
+  if argv_contains "--version" then
+    Node.Process.exit 0;
+  let marker =
+    match Js.Dict.get Node.Process.process##env "AGENT_BRIDGE_TEST_MARKER" with
+    | Some value -> value
+    | None -> failwith "missing AGENT_BRIDGE_TEST_MARKER"
+  in
+  let worker_script =
+    match Js.Dict.get Node.Process.process##env "LOGSEQ_DB_WORKER_NODE_SCRIPT" with
+    | Some value -> value
+    | None -> ""
+  in
+  print_endline "{\"session_id\":\"cli-live-session\"}";
+  set_timeout (fun [@u] () -> print_endline "still alive after session id") 200;
+  set_timeout (fun [@u] () -> Node.Fs.writeFileAsUtf8Sync marker worker_script) 500;
+  set_timeout (fun [@u] () -> ()) 5000
