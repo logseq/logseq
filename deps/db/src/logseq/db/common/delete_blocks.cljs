10aeaefec2d306e6fc1734f2a985bba97e4f3e21
Tienson Qin <tiensonqin@gmail.com>
2026-07-03 01:10:04 +0800

    fix: handle normalized property history deletes

--- deps/db/src/logseq/db/common/delete_blocks.cljs
+++ deps/db/src/logseq/db/common/delete_blocks.cljs
@@ -60,14 +60,10 @@
                  (d/entity db (second tx)))))
        (common-util/distinct-by :db/id)))
 
-(def ^:private property-history-delete-attrs
+(def ^:private property-history-ref-attrs
   #{:logseq.property.history/block
     :logseq.property.history/property
-    :logseq.property.history/ref-value
-    :block/title
-    :block/page
-    :block/parent
-    :block/order})
+    :logseq.property.history/ref-value})
 
 (defn- property-history-entity?
   [entity]
@@ -78,17 +74,77 @@
             (:logseq.property.history/ref-value entity)
             (contains? entity :logseq.property.history/scalar-value)))))
 
-(defn- property-history-attr-retracted-entities
+(defn- property-history-ref-retracted-entities
   [db txs]
   (->> txs
        (keep (fn [tx]
                (when (and (vector? tx)
                           (= :db/retract (first tx))
-                          (contains? property-history-delete-attrs (nth tx 2 nil)))
+                          (contains? property-history-ref-attrs (nth tx 2 nil)))
                  (let [entity (d/entity db (second tx))]
                    (when (property-history-entity? entity)
                      entity)))))
        (common-util/distinct-by :db/id)))
+
+(defn- tx-entity-id
+  [db eid]
+  (cond
+    (integer? eid)
+    eid
+
+    (vector? eid)
+    (some-> (d/entity db eid) :db/id)))
+
+(defn- vector-adds-by-eid
+  [txs]
+  (reduce (fn [result tx]
+            (if (and (vector? tx)
+                     (= :db/add (first tx))
+                     (integer? (second tx)))
+              (update result (second tx) assoc (nth tx 2) (nth tx 3))
+              result))
+          {}
+          txs))
+
+(defn- property-history-ref-retracted-ids
+  [txs]
+  (->> txs
+       (keep (fn [tx]
+               (when (and (vector? tx)
+                          (= :db/retract (first tx))
+                          (integer? (second tx))
+                          (contains? property-history-ref-attrs (nth tx 2 nil)))
+                 (second tx))))
+       set))
+
+(defn- new-property-history-retract-tx
+  [db txs retracted-ids]
+  (let [referencing-retracted-entity?
+        (fn [history]
+          (or (contains? retracted-ids
+                         (tx-entity-id db (:logseq.property.history/block history)))
+              (contains? retracted-ids
+                         (tx-entity-id db (:logseq.property.history/property history)))
+              (contains? retracted-ids
+                         (tx-entity-id db (:logseq.property.history/ref-value history)))))
+        map-retract-tx
+        (keep (fn [tx]
+                (when (and (map? tx)
+                           (:block/uuid tx)
+                           (property-history-entity? tx)
+                           (referencing-retracted-entity? tx))
+                  [:db/retractEntity [:block/uuid (:block/uuid tx)]]))
+              txs)
+        retracted-history-ref-ids (property-history-ref-retracted-ids txs)
+        vector-retract-tx
+        (->> (vector-adds-by-eid txs)
+             (keep (fn [[eid tx-entity]]
+                     (when (and (:block/uuid tx-entity)
+                                (property-history-entity? tx-entity)
+                                (or (contains? retracted-history-ref-ids eid)
+                                    (referencing-retracted-entity? tx-entity)))
+                       [:db/retractEntity eid]))))]
+    (distinct (concat map-retract-tx vector-retract-tx))))
 
 (defn- block-subtree-entities
   [root]
@@ -142,16 +198,17 @@
 
 (defn- build-cleanup-tx
   [db txs]
-  (loop [pending-entities (concat (retracted-entities db txs)
-                                  (property-history-attr-retracted-entities db txs))
-         seen-ids #{}
-         cleanup-tx []]
-    (if-let [entities (seq (remove #(contains? seen-ids (:db/id %))
-                                   pending-entities))]
-      (let [seen-ids' (into seen-ids (map :db/id) entities)
-            next-tx (direct-cleanup-tx entities)]
-        (recur (retracted-entities db next-tx) seen-ids' (into cleanup-tx next-tx)))
-      (distinct cleanup-tx))))
+  (let [initial-entities (concat (retracted-entities db txs)
+                                 (property-history-ref-retracted-entities db txs))]
+    (loop [pending-entities initial-entities
+           seen-ids #{}
+           cleanup-tx (new-property-history-retract-tx db txs (set (map :db/id initial-entities)))]
+      (if-let [entities (seq (remove #(contains? seen-ids (:db/id %))
+                                     pending-entities))]
+        (let [seen-ids' (into seen-ids (map :db/id) entities)
+              next-tx (direct-cleanup-tx entities)]
+          (recur (retracted-entities db next-tx) seen-ids' (into cleanup-tx next-tx)))
+        (distinct cleanup-tx)))))
 
 (defn update-refs-history
   "When an entity is deleted, related property history, views and reactions
