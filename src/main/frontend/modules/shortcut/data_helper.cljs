9f4ee0333956eb10b5f833f9c0f508146240798a
Mega Yu <yuhg2310@gmail.com>
2026-08-25 11:38:53 +0800

    fix(shortcuts): support disabled binding strings

--- src/main/frontend/modules/shortcut/data_helper.cljs
+++ src/main/frontend/modules/shortcut/data_helper.cljs
@@ -11,6 +11,10 @@
 
 (declare get-group)
 
+(defn- normalize-disabled-binding
+  [binding]
+  (if (= "disabled" binding) false binding))
+
 ;; function vals->bindings is too time-consuming. Here we cache the results.
 (defn- flatten-bindings-by-id
   [config user-shortcuts binding-only?]
@@ -18,8 +22,9 @@
        (apply merge)
        (map (fn [[id {:keys [binding] :as opts}]]
               {id (if binding-only?
-                    (get user-shortcuts id binding)
-                    (assoc opts :user-binding (get user-shortcuts id)
+                    (normalize-disabled-binding (get user-shortcuts id binding))
+                    (assoc opts
+                           :user-binding (normalize-disabled-binding (get user-shortcuts id))
                            :handler-id (get-group id)
                            :id id))}))
        (into {})))
@@ -30,7 +35,7 @@
    (fn [r handler-id vs]
      (reduce-kv
       (fn [r id {:keys [binding]}]
-        (if-let [ks (get user-shortcuts id binding)]
+        (if-let [ks (normalize-disabled-binding (get user-shortcuts id binding))]
           (let [ks (if (sequential? ks) ks [ks])]
             (reduce (fn [a k]
                       (let [k (shortcut-utils/undecorate-binding k)
