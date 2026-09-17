e59af2b01e3396bcc882c9b11c541d9f73d16fcc
Mega Yu <yuhg2310@gmail.com>
2026-09-17 14:47:45 +0800

    perf(db-sync): prepare order repairs once per parent

--- deps/db-sync/src/logseq/db_sync/order.cljs
+++ deps/db-sync/src/logseq/db_sync/order.cljs
@@ -2,45 +2,50 @@
   (:require [datascript.core :as d]
             [logseq.db.common.order :as db-order]))
 
+(defn- parent-order-fixes
+  [db parent-eid values]
+  (let [siblings-by-order (->> (d/datoms db :avet :block/parent parent-eid)
+                              (keep (fn [datom]
+                                      (let [sibling (d/entity db (:e datom))]
+                                        (when (:block/order sibling)
+                                          sibling))))
+                              (group-by :block/order))]
+    (reduce
+     (fn [acc [value end]]
+       (let [siblings (get siblings-by-order value)]
+         (if (and (contains? values value) (> (count siblings) 1))
+           (let [same-order-siblings (sort-by :block/uuid siblings)
+                 ;; Generate the open-ended sequence incrementally to avoid repeated tail scans.
+                 new-orders (if end
+                              (db-order/gen-n-keys (count siblings) value end)
+                              (rest (reductions (fn [previous _]
+                                                  (db-order/gen-key previous nil))
+                                                value
+                                                siblings)))]
+             (into acc (map (fn [sibling order]
+                             [:db/add (:db/id sibling) :block/order order])
+                           same-order-siblings new-orders)))
+           acc)))
+     []
+     ;; Adjacent distinct orders keep all repairs inside disjoint snapshot intervals.
+     (partition-all 2 1 (sort (keys siblings-by-order))))))
+
 (defn fix-duplicate-orders! [conn tx-data tx-meta]
   (let [db @conn
-        updates (->> tx-data
-                     (filter (fn [[e a v _tx added]]
-                               (and (= a :block/order) added e v
-                                    (:block/uuid (d/entity db e))))))
-        groups (group-by (fn [{:keys [e v]}]
-                           (let [parent (:block/parent (d/entity db e))]
-                             [(:db/id parent) v]))
-                         updates)
-        fixes (reduce-kv
-               (fn [acc [parent-eid value] _group]
-                 (if (and parent-eid value)
-                   (let [siblings (d/datoms db :avet :block/parent parent-eid)
-                         existing-same (->> siblings
-                                            (keep (fn [datom]
-                                                    (let [sib-eid (:e datom)]
-                                                      (when (= value (:block/order (d/entity db sib-eid)))
-                                                        sib-eid)))))
-                         same-order-siblings (->> (map (fn [id] (d/entity db id)) existing-same)
-                                                  (sort-by :block/uuid))
-                         need-fix? (> (count same-order-siblings) 1)]
-                     (if need-fix?
-                       (let [orders (->> siblings
-                                         (keep (fn [d]
-                                                 (let [sib-eid (:e d)]
-                                                   (:block/order (d/entity db sib-eid))))))
-                             end (some (fn [order]
-                                         (when (> (compare order value) 0)
-                                           order))
-                                       orders)
-                             new-orders (db-order/gen-n-keys (count same-order-siblings) value end)]
-                         (into acc
-                               (map-indexed (fn [idx id]
-                                              [:db/add id :block/order (nth new-orders idx)])
-                                            (map :db/id same-order-siblings))))
-                       acc))
-                   acc))
-               []
-               groups)]
+        values-by-parent (reduce
+                          (fn [acc {:keys [e a v added]}]
+                            (if (and (= a :block/order) added e v)
+                              (let [block (d/entity db e)
+                                    parent-eid (:db/id (:block/parent block))]
+                                (if (and (:block/uuid block) parent-eid)
+                                  (update acc parent-eid (fnil conj #{}) v)
+                                  acc))
+                              acc))
+                          {}
+                          tx-data)
+        fixes (reduce-kv (fn [acc parent-eid values]
+                           (into acc (parent-order-fixes db parent-eid values)))
+                         []
+                         values-by-parent)]
     (when (seq fixes)
       (d/transact! conn fixes (merge tx-meta {:op :fix-duplicate-order})))))
--- deps/db-sync/test/logseq/db_sync/worker_test.cljs
+++ deps/db-sync/test/logseq/db_sync/worker_test.cljs
@@ -9,22 +9,34 @@
   (d/create-conn db-schema/schema))
 
 (deftest duplicate-order-fix-test
-  (let [conn (new-conn)
-        parent (random-uuid)
-        block-a (random-uuid)
-        block-b (random-uuid)
-        order-a (db-order/gen-key nil nil)
-        order-b (db-order/gen-key order-a nil)]
-    (d/transact! conn [{:block/uuid parent}
-                       {:block/uuid block-a
-                        :block/parent [:block/uuid parent]
-                        :block/order order-a}
-                       {:block/uuid block-b
-                        :block/parent [:block/uuid parent]
-                        :block/order order-b}])
-    (let [tx [[:db/add [:block/uuid block-b] :block/order order-a]]
-          _ (sync-order/fix-duplicate-orders! conn tx {})
-          order-a' (:block/order (d/entity @conn [:block/uuid block-a]))
-          order-b' (:block/order (d/entity @conn [:block/uuid block-b]))]
-      (is (= order-a order-a'))
-      (is (not= order-a' order-b')))))
+  (let [parent #uuid "00000000-0000-0000-0000-000000000001"
+        block-ids (mapv #(uuid (str "00000000-0000-0000-0000-00000000000" %)) (range 2 9))
+        orders (db-order/gen-n-keys 4 nil nil)
+        results (for [allocation [[4 1 0 3 2 6 5] [0 1 2 3 4 5 6]]]
+                  (let [conn (new-conn)
+                        _ (d/transact! conn (into [{:block/uuid parent}]
+                                                 (map (fn [idx]
+                                                        {:block/uuid (nth block-ids idx)
+                                                         :block/parent [:block/uuid parent]})
+                                                      allocation)))
+                        tx-report (d/transact! conn
+                                               (mapv (fn [block-id order]
+                                                       [:db/add [:block/uuid block-id] :block/order order])
+                                                     block-ids
+                                                     [(orders 0) (orders 0) (orders 1) (orders 1)
+                                                      (orders 2) (orders 3) (orders 3)]))
+                        repair (sync-order/fix-duplicate-orders! conn (:tx-data tx-report) {:outliner-op :fix})
+                        repaired-orders (mapv #(:block/order (d/entity @conn [:block/uuid %])) block-ids)]
+                    (is (= (sort repaired-orders) repaired-orders))
+                    (is (= (count block-ids) (count (distinct repaired-orders))))
+                    (is (every? #(neg? (compare (orders 0) %)) (take 2 repaired-orders)))
+                    (is (every? #(neg? (compare % (orders 1))) (take 2 repaired-orders)))
+                    (is (every? #(neg? (compare (orders 1) %)) (take 2 (drop 2 repaired-orders))))
+                    (is (every? #(neg? (compare % (orders 2))) (take 2 (drop 2 repaired-orders))))
+                    (is (= (orders 2) (nth repaired-orders 4)))
+                    (is (every? #(pos? (compare % (orders 3))) (drop 5 repaired-orders)))
+                    (is (= {:outliner-op :fix :op :fix-duplicate-order} (:tx-meta repair)))
+                    (is (nil? (sync-order/fix-duplicate-orders! conn (:tx-data repair) {})))
+                    (is (nil? (sync-order/fix-duplicate-orders! conn (:tx-data tx-report) {})))
+                    repaired-orders))]
+    (is (apply = results))))
