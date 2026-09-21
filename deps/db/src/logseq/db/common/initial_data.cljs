98dd3428f74652482b9fe511e859b5291d46f868
Tienson Qin <tiensonqin@gmail.com>
2026-09-21 12:45:20 +0800

    fix: bound reverse journal scans to journal dates
    Exclude unrelated index entries and cover future journals with aliases.

--- deps/db/src/logseq/db/common/initial_data.cljs
+++ deps/db/src/logseq/db/common/initial_data.cljs
@@ -334,6 +334,7 @@
   [db]
   (let [today (date-time-util/date->int (js/Date.))]
     (->> (d/rseek-datoms db :avet :block/journal-day today)
+         (take-while #(= :block/journal-day (:a %)))
          (keep (fn [d]
                  (when (<= (:v d) today)
                    (let [e (d/entity db (:e d))]
