fc9e91ed67bdde62c0286c8d3fae4f91be04d26c
Tienson Qin <tiensonqin@gmail.com>
2026-09-23 12:55:21 +0000

    fix: flush editor buffer before redo like undo
    redo-aux! never saved the active buffer, so restore-cursor!'s
    :save-current-block? false would drop unsaved text when redo retargets
    editing. Flush before invoking the redo op, mirroring undo-aux!.

--- src/main/frontend/handler/history.cljs
+++ src/main/frontend/handler/history.cljs
@@ -90,8 +90,10 @@
        @*last-request
        (when-let [repo (state/get-current-repo)]
          (util/stop e)
-         (state/clear-editor-action!)
-         (reset! *last-request (state/<invoke-db-worker :thread-api/undo-redo-redo repo))
-         (p/let [result @*last-request]
-           (restore-cursor-and-state! result)))))))
+         (p/do!
+          (editor/save-current-block!)
+          (state/clear-editor-action!)
+          (reset! *last-request (state/<invoke-db-worker :thread-api/undo-redo-redo repo))
+          (p/let [result @*last-request]
+            (restore-cursor-and-state! result))))))))
 (defonce redo! (debounce redo-aux! 20))
