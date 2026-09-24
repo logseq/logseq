703973ee816b20328b56896c5c8aa27f458524f5
Tienson Qin <tiensonqin@gmail.com>
2026-09-24 13:52:47 +0000

    test(e2e): wait for editor after deleting blocks
    The delete assertions read get-edit-content synchronously, racing the
    worker roundtrip that remounts the editor. Stamping the page's
    :block/updated-at on delete adds a few ms to that path, which the slow
    CI runner could not absorb. Every other post-delete assertion in this
    file already calls wait-editor-visible; these two were outliers.

--- clj-e2e/test/logseq/e2e/outliner_basic_test.clj
+++ clj-e2e/test/logseq/e2e/outliner_basic_test.clj
@@ -135,6 +135,7 @@
     (b/delete-blocks)                        ; delete b4
     (util/repeat-keyboard 2 "Shift+ArrowUp") ; select b3 and b2
     (b/delete-blocks)
+    (util/wait-editor-visible)
     (is (= "b1" (util/get-edit-content)))
     (is (= 1 (util/page-blocks-count)))))
 
@@ -143,6 +144,7 @@
     (b/new-blocks ["b1" "b2" "b3"])
     (k/arrow-up)
     (k/delete)
+    (util/wait-editor-visible)
     (is (= "b2b3" (util/get-edit-content)))
     (is (= 2 (util/page-blocks-count)))))
 
