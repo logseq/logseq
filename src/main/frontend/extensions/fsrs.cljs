bd2123ecf0f51e171be1b5a93c578e1084920d03
Tienson Qin <tiensonqin@gmail.com>
2026-09-24 12:43:53 +0000

    enhance: show no-cards-due state with practice-again in flashcards

--- src/main/frontend/extensions/fsrs.cljs
+++ src/main/frontend/extensions/fsrs.cljs
@@ -100,6 +100,11 @@
   [repo cards-id]
   (state/<invoke-db-worker :thread-api/get-fsrs-due-card-block-ids repo cards-id))
 
+(defn- <get-card-block-ids
+  "Block uuids of all cards in the scope of `cards-id`, whether due or not."
+  [repo cards-id]
+  (state/<invoke-db-worker :thread-api/get-fsrs-card-block-ids repo cards-id))
+
 (defn- global-cards-id?
   [cards-id]
   (contains? #{:global "global"} cards-id))
@@ -292,6 +297,8 @@
                         :block/title (t :flashcard/all-cards)}])
         *block-ids (hooks/use-memo #(atom nil) [])
         [block-ids] (hooks/use-atom *block-ids)
+        *all-block-ids (hooks/use-memo #(atom nil) [])
+        [all-block-ids] (hooks/use-atom *all-block-ids)
         *loading? (hooks/use-memo #(atom nil) [])
         [loading?] (hooks/use-atom *loading?)
         *card-index (hooks/use-memo #(atom 0) [])
@@ -301,9 +308,11 @@
         select-card! (fn [v]
                        (reset! *cards-id v)
                        (let [cards-id' (when-not (global-cards-id? v) v)]
-                         (p/let [result (<get-due-card-block-ids repo cards-id')]
+                         (p/let [result (<get-due-card-block-ids repo cards-id')
+                                 all-result (<get-card-block-ids repo cards-id')]
                            (reset! *card-index 0)
                            (reset! *phase :init)
+                           (reset! *all-block-ids all-result)
                            (reset! *block-ids result))))]
     (shortcut/use-shortcut-handler! :shortcut.handler/cards
                                     {:cards-id initial-cards-id
@@ -312,7 +321,9 @@
      (fn []
        (reset! *loading? true)
        (p/let [cards-class (state/<invoke-db-worker :thread-api/pull repo [:db/id] :logseq.class/Cards)
-               result (<get-due-card-block-ids repo initial-cards-id)]
+               result (<get-due-card-block-ids repo initial-cards-id)
+               all-result (<get-card-block-ids repo initial-cards-id)]
+         (reset! *all-block-ids all-result)
          (reset! *block-ids result)
          (reset! *loading? false)
          (when-let [cards-class-id (:db/id cards-class)]
@@ -384,11 +395,28 @@
             [card-view repo block-id *card-index *phase opts]]
 
            (empty? block-ids)
-           [:div.ls-card.content.ml-2
-            [:h2.font-medium (t :flashcard.empty/title)]
+           (if (empty? all-block-ids)
+             [:div.ls-card.content.ml-2
+              [:h2.font-medium (t :flashcard.empty/title)]
 
-            [:div
-             [:p (t :flashcard.empty/desc "#Card")]]]
+              [:div
+               [:p (t :flashcard.empty/desc "#Card")]]]
+             [:div.ls-card.content.ml-2
+              [:h2.font-medium (t :flashcard.empty/no-due-title)]
+
+              [:div
+               [:p (t :flashcard.empty/no-due-desc)]]
+
+              [:div.mt-4
+               (shui/button
+                {:variant :outline
+                 :size :sm
+                 :id "card-practice-again"
+                 :on-click (fn [_e]
+                             (reset! *card-index 0)
+                             (reset! *phase :init)
+                             (reset! *block-ids all-block-ids))}
+                (t :flashcard.review/practice-again))]])
 
            :else
            [:p (t :flashcard.review/finished)]))])))
--- src/main/frontend/worker/handler/flashcard.cljs
+++ src/main/frontend/worker/handler/flashcard.cljs
@@ -8,8 +8,10 @@
    [frontend.worker.state :as worker-state]
    [logseq.db.frontend.class :as db-class]))
 
-(defn- fsrs-due-card-block-ids
-  [db cards-id]
+(defn- fsrs-card-block-ids
+  "Block uuids of cards in the scope of `cards-id`.
+  When `due-only?`, restricts to cards due now or without a scheduled due date."
+  [db cards-id due-only?]
   (let [now-inst-ms (inst-ms (js/Date.))
         cards (when (and cards-id (not (contains? #{:global "global"} cards-id)))
                 (d/entity db cards-id))
@@ -21,16 +23,18 @@
         card-tag-id (:db/id (d/entity db :logseq.class/Card))
         card-tag-children-ids (db-class/get-structured-children db card-tag-id)
         card-ids (cons card-tag-id card-tag-children-ids)
-        q '[:find [?b ...]
-            :in $ [?t ...] ?now-inst-ms %
-            :where
-            [?b :block/tags ?t]
-            (or-join [?b ?now-inst-ms]
-                     (and
-                      [?b :logseq.property.fsrs/due ?due]
-                      [(>= ?now-inst-ms ?due)])
-                     [(missing? $ ?b :logseq.property.fsrs/due)])
-            [?b :block/uuid]]
+        q (cond-> '[:find [?b ...]
+                    :in $ [?t ...] ?now-inst-ms %
+                    :where
+                    [?b :block/tags ?t]]
+            due-only?
+            (concat '[(or-join [?b ?now-inst-ms]
+                               (and
+                                [?b :logseq.property.fsrs/due ?due]
+                                [(>= ?now-inst-ms ?due)])
+                               [(missing? $ ?b :logseq.property.fsrs/due)])])
+            true
+            (concat '[[?b :block/uuid]]))
         q' (if query
              (let [query* (:query result)]
                (concat q (if (coll? (first query*)) query* [query*])))
@@ -40,4 +44,9 @@
 (def-thread-api :thread-api/get-fsrs-due-card-block-ids
   [repo cards-id]
   (when-let [conn (worker-state/get-datascript-conn repo)]
-    (fsrs-due-card-block-ids @conn cards-id)))
+    (fsrs-card-block-ids @conn cards-id true)))
+
+(def-thread-api :thread-api/get-fsrs-card-block-ids
+  [repo cards-id]
+  (when-let [conn (worker-state/get-datascript-conn repo)]
+    (fsrs-card-block-ids @conn cards-id false)))
