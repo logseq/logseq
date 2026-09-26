551de765ec00153201602d18d0a9cc6f516f1f54
rcmerci <rcmerci@gmail.com>
2026-09-26 19:24:23 +0800

    fix(db-worker): use opam packages by default

--- deps/db-worker/scripts/install-opam-deps.sh
+++ deps/db-worker/scripts/install-opam-deps.sh
@@ -5,18 +5,18 @@
 # because dune regenerates that file from dune-project
 # (generate_opam_files) and the runtest diff check rejects hand edits.
 set -eu
-opam pin add -y -n datascript_ocaml git+https://github.com/logseq/datascript-ocaml.git
-opam pin add -y -n datascript-ocaml-melange git+https://github.com/logseq/datascript-ocaml.git
-opam pin add -y -n datascript-ocaml-native git+https://github.com/logseq/datascript-ocaml.git
+opam pin add -y -n datascript_ocaml git+https://github.com/logseq/datascript-ocaml.git#main
+opam pin add -y -n datascript-ocaml-melange git+https://github.com/logseq/datascript-ocaml.git#main
+opam pin add -y -n datascript-ocaml-native git+https://github.com/logseq/datascript-ocaml.git#main
 opam pin add -y -n persistent_sorted_set_ocaml git+https://github.com/logseq/persistent-sorted-set-ocaml.git#main
-opam pin add -y -n melange-edn-core git+https://github.com/RCmerci/melange-edn.git#main
-opam pin add -y -n melange-edn-melange git+https://github.com/RCmerci/melange-edn.git#main
-opam pin add -y -n melange-edn-native git+https://github.com/RCmerci/melange-edn.git#main
-opam pin add -y -n melange-transit-core git+https://github.com/RCmerci/melange-transit.git#main
-opam pin add -y -n melange-transit-melange git+https://github.com/RCmerci/melange-transit.git#main
-opam pin add -y -n melange-transit-native git+https://github.com/RCmerci/melange-transit.git#main
-opam pin add -y -n rrbvec git+https://github.com/RCmerci/rrbvec.git
+opam pin add -y -n melange-edn-core git+https://github.com/logseq/melange-edn.git#main
+opam pin add -y -n melange-edn-melange git+https://github.com/logseq/melange-edn.git#main
+opam pin add -y -n melange-edn-native git+https://github.com/logseq/melange-edn.git#main
+opam pin add -y -n melange-transit-core git+https://github.com/logseq/melange-transit.git#main
+opam pin add -y -n melange-transit-melange git+https://github.com/logseq/melange-transit.git#main
+opam pin add -y -n melange-transit-native git+https://github.com/logseq/melange-transit.git#main
+opam pin add -y -n rrbvec git+https://github.com/logseq/rrbvec.git#main
 opam pin add -y -n angstrom git+https://github.com/logseq/angstrom#fork
 opam pin add -y -n xmlm git+https://github.com/logseq/xmlm#eb469d536e98c98f2754c0ff8813c92b3a17fe9f
-opam pin add -y -n mldoc git+https://github.com/logseq/mldoc
+opam pin add -y -n mldoc git+https://github.com/logseq/mldoc#master
 opam install . --deps-only --with-test --yes
